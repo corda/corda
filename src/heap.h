3a2f478521f5112d0635550932f07d027950544c
Joel Dice <git@seibutsu.mailsnare.net>
2007-06-02 17:36:55 -0600

    add heap.h

---
+++ src/heap.h
@@ -0,0 +1,25 @@
+#ifndef HEAP_H
+#define HEAP_H
+
+class Heap {
+ public:
+  enum CollectionType {
+    MinorCollection,
+    MajorCollection
+  };
+
+  class Visitor {
+    virtual ~Visitor() { }
+    virtual void visit(void**) = 0;
+  };
+
+  class Iterator {
+    virtual ~Iterator() { }
+    virtual void iterate(Visitor*) = 0;
+  };
+
+  virtual ~Heap() { }
+  virtual void collect(CollectionType type, Iterator* it) = 0;
+};
+
+#endif//HEAP_H
