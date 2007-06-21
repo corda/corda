bbae384bd2d2bb3ede6674a024b4ff89e29304a7
Joel Dice <git@seibutsu.mailsnare.net>
2007-06-21 16:51:13 -0600

    update Test.java to work the GC harder

--- input/Test.java
+++ input/Test.java
@@ -1,8 +1,15 @@
 public class Test {
 
   public static void main(String[] args) {
-    while (true) {
-      byte[] array = new byte[4 * 1024];
+    for (int i = 0; i < 1024; ++i) {
+      byte[] a = new byte[4 * 1024];
+    }
+
+    for (int i = 0; i < 1024; ++i) {
+      Object[] array = new Object[32];
+      for (int j = 0; j < 32; ++j) {
+        array[j] = new byte[32 * 1024];
+      }
     }
   }
 
