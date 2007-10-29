c97b16b8bcd03046fc58a66444fc53daeeb86c51
Eric Scharff <eric.scharff@readytalk.com>
2007-10-29 15:23:13 -0600

    implement a few classpath methods required by SWT for Mac OS X

--- classpath/java/text/DateFormatSymbols.java
+++ classpath/java/text/DateFormatSymbols.java
@@ -2,6 +2,11 @@ package java.text;
 
 public class DateFormatSymbols {
   private String[] ampm = new String[] { "AM", "PM" };
+  private String[] shortWeekdays = new String[] { "Sun", "Mon", "Tue",
+						  "Wed", "Thu", "Fri", "Sat" };
+  private String[] shortMonths = new String[] { "Jan", "Feb", "Mar", "Apr",
+						"May", "Jun", "Jul", "Aug",
+						"Sep", "Oct", "Nov", "Dec" };
 
   public String[] getAmPmStrings() {
     return ampm;
@@ -9,5 +14,13 @@ public class DateFormatSymbols {
 
   public void setAmPmStrings(String[] v) {
     ampm = v;
+  }
+
+  public String[] getShortWeekdays() {
+    return shortWeekdays;
+  }
+
+  public String[] getShortMonths() {
+    return shortMonths;
   }
 }
