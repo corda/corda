823148841a890f3daf5560de3e65b54d46cba8cf
Joel Dice <git@seibutsu.mailsnare.net>
2008-02-20 10:51:54 -0700

    fix generation of UnsatisfiedLinkError message in resolveNativeMethodData

--- src/interpret.cpp
+++ src/interpret.cpp
@@ -471,7 +471,10 @@ resolveNativeMethodData(Thread* t, object method)
       return data;
     } else {
       object message = makeString
-        (t, "%s", &byteArrayBody(t, methodCode(t, method), 0));
+        (t, "%s.%s%s",
+         &byteArrayBody(t, className(t, methodClass(t, method)), 0),
+         &byteArrayBody(t, methodName(t, method), 0),
+         &byteArrayBody(t, methodSpec(t, method), 0));
       t->exception = makeUnsatisfiedLinkError(t, message);
       return 0;
     }
