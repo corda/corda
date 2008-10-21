5eb905fd2d859e4baf061f99e86cb254e06adfa1
Joel Dice <git@seibutsu.mailsnare.net>
2008-10-21 17:42:05 -0600

    revert accidentally-committed change to Hello.java

--- test/Hello.java
+++ test/Hello.java
@@ -1,11 +1,5 @@
 public class Hello {
   public static void main(String[] args) {
     System.out.println("hello, world!");
-    try {
-      Runtime.class.getMethod("dumpHeap", String.class)
-        .invoke(null, "/tmp/heap.bin");
-    } catch (Exception e) {
-      e.printStackTrace();
-    }
   }
 }
