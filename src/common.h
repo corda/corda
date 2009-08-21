637a676fb046c88c205acde7233aa28efbd8ebed
Joel Dice <git@seibutsu.mailsnare.net>
2009-08-21 09:37:49 -0600

    fix MinGW 3.4 build

--- src/common.h
+++ src/common.h
@@ -31,7 +31,7 @@
 
 #if (defined __i386__) || (defined __POWERPC__) || (defined __arm__)
 #  define LD "ld"
-#  ifdef __MINGW32__
+#  if defined(__MINGW32__) && __GNUC__ == 4
 #    define LLD "I64d"
 #  else
 #    define LLD "lld"
