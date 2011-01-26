966650f105c64b93a7fcdda2c6392beefcbef2f0
Joel Dice <joel.dice@gmail.com>
2011-01-25 17:13:59 -0700

    make StackOverflow test useful in tails=true build

--- test/StackOverflow.java
+++ test/StackOverflow.java
@@ -1,14 +1,14 @@
 public class StackOverflow {
-  private static void test1() {
-    test1();
+  private static int test1() {
+    return test1() + 1;
   }
 
-  private static void test2() {
-    test3();
+  private static int test2() {
+    return test3() + 1;
   }
 
-  private static void test3() {
-    test2();
+  private static int test3() {
+    return test2() + 1;
   }
 
   public static void main(String[] args) {
