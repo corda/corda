corda/corda

commit 7fffba29e62e8d01f09aba2e8d374d269a2c6476

Keep reading in BufferedInputStream.read until in.available() <= 0
This corresponds to the documented behavior of OpenJDK's version.

classpath/java/io/BufferedInputStream.java
@@ -58,14 +58,19 @@ public class BufferedInputStream extends InputStream {
       length -= remaining;
     }
 
-    if (length > 0) {
+    while (length > 0) {
       int c = in.read(b, offset, length);
       if (c == -1) {
         if (count == 0) {
           count = -1;
         }
+        break;
       } else {
         count += c;
+
+        if (in.available() <= 0) {
+          break;
+        }
       }
     }
 

commit a8645b6b2da383da98cff9ee31300291b31653be

fix BufferedInputStream.read regression
In commit 7fffba2, I had modified BufferedInputStream.read to keep
reading until in.available() <= 0 or an EOF was reached, but neglected
to update the offset into the destination buffer after each read.
This caused the previously-read data to be overwritten.  This commit
fixes that regression.

classpath/java/io/BufferedInputStream.java
@@ -66,6 +66,7 @@ public class BufferedInputStream extends InputStream {
         }
         break;
       } else {
+        offset += c;
         count += c;
         length -= c;
 