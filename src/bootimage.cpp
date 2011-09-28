corda/corda

commit 5b4f17997f05321ddcb559baa45da18185de6bf5

progress towards cross-architecture bootimage builds
This monster commit is the first step towards supporting
cross-architecture bootimage builds.  The challenge is to build a heap
and code image for the target platform where the word size and
endianess may differ from those of the build architecture.  That means
the memory layout of objects may differ due to alignment and size
differences, so we can't just copy objects into the heap image
unchanged; we must copy field by field, resizing values, reversing
endianess and shifting offsets as necessary.

This commit also removes POD (plain old data) type support from the
type generator because it added a lot of complication and little
value.

src/machine.cpp
@@ -1143,11 +1143,10 @@ parseFieldTable(Thread* t, Stream& s, object class_, object pool)
          addendum,
          class_);
 
+      unsigned size = fieldSize(t, code);
       if (flags & ACC_STATIC) {
-        unsigned size = fieldSize(t, code);
-        unsigned excess = (staticOffset % size) % BytesPerWord;
-        if (excess) {
-          staticOffset += BytesPerWord - excess;
+        while (staticOffset % size) {
+          ++ staticOffset;
         }
 
         fieldOffset(t, field) = staticOffset;
@@ -1162,12 +1161,13 @@ parseFieldTable(Thread* t, Stream& s, object class_, object pool)
           classVmFlags(t, class_) |= HasFinalMemberFlag;
         }
 
-        while (memberOffset % fieldSize(t, code)) {
+        while (memberOffset % size) {
           ++ memberOffset;
         }
 
         fieldOffset(t, field) = memberOffset;
-        memberOffset += fieldSize(t, code);
+
+        memberOffset += size;
       }
 
       set(t, fieldTable, ArrayBody + (i * BytesPerWord), field);
@@ -1298,11 +1298,12 @@ parseCode(Thread* t, Stream& s, object pool)
   if (ehtLength) {
     object eht = makeExceptionHandlerTable(t, ehtLength);
     for (unsigned i = 0; i < ehtLength; ++i) {
-      ExceptionHandler* eh = exceptionHandlerTableBody(t, eht, i);
-      exceptionHandlerStart(eh) = s.read2();
-      exceptionHandlerEnd(eh) = s.read2();
-      exceptionHandlerIp(eh) = s.read2();
-      exceptionHandlerCatchType(eh) = s.read2();
+      unsigned start = s.read2();
+      unsigned end = s.read2();
+      unsigned ip = s.read2();
+      unsigned catchType = s.read2();
+      exceptionHandlerTableBody(t, eht, i) = exceptionHandler
+        (start, end, ip, catchType);
     }
 
     set(t, code, CodeExceptionHandlerTable, eht);
@@ -1319,9 +1320,9 @@ parseCode(Thread* t, Stream& s, object pool)
       unsigned lntLength = s.read2();
       object lnt = makeLineNumberTable(t, lntLength);
       for (unsigned i = 0; i < lntLength; ++i) {
-        LineNumber* ln = lineNumberTableBody(t, lnt, i);
-        lineNumberIp(ln) = s.read2();
-        lineNumberLine(ln) = s.read2();
+        unsigned ip = s.read2();
+        unsigned line = s.read2();
+        lineNumberTableBody(t, lnt, i) = lineNumber(ip, line);
       }
 
       set(t, code, CodeLineNumberTable, lnt);
@@ -2217,9 +2218,9 @@ boot(Thread* t)
 
 #include "type-java-initializations.cpp"
 
-#ifdef AVIAN_HEAPDUMP
+    //#ifdef AVIAN_HEAPDUMP
 #  include "type-name-initializations.cpp"
-#endif
+      //#endif
   }
 }
 
@@ -3428,6 +3429,16 @@ parseClass(Thread* t, object loader, const uint8_t* data, unsigned size,
 
   updateClassTables(t, real, class_);
 
+  if (root(t, Machine::PoolMap)) {
+    object bootstrapClass = hashMapFind
+      (t, root(t, Machine::BootstrapClassMap), className(t, class_),
+       byteArrayHash, byteArrayEqual);
+
+    hashMapInsert
+      (t, root(t, Machine::PoolMap), bootstrapClass ? bootstrapClass : real,
+       pool, objectHash);
+  }
+
   return real;
 }
 
@@ -4426,7 +4437,7 @@ vmAddressFromLine(Thread* t, object m, unsigned line)
     unsigned top = lineNumberTableLength(t, lnt);
     for(unsigned i = bottom; i < top; i++)
     {
-      LineNumber* ln = lineNumberTableBody(t, lnt, i);
+      uint64_t ln = lineNumberTableBody(t, lnt, i);
       if(lineNumberLine(ln) == line)
         return reinterpret_cast<void*>(lineNumberIp(ln));
       else if(lineNumberLine(ln) > line)

makefile
@@ -169,7 +169,8 @@ common-cflags = $(warnings) -fno-rtti -fno-exceptions \
 	"-I$(JAVA_HOME)/include" -idirafter $(src) -I$(build) $(classpath-cflags) \
 	-D__STDC_LIMIT_MACROS -D_JNI_IMPLEMENTATION_ -DAVIAN_VERSION=\"$(version)\" \
 	-DUSE_ATOMIC_OPERATIONS -DAVIAN_JAVA_HOME=\"$(javahome)\" \
-	-DAVIAN_EMBED_PREFIX=\"$(embed-prefix)\"
+	-DAVIAN_EMBED_PREFIX=\"$(embed-prefix)\" \
+	-DTARGET_BYTES_PER_WORD=$(pointer-size)
 
 ifneq (,$(filter i386 x86_64,$(arch)))
 	ifeq ($(use-frame-pointer),true)
@@ -208,12 +209,22 @@ shared = -shared
 
 openjdk-extra-cflags = -fvisibility=hidden
 
+ifeq ($(build-arch),powerpc)
+	ifneq ($(arch),$(build-arch))
+		cflags += -DTARGET_OPPOSITE_ENDIAN
+	endif
+endif
+
 ifeq ($(arch),i386)
 	pointer-size = 4
 endif
 ifeq ($(arch),powerpc)
 	asm = powerpc
 	pointer-size = 4
+
+	ifneq ($(arch),$(build-arch))
+		cflags += -DTARGET_OPPOSITE_ENDIAN
+	endif
 
 	ifneq ($(platform),darwin)
 		ifneq ($(arch),$(build-arch))
@@ -251,7 +262,13 @@ ifeq ($(arch),arm)
 	endif
 endif
 
+ifeq ($(platform),linux)
+	cflags += -DTARGET_PLATFORM_LINUX
+endif
+
 ifeq ($(platform),darwin)
+	cflags += -DTARGET_PLATFORM_DARWIN
+
 	ifeq (${OSX_SDK_SYSROOT},)
 		OSX_SDK_SYSROOT = 10.4u
 	endif
@@ -330,6 +347,8 @@ ifeq ($(platform),darwin)
 endif
 
 ifeq ($(platform),windows)
+	cflags += -DTARGET_PLATFORM_WINDOWS
+
 	inc = "$(root)/win32/include"
 	lib = "$(root)/win32/lib"
 
@@ -479,7 +498,8 @@ generated-code = \
 	$(build)/type-constructors.cpp \
 	$(build)/type-initializations.cpp \
 	$(build)/type-java-initializations.cpp \
-	$(build)/type-name-initializations.cpp
+	$(build)/type-name-initializations.cpp \
+	$(build)/type-maps.cpp
 
 vm-depends := $(generated-code) $(wildcard $(src)/*.h)
 
@@ -537,16 +557,6 @@ bootimage-bin = $(build)/bootimage.bin
 bootimage-object = $(build)/bootimage-bin.o
 
 ifeq ($(bootimage),true)
-	ifneq ($(build-arch),$(arch))
-$(error "bootimage cross-builds not yet supported")
-	endif
-
-	ifeq ($(arch),x86_64)
-		ifneq ($(build-platform),$(platform))
-$(error "bootimage cross-builds not yet supported")
-		endif
-	endif
-
 	vm-classpath-object = $(bootimage-object)
 	cflags += -DBOOT_IMAGE=\"bootimageBin\" -DAVIAN_CLASSPATH=\"\"
 else

src/assembler.h
@@ -343,7 +343,7 @@ class Assembler {
     virtual void updateCall(UnaryOperation op, void* returnAddress,
                             void* newTarget) = 0;
 
-    virtual void setConstant(void* dst, uintptr_t constant) = 0;
+    virtual void setConstant(void* dst, uint64_t constant) = 0;
 
     virtual unsigned alignFrameSize(unsigned sizeInWords) = 0;
 

src/heap.cpp
@@ -19,12 +19,6 @@ namespace {
 
 namespace local {
 
-// an object must survive TenureThreshold + 2 garbage collections
-// before being copied to gen2 (must be at least 1):
-const unsigned TenureThreshold = 3;
-
-const unsigned FixieTenureThreshold = TenureThreshold + 2;
-
 const unsigned Top = ~static_cast<unsigned>(0);
 
 const unsigned InitialGen2CapacityInBytes = 4 * 1024 * 1024;
@@ -546,11 +540,14 @@ class Fixie {
     return totalSize(size, hasMask);
   }
 
+  // be sure to update e.g. TargetFixieSizeInBytes in bootimage.cpp if
+  // you add/remove/change fields in this class:
+
   uint8_t age;
   bool hasMask;
   bool marked;
   bool dirty;
-  unsigned size;
+  uint32_t size;
   Fixie* next;
   Fixie** handle;
   uintptr_t body_[0];

src/heap.h
@@ -16,6 +16,12 @@
 
 namespace vm {
 
+// an object must survive TenureThreshold + 2 garbage collections
+// before being copied to gen2 (must be at least 1):
+const unsigned TenureThreshold = 3;
+
+const unsigned FixieTenureThreshold = TenureThreshold + 2;
+
 class Heap: public Allocator {
  public:
   enum CollectionType {

commit 4e4d109787fdb9b38b48ed83eae7cf0d4bcb25c3

fix regression in static field offset calculation
One of the changes in commit 5b4f179 broke this calculation.

src/bootimage.cpp
@@ -496,8 +496,10 @@ makeCodeImage(Thread* t, Zone* zone, BootImage* image, uint8_t* code,
             }
 
             if (fieldFlags(t, field) & ACC_STATIC) {
-              while (targetStaticOffset % targetSize) {
-                ++ targetStaticOffset;
+              unsigned excess = (targetStaticOffset % targetSize)
+                % TargetBytesPerWord;
+              if (excess) {
+                targetStaticOffset += TargetBytesPerWord - excess;
               }
 
               buildStaticOffset = fieldOffset(t, field);

src/machine.cpp
@@ -1145,8 +1145,9 @@ parseFieldTable(Thread* t, Stream& s, object class_, object pool)
 
       unsigned size = fieldSize(t, code);
       if (flags & ACC_STATIC) {
-        while (staticOffset % size) {
-          ++ staticOffset;
+        unsigned excess = (staticOffset % size) % BytesPerWord;
+        if (excess) {
+          staticOffset += BytesPerWord - excess;
         }
 
         fieldOffset(t, field) = staticOffset;