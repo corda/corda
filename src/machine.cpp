corda/corda

commit 6fceca940fd363827350d5e8348be52686372212

fix makeByteArray and makeString for strings longer than 256 characters

src/machine.cpp
@@ -3048,17 +3048,32 @@ popResources(Thread* t)
 }
 
 object
-makeByteArray(Thread* t, const char* format, va_list a)
+makeByteArray(Thread* t, const char* format, va_list a, int size)
+{
+  THREAD_RUNTIME_ARRAY(t, char, buffer, size);
+  
+  int r = vm::vsnprintf(RUNTIME_ARRAY_BODY(buffer), size - 1, format, a);
+  if (r >= 0 and r < size - 1) {
+    object s = makeByteArray(t, strlen(RUNTIME_ARRAY_BODY(buffer)) + 1);
+    memcpy(&byteArrayBody(t, s, 0), RUNTIME_ARRAY_BODY(buffer),
+           byteArrayLength(t, s));
+    return s;
+  } else {
+    return 0;
+  }
+}
+
+object
+makeByteArray(Thread* t, const char* format, ...)
 {
   int size = 256;
   while (true) {
-    THREAD_RUNTIME_ARRAY(t, char, buffer, size);
-  
-    int r = vm::vsnprintf(RUNTIME_ARRAY_BODY(buffer), size - 1, format, a);
-    if (r >= 0 and r < size - 1) {
-      object s = makeByteArray(t, strlen(RUNTIME_ARRAY_BODY(buffer)) + 1);
-      memcpy(&byteArrayBody(t, s, 0), RUNTIME_ARRAY_BODY(buffer),
-             byteArrayLength(t, s));
+    va_list a;
+    va_start(a, format);
+    object s = makeByteArray(t, format, a, size);
+    va_end(a);
+
+    if (s) {
       return s;
     } else {
       size *= 2;
@@ -3067,25 +3082,21 @@ makeByteArray(Thread* t, const char* format, va_list a)
 }
 
 object
-makeByteArray(Thread* t, const char* format, ...)
-{
-  va_list a;
-  va_start(a, format);
-  object s = makeByteArray(t, format, a);
-  va_end(a);
-
-  return s;
-}
-
-object
 makeString(Thread* t, const char* format, ...)
 {
-  va_list a;
-  va_start(a, format);
-  object s = makeByteArray(t, format, a);
-  va_end(a);
+  int size = 256;
+  while (true) {
+    va_list a;
+    va_start(a, format);
+    object s = makeByteArray(t, format, a, size);
+    va_end(a);
 
-  return t->m->classpath->makeString(t, s, 0, byteArrayLength(t, s) - 1);
+    if (s) {
+      return t->m->classpath->makeString(t, s, 0, byteArrayLength(t, s) - 1);
+    } else {
+      size *= 2;
+    }
+  }
 }
 
 int

src/machine.h
@@ -2214,7 +2214,7 @@ make(Thread* t, object class_)
 }
 
 object
-makeByteArray(Thread* t, const char* format, va_list a);
+makeByteArray(Thread* t, const char* format, va_list a, int size);
 
 object
 makeByteArray(Thread* t, const char* format, ...);

commit 1d77b0654082f61cfe4577cbcfed030541036913

fix recent throwNew/makeThrowable regression
6fceca9 introduced a string formatting regression in these methods,
which this commit fixes.

src/machine.cpp
@@ -3058,7 +3058,7 @@ popResources(Thread* t)
 }
 
 object
-makeByteArray(Thread* t, const char* format, va_list a, int size)
+makeByteArrayV(Thread* t, const char* format, va_list a, int size)
 {
   THREAD_RUNTIME_ARRAY(t, char, buffer, size);
   
@@ -3080,7 +3080,7 @@ makeByteArray(Thread* t, const char* format, ...)
   while (true) {
     va_list a;
     va_start(a, format);
-    object s = makeByteArray(t, format, a, size);
+    object s = makeByteArrayV(t, format, a, size);
     va_end(a);
 
     if (s) {
@@ -3098,7 +3098,7 @@ makeString(Thread* t, const char* format, ...)
   while (true) {
     va_list a;
     va_start(a, format);
-    object s = makeByteArray(t, format, a, size);
+    object s = makeByteArrayV(t, format, a, size);
     va_end(a);
 
     if (s) {

src/machine.h
@@ -2216,7 +2216,7 @@ make(Thread* t, object class_)
 }
 
 object
-makeByteArray(Thread* t, const char* format, va_list a, int size);
+makeByteArrayV(Thread* t, const char* format, va_list a, int size);
 
 object
 makeByteArray(Thread* t, const char* format, ...);
@@ -2642,25 +2642,37 @@ makeThrowable
 }
 
 inline object
-makeThrowable(Thread* t, Machine::Type type, const char* format, va_list a)
+makeThrowableV(Thread* t, Machine::Type type, const char* format, va_list a,
+               int size)
 {
-  object s = makeByteArray(t, format, a);
+  object s = makeByteArrayV(t, format, a, size);
 
-  object message = t->m->classpath->makeString
-    (t, s, 0, byteArrayLength(t, s) - 1);
+  if (s) {
+    object message = t->m->classpath->makeString
+      (t, s, 0, byteArrayLength(t, s) - 1);
 
-  return makeThrowable(t, type, message);
+    return makeThrowable(t, type, message);
+  } else {
+    return 0;
+  }
 }
 
 inline object
 makeThrowable(Thread* t, Machine::Type type, const char* format, ...)
 {
-  va_list a;
-  va_start(a, format);
-  object r = makeThrowable(t, type, format, a);
-  va_end(a);
+  int size = 256;
+  while (true) {
+    va_list a;
+    va_start(a, format);
+    object r = makeThrowableV(t, type, format, a, size);
+    va_end(a);
 
-  return r;
+    if (r) {
+      return r;
+    } else {
+      size *= 2;
+    }
+  }
 }
 
 void
@@ -2696,12 +2708,19 @@ throwNew
 inline void NO_RETURN
 throwNew(Thread* t, Machine::Type type, const char* format, ...)
 {
-  va_list a;
-  va_start(a, format);
-  object r = makeThrowable(t, type, format, a);
-  va_end(a);
+  int size = 256;
+  while (true) {
+    va_list a;
+    va_start(a, format);
+    object r = makeThrowableV(t, type, format, a, size);
+    va_end(a);
 
-  throw_(t, r);
+    if (r) {
+      throw_(t, r);
+    } else {
+      size *= 2;
+    }
+  }
 }
 
 object