242a6a0d0f9552150febe2a24cbeeeba5f47a989
Joel Dice <joel.dice@gmail.com>
2012-08-11 06:58:07 -0600

    fix field offset calculation mismatch between type-generator.cpp and machine.cpp

--- src/type-generator.cpp
+++ src/type-generator.cpp
@@ -871,6 +871,13 @@ class MemberIterator {
       return true;
     } else {
       while (types) {
+        if (member) {
+          assert(member->type == Object::Scalar);
+          offset_ = ((offset_ + size_) + (BytesPerWord - 1))
+            & ~(BytesPerWord - 1);
+          member = 0;
+        }
+
         type = car(types);
         members = typeMembers(type);
         types = cdr(types);
