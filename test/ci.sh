27d7c8a3c0ac05cf87f9c3e237197f39c91dfd9a
Joel Dice <joel.dice@gmail.com>
2013-02-27 13:47:43 -0700

    skip bootimage test in ci.sh if openjdk is set

--- test/ci.sh
+++ test/ci.sh
@@ -5,5 +5,8 @@ set -e
 make mode=debug test
 make test
 make process=interpret test
-make bootimage=true test
+# bootimage and openjdk builds without openjdk-src don't work:
+if [ -z "${openjdk}" ]; then
+  make bootimage=true test
+fi
 make tails=true continuations=true test
