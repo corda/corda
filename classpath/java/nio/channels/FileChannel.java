3a67f81b5024e963b99d3063424fe8ca8f12efe0
Johannes Schindelin <johannes.schindelin@gmx.de>
2013-10-17 14:58:58 -0500

    Add the FileChannel class
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

---
+++ classpath/java/nio/channels/FileChannel.java
@@ -0,0 +1,33 @@
+/* Copyright (c) 2008-2013, Avian Contributors
+
+   Permission to use, copy, modify, and/or distribute this software
+   for any purpose with or without fee is hereby granted, provided
+   that the above copyright notice and this permission notice appear
+   in all copies.
+
+   There is NO WARRANTY for this software.  See license.txt for
+   details. */
+
+package java.nio.channels;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+
+public abstract class FileChannel implements Channel {
+
+  public static enum MapMode {
+    PRIVATE, READ_ONLY, READ_WRITE
+  };
+
+  public abstract int read(ByteBuffer dst) throws IOException;
+
+  public abstract int read(ByteBuffer dst, long position) throws IOException;
+
+  public abstract int write(ByteBuffer dst) throws IOException;
+
+  public abstract int write(ByteBuffer dst, long position) throws IOException;
+
+  public abstract long position() throws IOException;
+
+  public abstract FileChannel position(long position) throws IOException;
+}
