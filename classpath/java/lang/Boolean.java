0e3e719dd6f9592cb8dcb0beaff616c1d99fae03
Johannes Schindelin <johannes.schindelin@gmx.de>
2013-10-25 13:25:46 -0500

    Add the Boolean#getBoolean method
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- classpath/java/lang/Boolean.java
+++ classpath/java/lang/Boolean.java
@@ -59,6 +59,10 @@ public final class Boolean implements Comparable<Boolean> {
     return value;
   }
 
+  public static boolean getBoolean(String name) {
+    return parseBoolean(System.getProperty(name));
+  }
+
   public static boolean parseBoolean(String string) {
     return string != null && string.equalsIgnoreCase("true");
   }
