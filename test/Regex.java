d00f799d2e0699fe6499e366fcaca71eac98ef23
Johannes Schindelin <johannes.schindelin@gmx.de>
2013-11-11 09:29:24 -0600

    Regex: special-case a(a*?)(a?)(a??)(a+)(a*)a
    Among other challenges, this regular expression is designed to demonstrate
    that thread prioritization is finicky: Given the string 'aaaaaa' to match,
    the first four threads will try to grab the second 'a', the third thread
    (the one that matched the '(a??)' group) having scheduled the same
    instruction pointer to the '(a+)' group that the second -- higher-priority
    -- thread will try to advance to only after processing the '(a??)' group's
    SPLIT. The second thread must override the third thread in that case,
    essentially stopping the latter.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- test/Regex.java
+++ test/Regex.java
@@ -18,9 +18,20 @@ public class Regex {
     expect(!getMatcher(regex, string).matches());
   }
 
+  private static void expectGroups(String regex, String string,
+      String... groups) {
+    Matcher matcher = getMatcher(regex, string);
+    expect(matcher.matches());
+    expect(matcher.groupCount() == groups.length);
+    for (int i = 1; i <= groups.length; ++i) {
+      expect(groups[i - 1].equals(matcher.group(i)));
+    }
+  }
+
   public static void main(String[] args) {
     expectMatch("a(bb)?a", "abba");
     expectNoMatch("a(bb)?a", "abbba");
     expectNoMatch("a(bb)?a", "abbaa");
+    expectGroups("a(a*?)(a?)(a??)(a+)(a*)a", "aaaaaa", "", "a", "", "aaa", "");
   }
 }
