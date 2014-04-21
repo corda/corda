9b7d0d1624247b580922d572c85fa9599338317e
Joel Dice <joel.dice@gmail.com>
2014-04-20 20:14:48 -0600

    update copyright years

--- src/codegen/target/x86/detect.cpp
+++ src/codegen/target/x86/detect.cpp
@@ -1,5 +1,5 @@
 
-/* Copyright (c) 2008-2013, Avian Contributors
+/* Copyright (c) 2008-2014, Avian Contributors
 
    Permission to use, copy, modify, and/or distribute this software
    for any purpose with or without fee is hereby granted, provided
