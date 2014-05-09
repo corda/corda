27ee3114ae172b8e5f9cc2c543d6732da9c80701
Joshua Warner <joshua.warner@readytalk.com>
2014-05-09 15:16:55 -0600

    add build command logging to ci.sh

--- test/ci.sh
+++ test/ci.sh
@@ -2,21 +2,28 @@
 
 set -e
 
+run() {
+  echo '==============================================='
+  echo "${@}"
+  echo '==============================================='
+  "${@}"
+}
+
 if [ -z "${test_target}" ]; then
   test_target=test
 fi
 
 # we shouldn't run jdk-test builds if we're not running the test target
 if [ ${test_target} = test ]; then
-  make ${flags} jdk-test
+ run make ${flags} jdk-test
 fi
 
-make ${flags} ${test_target}
-make ${flags} mode=debug ${test_target}
-make ${flags} process=interpret ${test_target}
+run make ${flags} ${test_target}
+run make ${flags} mode=debug ${test_target}
+run make ${flags} process=interpret ${test_target}
 # bootimage and openjdk builds without openjdk-src don't work:
 if [ -z "${openjdk}" ]; then
-  make ${flags} bootimage=true ${test_target}
+  run make ${flags} bootimage=true ${test_target}
 fi
-make ${flags} tails=true continuations=true ${test_target}
-make ${flags} codegen-targets=all
+run make ${flags} tails=true continuations=true ${test_target}
+run make ${flags} codegen-targets=all
