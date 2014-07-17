joshuawarner32/avian

reduce Allocator interface

src/system/windows.cpp
@@ -837,7 +837,7 @@ class MySystem: public System {
     return SO_SUFFIX;
   }
 
-  virtual const char* toAbsolutePath(avian::util::Allocator* allocator,
+  virtual const char* toAbsolutePath(avian::util::Alloc* allocator,
                                      const char* name)
   {
 #if !defined(WINAPI_FAMILY) || WINAPI_FAMILY_PARTITION(WINAPI_PARTITION_DESKTOP)

corda/corda

src/system/windows.cpp
@@ -893,7 +893,7 @@ class MySystem : public System {
     return SO_SUFFIX;
   }
 
-  virtual const char* toAbsolutePath(avian::util::Allocator* allocator,
+  virtual const char* toAbsolutePath(avian::util::AllocOnly* allocator,
                                      const char* name)
   {
 #if !defined(WINAPI_FAMILY) || WINAPI_FAMILY_PARTITION(WINAPI_PARTITION_DESKTOP)