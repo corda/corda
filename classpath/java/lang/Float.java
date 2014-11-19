0ec230497b1b1bb1c45558cc5af33f89462fc12a
lostdj <lostdj@gmail.com>
2014-11-19 14:27:11 +0300

    Added missing MIX/MAX_VALUE for basic types.

--- classpath/java/lang/Float.java
+++ classpath/java/lang/Float.java
@@ -18,7 +18,10 @@ public final class Float extends Number {
   public static final float NEGATIVE_INFINITY = -1.0f / 0.0f;
   public static final float POSITIVE_INFINITY =  1.0f / 0.0f;
   public static final float NaN =  0.0f / 0.0f;
-  
+
+  public static final float MIN_VALUE = 1.17549435082228750797e-38f;
+  public static final float MAX_VALUE = 3.40282346638528859812e+38f;
+
   private final float value;  
 
   public Float(String value) {
