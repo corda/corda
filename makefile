corda/corda

commit 80ce92a99943365e5e61d8ef64b2b85b16ec2d66

fix iOS ARM64 build

makefile
@@ -1655,7 +1655,8 @@ $(classpath-dep): $(classpath-sources) $(classpath-jar-dep)
 	@echo "compiling classpath classes"
 	@mkdir -p $(classpath-build)
 	classes="$(shell $(MAKE) -s --no-print-directory build=$(build) \
-		$(classpath-classes))"; if [ -n "$${classes}" ]; then \
+		$(classpath-classes) arch=$(build-arch) platform=$(build-platform))"; \
+	if [ -n "$${classes}" ]; then \
 		$(javac) -source 1.6 -target 1.6 \
 			-d $(classpath-build) -bootclasspath $(boot-classpath) \
 		$${classes}; fi

src/compile-arm64.S
@@ -58,8 +58,11 @@ GLOBAL(vmInvoke):
   mov   x5, sp
   str   x5, [x0,#TARGET_THREAD_SCRATCH]
 
-  // copy arguments into place
-  sub   sp, sp, w3, uxtw
+  // copy arguments into place, reserving enough space for them, plus
+  // alignment padding
+  sub   x5, sp, w3, uxtw
+  and   sp, x5, #-16
+
   mov   x4, #0
   b     LOCAL(vmInvoke_argumentTest)
 

commit b777f0b4a2b24b33f54954b29fa675e9f3d6bd1b

fix Cygwin build
80ce92a introduced a regression which caused the Cygwin build to fail,
since it tried to set platform=$(build-platform) when calling make
recursively, and platform=cygwin is rejected by the makefile.
Instead, we need to use $(bootimage-platform), which normalizes
"cygwin" and "mingw32" to "windows".

makefile
@@ -352,13 +352,13 @@ ifneq ($(android),)
 
 	okio-java = $(android)/external/okhttp/okio/src/main/java
 	okio-javas := $(shell find $(okio-java) -name '*.java')
-	
+
 	bcpkix-java = $(android)/external/bouncycastle/bcpkix/src/main/java
 	bcpkix-javas := $(shell find $(bcpkix-java) -name '*.java')
 
 	bcprov-java = $(android)/external/bouncycastle/bcprov/src/main/java
 	bcprov-javas := $(shell find $(bcprov-java) -name '*.java')
-	
+
 	android-classes = \
 		$(call java-classes,$(luni-javas),$(luni-java),$(build)/android) \
 		$(call java-classes,$(crypto-javas),$(crypto-java),$(build)/android) \
@@ -1677,7 +1677,7 @@ $(classpath-dep): $(classpath-sources) $(classpath-jar-dep)
 	@echo "compiling classpath classes"
 	@mkdir -p $(classpath-build)
 	classes="$(shell $(MAKE) -s --no-print-directory build=$(build) \
-		$(classpath-classes) arch=$(build-arch) platform=$(build-platform))"; \
+		$(classpath-classes) arch=$(build-arch) platform=$(bootimage-platform))"; \
 	if [ -n "$${classes}" ]; then \
 		$(javac) -source 1.6 -target 1.6 \
 			-d $(classpath-build) -bootclasspath $(boot-classpath) \