0dc92822b536d8d1ed3d5379325cadc6bdefaa57
Mike Hearn <mike@plan99.net>
2015-11-09 18:27:10 +0000

    Remove the todo list in Main.kt, it just changes too fast to be worth keeping it up to date. TODOs in the code itself remain.

--- src/core/Main.kt
+++
@@ -1,9 +0,0 @@
-package core
-
-// TODO: Implement transaction processing logic first pass (selection of contract types to run). Ensure that outputs are all accounted for.
-//
-// TODO: Make a decision on basic timestamping approach (use external TSAs? use fake TSA?)
-// TODO: Pick a serialisation solution for the prototype (kryo?)
-// TODO: Store transactions to a database, design some conflict detection and rollback
-// TODO: Implement a basic UI that shows contracts and very basic network layer (with chat?)
-
