f98ebce58be91f1d38b252f96a0ea23549b3cd43
Mike Hearn <mike@r3cev.com>
2015-12-02 18:53:16 +0000

    Update copyrights

--- src/test/kotlin/core/visualiser/GroupToGraphConversion.kt
+++ src/test/kotlin/core/visualiser/GroupToGraphConversion.kt
@@ -1,5 +1,9 @@
 /*
- * Copyright 2015, R3 CEV. All rights reserved.
+ * Copyright 2015 Distributed Ledger Group LLC.  Distributed as Licensed Company IP to DLG Group Members
+ * pursuant to the August 7, 2015 Advisory Services Agreement and subject to the Company IP License terms
+ * set forth therein.
+ *
+ * All other rights reserved.
  */
 
 package core.visualiser
