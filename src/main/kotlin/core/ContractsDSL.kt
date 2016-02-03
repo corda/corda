af116f69904a86e81c5db07310d62cbee4682d95
Mike Hearn <mike@r3cev.com>
2016-02-03 15:34:37 +0100

    Minor: bugfix for the Java requireSingleCommand API.

--- src/main/kotlin/core/ContractsDSL.kt
+++ src/main/kotlin/core/ContractsDSL.kt
@@ -124,7 +124,7 @@ inline fun <reified T : CommandData> List<AuthenticatedObject<CommandData>>.requ
 
 // For Java
 fun List<AuthenticatedObject<CommandData>>.requireSingleCommand(klass: Class<out CommandData>) =
-        filter { klass.isInstance(it) }.single()
+        filter { klass.isInstance(it.value) }.single()
 
 /** Returns a timestamp that was signed by the given authority, or returns null if missing. */
 fun List<AuthenticatedObject<CommandData>>.getTimestampBy(timestampingAuthority: Party): TimestampCommand? {
