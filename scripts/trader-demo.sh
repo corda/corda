478b6c5fbabe94258756968e560e9aa0fc376dbc
Mike Hearn <mike@r3cev.com>
2016-02-23 18:26:13 +0100

    Trader demo: use a start script instead of Gradle, which simplifies setup.
    It also stops Gradle messing with the console output.

---
+++ scripts/trader-demo.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+mode=$1
+
+if [ ! -e ./gradlew ]; then
+    echo "Run from the root directory please"
+    exit 1
+fi
+
+if [ ! -d build/install/r3prototyping ]; then
+    ./gradlew installDist
+fi
+
+if [[ "$mode" == "buyer" ]]; then
+    if [ ! -d buyer ]; then
+        mkdir buyer
+        echo "myLegalName = Bank of Zurich" >buyer/config
+    fi
+
+    build/install/r3prototyping/bin/r3prototyping --dir=buyer --service-fake-trades --network-address=localhost
+elif [[ "$mode" == "seller" ]]; then
+    if [ ! -d seller ]; then
+        mkdir seller
+        echo "myLegalName = Bank of Giza" >seller/config
+    fi
+
+    build/install/r3prototyping/bin/r3prototyping --dir=seller --fake-trade-with=localhost --network-address=localhost:31338 --timestamper-identity-file=buyer/identity-public --timestamper-address=localhost
+else
+    echo "Run like this, one in each tab:"
+    echo
+    echo "  scripts/trader-demo.sh buyer"
+    echo "  scripts/trader-demo.sh seller"
+fi
