c18579fd60b85d50cc2081c1c2c3eda4298e92a0
Mike Hearn <mike@r3cev.com>
2016-03-08 13:55:03 +0100

    Minor: add extension method to support "1.234".d to create a BigDecimal

--- core/src/main/kotlin/core/Utils.kt
+++ core/src/main/kotlin/core/Utils.kt
@@ -15,6 +15,7 @@ import com.google.common.util.concurrent.SettableFuture
 import org.slf4j.Logger
 import java.io.BufferedInputStream
 import java.io.InputStream
+import java.math.BigDecimal
 import java.nio.file.Files
 import java.nio.file.Path
 import java.security.SecureRandom
@@ -31,6 +32,8 @@ val Int.days: Duration get() = Duration.ofDays(this.toLong())
 val Int.hours: Duration get() = Duration.ofHours(this.toLong())
 val Int.minutes: Duration get() = Duration.ofMinutes(this.toLong())
 val Int.seconds: Duration get() = Duration.ofSeconds(this.toLong())
+
+val String.d: BigDecimal get() = BigDecimal(this)
 
 /**
  * Returns a random positive long generated using a secure RNG. This function sacrifies a bit of entropy in order to
