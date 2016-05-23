8024f015b3dce6cc76e2b01ace821acac81d1bf7
Matthew Nesbit <matthew.nesbit@r3cev.com>
2016-05-23 15:34:00 +0100

    Fixup dokka path to pickup new locations of modules

--- scripts/generate-docsite.sh
+++ scripts/generate-docsite.sh
@@ -20,7 +20,7 @@ echo
 echo "Generating API docs ..."
 echo
 
-java -jar lib/dokka.jar -output docs/build/html/api src/main/kotlin core/src/main/kotlin contracts/src/main/kotlin  | grep -v "No documentation for"
+java -jar lib/dokka.jar -output docs/build/html/api core/src/main/kotlin contracts/src/main/kotlin node/src/main/kotlin src/main/kotlin   | grep -v "No documentation for"
 
 echo
 echo "Writing robots.txt"
