ba96282591f4b40f202a650bc48aabbd1d02a2e3
Andras Slemmer <andras.slemmer@r3cev.com>
2016-06-16 17:07:12 +0100

    node: use newSecureRandom() instead of SecureRandom.getInstanceStrong()

--- node/src/main/kotlin/com/r3corda/node/internal/AbstractNode.kt
+++ node/src/main/kotlin/com/r3corda/node/internal/AbstractNode.kt
@@ -44,6 +44,7 @@ import java.nio.file.FileAlreadyExistsException
 import java.nio.file.Files
 import java.nio.file.Path
 import java.security.KeyPair
+import java.security.Security
 import java.time.Clock
 import java.time.Instant
 import java.util.*
