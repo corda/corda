8b2211ee0fe17b2c86b2b4c7f8800380fc3aa3fb
Andras Slemmer <andras.slemmer@r3cev.com>
2016-06-17 10:33:13 +0100

    build: compile config depends on quasar, add comment on local .jar dep

--- buildSrc/src/main/groovy/QuasarPlugin.groovy
+++ buildSrc/src/main/groovy/QuasarPlugin.groovy
@@ -14,9 +14,10 @@ class QuasarPlugin implements Plugin<Project> {
         }
 
         project.configurations.create("quasar")
+//        To add a local .jar dependency:
+//        project.dependencies.add("quasar", project.files("${project.rootProject.projectDir}/lib/quasar.jar"))
         project.dependencies.add("quasar", "co.paralleluniverse:quasar-core:${project.rootProject.ext.quasar_version}:jdk8@jar")
-        project.dependencies.add("compile", "co.paralleluniverse:quasar-core:${project.rootProject.ext.quasar_version}:jdk8")
-
+        project.dependencies.add("compile", project.configurations.getByName("quasar"))
 
         project.tasks.withType(Test) {
             jvmArgs "-javaagent:${project.configurations.quasar.singleFile}"
