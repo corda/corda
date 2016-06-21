3ea10904469dac5c5196ba91364cf7564a9c9e31
rick.parker <rick.parker@r3cev.com>
2016-06-21 11:42:35 +0100

    Support for moving TestClock to just beyond a specific instant (helpful for schedule related testing)
    Added TimeWindow to PR
    
    Review feedback
    
    Review feedback

--- node/src/main/kotlin/com/r3corda/node/internal/testing/TestClock.kt
+++ node/src/main/kotlin/com/r3corda/node/internal/testing/TestClock.kt
@@ -1,5 +1,8 @@
 package com.r3corda.node.internal.testing
 
+import com.r3corda.core.serialization.SerializeAsToken
+import com.r3corda.core.serialization.SerializeAsTokenContext
+import com.r3corda.core.serialization.SingletonSerializationToken
 import com.r3corda.node.utilities.MutableClock
 import java.time.Clock
 import java.time.Duration
@@ -12,24 +15,34 @@ import javax.annotation.concurrent.ThreadSafe
  * A [Clock] that can have the time advanced for use in testing
  */
 @ThreadSafe
-class TestClock(private var delegateClock: Clock = Clock.systemUTC()) : MutableClock() {
+class TestClock(private var delegateClock: Clock = Clock.systemUTC()) : MutableClock(), SerializeAsToken {
 
-    @Synchronized fun advanceBy(duration: Duration): Boolean {
-        if (!duration.isNegative) {
-            // It's ok to increment
-            delegateClock = offset(delegateClock, duration)
-            notifyMutationObservers()
-            return true
-        }
-        return false
+    private val token = SingletonSerializationToken(this)
+
+    override fun toToken(context: SerializeAsTokenContext) = SingletonSerializationToken.registerWithContext(token, this, context)
+
+    /**
+     * Advance this [Clock] by the specified [Duration] for testing purposes.
+     */
+    @Synchronized fun advanceBy(duration: Duration) {
+        delegateClock = offset(delegateClock, duration)
+        notifyMutationObservers()
     }
+
+    /**
+     * Move this [Clock] to the specified [Instant] for testing purposes.
+     *
+     * This will only be approximate due to the time ticking away, but will be some time shortly after the requested [Instant].
+     */
+    @Synchronized fun setTo(newInstant: Instant) = advanceBy(Duration.between(instant(), newInstant))
 
     @Synchronized override fun instant(): Instant {
         return delegateClock.instant()
     }
 
-    @Synchronized override fun withZone(zone: ZoneId): Clock {
-        return TestClock(delegateClock.withZone(zone))
+    @Deprecated("Do not use this. Instead seek to use ZonedDateTime methods.", level = DeprecationLevel.ERROR)
+    override fun withZone(zone: ZoneId): Clock {
+        throw UnsupportedOperationException("Tokenized clock does not support withZone()")
     }
 
     @Synchronized override fun getZone(): ZoneId {
