d07db8046186761136cafcac1a8d060a2aa1e151
Matthew Nesbit <matthew.nesbit@r3cev.com>
2016-07-12 16:10:07 +0100

    Repair broken RateFixDemo code and script

--- scripts/get-rate-fix.sh
+++ scripts/get-rate-fix.sh
@@ -13,7 +13,7 @@ if [ ! -e $bin ]; then
     ./gradlew installDist
 fi
 
-if [ ! -e buyer/identity-public ]; then
+if [ ! -e build/trader-demo/buyer/identity-public ]; then
     echo "You must run scripts/trade-demo.sh buyer before running this script (and keep it running)"
     exit 1
 fi
@@ -21,4 +21,4 @@ fi
 # Upload the rates to the buyer node
 curl -F rates=@scripts/example.rates.txt http://localhost:31338/upload/interest-rates
 
-$bin --network-address=localhost:31300 --oracle=localhost --oracle-identity-file=buyer/identity-public
+$bin --network-address=localhost:31300 --directory=build/trader-demo/rates-fix --network-map=localhost:31337 --network-map-identity-file=build/trader-demo/buyer/identity-public --oracle=localhost --oracle-identity-file=build/trader-demo/buyer/identity-public
