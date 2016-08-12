8cfc88ed3a7abc745bac6069a4e4a8b3afa5f0e5
Ross Nicoll <ross.nicoll@r3cev.com>
2016-08-12 13:51:16 +0100

    Remove unused timestamp from LCApplication

--- experimental/src/main/kotlin/com/r3corda/contracts/LCApplication.kt
+++ experimental/src/main/kotlin/com/r3corda/contracts/LCApplication.kt
@@ -30,12 +30,6 @@ class LCApplication : Contract {
         val inputs = tx.inputs.filterIsInstance<State>()
         val outputs = tx.outputs.filterIsInstance<State>()
 
-        // Here, we match acceptable timestamp authorities by name. The list of acceptable TSAs (oracles) must be
-        // hard coded into the contract because otherwise we could fail to gain consensus, if nodes disagree about
-        // who or what is a trusted authority.
-        // FIXME: This isn't used anywhere
-        tx.timestamp
-
         when (command.value) {
             is Commands.ApplyForLC -> {
                 verifyApply(inputs, outputs, command as AuthenticatedObject<Commands.ApplyForLC>, tx)
