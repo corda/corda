c45aa9ffb1ef537a75b7f72ca855e4925aba90af
Clinton Alexander <clinton.alexander@r3cev.com>
2016-08-22 11:49:55 +0100

    Started capsule work.

---
+++ src/main/java/CordaCaplet.java
@@ -0,0 +1,8 @@
+// Please do not move me. I need to be in the global namespace :(
+
+public class CordaCaplet extends Capsule {
+
+    protected CordaCaplet(Capsule pred) {
+        super(pred);
+    }
+}
