210ea2ed95faae6f46ecbbc969f4b4ee544a6fbc
Mike Hearn <mike@r3cev.com>
2016-09-06 12:32:21 +0200

    Minor: remove the TODO about the notary signing last. It is not important at this level as the notary signature does not cover the other signatures. Also a small formatting change to the withAdditionalSignatures methods.

--- core/src/main/kotlin/com/r3corda/core/transactions/SignedTransaction.kt
+++ core/src/main/kotlin/com/r3corda/core/transactions/SignedTransaction.kt
@@ -75,19 +75,14 @@ data class SignedTransaction(val txBits: SerializedBytes<WireTransaction>,
         return missingElements
     }
 
-    /** Returns the same transaction but with an additional (unchecked) signature */
-    fun withAdditionalSignature(sig: DigitalSignature.WithKey): SignedTransaction {
-        // TODO: need to make sure the Notary signs last
-        return copy(sigs = sigs + sig)
-    }
-
-    fun withAdditionalSignatures(sigList: Iterable<DigitalSignature.WithKey>): SignedTransaction {
-        return copy(sigs = sigs + sigList)
-    }
+    /** Returns the same transaction but with an additional (unchecked) signature. */
+    fun withAdditionalSignature(sig: DigitalSignature.WithKey) = copy(sigs = sigs + sig)
+    /** Returns the same transaction but with an additional (unchecked) signatures. */
+    fun withAdditionalSignatures(sigList: Iterable<DigitalSignature.WithKey>) = copy(sigs = sigs + sigList)
 
     /** Alias for [withAdditionalSignature] to let you use Kotlin operator overloading. */
     operator fun plus(sig: DigitalSignature.WithKey) = withAdditionalSignature(sig)
-
+    /** Alias for [withAdditionalSignatures] to let you use Kotlin operator overloading. */
     operator fun plus(sigList: Collection<DigitalSignature.WithKey>) = withAdditionalSignatures(sigList)
 
     /**
