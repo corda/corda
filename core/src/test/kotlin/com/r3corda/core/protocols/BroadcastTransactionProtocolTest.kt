bd59022e8fc106b0e63dc48351d32ade5f32c133
Andras Slemmer <andras.slemmer@r3cev.com>
2016-09-06 13:53:42 +0100

    Revert "core, contracts: Rewrite Generators to use functions instead of classes"
    This reverts commit deddcc91c7f6ad436c44daf69b4cb81a406cb28e.

--- core/src/test/kotlin/com/r3corda/core/protocols/BroadcastTransactionProtocolTest.kt
+++ core/src/test/kotlin/com/r3corda/core/protocols/BroadcastTransactionProtocolTest.kt
@@ -2,17 +2,15 @@ package com.r3corda.core.protocols
 
 import com.esotericsoftware.kryo.io.Input
 import com.pholser.junit.quickcheck.From
-import com.pholser.junit.quickcheck.Produced
 import com.pholser.junit.quickcheck.Property
 import com.pholser.junit.quickcheck.generator.GenerationStatus
 import com.pholser.junit.quickcheck.generator.Generator
 import com.pholser.junit.quickcheck.random.SourceOfRandomness
 import com.pholser.junit.quickcheck.runner.JUnitQuickcheck
-import com.r3corda.contracts.testing.signedTransactionGenerator
+import com.r3corda.contracts.testing.SignedTransactionGenerator
 import com.r3corda.core.serialization.createKryo
 import com.r3corda.core.serialization.serialize
-import com.r3corda.core.testing.generator
-import com.r3corda.core.testing.partyGenerator
+import com.r3corda.core.testing.PartyGenerator
 import com.r3corda.protocols.BroadcastTransactionProtocol
 import org.junit.runner.RunWith
 import kotlin.test.assertEquals
@@ -20,17 +18,14 @@ import kotlin.test.assertEquals
 @RunWith(JUnitQuickcheck::class)
 class BroadcastTransactionProtocolTest {
 
-    companion object {
-        val notifyTxRequestMessageGenerator = generator { random, status ->
-            BroadcastTransactionProtocol.NotifyTxRequestMessage(
-                    tx = signedTransactionGenerator.generate(random, status),
+    class NotifyTxRequestMessageGenerator : Generator<BroadcastTransactionProtocol.NotifyTxRequestMessage>(BroadcastTransactionProtocol.NotifyTxRequestMessage::class.java) {
+        override fun generate(random: SourceOfRandomness, status: GenerationStatus): BroadcastTransactionProtocol.NotifyTxRequestMessage {
+            return BroadcastTransactionProtocol.NotifyTxRequestMessage(
+                    tx = SignedTransactionGenerator().generate(random, status),
                     events = setOf(),
-                    replyToParty = partyGenerator.generate(random, status),
+                    replyToParty = PartyGenerator().generate(random, status),
                     sessionID = random.nextLong()
             )
-        }
-        class NotifyTxRequestMessageGenerator: Generator<BroadcastTransactionProtocol.NotifyTxRequestMessage>(BroadcastTransactionProtocol.NotifyTxRequestMessage::class.java) {
-            override fun generate(random: SourceOfRandomness, status: GenerationStatus) = notifyTxRequestMessageGenerator.generate(random, status)
         }
     }
 
