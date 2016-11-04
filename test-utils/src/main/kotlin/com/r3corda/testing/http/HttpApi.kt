5ac679dd0e8d559671cf0c9d01e910e07de37089
Clinton Alexander <clinton.alexander@r3cev.com>
2016-11-04 15:59:56 +0000

    String payloads to the HttpApi are assumed to be valid JSON and sent as is, previous behaviour would escape the string.

--- test-utils/src/main/kotlin/com/r3corda/testing/http/HttpApi.kt
+++ test-utils/src/main/kotlin/com/r3corda/testing/http/HttpApi.kt
@@ -5,10 +5,20 @@ import com.google.common.net.HostAndPort
 import java.net.URL
 
 class HttpApi(val root: URL) {
+    /**
+     * Send a PUT with a payload to the path on the API specified.
+     *
+     * @param data String values are assumed to be valid JSON. All other values will be mapped to JSON.
+     */
     fun putJson(path: String, data: Any = Unit) = HttpUtils.putJson(URL(root, path), toJson(data))
+    /**
+     * Send a POST with a payload to the path on the API specified.
+     *
+     * @param data String values are assumed to be valid JSON. All other values will be mapped to JSON.
+     */
     fun postJson(path: String, data: Any = Unit) = HttpUtils.postJson(URL(root, path), toJson(data))
 
-    private fun toJson(any: Any) = ObjectMapper().writeValueAsString(any)
+    private fun toJson(any: Any) = if (any is String) any else ObjectMapper().writeValueAsString(any)
 
     companion object {
         fun fromHostAndPort(hostAndPort: HostAndPort, base: String, protocol: String = "http"): HttpApi
