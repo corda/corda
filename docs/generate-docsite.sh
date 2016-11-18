f5fc11bf073d1ff530f8a19e1750a17146134bf4
Mike Hearn <mike@r3cev.com>
2016-11-18 13:28:52 +0100

    Docs: Remove no longer used directory from the Dokka paths to search.

--- docs/generate-docsite.sh
+++ docs/generate-docsite.sh
@@ -36,7 +36,7 @@ fi
 echo
 echo "Generating API docs ..."
 echo
-java -jar lib/dokka.jar -output docs/build/html/api core/src/main/kotlin finance/src/main/kotlin node/src/main/kotlin src/main/kotlin client/src/main/kotlin  | grep -v "No documentation for"
+java -jar lib/dokka.jar -output docs/build/html/api core/src/main/kotlin finance/src/main/kotlin node/src/main/kotlin client/src/main/kotlin  | grep -v "No documentation for"
 
 echo
 echo "Writing robots.txt"
