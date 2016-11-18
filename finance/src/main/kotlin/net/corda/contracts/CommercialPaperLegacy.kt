c0be0db762813c8d4a1120ac55e0a809575f3ffc
RogerWillis <roger.willis@r3cev.com>
2016-11-18 17:43:10 +0000

    Clean up.

--- finance/src/main/kotlin/net/corda/contracts/CommercialPaperLegacy.kt
+++ finance/src/main/kotlin/net/corda/contracts/CommercialPaperLegacy.kt
@@ -20,7 +20,6 @@ import java.util.*
 val CP_LEGACY_PROGRAM_ID = CommercialPaperLegacy()
 
 // TODO: Generalise the notion of an owned instrument into a superclass/supercontract. Consider composition vs inheritance.
-@SuppressWarnings("unused")
 class CommercialPaperLegacy : Contract {
     // TODO: should reference the content of the legal agreement, not its URI
     override val legalContractReference: SecureHash = SecureHash.sha256("https://en.wikipedia.org/wiki/Commercial_paper")
