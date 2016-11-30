bb2b1f4fd03ead49606c51ad1916bd04b646cc32
Patrick Kuo <patrick.kuo@r3cev.com>
2016-11-30 11:17:07 +0000

    shows other nodes on network map

--- tools/explorer/src/main/kotlin/net/corda/explorer/views/Network.kt
+++ tools/explorer/src/main/kotlin/net/corda/explorer/views/Network.kt
@@ -4,20 +4,24 @@ import de.jensd.fx.glyphs.fontawesome.FontAwesomeIcon
 import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconView
 import javafx.beans.binding.Bindings
 import javafx.beans.property.SimpleObjectProperty
+import javafx.collections.FXCollections
 import javafx.scene.Node
 import javafx.scene.Parent
 import javafx.scene.control.ContentDisplay
-import javafx.scene.control.Label
 import javafx.scene.control.ScrollPane
 import javafx.scene.layout.BorderPane
 import javafx.scene.layout.Pane
 import javafx.scene.layout.VBox
 import javafx.scene.text.Font
 import javafx.scene.text.FontWeight
+import net.corda.client.fxutils.concatenate
+import net.corda.client.fxutils.last
 import net.corda.client.fxutils.map
-import net.corda.client.model.NetworkIdentityModel
-import net.corda.client.model.observableList
-import net.corda.client.model.observableValue
+import net.corda.client.fxutils.sequence
+import net.corda.client.model.*
+import net.corda.core.contracts.ContractState
+import net.corda.core.contracts.StateAndRef
+import net.corda.core.contracts.StateRef
 import net.corda.core.node.NodeInfo
 import net.corda.explorer.model.CordaView
 import tornadofx.*
@@ -32,6 +36,7 @@ class Network : CordaView() {
     val myIdentity by observableValue(NetworkIdentityModel::myIdentity)
     val notaries by observableList(NetworkIdentityModel::notaries)
     val peers by observableList(NetworkIdentityModel::parties)
+    val transactions by observableList(GatheredTransactionDataModel::partiallyResolvedTransactions)
 
     // Components
     private val myIdentityPane by fxid<BorderPane>()
@@ -43,11 +48,27 @@ class Network : CordaView() {
     // Create a strong ref to prevent GC.
     private val notaryButtons = notaries.map { it.render() }
     private val peerButtons = peers.filtered { it != myIdentity.value }.map { it.render() }
-    private val coordinate = Bindings.createObjectBinding({
+    private val myCoordinate = Bindings.createObjectBinding({
         myIdentity.value?.physicalLocation?.coordinate?.project(mapPane.width, mapPane.height, 85.0511, -85.0511, -180.0, 180.0)?.let {
-            Pair(it.first - 15, it.second - 10)
-        }
+            Pair(it.first, it.second)
+        } ?: Pair(0.0, 0.0)
     }, arrayOf(mapPane.widthProperty(), mapPane.heightProperty(), myIdentity))
+
+    private val nodesOnMap = FXCollections.observableArrayList(notaries, peers).concatenate().map { node ->
+        label {
+            graphic = FontAwesomeIconView(FontAwesomeIcon.DOT_CIRCLE_ALT)
+            contentDisplay = ContentDisplay.TOP
+            text = node.legalIdentity.name
+            val coordinate = Bindings.createObjectBinding({
+                node.physicalLocation?.coordinate?.project(mapPane.width, mapPane.height, 85.0511, -85.0511, -180.0, 180.0)?.let {
+                    Pair(it.first, it.second)
+                } ?: Pair(0.0, 0.0)
+            }, arrayOf(mapPane.widthProperty(), mapPane.heightProperty()))
+
+            layoutXProperty().bind(widthProperty().map { coordinate.value.first - it.toDouble() / 2 })
+            layoutYProperty().bind(heightProperty().map { coordinate.value.second - it.toDouble() / 4 })
+        }
+    }
 
     private fun NodeInfo.render(): Node {
         return button {
@@ -79,22 +100,33 @@ class Network : CordaView() {
         Bindings.bindContent(notaryList.children, notaryButtons)
         Bindings.bindContent(peerList.children, peerButtons)
 
-        val myLocation = Label("", FontAwesomeIconView(FontAwesomeIcon.DOT_CIRCLE_ALT)).apply { contentDisplay = ContentDisplay.TOP }
-        myLocation.textProperty().bind(myIdentity.map { it?.legalIdentity?.name })
+        Bindings.bindContent(mapPane.children, nodesOnMap)
 
-        myLocation.layoutXProperty().bind(coordinate.map { it?.first })
-        myLocation.layoutYProperty().bind(coordinate.map { it?.second })
-        mapPane.add(myLocation)
-
-        val scroll = Bindings.createObjectBinding({
-            val width = mapScrollPane.content.boundsInLocal.width
-            val height = mapScrollPane.content.boundsInLocal.height
-            val x = myLocation.boundsInParent.maxX
-            val y = myLocation.boundsInParent.minY
-            Pair(x / width, y / height)
-        }, arrayOf(coordinate))
+        val scroll = myCoordinate.map {
+            val width = mapPane.boundsInLocal.width
+            val height = mapPane.boundsInLocal.height
+            Pair(it.first / width, it.second / height)
+        }
 
         mapScrollPane.vvalueProperty().bind(scroll.map { it.second })
         mapScrollPane.hvalueProperty().bind(scroll.map { it.first })
+
+        val transactions = transactions.map {
+            val inputParties = it.inputs.sequence()
+                    .map { it as? PartiallyResolvedTransaction.InputResolution.Resolved }
+                    .filterNotNull()
+                    .map { it.stateAndRef }.getParties()
+            val outputParties = it.transaction.tx.outputs
+                    .mapIndexed { index, transactionState ->
+                        val stateRef = StateRef(it.id, index)
+                        StateAndRef(transactionState, stateRef)
+                    }.observable().getParties()
+            val signingParties = it.transaction.sigs.map { getModel<NetworkIdentityModel>().lookup(it.by) }
+            (inputParties + outputParties + signingParties).map { it.value }.filterNotNull().toSet()
+        }.last()
+
     }
+
+    private fun List<StateAndRef<ContractState>>.getParties() = map { it.state.data.participants.map { getModel<NetworkIdentityModel>().lookup(it) } }.flatten()
+
 }
