b49c97d409bde4ca14793aac2a67b5fde8fde164
Clinton Alexander <clinton.alexander@r3cev.com>
2017-01-05 16:07:31 +0000

    runnodes.bat returns the user to the original directory.

--- gradle-plugins/cordformation/src/main/resources/net/corda/plugins/runnodes.bat
+++ gradle-plugins/cordformation/src/main/resources/net/corda/plugins/runnodes.bat
@@ -1,10 +1,12 @@
 @echo off
 
-REM Change to the directory of this script
-cd /d %~dp0
+REM Change to the directory of this script (%~dp0)
+Pushd %~dp0
 
 FOR /R ".\" %%G in (.) DO (
  Pushd %%G
  start java -jar corda.jar
  Popd
 )
+
+Popd
