9a75adb0ab26ccada094d8f67857b140416ebc07
Mike Hearn <mike@r3cev.com>
2017-01-06 14:35:07 +0100

    Simplify the attachments demo by getting rid of the web API and just using the RPC interface directly. The web API in this case wasn't actually adding anything that had to be server side.

--- samples/attachment-demo/src/integration-test/kotlin/net/corda/attachmentdemo/AttachmentDemoTest.kt
+++ samples/attachment-demo/src/integration-test/kotlin/net/corda/attachmentdemo/AttachmentDemoTest.kt
@@ -4,33 +4,35 @@ import com.google.common.util.concurrent.Futures
 import net.corda.core.getOrThrow
 import net.corda.core.node.services.ServiceInfo
 import net.corda.node.driver.driver
+import net.corda.node.services.User
 import net.corda.node.services.transactions.SimpleNotaryService
 import org.junit.Test
-import kotlin.concurrent.thread
+import java.util.concurrent.CompletableFuture
 
 class AttachmentDemoTest {
     @Test fun `runs attachment demo`() {
         driver(dsl = {
+            val demoUser = listOf(User("demo", "demo", setOf("StartFlow.net.corda.flows.FinalityFlow")))
             val (nodeA, nodeB) = Futures.allAsList(
-                startNode("Bank A"),
-                startNode("Bank B"),
+                startNode("Bank A", rpcUsers = demoUser),
+                startNode("Bank B", rpcUsers = demoUser),
                 startNode("Notary", setOf(ServiceInfo(SimpleNotaryService.Companion.type)))
             ).getOrThrow()
 
-            var recipientReturn: Boolean? = null
-            var senderReturn: Boolean? = null
-            val recipientThread = thread {
-                recipientReturn = AttachmentDemoClientApi(nodeA.configuration.webAddress).runRecipient()
+            val senderThread = CompletableFuture.runAsync {
+                nodeA.rpcClientToNode().use(demoUser[0].username, demoUser[0].password) {
+                    sender(this)
+                }
             }
-            val senderThread = thread {
-                val counterpartyKey = nodeA.nodeInfo.legalIdentity.owningKey.toBase58String()
-                senderReturn = AttachmentDemoClientApi(nodeB.configuration.webAddress).runSender(counterpartyKey)
+            val recipientThread = CompletableFuture.runAsync {
+                nodeB.rpcClientToNode().use(demoUser[0].username, demoUser[0].password) {
+                    recipient(this)
+                }
             }
-            recipientThread.join()
-            senderThread.join()
 
-            assert(recipientReturn == true)
-            assert(senderReturn == true)
+            // Just check they don't throw any exceptions.d
+            recipientThread.get()
+            senderThread.get()
         }, isDebug = true)
     }
 }
--- samples/attachment-demo/src/main/kotlin/net/corda/attachmentdemo/plugin/AttachmentDemoPlugin.kt
+++ samples/attachment-demo/src/main/kotlin/net/corda/attachmentdemo/plugin/AttachmentDemoPlugin.kt
@@ -1,14 +1,10 @@
 package net.corda.attachmentdemo.plugin
 
-import net.corda.attachmentdemo.api.AttachmentDemoApi
+import net.corda.core.node.CordaPluginRegistry
 import net.corda.core.transactions.SignedTransaction
 import net.corda.flows.FinalityFlow
-import net.corda.core.node.CordaPluginRegistry
-import java.util.function.Function
 
 class AttachmentDemoPlugin : CordaPluginRegistry() {
-    // A list of classes that expose web APIs.
-    override val webApis = listOf(Function(::AttachmentDemoApi))
     // A list of Flows that are required for this cordapp
     override val requiredFlows: Map<String, Set<String>> = mapOf(
             FinalityFlow::class.java.name to setOf(SignedTransaction::class.java.name, setOf(Unit).javaClass.name, setOf(Unit).javaClass.name)
