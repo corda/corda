ee99d7e9d0fef1f7b9aa3444ff8d0e854f689cc6
kasiastreich <katarzyna.streich@r3cev.com>
2017-01-06 13:50:18 +0000

    Simm demo fix (#122)
    * Update main to match nodes in deployNodes configuration (no Bank C).
    
    * Update simm-valuation-demo documentation and comments.

--- samples/simm-valuation-demo/src/main/kotlin/net/corda/vega/Main.kt
+++ samples/simm-valuation-demo/src/main/kotlin/net/corda/vega/Main.kt
@@ -5,15 +5,16 @@ import net.corda.node.driver.driver
 import net.corda.node.services.transactions.SimpleNotaryService
 
 /**
- * Sample main used for running within an IDE. Starts 3 nodes (A, B and Notary) as an alternative to running via gradle
+ * Sample main used for running within an IDE. Starts 4 nodes (A, B, C and Notary/Controller) as an alternative to running via gradle
  * This does not start any tests but has the nodes running in preparation for a live web demo or to receive commands
  * via the web api.
  */
 fun main(args: Array<String>) {
     driver(dsl = {
-        startNode("Notary", setOf(ServiceInfo(SimpleNotaryService.type)))
+        startNode("Controller", setOf(ServiceInfo(SimpleNotaryService.type)))
         startNode("Bank A")
         startNode("Bank B")
+        startNode("Bank C")
         waitForAllNodesToFinish()
     }, isDebug = true)
 }
