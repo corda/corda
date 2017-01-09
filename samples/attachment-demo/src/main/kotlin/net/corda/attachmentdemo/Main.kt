88dca6a18d6d889adcfe89a78cc8fac474988084
Mike Hearn <mike@r3cev.com>
2017-01-09 14:44:31 +0100

    Make the attachments demo IntelliJ run configs work again.

--- samples/attachment-demo/src/main/kotlin/net/corda/attachmentdemo/Main.kt
+++ samples/attachment-demo/src/main/kotlin/net/corda/attachmentdemo/Main.kt
@@ -1,18 +1,22 @@
 package net.corda.attachmentdemo
 
+import net.corda.core.div
 import net.corda.core.node.services.ServiceInfo
 import net.corda.node.driver.driver
+import net.corda.node.services.User
 import net.corda.node.services.transactions.SimpleNotaryService
+import java.nio.file.Paths
 
 /**
  * This file is exclusively for being able to run your nodes through an IDE (as opposed to running deployNodes)
  * Do not use in a production environment.
  */
 fun main(args: Array<String>) {
-    driver(dsl = {
+    val demoUser = listOf(User("demo", "demo", setOf("StartFlow.net.corda.flows.FinalityFlow")))
+    driver(isDebug = true, driverDirectory = Paths.get("build") / "attachment-demo-nodes") {
         startNode("Controller", setOf(ServiceInfo(SimpleNotaryService.Companion.type)))
-        startNode("Bank A")
-        startNode("Bank B")
+        startNode("Bank A", rpcUsers = demoUser)
+        startNode("Bank B", rpcUsers = demoUser)
         waitForAllNodesToFinish()
-    }, isDebug = true)
+    }
 }
