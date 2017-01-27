bd604820beb2857231dd9d8300ca937b0fc0e4a1
Joel Dudley <joel.dudley@r3cev.com>
2017-01-27 14:49:23 +0000

    Adds signWithECDSA() to SignedTransaction for improved Java interop.

--- core/src/main/kotlin/net/corda/core/transactions/SignedTransaction.kt
+++ core/src/main/kotlin/net/corda/core/transactions/SignedTransaction.kt
@@ -5,9 +5,11 @@ import net.corda.core.contracts.TransactionResolutionException
 import net.corda.core.crypto.CompositeKey
 import net.corda.core.crypto.DigitalSignature
 import net.corda.core.crypto.SecureHash
+import net.corda.core.crypto.signWithECDSA
 import net.corda.core.node.ServiceHub
 import net.corda.core.serialization.SerializedBytes
 import java.io.FileNotFoundException
+import java.security.KeyPair
 import java.security.SignatureException
 import java.util.*
 
@@ -132,4 +134,7 @@ data class SignedTransaction(val txBits: SerializedBytes<WireTransaction>,
      */
     @Throws(FileNotFoundException::class, TransactionResolutionException::class, SignaturesMissingException::class)
     fun toLedgerTransaction(services: ServiceHub) = verifySignatures().toLedgerTransaction(services)
+
+    /** Alternative to CryptoUtilities' KeyPair.signWithECDSA for better Java interop. */
+    fun signWithECDSA(keyPair: KeyPair) = keyPair.signWithECDSA(this.id.bytes)
 }
