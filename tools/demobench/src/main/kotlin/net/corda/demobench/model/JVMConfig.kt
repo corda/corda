54e5d1a5ea568123036266214c929a550b24fc04
Chris Rankin <chris.rankin@r3.com>
2017-02-06 17:10:52 +0000

    Remove ".exe" from Java executable path, because Windows doesn't need it.

--- tools/demobench/src/main/kotlin/net/corda/demobench/model/JVMConfig.kt
+++ tools/demobench/src/main/kotlin/net/corda/demobench/model/JVMConfig.kt
@@ -1,15 +1,12 @@
 package net.corda.demobench.model
 
-import com.jediterm.terminal.ui.UIUtil
 import java.nio.file.Path
 import java.nio.file.Paths
 import tornadofx.Controller
 
 class JVMConfig : Controller() {
 
-    private val javaExe = if (UIUtil.isWindows) "java.exe" else "java"
-
-    val javaPath: Path = Paths.get(System.getProperty("java.home"), "bin", javaExe)
+    val javaPath: Path = Paths.get(System.getProperty("java.home"), "bin", "java")
 
     init {
         log.info("Java executable: " + javaPath)
