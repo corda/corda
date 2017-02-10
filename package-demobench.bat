5bafa7278000fc78a6a3f4782649e642565324d9
Chris Rankin <chris.rankin@r3.com>
2017-02-10 11:41:52 +0000

    CORPRIV-658: Add BAT file to create unsigned DemoBench.exe.

---
+++ package-demobench.bat
@@ -0,0 +1,17 @@
+@echo off
+
+@rem Creates an EXE installer for DemoBench.
+@rem Assumes that Inno Setup 5+ has already been installed (http://www.jrsoftware.org/isinfo.php)
+
+if not defined JAVA_HOME goto NoJavaHome
+
+set DIRNAME=%~dp0
+if "%DIRNAME%" == "" set DIRNAME=.
+
+call %DIRNAME%\gradlew -PpackageType=exe javapackage
+goto end
+
+:NoJavaHome
+echo "Please set JAVA_HOME correctly"
+
+:end
