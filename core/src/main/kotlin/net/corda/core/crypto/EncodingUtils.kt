ab5c30c3dab457a55b8125b069657773c2a5710f
Konstantinos Chalkias <konstantinos.chalkias@r3.com>
2017-02-14 14:23:38 +0000

    Comment fixes + split some unit-tests

--- core/src/main/kotlin/net/corda/core/crypto/EncodingUtils.kt
+++ core/src/main/kotlin/net/corda/core/crypto/EncodingUtils.kt
@@ -9,42 +9,38 @@ import javax.xml.bind.DatatypeConverter
 
 // [ByteArray] encoders
 
-/** Convert a byte array to a base 58 encoded string.*/
 fun ByteArray.toBase58(): String =
         Base58.encode(this)
 
-/** Convert a byte array to a base 64 encoded string.*/
 fun ByteArray.toBase64(): String =
         Base64.getEncoder().encodeToString(this)
 
-/** Convert a byte array to a hex (base 16) capitalised encoded string.*/
+/** Convert a byte array to a hex (base 16) capitalized encoded string.*/
 fun ByteArray.toHex(): String =
     DatatypeConverter.printHexBinary(this)
 
 
 // [String] encoders and decoders
 
-/** Base58-String to the actual real [String] */
+/** Base58-String to the actual real [String], i.e. "JxF12TrwUP45BMd" -> "Hello World". */
 fun String.base58ToRealString() =
         String(base58ToByteArray(), Charset.defaultCharset())
 
-/** Base64-String to the actual real [String] */
+/** Base64-String to the actual real [String], i.e. "SGVsbG8gV29ybGQ=" -> "Hello World". */
 fun String.base64ToRealString() =
         String(base64ToByteArray())
 
-/** Hex-String to the actual real [String] */
+/** HEX-String to the actual real [String], i.e. "48656C6C6F20576F726C64" -> "Hello World". */
 fun String.hexToRealString() =
         String(hexToByteArray())
 
-/** Base58-String to [ByteArray]. */
 fun String.base58ToByteArray(): ByteArray =
         Base58.decode(this)
 
-/** Base64-String to [ByteArray]. */
 fun String.base64ToByteArray(): ByteArray =
         Base64.getDecoder().decode(this)
 
-/** Hex-String to [ByteArray]. Accept lowercase or capital or mixed letters. */
+/** Hex-String to [ByteArray]. Accept any hex form (capitalized, lowercase, mixed). */
 fun String.hexToByteArray(): ByteArray =
         DatatypeConverter.parseHexBinary(this);
 
