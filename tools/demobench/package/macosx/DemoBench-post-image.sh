aa79ad61064d883b2fe4feb9578cb2886ca28da2
Chris Rankin <rankincj@googlemail.com>
2017-02-20 21:49:06 +0000

    CORPRIV-659: Tweak post-image script for DMG.

--- tools/demobench/package/macosx/DemoBench-post-image.sh
+++ tools/demobench/package/macosx/DemoBench-post-image.sh
@@ -1,7 +1,10 @@
 if [ -z "$JAVA_HOME" ]; then
     echo "Please set JAVA_HOME correctly."
 else
-    INSTALL_HOME=../images/DemoBench.app/Contents/PlugIns/Java.runtime/Contents/Home/jre/bin
+    # Switch to folder containing application.
+    cd ../images/DemoBench.app
+
+    INSTALL_HOME=Contents/PlugIns/Java.runtime/Contents/Home/jre/bin
     mkdir -p $INSTALL_HOME
     cp $JAVA_HOME/jre/bin/java $INSTALL_HOME
 fi
