23733160e17bc35b19f1ec566576320bad48baee
Chris Rankin <chris.rankin@r3.com>
2017-02-20 17:52:17 +0000

    CORPRIV-659: Update installer script and icons for DMG.

--- tools/demobench/package/macosx/DemoBench-post-image.sh
+++ tools/demobench/package/macosx/DemoBench-post-image.sh
@@ -1,5 +1,7 @@
 if [ -z "$JAVA_HOME" ]; then
     echo "Please set JAVA_HOME correctly."
 else
-    cp $JAVA_HOME/jre/bin/java ./DemoBench/runtime/bin
+    INSTALL_HOME=../images/DemoBench.app/Contents/PlugIns/Java.runtime/Contents/Home/jre/bin
+    mkdir -p $INSTALL_HOME
+    cp $JAVA_HOME/jre/bin/java $INSTALL_HOME
 fi
