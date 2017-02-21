a1be1018cc84155c04957fa26bdbbed071aad1d4
Chris Rankin <chris.rankin@r3.com>
2017-02-21 15:35:50 +0000

    CORPRIV-659: Fix DMG installer.

--- tools/demobench/package/macosx/DemoBench-post-image.sh
+++ tools/demobench/package/macosx/DemoBench-post-image.sh
@@ -1,10 +1,11 @@
 if [ -z "$JAVA_HOME" ]; then
-    echo "Please set JAVA_HOME correctly."
-else
-    # Switch to folder containing application.
-    cd ../images/DemoBench.app
-
-    INSTALL_HOME=Contents/PlugIns/Java.runtime/Contents/Home/jre/bin
-    mkdir -p $INSTALL_HOME
-    cp $JAVA_HOME/jre/bin/java $INSTALL_HOME
+    echo "**** Please set JAVA_HOME correctly."
+    exit 1
 fi
+
+# Switch to folder containing application.
+cd ../images/image-*/DemoBench.app
+
+INSTALL_HOME=Contents/PlugIns/Java.runtime/Contents/Home/jre/bin
+mkdir -p $INSTALL_HOME
+cp $JAVA_HOME/jre/bin/java $INSTALL_HOME
