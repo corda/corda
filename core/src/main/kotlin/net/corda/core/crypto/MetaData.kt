69222ef36c5bac4221c07bf3dfa597a590f13360
Konstantinos Chalkias <konstantinos.chalkias@r3.com>
2017-03-07 14:36:13 +0000

    Import @CordaSerializable

--- core/src/main/kotlin/net/corda/core/crypto/MetaData.kt
+++ core/src/main/kotlin/net/corda/core/crypto/MetaData.kt
@@ -1,5 +1,6 @@
 package net.corda.core.crypto
 
+import net.corda.core.serialization.CordaSerializable
 import net.corda.core.serialization.serialize
 import java.security.PublicKey
 import java.time.Instant
