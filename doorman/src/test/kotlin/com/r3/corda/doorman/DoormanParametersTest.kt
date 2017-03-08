db2840c76a99effaf63fc492c3aed3effa637f95
Patrick Kuo <patrick.kuo@r3.com>
2017-03-08 13:47:05 +0000

    address PR issues

--- doorman/src/test/kotlin/com/r3/corda/doorman/DoormanParametersTest.kt
+++ doorman/src/test/kotlin/com/r3/corda/doorman/DoormanParametersTest.kt
@@ -1,24 +1,32 @@
 package com.r3.corda.doorman
 
+import com.typesafe.config.ConfigException
 import org.junit.Test
 import java.io.File
 import kotlin.test.assertEquals
+import kotlin.test.assertFailsWith
 
 class DoormanParametersTest {
-
     private val testDummyPath = ".${File.separator}testDummyPath.jks"
 
     @Test
     fun `parse arg correctly`() {
-        val ref = javaClass.getResource("/reference.conf")
+        val ref = javaClass.getResource("/node.conf")
         val params = DoormanParameters(arrayOf("--keygen", "--keystorePath", testDummyPath, "--configFile", ref.path))
         assertEquals(DoormanParameters.Mode.CA_KEYGEN, params.mode)
         assertEquals(testDummyPath, params.keystorePath.toString())
-        assertEquals(0, params.port)
+        assertEquals(8080, params.port)
 
         val params2 = DoormanParameters(arrayOf("--keystorePath", testDummyPath, "--port", "1000"))
         assertEquals(DoormanParameters.Mode.DOORMAN, params2.mode)
         assertEquals(testDummyPath, params2.keystorePath.toString())
         assertEquals(1000, params2.port)
+    }
+
+    @Test
+    fun `should fail when config missing`() {
+        // dataSourceProperties is missing from node_fail.conf and it should fail when accessed, and shouldn't use default from reference.conf.
+        val params = DoormanParameters(arrayOf("--keygen", "--keystorePath", testDummyPath, "--configFile", javaClass.getResource("/node_fail.conf").path))
+        assertFailsWith<ConfigException.Missing> { params.dataSourceProperties }
     }
 }
