a0328e7228b896b43dcdef099d495786de78ce66
Chris Rankin <chris.rankin@r3.com>
2017-03-09 15:02:20 +0000

    CORDA-268: Reimplement to work on both JDK8 and JDK9 (for now).

--- tools/demobench/src/main/kotlin/net/corda/demobench/ui/CloseableTab.kt
+++ tools/demobench/src/main/kotlin/net/corda/demobench/ui/CloseableTab.kt
@@ -1,20 +1,25 @@
 package net.corda.demobench.ui
 
 import com.sun.javafx.scene.control.behavior.TabPaneBehavior
-import com.sun.javafx.scene.control.skin.TabPaneSkin
 import javafx.scene.Node
 import javafx.scene.control.Tab
+import tornadofx.*
 
+/**
+ * Using reflection, which works on both JDK8 and JDK9.
+ * @see: <a href="https://bugs.openjdk.java.net/browse/JDK-8091261">JDK-8091261</a>
+ */
 class CloseableTab(text: String, content: Node) : Tab(text, content) {
 
     fun requestClose() {
-        val b = behaviour
-        if ((b != null) && b.canCloseTab(this)) {
-            b.closeTab(this)
+        val skin = tabPane?.skin ?: return
+        val field = skin.javaClass.findFieldByName("behavior") ?: return
+        field.isAccessible = true
+
+        val behaviour = field.get(skin) as? TabPaneBehavior ?: return
+        if (behaviour.canCloseTab(this)) {
+            behaviour.closeTab(this)
         }
     }
-
-    private val behaviour: TabPaneBehavior?
-        get() = (tabPane?.skin as? TabPaneSkin)?.behavior
 
 }
