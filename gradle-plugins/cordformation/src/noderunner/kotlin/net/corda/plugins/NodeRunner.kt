6df71e236fbf696c26ca2c172e2ebb809241d1cf
Clinton Alexander <admin@clintonio.com>
2017-03-21 15:27:53 +0000

    Intermediate commit. Squash me.

--- gradle-plugins/cordformation/src/noderunner/kotlin/net/corda/plugins/NodeRunner.kt
+++ gradle-plugins/cordformation/src/noderunner/kotlin/net/corda/plugins/NodeRunner.kt
@@ -6,95 +6,86 @@ import java.nio.file.Files
 import java.nio.file.Paths
 import java.util.Locale
 
+private val nodeJarName = "corda.jar"
+private val webJarName = "corda-webserver.jar"
+private val nodeConfName = "node.conf"
+
 fun main(args: Array<String>) {
+    val startedProcesses = mutableListOf<Process>()
     val headless = (GraphicsEnvironment.isHeadless() || (!args.isEmpty() && (args[0] == "--headless")))
-    NodeRunner(headless).run()
+    val workingDir = Paths.get(System.getProperty("user.dir")).toFile()
+    println("Starting node runner in $workingDir")
+
+    workingDir.list().map { File(workingDir, it) }.forEach {
+        if (isNode(it)) {
+            println("Starting node in $nodeDir")
+            startedProcesses.add(startJava(nodeJarName, it, headless))
+        }
+
+        if (isWebserver(it)) {
+            println("Starting webserver in $webserverDir")
+            startedProcesses.add(startJava(webJarName, it, headless))
+        }
+    }
+
+    println("Started ${startedProcesses.size} processes")
+    println("Node runner finished")
 }
 
-class NodeRunner(val headless: Boolean) {
-    private companion object {
-        val nodeJarName = "corda.jar"
-        val webJarName = "corda-webserver.jar"
-        val nodeConfName = "node.conf"
+private fun isNode(maybeNodeDir: File) = maybeNodeDir.isDirectory
+        && File(maybeNodeDir, nodeJarName).exists()
+        && File(maybeNodeDir, webJarName).exists()
+        && File(maybeNodeDir, nodeConfName).exists()
+
+private fun isWebserver(maybeWebserverDir: File) = isNode(maybeWebserverDir) && hasWebserverPort(maybeWebserverDir)
+
+// TODO: Add a webserver.conf, or use TypeSafe config instead of this hack
+private fun hasWebserverPort(nodeConfDir: File) = Files.readAllLines(File(nodeConfDir, nodeConfName).toPath()).joinToString { it }.contains("webAddress")
+
+private fun startJava(jarName: String, dir: File, headless: Boolean, args: List<String> = listOf()): Process {
+    return if (headless) {
+        execJar(jarName, dir, args)
+    } else {
+        execJarInTerminalWindow(jarName, dir, args)
     }
+}
 
-    private val startedProcesses = mutableListOf<Process>()
-
-    fun run() {
-        val workingDir = Paths.get(System.getProperty("user.dir")).toFile()
-        println("Starting node runner in $workingDir")
-
-        workingDir.list().map { File(workingDir, it) }.forEach {
-            if (isNode(it)) {
-                startNode(it)
-            }
-
-            if (isWebserver(it)) {
-                startWebServer(it)
-            }
-        }
-
-        println("Started ${startedProcesses.size} processes")
-        println("Node runner finished")
+private fun getJavaFunc(headless: Boolean) {
+    return if (headless) {
+        execJar
+    } else {
+        execJarInTerminalWindow
     }
+}
 
-    private fun isNode(maybeNodeDir: File) = maybeNodeDir.isDirectory
-            && File(maybeNodeDir, nodeJarName).exists()
-            && File(maybeNodeDir, webJarName).exists()
-            && File(maybeNodeDir, nodeConfName).exists()
+private fun execJar(jarName: String, dir: File, args: List<String> = listOf()): Process {
+    val nodeName = dir.toPath().fileName
+    val separator = System.getProperty("file.separator")
+    val path = System.getProperty("java.home") + separator + "bin" + separator + "java"
+    val builder = ProcessBuilder(listOf(path, "-Dname=$nodeName", "-jar", jarName) + args)
+    builder.redirectError(Paths.get("error.${dir.toPath().fileName}.log").toFile())
+    builder.inheritIO()
+    builder.directory(dir)
+    return builder.start()
+}
 
-    private fun isWebserver(maybeWebserverDir: File) = isNode(maybeWebserverDir) && hasWebserverPort(maybeWebserverDir)
-
-    // TODO: Add a webserver.conf, or use TypeSafe config instead of this hack
-    private fun hasWebserverPort(nodeConfDir: File) = Files.readAllLines(File(nodeConfDir, nodeConfName).toPath()).joinToString { it }.contains("webAddress")
-
-    private fun startNode(nodeDir: File) {
-        println("Starting node in $nodeDir")
-        startedProcesses.add(startJava(nodeJarName, nodeDir))
-    }
-
-    private fun startWebServer(webserverDir: File) {
-        println("Starting webserver in $webserverDir")
-        startedProcesses.add(startJava(webJarName, webserverDir))
-    }
-
-    private fun startJava(jarName: String, dir: File, args: List<String> = listOf()): Process {
-        return if (headless) {
-            execJar(jarName, dir, args)
-        } else {
-            execJarInTerminalWindow(jarName, dir, args)
-        }
-    }
-
-    private fun execJar(jarName: String, dir: File, args: List<String> = listOf()): Process {
-        val nodeName = dir.toPath().fileName
-        val separator = System.getProperty("file.separator")
-        val path = System.getProperty("java.home") + separator + "bin" + separator + "java"
-        val builder = ProcessBuilder(listOf(path, "-Dname=$nodeName", "-jar", jarName) + args)
-        builder.redirectError(Paths.get("error.${dir.toPath().fileName}.log").toFile())
-        builder.inheritIO()
-        builder.directory(dir)
-        return builder.start()
-    }
-
-    private fun execJarInTerminalWindow(jarName: String, dir: File, args: List<String> = listOf()): Process {
-        val javaCmd = "java -jar $jarName " + args.joinToString(" ") { it }
-        val nodeName = dir.toPath().fileName + " " + jarName
-        val osName = System.getProperty("os.name", "generic").toLowerCase(Locale.ENGLISH)
-        val cmd = if ((osName.indexOf("mac") >= 0) || (osName.indexOf("darwin") >= 0)) {
-            """osascript -e "tell app "Terminal
+private fun execJarInTerminalWindow(jarName: String, dir: File, args: List<String> = listOf()): Process {
+    val javaCmd = "java -jar $jarName " + args.joinToString(" ") { it }
+    val nodeName = dir.toPath().fileName + " " + jarName
+    val osName = System.getProperty("os.name", "generic").toLowerCase(Locale.ENGLISH)
+    val cmd = if ((osName.indexOf("mac") >= 0) || (osName.indexOf("darwin") >= 0)) {
+        """osascript -e "tell app "Terminal
 activate
 tell application \"System Events\" to tell process \"Terminal\" to keystroke \"t\" using command down
 delay 0.5
 do script "bash -c 'cd $dir; /usr/libexec/java_home -v 1.8 --exec $javaCmd && exit'" in window 1"
 """
-        } else if (osName.indexOf("win") >= 0) {
-            """cmd /C "start $javaCmd""""
-        } else {
-            // Assume Linux
-            """xterm -T "$nodeName" -e $javaCmd"""
-        }
-
-        return Runtime.getRuntime().exec(cmd, null, dir)
+    } else if (osName.indexOf("win") >= 0) {
+        """cmd /C "start $javaCmd""""
+    } else {
+        // Assume Linux
+        """xterm -T "$nodeName" -e $javaCmd"""
     }
+
+    return Runtime.getRuntime().exec(cmd, null, dir)
 }
