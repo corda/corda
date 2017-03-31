48b121d145eba62c97eec98800a1b8c2d7b632b9
Ross Nicoll <ross.nicoll@r3.com>
2017-03-31 11:21:36 +0100

    Add Cash*Flow tests
    Add tests for CashIssueFlow, CashPaymentFlow and CashExitFlow. While these were mostly covered by other tests
    already, CashExistFlow was not, and any bugs would be harder to identify because they are mixed in with other
    functionality (i.e. vault tests)

---
+++ finance/src/test/kotlin/net/corda/flows/CashIssueFlowTests.kt
@@ -0,0 +1,65 @@
+package net.corda.flows
+
+import net.corda.contracts.asset.Cash
+import net.corda.core.contracts.DOLLARS
+import net.corda.core.contracts.`issued by`
+import net.corda.core.crypto.Party
+import net.corda.core.getOrThrow
+import net.corda.core.serialization.OpaqueBytes
+import net.corda.testing.node.InMemoryMessagingNetwork.ServicePeerAllocationStrategy.RoundRobin
+import net.corda.testing.node.MockNetwork
+import net.corda.testing.node.MockNetwork.MockNode
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertFailsWith
+
+class CashIssueFlowTests {
+    private val net = MockNetwork(servicePeerAllocationStrategy = RoundRobin())
+    private lateinit var bankOfCordaNode: MockNode
+    private lateinit var bankOfCorda: Party
+    private lateinit var notaryNode: MockNode
+    private lateinit var notary: Party
+
+    @Before
+    fun start() {
+        val nodes = net.createTwoNodes()
+        notaryNode = nodes.first
+        bankOfCordaNode = nodes.second
+        notary = notaryNode.info.notaryIdentity
+        bankOfCorda = bankOfCordaNode.info.legalIdentity
+
+        net.runNetwork()
+    }
+
+    @After
+    fun cleanUp() {
+        net.stopNodes()
+    }
+
+    @Test
+    fun `issue some cash`() {
+        val expected = 500.DOLLARS
+        val ref = OpaqueBytes.of(0x01)
+        val future = bankOfCordaNode.services.startFlow(CashIssueFlow(expected, ref,
+                bankOfCorda,
+                notary)).resultFuture
+        net.runNetwork()
+        val issueTx = future.getOrThrow()
+        val output = issueTx.tx.outputs.single().data as Cash.State
+        assertEquals(expected.`issued by`(bankOfCorda.ref(ref)), output.amount)
+    }
+
+    @Test
+    fun `issue zero cash`() {
+        val expected = 0.DOLLARS
+        val future = bankOfCordaNode.services.startFlow(CashIssueFlow(expected, OpaqueBytes.of(0x01),
+                bankOfCorda,
+                notary)).resultFuture
+        net.runNetwork()
+        assertFailsWith<IllegalArgumentException> {
+            future.getOrThrow()
+        }
+    }
+}
