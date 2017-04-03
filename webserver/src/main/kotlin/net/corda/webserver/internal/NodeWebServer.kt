352da7e4c7423728c0f5f3e33c0ec3cd17c090c0
Chris Rankin <chris.rankin@r3.com>
2017-04-03 17:06:30 +0100

    Add 'finance' module to Corda WebServer, for plugins to use. (#489)
    * Add 'finance' module to Corda WebServer, for plugins to use.
    * Fix wording of error message.

--- webserver/src/main/kotlin/net/corda/webserver/internal/NodeWebServer.kt
+++ webserver/src/main/kotlin/net/corda/webserver/internal/NodeWebServer.kt
@@ -161,6 +161,11 @@ class NodeWebServer(val config: WebServerConfig) {
             } catch (e: java.nio.file.NoSuchFileException) {
                 log.debug("Tried to open a file that doesn't yet exist, retrying", e)
                 Thread.sleep(retryDelay)
+            } catch (e: Throwable) {
+                // E.g. a plugin cannot be instantiated?
+                // Note that we do want the exception stacktrace.
+                log.error("Cannot start WebServer", e)
+                throw e
             }
         }
     }
