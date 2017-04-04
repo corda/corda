a93692e4344670e4f84770deb9bcedbf4170ed91
Clinton <clintonxa@gmail.com>
2017-04-04 15:43:55 +0100

    Added an exception mapper that prints out errors emitted by the webserver (#484)
    Added an exception mapper that prints out errors emitted by the JAX-RS server to aid debugging.

--- webserver/src/main/kotlin/net/corda/webserver/internal/APIServerImpl.kt
+++ webserver/src/main/kotlin/net/corda/webserver/internal/APIServerImpl.kt
@@ -1,13 +1,12 @@
 package net.corda.webserver.internal
 
 import net.corda.core.messaging.CordaRPCOps
-import net.corda.webserver.api.*
+import net.corda.webserver.api.APIServer
 import java.time.LocalDateTime
 import java.time.ZoneId
 import javax.ws.rs.core.Response
 
 class APIServerImpl(val rpcOps: CordaRPCOps) : APIServer {
-
     override fun serverTime(): LocalDateTime {
         return LocalDateTime.ofInstant(rpcOps.currentNodeTime(), ZoneId.of("UTC"))
     }
