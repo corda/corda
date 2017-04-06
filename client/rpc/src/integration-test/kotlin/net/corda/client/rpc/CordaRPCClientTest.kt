d6403ce8cb3f8d6484c841e306f58294defb483c
Chris Rankin <chris.rankin@r3.com>
2017-04-06 10:06:58 +0100

    Move CordaRPCClientTest into :client:rpc module. (#511)

--- client/rpc/src/integration-test/kotlin/net/corda/client/rpc/CordaRPCClientTest.kt
+++ client/rpc/src/integration-test/kotlin/net/corda/client/rpc/CordaRPCClientTest.kt
@@ -1,6 +1,5 @@
-package net.corda.client.jfx
+package net.corda.client.rpc
 
-import net.corda.client.rpc.CordaRPCClient
 import net.corda.core.contracts.DOLLARS
 import net.corda.core.flows.FlowException
 import net.corda.core.getOrThrow
