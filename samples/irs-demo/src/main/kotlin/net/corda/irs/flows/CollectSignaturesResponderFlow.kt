22a3fac9d8f0ce333d5f7b9feb2481bc84a556d8
RogerWillis <roger.willis@r3cev.com>
2017-04-19 16:57:14 +0100

    Added the subclassed CollectsigsFlow to the trader demo, whitelisted it and added a flow initiator for the responder.

---
+++ samples/irs-demo/src/main/kotlin/net/corda/irs/flows/CollectSignaturesResponderFlow.kt
@@ -0,0 +1,19 @@
+package net.corda.irs.flows
+
+import co.paralleluniverse.fibers.Suspendable
+import net.corda.core.crypto.Party
+import net.corda.core.node.PluginServiceHub
+import net.corda.core.transactions.SignedTransaction
+import net.corda.flows.AbstractCollectSignaturesFlowResponder
+import net.corda.flows.CollectSignaturesFlow
+
+class Service(services: PluginServiceHub) {
+    init {
+        services.registerFlowInitiator(CollectSignaturesFlow::class.java, ::Responder)
+    }
+}
+
+class Responder(otherParty: Party) : AbstractCollectSignaturesFlowResponder(otherParty) {
+    @Suspendable override fun checkTransaction(stx: SignedTransaction) = Unit
+}
+
