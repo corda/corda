d8ff6d76b485dcfcf6e52b598048a5be9f8ecd58
Andrzej Cichocki <andrzej.cichocki@r3.com>
2017-04-27 16:55:56 +0100

    * Use same java.home as caller in terminal case, previously only non-terminal case did that * On Linux, start sh on failure so that terminals don't disappear taking error messages with them

--- gradle-plugins/cordformation/src/noderunner/kotlin/net/corda/plugins/NodeRunner.kt
+++ gradle-plugins/cordformation/src/noderunner/kotlin/net/corda/plugins/NodeRunner.kt
@@ -3,7 +3,6 @@ package net.corda.plugins
 import java.awt.GraphicsEnvironment
 import java.io.File
 import java.nio.file.Files
-import java.nio.file.Paths
 import java.util.*
 
 private val nodeJarName = "corda.jar"
@@ -20,38 +19,29 @@ private val os: OS by lazy {
 
 private enum class OS { MACOS, WINDOWS, LINUX }
 
-data class IncrementalPortAllocator(var basePort: Int = 5005) {
-    fun next(): Int = basePort++
+private object debugPortAlloc {
+    private var basePort = 5005
+    internal fun next() = basePort++
 }
-
-val debugPortAlloc: IncrementalPortAllocator = IncrementalPortAllocator()
 
 fun main(args: Array<String>) {
     val startedProcesses = mutableListOf<Process>()
     val headless = GraphicsEnvironment.isHeadless() || (args.isNotEmpty() && args[0] == HEADLESS_FLAG)
-    val workingDir = Paths.get(System.getProperty("user.dir")).toFile()
+    val workingDir = File(System.getProperty("user.dir"))
     val javaArgs = args.filter { it != HEADLESS_FLAG }
     println("Starting nodes in $workingDir")
-
-    workingDir.list().map { File(workingDir, it) }.forEach {
-        if (isNode(it)) {
-            startedProcesses += startJarProcess(headless, it, nodeJarName, javaArgs)
-        }
-
-        if (isWebserver(it)) {
-            startedProcesses += startJarProcess(headless, it, webJarName, javaArgs)
-        }
+    workingDir.listFiles().forEach nodeDir@ {
+        val jarName = if (isNode(it)) nodeJarName else if (isWebserver(it)) webJarName else return@nodeDir
+        startedProcesses += startJarProcess(headless, it, jarName, javaArgs)
     }
-
     println("Started ${startedProcesses.size} processes")
     println("Finished starting nodes")
 }
 
-private fun startJarProcess(headless: Boolean, dir: File, jarName: String, javaArgs: List<String>) : Process {
-    val runJar = getJarRunner(headless)
+private fun startJarProcess(headless: Boolean, dir: File, jarName: String, javaArgs: List<String>): Process {
     val debugPort = debugPortAlloc.next()
     println("Starting $jarName in $dir on debug port $debugPort")
-    val proc = runJar(jarName, dir, javaArgs, debugPort)
+    val proc = (if (headless) ::execJar else ::execJarInTerminalWindow)(jarName, dir, javaArgs, debugPort)
     if (os == OS.MACOS) Thread.sleep(1000)
     return proc
 }
@@ -66,32 +56,35 @@ private fun isWebserver(maybeWebserverDir: File) = maybeWebserverDir.isDirectory
         && hasWebserverPort(maybeWebserverDir)
 
 // TODO: Add a webserver.conf, or use TypeSafe config instead of this hack
-private fun hasWebserverPort(nodeConfDir: File) = Files.readAllLines(File(nodeConfDir, nodeConfName).toPath()).joinToString { it }.contains("webAddress")
+private fun hasWebserverPort(nodeConfDir: File) = Files.lines(File(nodeConfDir, nodeConfName).toPath()).anyMatch { "webAddress" in it }
 
-private fun getDebugPortArg(debugPort: Int?) = if (debugPort != null) {
-    listOf("-Dcapsule.jvm.args=-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=$debugPort")
-} else {
-    emptyList()
+private class JavaCommand(jarName: String, debugPort: Int?, nodeName: String, init: MutableList<String>.() -> Unit) {
+    private val words = mutableListOf<String>().apply {
+        add(File(File(System.getProperty("java.home"), "bin"), "java").path)
+        add("-Dname=$nodeName")
+        null != debugPort && add("-Dcapsule.jvm.args=-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=$debugPort")
+        add("-jar"); add(jarName)
+        init()
+    }
+
+    internal fun processBuilder() = ProcessBuilder(words)
+    internal fun joinToString() = words.joinToString(" ")
 }
 
-private fun getJarRunner(headless: Boolean): (String, File, List<String>, Int?) -> Process = if (headless) ::execJar else ::execJarInTerminalWindow
-
-private fun execJar(jarName: String, dir: File, args: List<String> = listOf(), debugPort: Int?): Process {
-    val nodeName = dir.toPath().fileName
-    val separator = System.getProperty("file.separator")
-    val path = System.getProperty("java.home") + separator + "bin" + separator + "java"
-    val builder = ProcessBuilder(listOf(path, "-Dname=$nodeName") + getDebugPortArg(debugPort) + listOf("-jar", jarName, "--no-local-shell") + args)
-    builder.redirectError(Paths.get("error.${dir.toPath().fileName}.log").toFile())
-    builder.inheritIO()
-    builder.directory(dir)
-    return builder.start()
+private fun execJar(jarName: String, dir: File, args: List<String>, debugPort: Int?) = run {
+    val nodeName = dir.name
+    JavaCommand(jarName, debugPort, nodeName) { add("--no-local-shell"); addAll(args) }.processBuilder().apply {
+        redirectError(File("error.${nodeName}.log"))
+        inheritIO()
+        directory(dir)
+    }.start()
 }
 
-private fun execJarInTerminalWindow(jarName: String, dir: File, args: List<String> = listOf(), debugPort: Int?): Process {
-    val nodeName = "${dir.toPath().fileName}-$jarName"
-    val javaCmd = (listOf("java", "-Dname=$nodeName") + getDebugPortArg(debugPort) + listOf("-jar", jarName) + args).joinToString(" ") { it }
-    val builder = when (os) {
-        OS.MACOS -> ProcessBuilder(
+private fun execJarInTerminalWindow(jarName: String, dir: File, args: List<String>, debugPort: Int?) = run {
+    val nodeName = "${dir.name}-$jarName"
+    val javaCmd = JavaCommand(jarName, debugPort, nodeName) { addAll(args) }.joinToString()
+    ProcessBuilder(when (os) {
+        OS.MACOS -> listOf(
                 "osascript", "-e",
                 """tell app "Terminal"
     activate
@@ -100,22 +93,16 @@ private fun execJarInTerminalWindow(jarName: String, dir: File, args: List<Strin
     do script "bash -c 'cd $dir; /usr/libexec/java_home -v 1.8 --exec $javaCmd && exit'" in selected tab of the front window
 end tell"""
         )
-        OS.WINDOWS -> ProcessBuilder(
-                "cmd"
-                , "/C", "start $javaCmd"
-        )
+        OS.WINDOWS -> listOf("cmd", "/C", "start $javaCmd")
         OS.LINUX -> {
             val isTmux = System.getenv("TMUX")?.isNotEmpty() ?: false
-            if (isTmux) {
-                ProcessBuilder(
-                        "tmux", "new-window", "-n", nodeName, javaCmd
-                )
-            } else {
-                ProcessBuilder(
-                        "xterm", "-T", nodeName, "-e", javaCmd
-                )
+            "$javaCmd || sh".let { javaCmd ->
+                if (isTmux) {
+                    listOf("tmux", "new-window", "-n", nodeName, javaCmd)
+                } else {
+                    listOf("xterm", "-T", nodeName, "-e", javaCmd)
+                }
             }
         }
-    }
-    return builder.directory(dir).start()
+    }).directory(dir).start()
 }
