1c2ae893740176aa79bfb9146d0cd0230fe2c554
Clinton Alexander <clinton.alexander@r3cev.com>
2017-05-04 17:18:38 +0100

    Added floating leg fixings to the IRS web demo deals page.

--- samples/irs-demo/src/main/resources/irsweb/js/controllers/Deal.js
+++ samples/irs-demo/src/main/resources/irsweb/js/controllers/Deal.js
@@ -4,6 +4,16 @@ define(['angular', 'utils/semantic', 'services/NodeApi', 'services/HttpErrorHand
     angular.module('irsViewer').controller('DealController', function DealController($http, $scope, $routeParams, nodeService, httpErrorHandler) {
         semantic.init($scope, nodeService.isLoading);
         let handleHttpFail = httpErrorHandler.createErrorHandler($scope);
-        nodeService.getDeal($routeParams.dealId).then((deal) => $scope.deal = deal, handleHttpFail);
+        let decorateDeal = (deal) => {
+            let paymentSchedule = deal.calculation.floatingLegPaymentSchedule;
+            Object.keys(paymentSchedule).map((key, index) => {
+                const sign = paymentSchedule[key].rate.positive ? 1 : -1;
+                paymentSchedule[key].ratePercent = paymentSchedule[key].rate.ratioUnit ? (paymentSchedule[key].rate.ratioUnit.value * 100 * sign).toFixed(5) + "%": "";
+            });
+
+            return deal;
+        };
+
+        nodeService.getDeal($routeParams.dealId).then((deal) => $scope.deal = decorateDeal(deal), handleHttpFail);
     });
 });
