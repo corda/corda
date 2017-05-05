0ed009dfa0c5f0d6b1c51a47900339155335760b
Chris Rankin <chris.rankin@r3.com>
2017-05-05 17:30:51 +0100

    Set java.io.tmpdir to Gradle's build directory for unit and integration tests. (#638)

--- node/src/main/kotlin/net/corda/node/driver/Driver.kt
+++ node/src/main/kotlin/net/corda/node/driver/Driver.kt
@@ -589,7 +589,8 @@ class DriverDSL(
 
                 val systemProperties = overriddenSystemProperties + mapOf(
                         "name" to nodeConf.myLegalName,
-                        "visualvm.display.name" to "corda-${nodeConf.myLegalName}"
+                        "visualvm.display.name" to "corda-${nodeConf.myLegalName}",
+                        "java.io.tmpdir" to System.getProperty("java.io.tmpdir") // Inherit from parent process
                 )
                 val extraJvmArguments = systemProperties.map { "-D${it.key}=${it.value}" } +
                         "-javaagent:$quasarJarPath"
@@ -621,7 +622,10 @@ class DriverDSL(
                         className = className, // cannot directly get class for this, so just use string
                         arguments = listOf("--base-directory", handle.configuration.baseDirectory.toString()),
                         jdwpPort = debugPort,
-                        extraJvmArguments = listOf("-Dname=node-${handle.configuration.p2pAddress}-webserver"),
+                        extraJvmArguments = listOf(
+                            "-Dname=node-${handle.configuration.p2pAddress}-webserver",
+                            "-Djava.io.tmpdir=${System.getProperty("java.io.tmpdir")}" // Inherit from parent process
+                        ),
                         errorLogPath = Paths.get("error.$className.log")
                 )
             }.flatMap { process -> addressMustBeBound(executorService, handle.webAddress, process).map { process } }
