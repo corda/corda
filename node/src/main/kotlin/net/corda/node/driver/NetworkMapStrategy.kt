592f708c69be641f45d07c4daa1a1f2bca2948e9
Andrzej Cichocki <andrzej.cichocki@r3.com>
2017-05-11 12:17:22 +0100

    Document these classes.

--- node/src/main/kotlin/net/corda/node/driver/NetworkMapStrategy.kt
+++ node/src/main/kotlin/net/corda/node/driver/NetworkMapStrategy.kt
@@ -4,6 +4,12 @@ import com.google.common.net.HostAndPort
 import net.corda.core.utilities.DUMMY_MAP
 import org.bouncycastle.asn1.x500.X500Name
 
+/**
+ * Instruct the driver how to set up the network map, if at all.
+ * @see FalseNetworkMap
+ * @see DedicatedNetworkMap
+ * @see NominatedNetworkMap
+ */
 abstract class NetworkMapStrategy(internal val startDedicated: Boolean, internal val legalName: X500Name) {
     internal abstract fun serviceConfig(dedicatedAddress: HostAndPort, nodeName: X500Name, p2pAddress: HostAndPort): Map<String, String>?
 }
@@ -17,10 +23,20 @@ abstract class AbstractDedicatedNetworkMap(start: Boolean) : NetworkMapStrategy(
     override fun serviceConfig(dedicatedAddress: HostAndPort, nodeName: X500Name, p2pAddress: HostAndPort) = toServiceConfig(dedicatedAddress, legalName)
 }
 
+/**
+ * Do not start a network map.
+ */
 object FalseNetworkMap : AbstractDedicatedNetworkMap(false)
 
+/**
+ * Start a dedicated node to host the network map.
+ */
 object DedicatedNetworkMap : AbstractDedicatedNetworkMap(true)
 
+/**
+ * As in gradle-based demos, nominate a node to host the network map, so that there is one fewer node in total than in the [DedicatedNetworkMap] case.
+ * Will fail if the port you pass in does not match the P2P port the driver assigns to the named node.
+ */
 class NominatedNetworkMap(legalName: X500Name, private val address: HostAndPort) : NetworkMapStrategy(false, legalName) {
     override fun serviceConfig(dedicatedAddress: HostAndPort, nodeName: X500Name, p2pAddress: HostAndPort) = if (nodeName != legalName) {
         toServiceConfig(address, legalName)
