3f7acd3c687fa29a94f8a4e389d150ff87ef240a
Mike Hearn <mike@r3cev.com>
2017-05-18 12:13:03 +0200

    Minor: just print the X.500 name in Party toString not base58 key too.
    It's kind of annoying to have the big binary blog in the toString, which is often used for UI/debug purposes.

--- core/src/main/kotlin/net/corda/core/identity/Party.kt
+++ core/src/main/kotlin/net/corda/core/identity/Party.kt
@@ -28,7 +28,7 @@ import java.security.PublicKey
  */
 class Party(val name: X500Name, owningKey: PublicKey) : AbstractParty(owningKey) {
     constructor(certAndKey: CertificateAndKey) : this(X500Name(certAndKey.certificate.subjectDN.name), certAndKey.keyPair.public)
-    override fun toString() = "${owningKey.toBase58String()} ($name)"
+    override fun toString() = name.toString()
     override fun nameOrNull(): X500Name? = name
 
     override fun ref(bytes: OpaqueBytes): PartyAndReference = PartyAndReference(this, bytes)
