a7d8c1065bb0fa5154a5e9b8dfee11091cc073d5
Andrzej Cichocki <andrzej.cichocki@r3.com>
2017-05-22 17:14:20 +0100

    Use a random notary, not the first one returned by network map.

--- samples/raft-notary-demo/src/main/kotlin/net/corda/notarydemo/Notarise.kt
+++ samples/raft-notary-demo/src/main/kotlin/net/corda/notarydemo/Notarise.kt
@@ -6,19 +6,20 @@ import net.corda.client.rpc.CordaRPCClient
 import net.corda.client.rpc.notUsed
 import net.corda.core.crypto.toStringShort
 import net.corda.core.getOrThrow
+import net.corda.core.map
 import net.corda.core.messaging.CordaRPCOps
 import net.corda.core.messaging.startFlow
+import net.corda.core.randomOrNull
 import net.corda.core.transactions.SignedTransaction
 import net.corda.core.utilities.BOB
 import net.corda.notarydemo.flows.DummyIssueAndMove
 import net.corda.notarydemo.flows.RPCStartableNotaryFlowClient
 
 fun main(args: Array<String>) {
-    val host = HostAndPort.fromString("localhost:10003")
-    println("Connecting to the recipient node ($host)")
-    CordaRPCClient(host).start(notaryDemoUser.username, notaryDemoUser.password).use {
-        val api = NotaryDemoClientApi(it.proxy)
-        api.startNotarisation()
+    val address = HostAndPort.fromParts("localhost", 10003)
+    println("Connecting to the recipient node ($address)")
+    CordaRPCClient(address).start(notaryDemoUser.username, notaryDemoUser.password).use {
+        NotaryDemoClientApi(it.proxy).notarise(10)
     }
 }
 
@@ -27,34 +28,23 @@ private class NotaryDemoClientApi(val rpc: CordaRPCOps) {
     private val notary by lazy {
         val (parties, partyUpdates) = rpc.networkMapUpdates()
         partyUpdates.notUsed()
-        parties.first { it.advertisedServices.any { it.info.type.isNotary() } }.notaryIdentity
+        parties.randomOrNull { it.advertisedServices.any { it.info.type.isNotary() } }!!.notaryIdentity
     }
 
     private val counterpartyNode by lazy {
         val (parties, partyUpdates) = rpc.networkMapUpdates()
         partyUpdates.notUsed()
-        parties.first { it.legalIdentity.name == BOB.name }
-    }
-
-    private companion object {
-        private val TRANSACTION_COUNT = 10
+        parties.single { it.legalIdentity.name == BOB.name }
     }
 
     /** Makes calls to the node rpc to start transaction notarisation. */
-    fun startNotarisation() {
-        notarise(TRANSACTION_COUNT)
-    }
-
     fun notarise(count: Int) {
+        println("Notary: \"${notary.name}\", with composite key: ${notary.owningKey.toStringShort()}")
         val transactions = buildTransactions(count)
-        val signers = notariseTransactions(transactions)
-        val transactionSigners = transactions.zip(signers).map {
-            val (tx, signer) = it
-            "Tx [${tx.tx.id.prefixChars()}..] signed by $signer"
-        }.joinToString("\n")
-
-        println("Notary: \"${notary.name}\", with composite key: ${notary.owningKey.toStringShort()}\n" +
-                "Notarised ${transactions.size} transactions:\n" + transactionSigners)
+        println("Notarised ${transactions.size} transactions:")
+        transactions.zip(notariseTransactions(transactions)).forEach { (tx, signers) ->
+            println("Tx [${tx.tx.id.prefixChars()}..] signed by ${signers.getOrThrow().joinToString()}")
+        }
     }
 
     /**
@@ -62,11 +52,8 @@ private class NotaryDemoClientApi(val rpc: CordaRPCOps) {
      * and builds a transaction to transfer the asset to the counterparty. The *move* transaction requires notarisation,
      * as it consumes the original asset and creates a copy with the new owner as its output.
      */
-    private fun buildTransactions(count: Int): List<SignedTransaction> {
-        val moveTransactions = (1..count).map {
-            rpc.startFlow(::DummyIssueAndMove, notary, counterpartyNode.legalIdentity).returnValue
-        }
-        return Futures.allAsList(moveTransactions).getOrThrow()
+    private fun buildTransactions(count: Int) = (1..count).map {
+        rpc.startFlow(::DummyIssueAndMove, notary, counterpartyNode.legalIdentity).returnValue.getOrThrow()
     }
 
     /**
@@ -75,10 +62,7 @@ private class NotaryDemoClientApi(val rpc: CordaRPCOps) {
      *
      * @return a list of encoded signer public keys - one for every transaction
      */
-    private fun notariseTransactions(transactions: List<SignedTransaction>): List<String> {
-        // TODO: Remove this suppress when we upgrade to kotlin 1.1 or when JetBrain fixes the bug.
-        @Suppress("UNSUPPORTED_FEATURE")
-        val signatureFutures = transactions.map { rpc.startFlow(::RPCStartableNotaryFlowClient, it).returnValue }
-        return Futures.allAsList(signatureFutures).getOrThrow().map { it.map { it.by.toStringShort() }.joinToString() }
+    private fun notariseTransactions(transactions: List<SignedTransaction>) = transactions.map {
+        rpc.startFlow(::RPCStartableNotaryFlowClient, it).returnValue.map { it.map { it.by.toStringShort() } }
     }
 }
