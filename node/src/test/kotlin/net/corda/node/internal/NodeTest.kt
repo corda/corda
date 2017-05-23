91eb117454d22d3a83d0515f367b6a973c60e044
Andrzej Cichocki <andrzej.cichocki@r3.com>
2017-05-23 10:23:45 +0100

    Fix test.

--- node/src/test/kotlin/net/corda/node/internal/NodeTest.kt
+++ node/src/test/kotlin/net/corda/node/internal/NodeTest.kt
@@ -5,6 +5,7 @@ import net.corda.core.crypto.commonName
 import net.corda.core.div
 import net.corda.core.getOrThrow
 import net.corda.core.utilities.ALICE
+import net.corda.core.utilities.WHITESPACE
 import net.corda.testing.node.NodeBasedTest
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
@@ -12,11 +13,10 @@ import org.junit.Test
 class NodeTest : NodeBasedTest() {
     @Test
     fun `empty plugins directory`() {
-        val baseDirectory = tempFolder.root.toPath() / ALICE.name.commonName
+        val baseDirectory = baseDirectory(ALICE.name)
         (baseDirectory / "plugins").createDirectories()
         val node = startNode(ALICE.name).getOrThrow()
         // Make sure we created the plugins dir in the correct place
         assertThat(baseDirectory).isEqualTo(node.configuration.baseDirectory)
-
     }
 }
