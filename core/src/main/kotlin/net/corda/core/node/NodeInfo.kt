518157639ff6c863162ff01c0cd69c03bb50ba45
Ross Nicoll <ross.nicoll@r3.com>
2017-05-30 19:44:34 +0100

    Store PartyAndCertificate in network map service

--- core/src/main/kotlin/net/corda/core/node/NodeInfo.kt
+++ core/src/main/kotlin/net/corda/core/node/NodeInfo.kt
@@ -1,10 +1,14 @@
 package net.corda.core.node
 
 import net.corda.core.identity.Party
+import net.corda.core.identity.PartyAndCertificate
 import net.corda.core.messaging.SingleMessageRecipient
 import net.corda.core.node.services.ServiceInfo
 import net.corda.core.node.services.ServiceType
 import net.corda.core.serialization.CordaSerializable
+import org.bouncycastle.cert.jcajce.JcaX509CertificateConverter
+import java.security.cert.TrustAnchor
+import java.security.cert.X509Certificate
 
 /**
  * Information for an advertised service including the service specific identity information.
@@ -18,7 +22,7 @@ data class ServiceEntry(val info: ServiceInfo, val identity: Party)
  */
 @CordaSerializable
 data class NodeInfo(val address: SingleMessageRecipient,
-                    val legalIdentity: Party,
+                    val legalIdentity: PartyAndCertificate,
                     val platformVersion: Int,
                     var advertisedServices: List<ServiceEntry> = emptyList(),
                     val physicalLocation: PhysicalLocation? = null) {
