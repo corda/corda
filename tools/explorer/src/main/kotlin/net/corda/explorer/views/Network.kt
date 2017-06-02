927f1cf1cacdad7e6636fe6c2fc948a844f5e5e1
Katarzyna Streich <katarzyna.streich@r3cev.com>
2017-06-02 11:50:35 +0100

    Fix wrong layout of new added nodes to map view in explorer. Fix double rendering of myIdentity label.

--- tools/explorer/src/main/kotlin/net/corda/explorer/views/Network.kt
+++ tools/explorer/src/main/kotlin/net/corda/explorer/views/Network.kt
@@ -54,16 +54,13 @@ class Network : CordaView() {
     private val mapOriginalHeight = 2000.0
 
     // UI node observables, declare here to create a strong ref to prevent GC, which removes listener from observables.
+    private var centralLabel: Label? = null
     private val notaryComponents = notaries.map { it.render() }
     private val notaryButtons = notaryComponents.map { it.button }
     private val peerComponents = peers.map { it.render() }
     private val peerButtons = peerComponents.filtered { it.nodeInfo != myIdentity.value }.map { it.button }
-    private val myComponent = myIdentity.map { it?.render() }
-    private val myButton = myComponent.map { it?.button }
-    private val myMapLabel = myComponent.map { it?.label }
     private val allComponents = FXCollections.observableArrayList(notaryComponents, peerComponents).concatenate()
     private val allComponentMap = allComponents.associateBy { it.nodeInfo.legalIdentity }
-    private val mapLabels = allComponents.map { it.label }
 
     private data class MapViewComponents(val nodeInfo: NodeInfo, val button: Button, val label: Label)
 
@@ -83,21 +80,9 @@ class Network : CordaView() {
         }
     }
 
-    private fun NodeInfo.render(): MapViewComponents {
+    private fun NodeInfo.renderButton(mapLabel: Label): Button {
         val node = this
-        val mapLabel = label(PartyNameFormatter.short.format(node.legalIdentity.name)) {
-            graphic = FontAwesomeIconView(FontAwesomeIcon.DOT_CIRCLE_ALT)
-            contentDisplay = ContentDisplay.TOP
-            val coordinate = Bindings.createObjectBinding({
-                // These coordinates are obtained when we generate the map using TileMill.
-                node.physicalLocation?.coordinate?.project(mapPane.width, mapPane.height, 85.0511, -85.0511, -180.0, 180.0) ?: Pair(0.0, 0.0)
-            }, arrayOf(mapPane.widthProperty(), mapPane.heightProperty()))
-            // Center point of the label.
-            layoutXProperty().bind(coordinate.map { it.first - width / 2 })
-            layoutYProperty().bind(coordinate.map { it.second - height / 4 })
-        }
-
-        val button = button {
+        return button {
             graphic = vbox {
                 label(PartyNameFormatter.short.format(node.legalIdentity.name)) { font = Font.font(font.family, FontWeight.BOLD, 15.0) }
                 gridpane {
@@ -109,21 +94,57 @@ class Network : CordaView() {
                     node.physicalLocation?.apply { row("Location :") { label(this@apply.description) } }
                 }
             }
-            setOnMouseClicked { mapScrollPane.centerLabel(mapLabel) }
+            setOnMouseClicked {
+                centralLabel = mapLabel
+                mapScrollPane.centerLabel(mapLabel)
+            }
+        }
+    }
+
+    private fun NodeInfo.render(): MapViewComponents {
+        val node = this
+        val mapLabel = label(PartyNameFormatter.short.format(node.legalIdentity.name))
+        mapPane.add(mapLabel)
+        // applyCss: This method does not normally need to be invoked directly but may be used in conjunction with Parent.layout()
+        // to size a Node before the next pulse, or if the Scene is not in a Stage.
+        // It's needed to properly add node label to the map (before that width and height are 0 which results in wrong placement of
+        // nodes rendered after initial map rendering).
+        mapPane.applyCss()
+        mapPane.layout()
+        mapLabel.apply {
+            graphic = FontAwesomeIconView(FontAwesomeIcon.DOT_CIRCLE_ALT)
+            contentDisplay = ContentDisplay.TOP
+            val coordinate = Bindings.createObjectBinding({
+                // These coordinates are obtained when we generate the map using TileMill.
+                node.physicalLocation?.coordinate?.project(mapPane.width, mapPane.height, 85.0511, -85.0511, -180.0, 180.0) ?: Pair(0.0, 0.0)
+            }, arrayOf(mapPane.widthProperty(), mapPane.heightProperty()))
+            // Center point of the label.
+            layoutXProperty().bind(coordinate.map { it.first - width / 2 })
+            layoutYProperty().bind(coordinate.map { it.second - height / 4 })
+        }
+
+        val button = node.renderButton(mapLabel)
+        if (node == myIdentity.value) {
+            // It has to be a copy if we want to have notary both in notaries list and in identity (if we are looking at that particular notary node).
+            myIdentityPane.apply { center = node.renderButton(mapLabel) }
+            centralLabel = mapLabel
         }
         return MapViewComponents(this, button, mapLabel)
     }
 
     init {
-        myIdentityPane.centerProperty().bind(myButton)
         Bindings.bindContent(notaryList.children, notaryButtons)
         Bindings.bindContent(peerList.children, peerButtons)
-        Bindings.bindContent(mapPane.children, mapLabels)
         // Run once when the screen is ready.
         // TODO : Find a better way to do this.
         mapPane.heightProperty().addListener { _, old, _ ->
-            if (old == 0.0) myMapLabel.value?.let { mapScrollPane.centerLabel(it) }
+            if (old == 0.0) centralLabel?.let {
+                mapPane.applyCss()
+                mapPane.layout()
+                mapScrollPane.centerLabel(it)
+            }
         }
+
         // Listen on zooming gesture, if device has gesture support.
         mapPane.setOnZoom { zoom(it.zoomFactor, Point2D(it.x, it.y)) }
 
