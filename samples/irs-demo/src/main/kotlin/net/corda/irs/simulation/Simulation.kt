51ea9fec1ade8aec9007f2f250a8293a820a365e
Andrzej Cichocki <andrzej.cichocki@r3.com>
2017-06-02 16:23:05 +0100

    IRS demo fixes (#782)
    * Increase max network map request size so the notary can register
    * Suppress oracle service installation errors in non-oracle nodes
    * Make demos automatically build capsule jars

--- samples/irs-demo/src/main/kotlin/net/corda/irs/simulation/Simulation.kt
+++ samples/irs-demo/src/main/kotlin/net/corda/irs/simulation/Simulation.kt
@@ -121,7 +121,7 @@ abstract class Simulation(val networkSendManuallyPumped: Boolean,
         override fun create(config: NodeConfiguration, network: MockNetwork, networkMapAddr: SingleMessageRecipient?,
                             advertisedServices: Set<ServiceInfo>, id: Int, overrideServices: Map<ServiceInfo, KeyPair>?,
                             entropyRoot: BigInteger): MockNetwork.MockNode {
-            require(advertisedServices.containsType(NodeInterestRates.type))
+            require(advertisedServices.containsType(NodeInterestRates.Oracle.type))
             val cfg = TestNodeConfiguration(
                     baseDirectory = config.baseDirectory,
                     myLegalName = RATES_SERVICE_NAME,
@@ -166,7 +166,7 @@ abstract class Simulation(val networkSendManuallyPumped: Boolean,
             = network.createNode(networkMap.info.address, nodeFactory = NotaryNodeFactory, advertisedServices = ServiceInfo(SimpleNotaryService.type)) as SimulatedNode
     val regulators: List<SimulatedNode> = listOf(network.createNode(networkMap.info.address, start = false, nodeFactory = RegulatorFactory) as SimulatedNode)
     val ratesOracle: SimulatedNode
-            = network.createNode(networkMap.info.address, start = false, nodeFactory = RatesOracleFactory, advertisedServices = ServiceInfo(NodeInterestRates.type)) as SimulatedNode
+            = network.createNode(networkMap.info.address, start = false, nodeFactory = RatesOracleFactory, advertisedServices = ServiceInfo(NodeInterestRates.Oracle.type)) as SimulatedNode
 
     // All nodes must be in one of these two lists for the purposes of the visualiser tool.
     val serviceProviders: List<SimulatedNode> = listOf(notary, ratesOracle, networkMap)
