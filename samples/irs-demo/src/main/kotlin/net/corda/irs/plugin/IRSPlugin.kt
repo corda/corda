4195adfb7bae585ce80da2f87fd8852f63229ac9
szymonsztuka <szymon.sztuka@yahoo.com>
2017-06-20 15:29:35 +0100

    Move Web API from CordaPluginRegistry to new class WebPluginRegistry (#864)
    Move Web API from CordaPluginRegistry to new class WebPluginRegistry

--- samples/irs-demo/src/main/kotlin/net/corda/irs/plugin/IRSPlugin.kt
+++ samples/irs-demo/src/main/kotlin/net/corda/irs/plugin/IRSPlugin.kt
@@ -1,10 +1,10 @@
 package net.corda.irs.plugin
 
-import net.corda.core.node.CordaPluginRegistry
 import net.corda.irs.api.InterestRateSwapAPI
+import net.corda.webserver.services.WebServerPluginRegistry
 import java.util.function.Function
 
-class IRSPlugin : CordaPluginRegistry() {
+class IRSPlugin : WebServerPluginRegistry {
     override val webApis = listOf(Function(::InterestRateSwapAPI))
     override val staticServeDirs: Map<String, String> = mapOf(
             "irsdemo" to javaClass.classLoader.getResource("irsweb").toExternalForm()
