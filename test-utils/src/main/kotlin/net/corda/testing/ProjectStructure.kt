860b6f3610fd080d1e4cd5ff7f1454cd741009bd
Andrzej Cichocki <andrzej.cichocki@r3.com>
2017-06-23 10:26:25 +0100

    More OO.

--- test-utils/src/main/kotlin/net/corda/testing/ProjectStructure.kt
+++ test-utils/src/main/kotlin/net/corda/testing/ProjectStructure.kt
@@ -1,14 +1,14 @@
 package net.corda.testing
 
 import net.corda.core.div
-import java.nio.file.Files
+import net.corda.core.isDirectory
 import java.nio.file.Path
 import java.nio.file.Paths
 
 object ProjectStructure {
     val projectRootDir: Path = run {
         var dir = Paths.get(javaClass.getResource("/").toURI())
-        while (!Files.isDirectory(dir / ".git")) {
+        while (!(dir / ".git").isDirectory()) {
             dir = dir.parent
         }
         dir
