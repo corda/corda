0aadc037efce942b4723178d096b32e4e318117e
Andrzej Cichocki <andrzej.cichocki@r3.com>
2017-06-28 09:54:09 +0100

    Make logging available in IntelliJ between gradle clean and assemble (#929)
    * Enforce absence of node from client rpc smokeTest classpath

--- test-utils/src/main/kotlin/net/corda/testing/RPCDriver.kt
+++ test-utils/src/main/kotlin/net/corda/testing/RPCDriver.kt
@@ -12,7 +12,7 @@ import net.corda.core.div
 import net.corda.core.map
 import net.corda.core.messaging.RPCOps
 import net.corda.core.random63BitValue
-import net.corda.core.utilities.ProcessUtilities
+import net.corda.testing.driver.ProcessUtilities
 import net.corda.node.services.RPCUserService
 import net.corda.node.services.messaging.ArtemisMessagingServer
 import net.corda.node.services.messaging.RPCServer
