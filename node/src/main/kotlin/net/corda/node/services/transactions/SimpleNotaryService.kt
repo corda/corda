00b272906abe8159a40d855a983f4e32601347df
Andrius Dagys <andrius.dagys@r3.com>
2017-06-29 17:25:10 +0100

    Decouple notary implementations from AbstractNode. Allow custom notaries to be provided via CorDapps.

--- node/src/main/kotlin/net/corda/node/services/transactions/SimpleNotaryService.kt
+++ node/src/main/kotlin/net/corda/node/services/transactions/SimpleNotaryService.kt
@@ -1,19 +1,25 @@
 package net.corda.node.services.transactions
 
-import net.corda.core.flows.FlowLogic
 import net.corda.core.identity.Party
+import net.corda.core.node.services.TrustedAuthorityNotaryService
 import net.corda.core.node.services.ServiceType
 import net.corda.core.node.services.TimeWindowChecker
-import net.corda.core.node.services.UniquenessProvider
+import net.corda.flows.NotaryFlow
+import net.corda.node.services.api.ServiceHubInternal
 
 /** A simple Notary service that does not perform transaction validation */
-class SimpleNotaryService(val timeWindowChecker: TimeWindowChecker,
-                          val uniquenessProvider: UniquenessProvider) : NotaryService {
+class SimpleNotaryService(override val services: ServiceHubInternal) : TrustedAuthorityNotaryService() {
     companion object {
         val type = ServiceType.notary.getSubType("simple")
     }
 
-    override val serviceFlowFactory: (Party, Int) -> FlowLogic<Void?> = { otherParty, _ ->
-        NonValidatingNotaryFlow(otherParty, timeWindowChecker, uniquenessProvider)
+    override val timeWindowChecker = TimeWindowChecker(services.clock)
+    override val uniquenessProvider = PersistentUniquenessProvider()
+
+    override fun createServiceFlow(otherParty: Party, platformVersion: Int): NotaryFlow.Service {
+        return NonValidatingNotaryFlow(otherParty, this)
     }
+
+    override fun start() {}
+    override fun stop() {}
 }
