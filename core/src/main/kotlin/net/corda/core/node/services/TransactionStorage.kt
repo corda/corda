46e23b77160bfdf1d49b5151f26f4e3cecbbf039
Shams Asari <shams.asari@r3.com>
2017-07-03 14:29:18 +0100

    Clean up of ServiceHubInternal, including how it's created in AbstractNode

--- core/src/main/kotlin/net/corda/core/node/services/TransactionStorage.kt
+++ core/src/main/kotlin/net/corda/core/node/services/TransactionStorage.kt
@@ -8,7 +8,7 @@ import rx.Observable
 /**
  * Thread-safe storage of transactions.
  */
-interface ReadOnlyTransactionStorage {
+interface TransactionStorage {
     /**
      * Return the transaction with the given [id], or null if no such transaction exists.
      */
@@ -24,18 +24,4 @@ interface ReadOnlyTransactionStorage {
      * Returns all currently stored transactions and further fresh ones.
      */
     fun track(): DataFeed<List<SignedTransaction>, SignedTransaction>
-}
-
-/**
- * Thread-safe storage of transactions.
- */
-interface TransactionStorage : ReadOnlyTransactionStorage {
-    /**
-     * Add a new transaction to the store. If the store already has a transaction with the same id it will be
-     * overwritten.
-     * @param transaction The transaction to be recorded.
-     * @return true if the transaction was recorded successfully, false if it was already recorded.
-     */
-    // TODO: Throw an exception if trying to add a transaction with fewer signatures than an existing entry.
-    fun addTransaction(transaction: SignedTransaction): Boolean
 }
