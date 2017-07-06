54aa4802f986189c2c11a9516fe3e02f54d0b0ca
Shams Asari <shams.asari@r3.com>
2017-07-06 12:27:38 +0100

    Clarifying the need for a single Party c'tor for InitiatedBy flows

--- core/src/main/kotlin/net/corda/core/flows/InitiatedBy.kt
+++ core/src/main/kotlin/net/corda/core/flows/InitiatedBy.kt
@@ -4,8 +4,10 @@ import kotlin.annotation.AnnotationTarget.CLASS
 import kotlin.reflect.KClass
 
 /**
- * This annotation is required by any [FlowLogic] that is designed to be initiated by a counterparty flow. The flow that
- * does the initiating is specified by the [value] property and itself must be annotated with [InitiatingFlow].
+ * This annotation is required by any [FlowLogic] that is designed to be initiated by a counterparty flow. The class must
+ * have at least a constructor which takes in a single [net.corda.core.identity.Party] parameter which represents the
+ * initiating counterparty. The [FlowLogic] that does the initiating is specified by the [value] property and itself must be annotated
+ * with [InitiatingFlow].
  *
  * The node on startup scans for [FlowLogic]s which are annotated with this and automatically registers the initiating
  * to initiated flow mapping.
