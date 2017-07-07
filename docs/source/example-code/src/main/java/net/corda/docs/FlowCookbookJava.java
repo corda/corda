dc2ea79c04f5eb9ef06974e9a2d70d3cf5211c3c
Joel Dudley <joeldudleyr3@users.noreply.github.com>
2017-07-07 11:34:36 +0100

    Update FlowCookbookJava.java

--- docs/source/example-code/src/main/java/net/corda/docs/FlowCookbookJava.java
+++ docs/source/example-code/src/main/java/net/corda/docs/FlowCookbookJava.java
@@ -239,7 +239,9 @@ public class FlowCookbookJava {
             // When building a transaction, input states are passed in as
             // ``StateRef`` instances, which pair the hash of the transaction
             // that generated the state with the state's index in the outputs
-            // of that transaction.
+            // of that transaction. In practice, we'd pass the transaction hash 
+            // or the ``StateRef`` as a parameter to the flow, or extract the 
+            // ``StateRef`` from our vault.
             // DOCSTART 20
             StateRef ourStateRef = new StateRef(SecureHash.sha256("DummyTransactionHash"), 0);
             // DOCEND 20
