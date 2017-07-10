942f3e06ec305e5cc72d442574d6bd7be3c0bfb4
Patrick Kuo <patrick.kuo@r3.com>
2017-07-10 15:50:17 +0100

    refactoring

--- core/src/main/kotlin/net/corda/core/crypto/provider/CordaSecurityProvider.kt
+++ core/src/main/kotlin/net/corda/core/crypto/provider/CordaSecurityProvider.kt
@@ -19,11 +19,12 @@ class CordaSecurityProvider : Provider(PROVIDER_NAME, 0.1, "$PROVIDER_NAME secur
         put("KeyFactory.${CompositeKey.KEY_ALGORITHM}", "net.corda.core.crypto.composite.KeyFactory")
         put("Signature.${CompositeSignature.SIGNATURE_ALGORITHM}", "net.corda.core.crypto.composite.CompositeSignature")
 
-        put("Alg.Alias.KeyFactory.${CompositeSignature.SIGNATURE_ALGORITHM_IDENTIFIER.algorithm.id}", CompositeKey.KEY_ALGORITHM)
-        put("Alg.Alias.KeyFactory.OID.${CompositeSignature.SIGNATURE_ALGORITHM_IDENTIFIER.algorithm.id}", CompositeKey.KEY_ALGORITHM)
+        val compositeKeyOID = CompositeSignature.SIGNATURE_ALGORITHM_IDENTIFIER.algorithm.id
 
-        put("Alg.Alias.Signature.${CompositeSignature.SIGNATURE_ALGORITHM_IDENTIFIER.algorithm.id}", CompositeSignature.SIGNATURE_ALGORITHM)
-        put("Alg.Alias.Signature.OID.${CompositeSignature.SIGNATURE_ALGORITHM_IDENTIFIER.algorithm.id}", CompositeSignature.SIGNATURE_ALGORITHM)
+        put("Alg.Alias.KeyFactory.$compositeKeyOID", CompositeKey.KEY_ALGORITHM)
+        put("Alg.Alias.KeyFactory.OID.$compositeKeyOID", CompositeKey.KEY_ALGORITHM)
+        put("Alg.Alias.Signature.$compositeKeyOID", CompositeSignature.SIGNATURE_ALGORITHM)
+        put("Alg.Alias.Signature.OID.$compositeKeyOID", CompositeSignature.SIGNATURE_ALGORITHM)
     }
 
 }
