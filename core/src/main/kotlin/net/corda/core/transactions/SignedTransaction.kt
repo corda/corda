d5f79f8c2c0985a5bee887dc87d4028111f18f74
Andrzej Cichocki <andrzej.cichocki@r3.com>
2017-07-31 10:49:23 +0100

    Fix compile error.

--- core/src/main/kotlin/net/corda/core/transactions/SignedTransaction.kt
+++ core/src/main/kotlin/net/corda/core/transactions/SignedTransaction.kt
@@ -3,8 +3,8 @@ package net.corda.core.transactions
 import net.corda.core.contracts.*
 import net.corda.core.crypto.DigitalSignature
 import net.corda.core.crypto.SecureHash
-import net.corda.core.getOrThrow
 import net.corda.core.identity.Party
+import net.corda.core.internal.concurrent.getOrThrow
 import net.corda.core.node.ServiceHub
 import net.corda.core.serialization.CordaSerializable
 import net.corda.core.serialization.SerializedBytes
