7fd7551fca53ae80180d4bacfdffe42dc59df187
Ross Nicoll <ross.nicoll@r3.com>
2017-08-02 15:01:10 +0100

    Use Cash contract instead of DummyContract in notary demo

--- samples/notary-demo/src/main/kotlin/net/corda/notarydemo/flows/DummyIssueAndMove.kt
+++ samples/notary-demo/src/main/kotlin/net/corda/notarydemo/flows/DummyIssueAndMove.kt
@@ -1,28 +1,33 @@
 package net.corda.notarydemo.flows
 
 import co.paralleluniverse.fibers.Suspendable
-import net.corda.testing.contracts.DummyContract
+import net.corda.contracts.asset.Cash
+import net.corda.core.contracts.Amount
+import net.corda.core.contracts.GBP
+import net.corda.core.contracts.Issued
 import net.corda.core.flows.FlowLogic
 import net.corda.core.flows.StartableByRPC
 import net.corda.core.identity.Party
 import net.corda.core.transactions.SignedTransaction
-import java.util.*
+import net.corda.core.transactions.TransactionBuilder
 
 @StartableByRPC
 class DummyIssueAndMove(private val notary: Party, private val counterpartyNode: Party) : FlowLogic<SignedTransaction>() {
     @Suspendable
     override fun call(): SignedTransaction {
-        val random = Random()
         // Self issue an asset
-        val issueTxBuilder = DummyContract.generateInitial(random.nextInt(), notary, serviceHub.myInfo.legalIdentity.ref(0))
-        val issueTx = serviceHub.signInitialTransaction(issueTxBuilder)
+        val amount = Amount(1000000, Issued(serviceHub.myInfo.legalIdentity.ref(0), GBP))
+        val issueTxBuilder = TransactionBuilder(notary = notary)
+        val signers = Cash().generateIssue(issueTxBuilder, amount, serviceHub.myInfo.legalIdentity, notary)
+        val issueTx = serviceHub.signInitialTransaction(issueTxBuilder, signers)
         serviceHub.recordTransactions(issueTx)
         // Move ownership of the asset to the counterparty
         val counterPartyKey = counterpartyNode.owningKey
-        val asset = issueTx.tx.outRef<DummyContract.SingleOwnerState>(0)
-        val moveTxBuilder = DummyContract.move(asset, counterpartyNode)
-        val moveTx = serviceHub.signInitialTransaction(moveTxBuilder)
+        val asset = issueTx.tx.outRef<Cash.State>(0)
+        val moveTxBuilder = TransactionBuilder(notary = notary)
+
+        val (_, keys) = serviceHub.vaultService.generateSpend(moveTxBuilder, Amount(amount.quantity, GBP), counterpartyNode)
         // We don't check signatures because we know that the notary's signature is missing
-        return moveTx
+        return serviceHub.signInitialTransaction(moveTxBuilder, keys)
     }
 }
