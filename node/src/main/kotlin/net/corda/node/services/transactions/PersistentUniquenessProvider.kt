a9bf0620f41b255ee7c804da3cec19e7ddc3e8b3
szymonsztuka <szymon.sztuka@yahoo.com>
2017-08-07 08:40:58 +0100

    ewrite PersistentUniquenessProvider to use Hibernate

--- node/src/main/kotlin/net/corda/node/services/transactions/PersistentUniquenessProvider.kt
+++ node/src/main/kotlin/net/corda/node/services/transactions/PersistentUniquenessProvider.kt
@@ -1,82 +1,118 @@
 package net.corda.node.services.transactions
 
-import net.corda.core.internal.ThreadBox
 import net.corda.core.contracts.StateRef
 import net.corda.core.crypto.SecureHash
+import net.corda.core.crypto.parsePublicKeyBase58
 import net.corda.core.identity.Party
+import net.corda.core.internal.ThreadBox
 import net.corda.core.node.services.UniquenessException
 import net.corda.core.node.services.UniquenessProvider
+import net.corda.core.schemas.MappedSchema
 import net.corda.core.serialization.SingletonSerializeAsToken
 import net.corda.core.utilities.loggerFor
 import net.corda.node.utilities.*
 import org.bouncycastle.asn1.x500.X500Name
-import org.jetbrains.exposed.sql.ResultRow
-import org.jetbrains.exposed.sql.statements.InsertStatement
 import java.util.*
 import javax.annotation.concurrent.ThreadSafe
+import javax.persistence.*
 
 /** A RDBMS backed Uniqueness provider */
 @ThreadSafe
 class PersistentUniquenessProvider : UniquenessProvider, SingletonSerializeAsToken() {
+
+
+    object PersistentUniquenessSchema
+
+    object PersistentUniquenessSchemaV1 : MappedSchema(schemaFamily = PersistentUniquenessSchema.javaClass, version = 1,
+            mappedTypes = listOf(PersistentUniqueness::class.java)) {
+
+        @Entity
+        @javax.persistence.Table(name = "${NODE_DATABASE_PREFIX}notary_commit_log")
+        class PersistentUniqueness (
+
+                @EmbeddedId
+                var id: StateRef = StateRef(),
+
+                @Column(name = "consuming_transaction_id")
+                var consumingTxHash: String = "",
+
+                @Column(name = "consuming_input_index", length = 36)
+                var consumingIndex: Int = 0,
+
+                @Embedded
+                var party: Party = Party()
+        ) {
+
+            @Embeddable
+            data class StateRef (
+                    @Column(name = "transaction_id")
+                    var txId: String = "",
+
+                    @Column(name = "output_index", length = 36)
+                    var index: Int = 0
+            ) : java.io.Serializable
+
+            @Embeddable
+            data class Party  (
+                    @Column(name = "requesting_party_name")
+                    var name: String = "",
+
+                    @Column(name = "requesting_party_key", length = 255)
+                    var owningKey: String = ""
+            ) : java.io.Serializable
+        }
+    }
+
+    private class InnerState {
+        val committedStates = createTransactionsMap()
+    }
+
+    private val mutex = ThreadBox(InnerState())
+
     companion object {
-        private val TABLE_NAME = "${NODE_DATABASE_PREFIX}notary_commit_log"
         private val log = loggerFor<PersistentUniquenessProvider>()
-    }
 
-    /**
-     * For each input state store the consuming transaction information.
-     */
-    private object Table : JDBCHashedTable(TABLE_NAME) {
-        val output = stateRef("transaction_id", "output_index")
-        val consumingTxHash = secureHash("consuming_transaction_id")
-        val consumingIndex = integer("consuming_input_index")
-        val requestingParty = party("requesting_party_name", "requesting_party_key")
-    }
-
-    private val committedStates = ThreadBox(object : AbstractJDBCHashMap<StateRef, UniquenessProvider.ConsumingTx, Table>(Table, loadOnInit = false) {
-        override fun keyFromRow(row: ResultRow): StateRef = StateRef(row[table.output.txId], row[table.output.index])
-
-        override fun valueFromRow(row: ResultRow): UniquenessProvider.ConsumingTx = UniquenessProvider.ConsumingTx(
-                row[table.consumingTxHash],
-                row[table.consumingIndex],
-                Party(X500Name(row[table.requestingParty.name]), row[table.requestingParty.owningKey])
-        )
-
-        override fun addKeyToInsert(insert: InsertStatement,
-                                    entry: Map.Entry<StateRef, UniquenessProvider.ConsumingTx>,
-                                    finalizables: MutableList<() -> Unit>) {
-            insert[table.output.txId] = entry.key.txhash
-            insert[table.output.index] = entry.key.index
+        fun createTransactionsMap(): AppendOnlyPersistentMap<StateRef, UniquenessProvider.ConsumingTx, PersistentUniquenessSchemaV1.PersistentUniqueness, PersistentUniquenessSchemaV1.PersistentUniqueness.StateRef> {
+            return AppendOnlyPersistentMap(
+                    cacheBound = 1024,
+                    toPersistentEntityKey = { PersistentUniquenessSchemaV1.PersistentUniqueness.StateRef(it.txhash.toString(), it.index) },
+                    fromPersistentEntity = {
+                        Pair(StateRef(SecureHash.parse(it.id.txId), it.id.index),
+                                UniquenessProvider.ConsumingTx(SecureHash.parse(it.consumingTxHash), it.consumingIndex,
+                                        Party(X500Name(it.party.name), parsePublicKeyBase58(it.party.owningKey))))
+                    },
+                    toPersistentEntity = { key: StateRef, value: UniquenessProvider.ConsumingTx ->
+                        PersistentUniquenessSchemaV1.PersistentUniqueness().apply {
+                            id = PersistentUniquenessSchemaV1.PersistentUniqueness.StateRef(key.txhash.toString(), key.index)
+                            consumingTxHash = value.id.toString()
+                            consumingIndex = value.inputIndex
+                            party = PersistentUniquenessSchemaV1.PersistentUniqueness.Party(value.requestingParty.name.toString())
+                        }
+                    },
+                    persistentEntityClass = PersistentUniquenessSchemaV1.PersistentUniqueness::class.java
+            )
         }
-
-        override fun addValueToInsert(insert: InsertStatement,
-                                      entry: Map.Entry<StateRef, UniquenessProvider.ConsumingTx>,
-                                      finalizables: MutableList<() -> Unit>) {
-            insert[table.consumingTxHash] = entry.value.id
-            insert[table.consumingIndex] = entry.value.inputIndex
-            insert[table.requestingParty.name] = entry.value.requestingParty.name.toString()
-            insert[table.requestingParty.owningKey] = entry.value.requestingParty.owningKey
-        }
-    })
+    }
 
     override fun commit(states: List<StateRef>, txId: SecureHash, callerIdentity: Party) {
-        val conflict = committedStates.locked {
-            val conflictingStates = LinkedHashMap<StateRef, UniquenessProvider.ConsumingTx>()
-            for (inputState in states) {
-                val consumingTx = get(inputState)
-                if (consumingTx != null) conflictingStates[inputState] = consumingTx
-            }
-            if (conflictingStates.isNotEmpty()) {
-                log.debug("Failure, input states already committed: ${conflictingStates.keys}")
-                UniquenessProvider.Conflict(conflictingStates)
-            } else {
-                states.forEachIndexed { i, stateRef ->
-                    put(stateRef, UniquenessProvider.ConsumingTx(txId, i, callerIdentity))
+
+        val conflict = mutex.locked {
+                    val conflictingStates = LinkedHashMap<StateRef, UniquenessProvider.ConsumingTx>()
+                    for (inputState in states) {
+                        val consumingTx = committedStates.get(inputState)
+                        if (consumingTx != null) conflictingStates[inputState] = consumingTx
+                    }
+                    if (conflictingStates.isNotEmpty()) {
+                        log.debug("Failure, input states already committed: ${conflictingStates.keys}")
+                        UniquenessProvider.Conflict(conflictingStates)
+                    } else {
+                        states.forEachIndexed { i, stateRef ->
+                            committedStates[stateRef] = UniquenessProvider.ConsumingTx(txId, i, callerIdentity)
+                        }
+                        log.debug("Successfully committed all input states: $states")
+                        null
+                    }
                 }
-                log.debug("Successfully committed all input states: $states")
-                null
-            }
-        }
 
         if (conflict != null) throw UniquenessException(conflict)
     }
