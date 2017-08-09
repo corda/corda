4458851e361d18bac5e73a7d2fbe84a3c971da0a
josecoll <jose.coll@r3cev.com>
2017-08-09 17:01:02 +0100

    Use partyFromX500Name. Add meaningful exception messages.

--- core/src/main/kotlin/net/corda/core/schemas/converters/AbstractPartyConverter.kt
+++ core/src/main/kotlin/net/corda/core/schemas/converters/AbstractPartyConverter.kt
@@ -4,6 +4,7 @@ import net.corda.core.identity.AbstractParty
 import net.corda.core.identity.AnonymousParty
 import net.corda.core.identity.Party
 import net.corda.core.node.services.IdentityService
+import org.bouncycastle.asn1.x500.X500Name
 import javax.persistence.AttributeConverter
 import javax.persistence.Converter
 
@@ -15,16 +16,16 @@ class AbstractPartyConverter(val identitySvc: IdentityService) : AttributeConver
             when (party) {
                 is AnonymousParty -> identitySvc.partyFromAnonymous(party).toString()
                 is Party -> party.nameOrNull().toString()
-                else -> ""
+                else -> throw IdentityService.UnknownAnonymousPartyException("Unable to resolve identity: $party")
         }
         return partyName
     }
 
     override fun convertToEntityAttribute(dbData: String?): AbstractParty {
         dbData?.let {
-            val party = identitySvc.partyFromName(dbData)
+            val party = identitySvc.partyFromX500Name(X500Name(dbData))
             return party as AbstractParty
         }
-        throw IdentityService.UnknownAnonymousPartyException("")
+        throw IdentityService.UnknownAnonymousPartyException("Unable to resolve identity: $dbData")
     }
 }
