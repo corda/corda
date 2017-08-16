92fbb595763675ba8b488421b5004f482bca6231
Ross Nicoll <ross.nicoll@r3.com>
2017-08-16 11:22:46 +0100

    Correct expected state machine events when moving cash

--- node/src/test/kotlin/net/corda/node/CordaRPCOpsImplTest.kt
+++ node/src/test/kotlin/net/corda/node/CordaRPCOpsImplTest.kt
@@ -156,6 +156,7 @@ class CordaRPCOpsImplTest {
 
         var issueSmId: StateMachineRunId? = null
         var moveSmId: StateMachineRunId? = null
+        var receiveSmId: StateMachineRunId? = null
         stateMachineUpdates.expectEvents {
             sequence(
                     // ISSUE
@@ -168,6 +169,12 @@ class CordaRPCOpsImplTest {
                     // MOVE
                     expect { add: StateMachineUpdate.Added ->
                         moveSmId = add.id
+                    },
+                    expect { add: StateMachineUpdate.Added ->
+                        receiveSmId = add.id
+                    },
+                    expect { remove: StateMachineUpdate.Removed ->
+                        require(remove.id == receiveSmId)
                     },
                     expect { remove: StateMachineUpdate.Removed ->
                         require(remove.id == moveSmId)
