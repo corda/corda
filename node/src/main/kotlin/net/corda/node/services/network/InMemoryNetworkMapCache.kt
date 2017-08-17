e49d3aa834642bea960d9af87ab659b700608000
Andrzej Cichocki <andrzej.cichocki@r3.com>
2017-08-17 18:05:19 +0100

    Use native VisibleForTesting everywhere. (#1189)

--- node/src/main/kotlin/net/corda/node/services/network/InMemoryNetworkMapCache.kt
+++ node/src/main/kotlin/net/corda/node/services/network/InMemoryNetworkMapCache.kt
@@ -1,6 +1,6 @@
 package net.corda.node.services.network
 
-import com.google.common.annotations.VisibleForTesting
+import net.corda.core.internal.VisibleForTesting
 import net.corda.core.concurrent.CordaFuture
 import net.corda.core.internal.bufferUntilSubscribed
 import net.corda.core.identity.AbstractParty
