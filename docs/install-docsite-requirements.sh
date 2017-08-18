adc33273f6afd36fec12e70c84e199a51b893bdf
Andrzej Cichocki <andrzej.cichocki@r3.com>
2017-08-18 13:31:00 +0100

    Make virtualenv installable if the checkout path has whitespace. (#1187)

--- docs/install-docsite-requirements.sh
+++ docs/install-docsite-requirements.sh
@@ -7,11 +7,15 @@ set -xeo pipefail
 # Install the virtualenv
 if [ ! -d "virtualenv" ]
 then
+    # If the canonical working directory contains whitespace, virtualenv installs broken scripts.
+    # But if we pass in an absolute path that uses symlinks to avoid whitespace, that fixes the problem.
+    # If you run this script manually (not via gradle) from such a path alias, it's available in PWD:
+    absolutevirtualenv="$PWD/virtualenv"
     # Check if python2.7 is installed explicitly otherwise fall back to the default python
     if type "python2.7" > /dev/null; then
-        virtualenv -p python2.7 virtualenv
+        virtualenv -p python2.7 "$absolutevirtualenv"
     else
-        virtualenv virtualenv
+        virtualenv "$absolutevirtualenv"
     fi
 fi
 
