7c397b8415d6b90b613e31b8e1b2bb5051ec6549
Viktor Kolomeyko <viktor.kolomeyko@r3.com>
2017-08-23 15:44:11 +0100

    Use correct classloader from a factory

--- node-api/src/main/kotlin/net/corda/nodeapi/internal/serialization/amqp/custom/ClassSerializer.kt
+++ node-api/src/main/kotlin/net/corda/nodeapi/internal/serialization/amqp/custom/ClassSerializer.kt
@@ -9,7 +9,7 @@ import net.corda.nodeapi.internal.serialization.amqp.SerializerFactory
 class ClassSerializer(factory: SerializerFactory) : CustomSerializer.Proxy<Class<*>, ClassSerializer.ClassProxy>(Class::class.java, ClassProxy::class.java, factory) {
     override fun toProxy(obj: Class<*>): ClassProxy = ClassProxy(obj.name)
 
-    override fun fromProxy(proxy: ClassProxy): Class<*> = Class.forName(proxy.className)
+    override fun fromProxy(proxy: ClassProxy): Class<*> = Class.forName(proxy.className, true, factory.classloader)
 
     data class ClassProxy(val className: String)
 }
