62c64ace23414cb0f0ed10ba34bb30fe5dd46586
Konstantinos Chalkias <konstantinos.chalkias@r3.com>
2017-09-01 10:45:01 +0100

    Adding createSignature(filteredTransaction) to serviceHub (#1380)

--- node/src/main/kotlin/net/corda/node/services/transactions/BFTNonValidatingNotaryService.kt
+++ node/src/main/kotlin/net/corda/node/services/transactions/BFTNonValidatingNotaryService.kt
@@ -85,7 +85,7 @@ class BFTNonValidatingNotaryService(override val services: ServiceHubInternal, c
             when (response) {
                 is BFTSMaRt.ClusterResponse.Error -> throw NotaryException(response.error)
                 is BFTSMaRt.ClusterResponse.Signatures -> {
-                    log.debug("All input states of transaction ${stx.rootHash} have been committed")
+                    log.debug("All input states of transaction ${stx.id} have been committed")
                     return response.txSignatures
                 }
             }
@@ -139,16 +139,13 @@ class BFTNonValidatingNotaryService(override val services: ServiceHubInternal, c
 
         fun verifyAndCommitTx(ftx: FilteredTransaction, callerIdentity: Party): BFTSMaRt.ReplicaResponse {
             return try {
-                val id = ftx.rootHash
+                val id = ftx.id
                 val inputs = ftx.filteredLeaves.inputs
 
                 validateTimeWindow(ftx.filteredLeaves.timeWindow)
                 commitInputStates(inputs, id, callerIdentity)
-
                 log.debug { "Inputs committed successfully, signing $id" }
-                val signableData = SignableData(id, SignatureMetadata(services.myInfo.platformVersion, Crypto.findSignatureScheme(services.notaryIdentityKey).schemeNumberID))
-                val sig = sign(signableData)
-                BFTSMaRt.ReplicaResponse.Signature(sig)
+                BFTSMaRt.ReplicaResponse.Signature(sign(ftx))
             } catch (e: NotaryException) {
                 log.debug { "Error processing transaction: ${e.error}" }
                 BFTSMaRt.ReplicaResponse.Error(e.error)
