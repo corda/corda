6bf6bb6e778f48fc0b65956620786820c2372a1c
Michal Kit <michal.kit@r3.com>
2017-09-04 09:51:45 +0100

    Addressing review comments

--- test-utils/src/main/kotlin/net/corda/testing/driver/Driver.kt
+++ test-utils/src/main/kotlin/net/corda/testing/driver/Driver.kt
@@ -326,7 +326,12 @@ fun addressMustBeBoundFuture(executorService: ScheduledExecutorService, hostAndP
     }
 }
 
-fun addressMustNotBeBound(executorService: ScheduledExecutorService, hostAndPort: NetworkHostAndPort, timeout:Duration = 40.seconds) {
+/*
+ * The default timeout value of 40 seconds have been chosen based on previous node shutdown time estimate.
+ * It's been observed that nodes can take up to 30 seconds to shut down, so just to stay on the safe side the 40 seconds
+ * timeout has been chosen.
+ */
+fun addressMustNotBeBound(executorService: ScheduledExecutorService, hostAndPort: NetworkHostAndPort, timeout: Duration = 40.seconds) {
     addressMustNotBeBoundFuture(executorService, hostAndPort).getOrThrow(timeout)
 }
 
