f05a8dafb0134b02b89bd66f7564b3d13f97c943
Ross Nicoll <ross.nicoll@r3.com>
2017-09-04 18:49:01 +0100

    Add signature exchange to transaction key flow

--- confidential-identities/src/main/kotlin/net/corda/confidential/SwapIdentitiesFlow.kt
+++ confidential-identities/src/main/kotlin/net/corda/confidential/SwapIdentitiesFlow.kt
@@ -4,12 +4,17 @@ import co.paralleluniverse.fibers.Suspendable
 import net.corda.core.flows.FlowLogic
 import net.corda.core.flows.InitiatingFlow
 import net.corda.core.flows.StartableByRPC
+import net.corda.core.crypto.DigitalSignature
+import net.corda.core.crypto.secureRandomBytes
 import net.corda.core.identity.AnonymousParty
 import net.corda.core.identity.Party
 import net.corda.core.identity.PartyAndCertificate
 import net.corda.core.node.services.IdentityService
+import net.corda.core.serialization.CordaSerializable
+import net.corda.core.serialization.serialize
 import net.corda.core.utilities.ProgressTracker
 import net.corda.core.utilities.unwrap
+import java.io.ByteArrayOutputStream
 
 /**
  * Very basic flow which generates new confidential identities for parties in a transaction and exchanges the transaction
@@ -25,10 +30,20 @@ class SwapIdentitiesFlow(private val otherParty: Party,
 
     companion object {
         object AWAITING_KEY : ProgressTracker.Step("Awaiting key")
+        val NONCE_SIZE_BYTES = 16
 
         fun tracker() = ProgressTracker(AWAITING_KEY)
-        fun validateAndRegisterIdentity(identityService: IdentityService, otherSide: Party, anonymousOtherSide: PartyAndCertificate): PartyAndCertificate {
+        fun buildDataToSign(identity: PartyAndCertificate, nonce: ByteArray): ByteArray {
+            val buffer = ByteArrayOutputStream(1024)
+            buffer.write(identity.serialize().bytes)
+            buffer.write(nonce)
+            return buffer.toByteArray()
+        }
+
+        fun validateAndRegisterIdentity(identityService: IdentityService, otherSide: Party, anonymousOtherSide: PartyAndCertificate, nonce: ByteArray, signature: DigitalSignature): PartyAndCertificate {
             require(anonymousOtherSide.name == otherSide.name)
+            val sigWithKey = DigitalSignature.WithKey(anonymousOtherSide.owningKey, signature.bytes)
+            require(sigWithKey.verify(buildDataToSign(anonymousOtherSide, nonce)))
             // Validate then store their identity so that we can prove the key in the transaction is owned by the
             // counterparty.
             identityService.verifyAndRegisterIdentity(anonymousOtherSide)
@@ -47,8 +62,16 @@ class SwapIdentitiesFlow(private val otherParty: Party,
             identities.put(otherParty, legalIdentityAnonymous.party.anonymise())
         } else {
             val otherSession = initiateFlow(otherParty)
-            val anonymousOtherSide = otherSession.sendAndReceive<PartyAndCertificate>(legalIdentityAnonymous).unwrap { confidentialIdentity ->
-                validateAndRegisterIdentity(serviceHub.identityService, otherSession.counterparty, confidentialIdentity)
+            val ourNonce = secureRandomBytes(NONCE_SIZE_BYTES)
+            val theirNonce = otherSession.sendAndReceive<ByteArray>(ourNonce).unwrap { nonce ->
+                require(nonce.size == NONCE_SIZE_BYTES)
+                nonce
+            }
+            val data = buildDataToSign(legalIdentityAnonymous, theirNonce)
+            val ourSig: DigitalSignature = serviceHub.keyManagementService.sign(data, legalIdentityAnonymous.owningKey)
+            val anonymousOtherSide = otherSession.sendAndReceive<IdentityWithSignature>(IdentityWithSignature(legalIdentityAnonymous, ourSig.bytes)).unwrap { (confidentialIdentity, theirSigBytes) ->
+                val theirSig = DigitalSignature.WithKey(confidentialIdentity.owningKey, theirSigBytes)
+                validateAndRegisterIdentity(serviceHub.identityService, otherParty, confidentialIdentity, ourNonce, theirSig)
             }
             identities.put(ourIdentity, legalIdentityAnonymous.party.anonymise())
             identities.put(otherSession.counterparty, anonymousOtherSide.party.anonymise())
@@ -56,4 +79,6 @@ class SwapIdentitiesFlow(private val otherParty: Party,
         return identities
     }
 
+    @CordaSerializable
+    data class IdentityWithSignature(val identity: PartyAndCertificate, val signature: ByteArray)
 }
--- confidential-identities/src/main/kotlin/net/corda/confidential/SwapIdentitiesHandler.kt
+++ confidential-identities/src/main/kotlin/net/corda/confidential/SwapIdentitiesHandler.kt
@@ -1,8 +1,11 @@
 package net.corda.confidential
 
 import co.paralleluniverse.fibers.Suspendable
+import net.corda.core.crypto.DigitalSignature
+import net.corda.core.crypto.secureRandomBytes
 import net.corda.core.flows.FlowLogic
 import net.corda.core.flows.FlowSession
+import net.corda.core.identity.Party
 import net.corda.core.identity.PartyAndCertificate
 import net.corda.core.utilities.ProgressTracker
 import net.corda.core.utilities.unwrap
@@ -19,10 +22,18 @@ class SwapIdentitiesHandler(val otherSideSession: FlowSession, val revocationEna
     @Suspendable
     override fun call() {
         val revocationEnabled = false
+        val ourNonce = secureRandomBytes(SwapIdentitiesFlow.NONCE_SIZE_BYTES)
+        val theirNonce = otherSideSession.sendAndReceive<ByteArray>(ourNonce).unwrap { nonce ->
+            require(nonce.size == SwapIdentitiesFlow.NONCE_SIZE_BYTES)
+            nonce
+        }
         progressTracker.currentStep = SENDING_KEY
         val legalIdentityAnonymous = serviceHub.keyManagementService.freshKeyAndCert(ourIdentityAndCert, revocationEnabled)
-        otherSideSession.sendAndReceive<PartyAndCertificate>(legalIdentityAnonymous).unwrap { confidentialIdentity ->
-            SwapIdentitiesFlow.validateAndRegisterIdentity(serviceHub.identityService, otherSideSession.counterparty, confidentialIdentity)
+        val data = SwapIdentitiesFlow.buildDataToSign(legalIdentityAnonymous, theirNonce)
+        val ourSig: DigitalSignature = serviceHub.keyManagementService.sign(data, legalIdentityAnonymous.owningKey)
+        otherSideSession.sendAndReceive<SwapIdentitiesFlow.IdentityWithSignature>(SwapIdentitiesFlow.IdentityWithSignature(legalIdentityAnonymous, ourSig.bytes)).unwrap { (confidentialIdentity, theirSigBytes) ->
+            val theirSig = DigitalSignature.WithKey(confidentialIdentity.owningKey, theirSigBytes)
+            SwapIdentitiesFlow.validateAndRegisterIdentity(serviceHub.identityService, otherSideSession.counterparty, confidentialIdentity, ourNonce, theirSig)
         }
     }
 }
