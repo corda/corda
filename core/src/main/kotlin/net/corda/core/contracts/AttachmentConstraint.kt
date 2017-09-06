b53dc7bf0ad9e3e209995d7237d59f60ad4efd2b
Andrzej Cichocki <andrzej.cichocki@r3.com>
2017-09-06 11:53:51 +0100

    Make AlwaysAcceptAttachmentConstraint corda-serializable.

--- core/src/main/kotlin/net/corda/core/contracts/AttachmentConstraint.kt
+++ core/src/main/kotlin/net/corda/core/contracts/AttachmentConstraint.kt
@@ -1,5 +1,7 @@
 package net.corda.core.contracts
 
+import net.corda.core.serialization.CordaSerializable
+
 /** Constrain which contract-code-containing attachments can be used with a [ContractState]. */
 interface AttachmentConstraint {
     /** Returns whether the given contract attachments can be used with the [ContractState] associated with this constraint object. */
@@ -7,6 +9,7 @@ interface AttachmentConstraint {
 }
 
 /** An [AttachmentConstraint] where [isSatisfiedBy] always returns true. */
+@CordaSerializable
 object AlwaysAcceptAttachmentConstraint : AttachmentConstraint {
     override fun isSatisfiedBy(attachments: List<Attachment>) = true
 }
