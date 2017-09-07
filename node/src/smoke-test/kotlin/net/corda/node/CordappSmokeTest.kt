9a77e63cf881443374c50801542d5b0e60bfcbaa
Shams Asari <shams.asari@r3.com>
2017-09-07 19:13:57 +0100

    Added missing app name on session-init messages

--- node/src/smoke-test/kotlin/net/corda/node/CordappSmokeTest.kt
+++ node/src/smoke-test/kotlin/net/corda/node/CordappSmokeTest.kt
@@ -10,6 +10,7 @@ import net.corda.core.internal.list
 import net.corda.core.messaging.startFlow
 import net.corda.core.utilities.getOrThrow
 import net.corda.core.utilities.getX500Name
+import net.corda.core.utilities.unwrap
 import net.corda.nodeapi.User
 import net.corda.smoketesting.NodeConfig
 import net.corda.smoketesting.NodeProcess
@@ -40,16 +41,19 @@ class CordappSmokeTest {
     fun `FlowContent appName returns the filename of the CorDapp jar`() {
         val pluginsDir = (factory.baseDirectory(aliceConfig) / "plugins").createDirectories()
         // Find the jar file for the smoke tests of this module
-        val selfCorDapp = Paths.get("build", "libs").list {
+        val selfCordapp = Paths.get("build", "libs").list {
             it.filter { "-smoke-test" in it.toString() }.toList().single()
         }
-        selfCorDapp.copyToDirectory(pluginsDir)
+        selfCordapp.copyToDirectory(pluginsDir)
 
         factory.create(aliceConfig).use { alice ->
             alice.connect().use { connectionToAlice ->
                 val aliceIdentity = connectionToAlice.proxy.nodeIdentity().legalIdentity
-                val future = connectionToAlice.proxy.startFlow(::DummyInitiatingFlow, aliceIdentity).returnValue
-                assertThat(future.getOrThrow().appName).isEqualTo(selfCorDapp.fileName.toString().removeSuffix(".jar"))
+                val future = connectionToAlice.proxy.startFlow(::GatherContextsFlow, aliceIdentity).returnValue
+                val (sessionInitContext, sessionConfirmContext) = future.getOrThrow()
+                val selfCordappName = selfCordapp.fileName.toString().removeSuffix(".jar")
+                assertThat(sessionInitContext.appName).isEqualTo(selfCordappName)
+                assertThat(sessionConfirmContext.appName).isEqualTo(selfCordappName)
             }
         }
     }
@@ -62,15 +66,26 @@ class CordappSmokeTest {
 
     @InitiatingFlow
     @StartableByRPC
-    class DummyInitiatingFlow(val otherParty: Party) : FlowLogic<FlowContext>() {
+    class GatherContextsFlow(private val otherParty: Party) : FlowLogic<Pair<FlowContext, FlowContext>>() {
         @Suspendable
-        override fun call() = getFlowContext(otherParty)
+        override fun call(): Pair<FlowContext, FlowContext> {
+            // This receive will kick off SendBackInitiatorFlowContext by sending a session-init with our app name.
+            // SendBackInitiatorFlowContext will send back our context using the information from this session-init
+            val sessionInitContext = receive<FlowContext>(otherParty).unwrap { it }
+            // This context is taken from the session-confirm message
+            val sessionConfirmContext = getFlowContext(otherParty)
+            return Pair(sessionInitContext, sessionConfirmContext)
+        }
     }
 
     @Suppress("unused")
-    @InitiatedBy(DummyInitiatingFlow::class)
-    class DummyInitiatedFlow(val otherParty: Party) : FlowLogic<Unit>() {
+    @InitiatedBy(GatherContextsFlow::class)
+    class SendBackInitiatorFlowContext(private val otherParty: Party) : FlowLogic<Unit>() {
         @Suspendable
-        override fun call() = Unit
+        override fun call() {
+            // An initiated flow calling getFlowContext on its initiator will get the context from the session-init
+            val sessionInitContext = getFlowContext(otherParty)
+            send(otherParty, sessionInitContext)
+        }
     }
 }
