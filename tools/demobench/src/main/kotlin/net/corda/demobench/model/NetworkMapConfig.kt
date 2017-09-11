1fc2a8d6dd47fea621691ec5cf698405b3452820
Ross Nicoll <ross.nicoll@r3.com>
2017-09-11 15:39:02 +0100

    Remove use of WHITESPACE regex

--- tools/demobench/src/main/kotlin/net/corda/demobench/model/NetworkMapConfig.kt
+++ tools/demobench/src/main/kotlin/net/corda/demobench/model/NetworkMapConfig.kt
@@ -1,11 +1,10 @@
 package net.corda.demobench.model
 
 import net.corda.core.identity.CordaX500Name
-import net.corda.core.identity.WHITESPACE
 
 open class NetworkMapConfig(val legalName: CordaX500Name, val p2pPort: Int) {
     val key: String = legalName.organisation.toKey()
 }
 
-fun String.stripWhitespace() = replace(WHITESPACE, "")
+fun String.stripWhitespace() = String(this.filter { !it.isWhitespace() }.toCharArray())
 fun String.toKey() = stripWhitespace().toLowerCase()
