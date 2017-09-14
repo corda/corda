8899389750028525babab4e085d523d66dc7a66e
Andras Slemmer <andras.slemmer@r3cev.com>
2017-09-14 16:59:24 +0100

    otherParty -> counterparty

--- core/src/main/kotlin/net/corda/core/flows/FlowSession.kt
+++ core/src/main/kotlin/net/corda/core/flows/FlowSession.kt
@@ -35,17 +35,17 @@ abstract class FlowSession {
     abstract val counterparty: Party
 
     /**
-     * Returns a [FlowInfo] object describing the flow [otherParty] is using. With [FlowInfo.flowVersion] it
+     * Returns a [FlowInfo] object describing the flow [counterparty] is using. With [FlowInfo.flowVersion] it
      * provides the necessary information needed for the evolution of flows and enabling backwards compatibility.
      *
-     * This method can be called before any send or receive has been done with [otherParty]. In such a case this will force
+     * This method can be called before any send or receive has been done with [counterparty]. In such a case this will force
      * them to start their flow.
      */
     @Suspendable
     abstract fun getCounterpartyFlowInfo(): FlowInfo
 
     /**
-     * Serializes and queues the given [payload] object for sending to the [otherParty]. Suspends until a response
+     * Serializes and queues the given [payload] object for sending to the [counterparty]. Suspends until a response
      * is received, which must be of the given [R] type.
      *
      * Remember that when receiving data from other parties the data should not be trusted until it's been thoroughly
@@ -62,7 +62,7 @@ abstract class FlowSession {
         return sendAndReceive(R::class.java, payload)
     }
     /**
-     * Serializes and queues the given [payload] object for sending to the [otherParty]. Suspends until a response
+     * Serializes and queues the given [payload] object for sending to the [counterparty]. Suspends until a response
      * is received, which must be of the given [receiveType]. Remember that when receiving data from other parties the data
      * should not be trusted until it's been thoroughly verified for consistency and that all expectations are
      * satisfied, as a malicious peer may send you subtly corrupted data in order to exploit your code.
@@ -76,7 +76,7 @@ abstract class FlowSession {
     abstract fun <R : Any> sendAndReceive(receiveType: Class<R>, payload: Any): UntrustworthyData<R>
 
     /**
-     * Suspends until the specified [otherParty] sends us a message of type [R].
+     * Suspends until [counterparty] sends us a message of type [R].
      *
      * Remember that when receiving data from other parties the data should not be trusted until it's been thoroughly
      * verified for consistency and that all expectations are satisfied, as a malicious peer may send you subtly
@@ -87,7 +87,7 @@ abstract class FlowSession {
         return receive(R::class.java)
     }
     /**
-     * Suspends until the specified [otherParty] sends us a message of type [receiveType].
+     * Suspends until [counterparty] sends us a message of type [receiveType].
      *
      * Remember that when receiving data from other parties the data should not be trusted until it's been thoroughly
      * verified for consistency and that all expectations are satisfied, as a malicious peer may send you subtly
@@ -99,9 +99,9 @@ abstract class FlowSession {
     abstract fun <R : Any> receive(receiveType: Class<R>): UntrustworthyData<R>
 
     /**
-     * Queues the given [payload] for sending to the [otherParty] and continues without suspending.
+     * Queues the given [payload] for sending to the [counterparty] and continues without suspending.
      *
-     * Note that the other party may receive the message at some arbitrary later point or not at all: if [otherParty]
+     * Note that the other party may receive the message at some arbitrary later point or not at all: if [counterparty]
      * is offline then message delivery will be retried until it comes back or until the message is older than the
      * network's event horizon time.
      */
