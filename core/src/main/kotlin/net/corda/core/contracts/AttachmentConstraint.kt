abac314b3198f076951c9171b9232182617db5bd
Viktor Kolomeyko <viktor.kolomeyko@r3.com>
2017-09-14 09:28:17 +0100

    CORDA-540: Correctly mark AttachmentConstraint to be CordaSerializable (#1496)
    Or else many core/contract unit tests failing in AMQP mode

--- core/src/main/kotlin/net/corda/core/contracts/AttachmentConstraint.kt
+++ core/src/main/kotlin/net/corda/core/contracts/AttachmentConstraint.kt
@@ -3,13 +3,13 @@ package net.corda.core.contracts
 import net.corda.core.serialization.CordaSerializable
 
 /** Constrain which contract-code-containing attachments can be used with a [ContractState]. */
+@CordaSerializable
 interface AttachmentConstraint {
     /** Returns whether the given contract attachments can be used with the [ContractState] associated with this constraint object. */
     fun isSatisfiedBy(attachments: List<Attachment>): Boolean
 }
 
 /** An [AttachmentConstraint] where [isSatisfiedBy] always returns true. */
-@CordaSerializable
 object AlwaysAcceptAttachmentConstraint : AttachmentConstraint {
     override fun isSatisfiedBy(attachments: List<Attachment>) = true
 }
