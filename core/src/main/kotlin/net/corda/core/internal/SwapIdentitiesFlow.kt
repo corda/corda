1f1273441268784735359908e98b76aa67353b49
Shams Asari <shams.asari@r3.com>
2017-09-15 11:28:34 +0100

    Made SwapIdentitiesFlow internal until we make it into an inlined flow

--- core/src/main/kotlin/net/corda/core/internal/SwapIdentitiesFlow.kt
+++ core/src/main/kotlin/net/corda/core/internal/SwapIdentitiesFlow.kt
@@ -1,9 +1,12 @@
-package net.corda.core.flows
+package net.corda.core.internal
 
 import co.paralleluniverse.fibers.Suspendable
+import net.corda.core.flows.FlowLogic
+import net.corda.core.flows.InitiatingFlow
+import net.corda.core.flows.StartableByRPC
 import net.corda.core.identity.AnonymousParty
-import net.corda.core.identity.PartyAndCertificate
 import net.corda.core.identity.Party
+import net.corda.core.identity.PartyAndCertificate
 import net.corda.core.node.services.IdentityService
 import net.corda.core.utilities.ProgressTracker
 import net.corda.core.utilities.unwrap
