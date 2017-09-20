7c71cc6c77da359feadafb4074a71f17239e5ce8
Viktor Kolomeyko <viktor.kolomeyko@r3.com>
2017-09-20 09:26:06 +0100

    Minor changes after code review

--- testing/node-driver/src/integration-test/kotlin/net/corda/testing/driver/ProcessUtilitiesTests.kt
+++ testing/node-driver/src/integration-test/kotlin/net/corda/testing/driver/ProcessUtilitiesTests.kt
@@ -1,13 +1,19 @@
 package net.corda.testing.driver
 
 import org.apache.commons.io.FileUtils
+import org.junit.Rule
 import org.junit.Test
+import org.junit.rules.TemporaryFolder
 import java.io.File
 import java.util.concurrent.TimeUnit
 import kotlin.test.assertEquals
 import kotlin.test.assertTrue
 
 class ProcessUtilitiesTests {
+
+    @Rule
+    @JvmField
+    val tempFolder = TemporaryFolder()
 
     companion object {
 
@@ -22,8 +28,7 @@ class ProcessUtilitiesTests {
 
     @Test
     fun `test dummy process can be started`() {
-        val tmpFile = File.createTempFile(ProcessUtilitiesTests::class.java.simpleName, null)
-        tmpFile.deleteOnExit()
+        val tmpFile = tempFolder.newFile("${ProcessUtilitiesTests::class.java.simpleName}.txt")
         val startedProcess = ProcessUtilities.startJavaProcess<ProcessUtilitiesTests>(listOf(tmpFile.absolutePath))
         assertTrue { startedProcess.waitFor(20, TimeUnit.SECONDS) }
         assertEquals(tmpString, FileUtils.readFileToString(tmpFile))
