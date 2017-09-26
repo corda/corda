f9270348689ecb7742f9753f11eab72e236799d9
Andras Slemmer <andras.slemmer@r3cev.com>
2017-09-26 16:40:29 +0100

    FlowSession docs

--- docs/source/example-code/src/main/kotlin/net/corda/docs/FlowCookbook.kt
+++ docs/source/example-code/src/main/kotlin/net/corda/docs/FlowCookbook.kt
@@ -122,6 +122,10 @@ object FlowCookbook {
                     throw IllegalArgumentException("Couldn't find counterparty with key: $dummyPubKey in identity service")
             // DOCEND 2
 
+            // DOCSTART initiateFlow
+            val counterpartySession = initiateFlow(counterparty)
+            // DOCEND initiateFlow
+
             /**-----------------------------
              * SENDING AND RECEIVING DATA *
             -----------------------------**/
@@ -138,7 +142,6 @@ object FlowCookbook {
             // registered to respond to this flow, and has a corresponding
             // ``receive`` call.
             // DOCSTART 4
-            val counterpartySession = initiateFlow(counterparty)
             counterpartySession.send(Any())
             // DOCEND 4
 
@@ -497,7 +500,7 @@ object FlowCookbook {
             // other required signers using ``CollectSignaturesFlow``.
             // The responder flow will need to call ``SignTransactionFlow``.
             // DOCSTART 15
-            val fullySignedTx: SignedTransaction = subFlow(CollectSignaturesFlow(twiceSignedTx, emptySet(), SIGS_GATHERING.childProgressTracker()))
+            val fullySignedTx: SignedTransaction = subFlow(CollectSignaturesFlow(twiceSignedTx, setOf(counterpartySession, regulatorSession), SIGS_GATHERING.childProgressTracker()))
             // DOCEND 15
 
             /**-----------------------
@@ -541,6 +544,13 @@ object FlowCookbook {
             val additionalParties: Set<Party> = setOf(regulator)
             val notarisedTx2: SignedTransaction = subFlow(FinalityFlow(fullySignedTx, additionalParties, FINALISATION.childProgressTracker()))
             // DOCEND 10
+
+            // DOCSTART FlowSession porting
+            send(regulator, Any()) // Old API
+            // becomes
+            val session = initiateFlow(regulator)
+            session.send(Any())
+            // DOCEND FlowSession porting
         }
     }
 
