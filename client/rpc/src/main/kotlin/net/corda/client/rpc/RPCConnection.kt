ff1652704ec7799770d88f03255e4858ce1c4d3b
Mike Hearn <mike@r3.com>
2017-09-27 12:39:41 +0200

    Merge "A variety of small fixes" into the 1.0 release branch (#1673)
    * Minor: improve javadocs in NodeInfo
    
    * Minor: use package descriptions in Kotlin build of api docs too, not just javadocs.
    
    * RPC: make RPCConnection non-internal, as it's a core API. Move docs around so they're on public API not internal API.
    
    * Add an IntelliJ scope that covers the currently supported Corda API.
    
    This is useful when used in combination with the "Highlight public
    declarations with missing KDoc" inspection.
    
    * Ironic: upgrade the version of the Gradle plugin that checks for upgraded versions of things.
    
    It had broken due being incompatible with the new versions of Gradle
    itself.
    
    * Docs: flesh out javadocs on ServiceHub
    
    * Docs: add @suppress to a few things that were polluting the Dokka docs.
    
    * Docs: mention RPC access in NodeInfo javadoc

---
+++ client/rpc/src/main/kotlin/net/corda/client/rpc/RPCConnection.kt
@@ -0,0 +1,38 @@
+package net.corda.client.rpc
+
+import net.corda.core.messaging.RPCOps
+import java.io.Closeable
+
+/**
+ * Holds a [proxy] object implementing [I] that forwards requests to the RPC server. The server version can be queried
+ * via this interface.
+ *
+ * [Closeable.close] may be used to shut down the connection and release associated resources. It is an
+ * alias for [notifyServerAndClose].
+ */
+interface RPCConnection<out I : RPCOps> : Closeable {
+    /**
+     * Holds a synthetic class that automatically forwards method calls to the server, and returns the response.
+     */
+    val proxy: I
+
+    /** The RPC protocol version reported by the server. */
+    val serverProtocolVersion: Int
+
+    /**
+     * Closes this client gracefully by sending a notification to the server, so it can immediately clean up resources.
+     * If the server is not available this method may block for a short period until it's clear the server is not
+     * coming back.
+     */
+    fun notifyServerAndClose()
+
+    /**
+     * Closes this client without notifying the server.
+     *
+     * The server will eventually clear out the RPC message queue and disconnect subscribed observers,
+     * but this may take longer than desired, so to conserve resources you should normally use [notifyServerAndClose].
+     * This method is helpful when the node may be shutting down or have already shut down and you don't want to
+     * block waiting for it to come back, which typically happens in integration tests and demos rather than production.
+     */
+    fun forceClose()
+}
