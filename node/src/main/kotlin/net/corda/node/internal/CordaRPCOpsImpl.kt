f5784f718d3c2fb60a67d1218d8a8e1f5927bff0
Ross Nicoll <ross.nicoll@r3.com>
2017-10-03 10:54:35 +0100

    Remove database transaction from notaryPartyFromX500Name (#1786)
    Remove database transaction from notaryPartyFromX500Name as it doesn't touch the database

--- node/src/main/kotlin/net/corda/node/internal/CordaRPCOpsImpl.kt
+++ node/src/main/kotlin/net/corda/node/internal/CordaRPCOpsImpl.kt
@@ -176,11 +176,7 @@ class CordaRPCOpsImpl(
 
     override fun currentNodeTime(): Instant = Instant.now(services.clock)
 
-    override fun waitUntilNetworkReady(): CordaFuture<Void?> {
-        return database.transaction {
-            services.networkMapCache.nodeReady
-        }
-    }
+    override fun waitUntilNetworkReady(): CordaFuture<Void?> = services.networkMapCache.nodeReady
 
     override fun wellKnownPartyFromAnonymous(party: AbstractParty): Party? {
         return database.transaction {
