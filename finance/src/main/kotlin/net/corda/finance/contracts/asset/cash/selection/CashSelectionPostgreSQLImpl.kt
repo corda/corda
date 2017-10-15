6db35523201fdba6f31ad7fb614d0a2450b159f4
Tomas Tauber <tomastauber@cryptoblk.io>
2017-10-15 20:11:29 +0800

    Changed the PostgreSQL Cash Selection implementation to use the new refactored AbstractCashSelection

--- finance/src/main/kotlin/net/corda/finance/contracts/asset/cash/selection/CashSelectionPostgreSQLImpl.kt
+++ finance/src/main/kotlin/net/corda/finance/contracts/asset/cash/selection/CashSelectionPostgreSQLImpl.kt
@@ -1,28 +1,18 @@
 package net.corda.finance.contracts.asset.cash.selection
 
-import co.paralleluniverse.fibers.Suspendable
-import co.paralleluniverse.strands.Strand
 import net.corda.core.contracts.Amount
-import net.corda.core.contracts.StateAndRef
-import net.corda.core.contracts.StateRef
-import net.corda.core.contracts.TransactionState
-import net.corda.core.crypto.SecureHash
 import net.corda.core.identity.AbstractParty
 import net.corda.core.identity.Party
-import net.corda.core.node.ServiceHub
-import net.corda.core.node.services.StatesNotAvailableException
-import net.corda.core.serialization.SerializationDefaults
-import net.corda.core.serialization.deserialize
-import net.corda.core.utilities.*
-import net.corda.finance.contracts.asset.Cash
-import net.corda.finance.contracts.asset.CashSelection
+import net.corda.core.utilities.OpaqueBytes
+import net.corda.core.utilities.debug
+import net.corda.core.utilities.loggerFor
+import net.corda.core.utilities.toBase58String
+import java.sql.Connection
 import java.sql.DatabaseMetaData
-import java.sql.SQLException
+import java.sql.ResultSet
 import java.util.*
-import java.util.concurrent.locks.ReentrantLock
-import kotlin.concurrent.withLock
 
-class CashSelectionPostgreSQLImpl : CashSelection {
+class CashSelectionPostgreSQLImpl : AbstractCashSelection() {
 
     companion object {
         val JDBC_DRIVER_NAME = "PostgreSQL JDBC Driver"
@@ -33,47 +23,16 @@ class CashSelectionPostgreSQLImpl : CashSelection {
         return metadata.driverName == JDBC_DRIVER_NAME
     }
 
-    // coin selection retry loop counter, sleep (msecs) and lock for selecting states
-    private val MAX_RETRIES = 5
-    private val RETRY_SLEEP = 100
-    private val spendLock: ReentrantLock = ReentrantLock()
+    override fun toString() = "${this::class.java} for $JDBC_DRIVER_NAME"
 
-    /**
-     * An optimised query to gather Cash states that are available and retry if they are temporarily unavailable.
-     * @param services The service hub to allow access to the database session
-     * @param amount The amount of currency desired (ignoring issues, but specifying the currency)
-     * @param onlyFromIssuerParties If empty the operation ignores the specifics of the issuer,
-     * otherwise the set of eligible states wil be filtered to only include those from these issuers.
-     * @param notary If null the notary source is ignored, if specified then only states marked
-     * with this notary are included.
-     * @param lockId The FlowLogic.runId.uuid of the flow, which is used to soft reserve the states.
-     * Also, previous outputs of the flow will be eligible as they are implicitly locked with this id until the flow completes.
-     * @param withIssuerRefs If not empty the specific set of issuer references to match against.
-     * @return The matching states that were found. If sufficient funds were found these will be locked,
-     * otherwise what is available is returned unlocked for informational purposes.
-     */
-    @Suspendable
-    override fun unconsumedCashStatesForSpending(services: ServiceHub,
-        amount: Amount<Currency>,
-        onlyFromIssuerParties: Set<AbstractParty>,
-        notary: Party?,
-        lockId: UUID,
-        withIssuerRefs: Set<OpaqueBytes>): List<StateAndRef<Cash.State>> {
-        // TODO: extract out commonalities between CashSelection implementations
-
-        val stateAndRefs = mutableListOf<StateAndRef<Cash.State>>()
-
-        //       This is using PostgreSQL window functions for selecting a minimum set of rows that match a request amount of coins:
-        //       1) This may also be possible with user-defined functions (e.g. using PL/pgSQL)
-        //       2) The window function accumulated column (`total`) does not include the current row (starts from 0) and cannot
-        //          appear in the WHERE clause, hence restricting row selection and adjusting the returned total in the outer query.
-        //       3) Currently (version 9.6), FOR UPDATE cannot be specified with window functions
-
-        for (retryCount in 1..MAX_RETRIES) {
-
-            spendLock.withLock {
-                val session = services.jdbcSession()
-                val selectJoin = """SELECT nested.transaction_id, nested.output_index, nested.contract_state, nested.pennies,
+    //       This is using PostgreSQL window functions for selecting a minimum set of rows that match a request amount of coins:
+    //       1) This may also be possible with user-defined functions (e.g. using PL/pgSQL)
+    //       2) The window function accumulated column (`total`) does not include the current row (starts from 0) and cannot
+    //          appear in the WHERE clause, hence restricting row selection and adjusting the returned total in the outer query.
+    //       3) Currently (version 9.6), FOR UPDATE cannot be specified with window functions
+    override fun executeQuery(connection: Connection, amount: Amount<Currency>, lockId: UUID, notary: Party?,
+                              onlyFromIssuerParties: Set<AbstractParty>, withIssuerRefs: Set<OpaqueBytes>) : ResultSet {
+        val selectJoin = """SELECT nested.transaction_id, nested.output_index, nested.contract_state, nested.pennies,
                         nested.total+nested.pennies as total_pennies, nested.lock_id
                        FROM
                        (SELECT vs.transaction_id, vs.output_index, vs.contract_state, ccs.pennies,
@@ -85,92 +44,40 @@ class CashSelectionPostgreSQLImpl : CashSelection {
                         AND ccs.ccy_code = ?
                         AND (vs.lock_id = ? OR vs.lock_id is null)
                         """ +
-                    (if (notary != null)
-                        " AND vs.notary_name = ?" else "") +
-                    (if (onlyFromIssuerParties.isNotEmpty())
-                        " AND ccs.issuer_key = ANY (?)" else "") +
-                    (if (withIssuerRefs.isNotEmpty())
-                        " AND ccs.issuer_ref = ANY (?)" else "") +
-                    """)
+                (if (notary != null)
+                    " AND vs.notary_name = ?" else "") +
+                (if (onlyFromIssuerParties.isNotEmpty())
+                    " AND ccs.issuer_key = ANY (?)" else "") +
+                (if (withIssuerRefs.isNotEmpty())
+                    " AND ccs.issuer_ref = ANY (?)" else "") +
+                """)
                         nested WHERE nested.total < ?
                      """
-                val statement = session.prepareStatement(selectJoin)
-                try {
-                    statement.setString(1, amount.token.toString())
-                    statement.setString(2, lockId.toString())
-                    var paramOffset = 0
-                    if (notary != null) {
-                        statement.setString(3, notary.name.toString())
-                        paramOffset += 1
-                    }
-                    if (onlyFromIssuerParties.isNotEmpty()) {
-                        val issuerKeys = session.createArrayOf("VARCHAR", onlyFromIssuerParties.map
-                        { it.owningKey.toBase58String() }.toTypedArray())
-                        statement.setArray(3 + paramOffset, issuerKeys)
-                        paramOffset += 1
-                    }
-                    if (withIssuerRefs.isNotEmpty()) {
-                        val issuerRefs = session.createArrayOf("BYTEA", withIssuerRefs.map
-                        { it.bytes }.toTypedArray())
-                        statement.setArray(3 + paramOffset, issuerRefs)
-                        paramOffset += 1
-                    }
-                    statement.setLong(3 + paramOffset, amount.quantity)
 
-                    // Retrieve spendable state refs
-                    val rs = statement.executeQuery()
-                    stateAndRefs.clear()
-                    log.debug(statement.toString())
-                    var totalPennies = 0L
-                    while (rs.next()) {
-                        val txHash = SecureHash.parse(rs.getString(1))
-                        val index = rs.getInt(2)
-                        val stateRef = StateRef(txHash, index)
-                        val stateBlob = rs.getBlob(3)
-                        // TODO: deserialize blob directly
-                        val blobLength = stateBlob.length()
-                        val blobAsBytes = stateBlob.getBytes(1, blobLength.toInt())
-                        stateBlob.free()
-                        val state = blobAsBytes.deserialize<TransactionState<Cash.State>>(context = SerializationDefaults.STORAGE_CONTEXT)
-                        val pennies = rs.getLong(4)
-                        totalPennies = rs.getLong(5)
-                        val rowLockId = rs.getString(6)
-                        stateAndRefs.add(StateAndRef(state, stateRef))
-                        log.trace { "ROW: $rowLockId ($lockId): $stateRef : $pennies ($totalPennies)" }
-                    }
-
-                    if (stateAndRefs.isNotEmpty() && totalPennies >= amount.quantity) {
-                        // we should have a minimum number of states to satisfy our selection `amount` criteria
-                        log.trace("Coin selection for $amount retrieved ${stateAndRefs.count()} states totalling $totalPennies pennies: $stateAndRefs")
-
-                        // With the current single threaded state machine available states are guaranteed to lock.
-                        // TODO However, we will have to revisit these methods in the future multi-threaded.
-                        services.vaultService.softLockReserve(lockId, (stateAndRefs.map { it.ref }).toNonEmptySet())
-                        return stateAndRefs
-                    }
-                    log.trace("Coin selection requested $amount but retrieved $totalPennies pennies with state refs: ${stateAndRefs.map { it.ref }}")
-                    // retry as more states may become available
-                } catch (e: SQLException) {
-                    log.error("""Failed retrieving unconsumed states for: amount [$amount], onlyFromIssuerParties [$onlyFromIssuerParties], notary [$notary], lockId [$lockId]
-                        $e.
-                    """)
-                } catch (e: StatesNotAvailableException) { // Should never happen with single threaded state machine
-                    stateAndRefs.clear()
-                    log.warn(e.message)
-                    // retry only if there are locked states that may become available again (or consumed with change)
-                } finally {
-                    statement.close()
-                }
-            }
-
-            log.warn("Coin selection failed on attempt $retryCount")
-            // TODO: revisit the back off strategy for contended spending.
-            if (retryCount != MAX_RETRIES) {
-                Strand.sleep(RETRY_SLEEP * retryCount.toLong())
-            }
+        val statement = connection.prepareStatement(selectJoin)
+        statement.setString(1, amount.token.toString())
+        statement.setString(2, lockId.toString())
+        var paramOffset = 0
+        if (notary != null) {
+            statement.setString(3, notary.name.toString())
+            paramOffset += 1
         }
+        if (onlyFromIssuerParties.isNotEmpty()) {
+            val issuerKeys = connection.createArrayOf("VARCHAR", onlyFromIssuerParties.map
+            { it.owningKey.toBase58String() }.toTypedArray())
+            statement.setArray(3 + paramOffset, issuerKeys)
+            paramOffset += 1
+        }
+        if (withIssuerRefs.isNotEmpty()) {
+            val issuerRefs = connection.createArrayOf("BYTEA", withIssuerRefs.map
+            { it.bytes }.toTypedArray())
+            statement.setArray(3 + paramOffset, issuerRefs)
+            paramOffset += 1
+        }
+        statement.setLong(3 + paramOffset, amount.quantity)
+        log.debug { statement.toString() }
 
-        log.warn("Insufficient spendable states identified for $amount")
-        return stateAndRefs
+        return statement.executeQuery()
     }
+
 }
