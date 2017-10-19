1e05f27bae76c4af9c7639f73217dbc983a94558
Katelyn Baker <katelyn.baker@r3.com>
2017-10-19 12:01:39 +0100

    CORDA-725 - Change AMQP identifier to officially assigned value
    This does change our header format so pre-cached test files need
    regenerating

--- node-api/src/main/kotlin/net/corda/nodeapi/internal/serialization/amqp/Schema.kt
+++ node-api/src/main/kotlin/net/corda/nodeapi/internal/serialization/amqp/Schema.kt
@@ -18,8 +18,15 @@ import java.util.*
 import net.corda.nodeapi.internal.serialization.carpenter.Field as CarpenterField
 import net.corda.nodeapi.internal.serialization.carpenter.Schema as CarpenterSchema
 
-// TODO: get an assigned number as per AMQP spec
-const val DESCRIPTOR_TOP_32BITS: Long = 0xc0da0000
+/**
+ * R3 AMQP assigned enterprise number
+ *
+ * see [https://www.iana.org/assignments/enterprise-numbers/enterprise-numbers]
+ *
+ * Repeated here for brevity:
+ *   50530 - R3 - Mike Hearn - mike&r3.com
+ */
+const val DESCRIPTOR_TOP_32BITS: Long = 0xc5620000
 
 const val DESCRIPTOR_DOMAIN: String = "net.corda"
 
