2caa13488503d9778f32d1dea3abd65a28610f78
Viktor Kolomeyko <viktor.kolomeyko@r3.com>
2017-10-20 15:51:20 +0100

    Fix-up: Bank Of Corda sample
    Use correct CorDapp packages to scan

--- samples/bank-of-corda-demo/src/main/kotlin/net/corda/bank/BankOfCordaDriver.kt
+++ samples/bank-of-corda-demo/src/main/kotlin/net/corda/bank/BankOfCordaDriver.kt
@@ -78,7 +78,7 @@ private class BankOfCordaDriver {
                         startNode(providedName = BIGCORP_LEGAL_NAME, rpcUsers = listOf(bigCorpUser))
                         startWebserver(bankOfCorda.get())
                         waitForAllNodesToFinish()
-                    }, isDebug = true)
+                    }, isDebug = true, extraCordappPackagesToScan = listOf("net.corda.finance.contracts.asset"))
                 }
                 Role.ISSUE_CASH_RPC -> {
                     println("Requesting Cash via RPC ...")
