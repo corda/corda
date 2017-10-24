71fe0daa08d97e1e4cf2bead7b943fd9ac0215e7
Christian Sailer <christian.sailer@r3.com>
2017-10-24 14:13:04 +0100

    Give the checkpoint checker thread a meaningful name

---
+++ node/src/main/kotlin/net/corda/node/utilities/NamedThreadFactory.kt
@@ -0,0 +1,19 @@
+package net.corda.node.utilities
+
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+import java.util.concurrent.ThreadFactory
+import java.util.concurrent.atomic.AtomicInteger
+
+class NamedThreadFactory(private val name:String, private val underlyingFactory: ThreadFactory) : ThreadFactory{
+    val threadNumber = AtomicInteger(1)
+    override fun newThread(r: Runnable?): Thread {
+        val t = underlyingFactory.newThread(r)
+        t.name = name + "-" + threadNumber.getAndIncrement()
+        return t
+    }
+}
+
+fun newNamedSinleThreadExecutor(name: String): ExecutorService {
+    return Executors.newSingleThreadExecutor(NamedThreadFactory(name, Executors.defaultThreadFactory()))
+}
