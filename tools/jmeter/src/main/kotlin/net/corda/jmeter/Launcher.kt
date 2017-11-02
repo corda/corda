029cc1c2f0399d4e4742540254da816b55381fe2
rick.parker <rick.parker@r3cev.com>
2017-11-02 14:58:47 +0000

    Some clean up.  Remote slave via capsule is working.

--- tools/jmeter/src/main/kotlin/net/corda/jmeter/Launcher.kt
+++ tools/jmeter/src/main/kotlin/net/corda/jmeter/Launcher.kt
@@ -1,13 +1,23 @@
 package net.corda.jmeter
 
 import org.apache.jmeter.JMeter
+import org.slf4j.LoggerFactory
 
 class Launcher {
     companion object {
         @JvmStatic
         fun main(args: Array<String>) {
+            val logger = LoggerFactory.getLogger(this::class.java)
             val jmeter = JMeter()
-            jmeter.start(args)
+            val capsuleDir = System.getProperty("capsule.dir")
+            if (capsuleDir != null) {
+                // We are running under Capsule, so assume we want a JMeter distributed server to be controlled from
+                // elsewhere.
+                logger.info("Starting JMeter in server mode from $capsuleDir")
+                jmeter.start(arrayOf("-s", "-d", capsuleDir, "-p", "$capsuleDir/jmeter.properties") + args)
+            } else {
+                jmeter.start(args)
+            }
         }
     }
 }
