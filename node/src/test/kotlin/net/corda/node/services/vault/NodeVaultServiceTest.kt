f42660b244e0a09cbabb36f838c23056596d25a1
Ross Nicoll <ross.nicoll@r3.com>
2017-11-10 13:11:19 +0000

    Clean up identity tests
    * Move test identities into MockServices
    * Step through cert path when registering

--- node/src/test/kotlin/net/corda/node/services/vault/NodeVaultServiceTest.kt
+++ node/src/test/kotlin/net/corda/node/services/vault/NodeVaultServiceTest.kt
@@ -70,9 +70,15 @@ class NodeVaultServiceTest {
         val databaseAndServices = MockServices.makeTestDatabaseAndMockServices(cordappPackages = cordappPackages)
         database = databaseAndServices.first
         services = databaseAndServices.second
+<<<<<<< d78308f5106e5185be59960a90dadb1d48096c2e
         issuerServices = MockServices(cordappPackages, DUMMY_CASH_ISSUER_KEY)
         bocServices = MockServices(cordappPackages, BOC_KEY)
         services.identityService.verifyAndRegisterIdentity(DUMMY_CASH_ISSUER_IDENTITY)
+=======
+        issuerServices = MockServices(cordappPackages, DUMMY_CASH_ISSUER_KEY, BOC_KEY)
+        services.identityService.verifyAndRegisterIdentity(DUMMY_CASH_ISSUER_IDENTITY)
+        services.identityService.verifyAndRegisterIdentity(BOC_IDENTITY)
+>>>>>>> Clean up identity tests
     }
 
     @After
@@ -506,30 +512,33 @@ class NodeVaultServiceTest {
 
     @Test
     fun `correct updates are generated for general transactions`() {
-        val service = vaultService
         val vaultSubscriber = TestSubscriber<Vault.Update<*>>().apply {
-            service.updates.subscribe(this)
+            vaultService.updates.subscribe(this)
         }
 
         val identity = services.myInfo.singleIdentityAndCert()
+        assertEquals(services.identityService.partyFromKey(identity.owningKey), identity.party)
         val anonymousIdentity = services.keyManagementService.freshKeyAndCert(identity, false)
-        val thirdPartyIdentity = AnonymousParty(generateKeyPair().public)
+        val thirdPartyServices = MockServices()
+        val thirdPartyIdentity = thirdPartyServices.keyManagementService.freshKeyAndCert(thirdPartyServices.myInfo.singleIdentityAndCert(), false)
+        services.identityService.verifyAndRegisterIdentity(thirdPartyIdentity)
         val amount = Amount(1000, Issued(BOC.ref(1), GBP))
 
         // Issue then move some cash
-        val issueTx = TransactionBuilder(identity.party).apply {
+        val issueTx = TransactionBuilder(services.myInfo.chooseIdentity()).apply {
             Cash().generateIssue(this, amount, anonymousIdentity.party.anonymise(), identity.party)
-        }.toWireTransaction(bocServices)
+        }.toWireTransaction(services)
         val cashState = StateAndRef(issueTx.outputs.single(), StateRef(issueTx.id, 0))
 
-        database.transaction { service.notify(StatesToRecord.ONLY_RELEVANT, issueTx) }
+        database.transaction { vaultService.notify(StatesToRecord.ONLY_RELEVANT, issueTx) }
         val expectedIssueUpdate = Vault.Update(emptySet(), setOf(cashState), null)
 
         database.transaction {
             val moveTx = TransactionBuilder(services.myInfo.chooseIdentity()).apply {
-                Cash.generateSpend(services, this, Amount(1000, GBP), thirdPartyIdentity)
+                val changeIdentity = services.keyManagementService.freshKeyAndCert(identity, false)
+                Cash.generateSpend(services, this, Amount(amount.quantity, GBP), changeIdentity, thirdPartyIdentity.party.anonymise())
             }.toWireTransaction(services)
-            service.notify(StatesToRecord.ONLY_RELEVANT, moveTx)
+            vaultService.notify(StatesToRecord.ONLY_RELEVANT, moveTx)
         }
         val expectedMoveUpdate = Vault.Update(setOf(cashState), emptySet(), null)
 
