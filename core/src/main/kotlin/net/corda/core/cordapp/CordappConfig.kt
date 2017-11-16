ee69da0805d7656bebac397f934ed1a65455e184
Clinton Alexander <clinton.alexander@r3.com>
2017-11-16 22:25:54 +0000

    Added exception on failure to parse.

--- core/src/main/kotlin/net/corda/core/cordapp/CordappConfig.kt
+++ core/src/main/kotlin/net/corda/core/cordapp/CordappConfig.kt
@@ -1,5 +1,7 @@
 package net.corda.core.cordapp
 
+class CordappConfigException(msg: String, e: Throwable) : Exception(msg, e)
+
 /**
  * Provides access to cordapp configuration independent of the configuration provider.
  */
@@ -7,6 +9,8 @@ interface CordappConfig {
     /**
      * Get the value of the configuration at "path" with type T. This is provider dependent and the default provider is
      * typesafe configuration.
+     *
+     * @throws CordappConfigException If the configuration fails to load, parse, or find a value.
      */
     fun <T> get(path: String): T?
 }
--- core/src/main/kotlin/net/corda/core/internal/cordapp/TypesafeCordappConfig.kt
+++ core/src/main/kotlin/net/corda/core/internal/cordapp/TypesafeCordappConfig.kt
@@ -1,15 +1,21 @@
 package net.corda.core.internal.cordapp
 
 import com.typesafe.config.Config
+import com.typesafe.config.ConfigException
 import net.corda.core.cordapp.CordappConfig
+import net.corda.core.cordapp.CordappConfigException
 
 class TypesafeCordappConfig(private val cordappConfig: Config) : CordappConfig {
     override fun <T> get(path: String): T? {
-        val value = cordappConfig.getAnyRef(path)
-        return if (value != null) {
-            value as T
-        } else {
-            null
+        try {
+            val value = cordappConfig.getAnyRef(path)
+            return if (value != null) {
+                value as T
+            } else {
+                null
+            }
+        } catch (e: ConfigException) {
+            throw CordappConfigException("Cordapp configuration is incorrect due to exception", e)
         }
     }
 }
