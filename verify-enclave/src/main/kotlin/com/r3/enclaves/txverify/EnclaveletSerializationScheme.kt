9403e231cc75701bea2df143ac8c6c218639aec7
Chris Rankin <chris.rankin@r3.com>
2017-11-20 10:49:02 +0000

    Register Kryo and AMQP serialisation schemes for the SGX enclave.

---
+++ verify-enclave/src/main/kotlin/com/r3/enclaves/txverify/EnclaveletSerializationScheme.kt
@@ -0,0 +1,52 @@
+@file:JvmName("EnclaveletSerializationScheme")
+package com.r3.enclaves.txverify
+
+import net.corda.core.serialization.SerializationContext
+import net.corda.core.serialization.internal.SerializationEnvironmentImpl
+import net.corda.core.serialization.internal.nodeSerializationEnv
+import net.corda.core.utilities.ByteSequence
+import net.corda.nodeapi.internal.serialization.*
+import net.corda.nodeapi.internal.serialization.amqp.AbstractAMQPSerializationScheme
+import net.corda.nodeapi.internal.serialization.amqp.AmqpHeaderV1_0
+import net.corda.nodeapi.internal.serialization.amqp.SerializerFactory
+import net.corda.nodeapi.internal.serialization.kryo.AbstractKryoSerializationScheme
+import net.corda.nodeapi.internal.serialization.kryo.KryoHeaderV0_1
+
+@Suppress("UNUSED")
+private class EnclaveletSerializationScheme {
+    /*
+     * Registers the serialisation schemes as soon as this class is loaded into the JVM.
+     */
+    private companion object {
+        init {
+            nodeSerializationEnv = SerializationEnvironmentImpl(
+                SerializationFactoryImpl().apply {
+                    registerScheme(KryoVerifierSerializationScheme)
+                    registerScheme(AMQPVerifierSerializationScheme)
+                },
+                /**
+                 * Even though default context is set to Kryo P2P, the encoding will be adjusted depending on the
+                 * incoming request received.
+                 */
+                KRYO_P2P_CONTEXT)
+        }
+    }
+}
+
+private object KryoVerifierSerializationScheme : AbstractKryoSerializationScheme() {
+    override fun canDeserializeVersion(byteSequence: ByteSequence, target: SerializationContext.UseCase): Boolean {
+        return byteSequence == KryoHeaderV0_1 && target == SerializationContext.UseCase.P2P
+    }
+
+    override fun rpcClientKryoPool(context: SerializationContext) = throw UnsupportedOperationException()
+    override fun rpcServerKryoPool(context: SerializationContext) = throw UnsupportedOperationException()
+}
+
+private object AMQPVerifierSerializationScheme : AbstractAMQPSerializationScheme() {
+    override fun canDeserializeVersion(byteSequence: ByteSequence, target: SerializationContext.UseCase): Boolean {
+        return (byteSequence == AmqpHeaderV1_0 && (target == SerializationContext.UseCase.P2P))
+    }
+
+    override fun rpcClientSerializerFactory(context: SerializationContext): SerializerFactory = throw UnsupportedOperationException()
+    override fun rpcServerSerializerFactory(context: SerializationContext): SerializerFactory = throw UnsupportedOperationException()
+}
