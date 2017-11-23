05a4a8e96a13b4960cea5fb8ac15369483e99ab4
Katarzyna Streich <katarzyna.streich@r3.com>
2017-11-23 16:27:56 +0000

    Address comments

--- gradle-plugins/cordform-common/src/main/java/net/corda/cordform/NetworkParametersGenerator.java
+++ gradle-plugins/cordform-common/src/main/java/net/corda/cordform/NetworkParametersGenerator.java
@@ -6,12 +6,11 @@ import java.util.Map;
 
 public interface NetworkParametersGenerator {
     /**
-     * Run generation of network parameters for [Cordformation]. Nodes need to have already all [NodeInfo]s files in their
+     * Run generation of network parameters for [Cordformation]. Nodes need to have already their own [NodeInfo] files in their
      * base directories, these files will be used to extract notary identities.
      *
-     * @param notaryMap - map where key is notary name, value indicates if a notary is validating or not
      * @param nodesDirs - nodes directories that will be used for network parameters generation. Network parameters
      *                  file will be dropped into each directory on this list.
      */
-    void run(Map<String, Boolean> notaryMap, List<Path> nodesDirs);
+    void run(List<Path> nodesDirs);
 }
