f931b74aec56e8bda66c49cefe2ad3455615e033
Katelyn Baker <katelyn.baker@r3.com>
2017-11-23 17:53:31 +0000

    CORDA-780 / CORDA-786 - Doc updates

--- core/src/main/kotlin/net/corda/core/serialization/CordaCustomSerializer.kt
+++ core/src/main/kotlin/net/corda/core/serialization/CordaCustomSerializer.kt
@@ -1,8 +1,18 @@
 package net.corda.core.serialization
 
+/**
+ * This annotation marks a class as being a serializer provided by a CorDapp to proxy some third party
+ * class Corda couldn't otherwise serialize. It should be applied to classes that implement the
+ * [SerializationCustomSerializer] interface
+ */
 @Target(AnnotationTarget.CLASS)
 annotation class CordaCustomSerializer
 
+/**
+ *  This annotation marks a class as being a proxy for some third party class and used by some
+ *  implementation of [SerializationCustomSerializer]. Such classes must be annotated to allow
+ *  them to be discovered in a CorDapp jar and loaded
+ */
 @Target(AnnotationTarget.CLASS)
 annotation class CordaCustomSerializerProxy
 
