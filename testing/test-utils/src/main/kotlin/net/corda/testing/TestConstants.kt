21e1118ea04f5da324b8b3434c39ea606af2910c
Andrzej Cichocki <andrzej.cichocki@r3.com>
2017-12-14 16:30:18 +0000

    Make test constants Java-visible fields. (#2258)

--- testing/test-utils/src/main/kotlin/net/corda/testing/TestConstants.kt
+++ testing/test-utils/src/main/kotlin/net/corda/testing/TestConstants.kt
@@ -13,14 +13,23 @@ import java.security.PublicKey
 import java.time.Instant
 
 // A dummy time at which we will be pretending test transactions are created.
-val TEST_TX_TIME: Instant get() = Instant.parse("2015-04-17T12:00:00.00Z")
+@JvmField
+val TEST_TX_TIME = Instant.parse("2015-04-17T12:00:00.00Z")
+@JvmField
 val DUMMY_NOTARY_NAME = CordaX500Name("Notary Service", "Zurich", "CH")
+@JvmField
 val DUMMY_BANK_A_NAME = CordaX500Name("Bank A", "London", "GB")
+@JvmField
 val DUMMY_BANK_B_NAME = CordaX500Name("Bank B", "New York", "US")
+@JvmField
 val DUMMY_BANK_C_NAME = CordaX500Name("Bank C", "Tokyo", "JP")
+@JvmField
 val BOC_NAME = CordaX500Name("BankOfCorda", "London", "GB")
+@JvmField
 val ALICE_NAME = CordaX500Name("Alice Corp", "Madrid", "ES")
+@JvmField
 val BOB_NAME = CordaX500Name("Bob Plc", "Rome", "IT")
+@JvmField
 val CHARLIE_NAME = CordaX500Name("Charlie Ltd", "Athens", "GR")
 val DEV_CA: CertificateAndKeyPair by lazy {
     // TODO: Should be identity scheme
