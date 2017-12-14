66d9987e7020cce090b98928d24fa27398f6ecad
Katarzyna Streich <katarzyna.streich@r3.com>
2017-12-14 11:09:15 +0000

    SignedNetworkMap verification fix
    SignedNetworkMap verification should also include cert path validation,
    which was probably moved away by accident, because docs say about the
    exception CertPathValidatorException.

--- node/src/main/kotlin/net/corda/node/services/network/NetworkMapClient.kt
+++ node/src/main/kotlin/net/corda/node/services/network/NetworkMapClient.kt
@@ -47,9 +47,7 @@ class NetworkMapClient(compatibilityZoneURL: URL, private val trustedRoot: X509C
     fun getNetworkMap(): NetworkMapResponse {
         val conn = networkMapUrl.openHttpConnection()
         val signedNetworkMap = conn.inputStream.use { it.readBytes() }.deserialize<SignedNetworkMap>()
-        val networkMap = signedNetworkMap.verified()
-        // Assume network map cert is issued by the root.
-        X509Utilities.validateCertificateChain(trustedRoot, signedNetworkMap.sig.by, trustedRoot)
+        val networkMap = signedNetworkMap.verified(trustedRoot)
         val timeout = CacheControl.parse(Headers.of(conn.headerFields.filterKeys { it != null }.mapValues { it.value.first() })).maxAgeSeconds().seconds
         return NetworkMapResponse(networkMap, timeout)
     }
