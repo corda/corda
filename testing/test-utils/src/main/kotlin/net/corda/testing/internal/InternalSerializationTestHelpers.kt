7f43cacdf7843f9bff5af7433b5013fda3738eb6
Andrzej Cichocki <andrzej.cichocki@r3.com>
2017-12-14 11:23:18 +0000

    Add todo.

--- testing/test-utils/src/main/kotlin/net/corda/testing/internal/InternalSerializationTestHelpers.kt
+++ testing/test-utils/src/main/kotlin/net/corda/testing/internal/InternalSerializationTestHelpers.kt
@@ -8,7 +8,7 @@ import net.corda.testing.SerializationEnvironmentRule
  * For example your test class uses [SerializationEnvironmentRule] but you want to turn it off for one method.
  * Use sparingly, ideally a test class shouldn't mix serializers init mechanisms.
  */
-fun <T> withoutTestSerialization(callable: () -> T): T {
+fun <T> withoutTestSerialization(callable: () -> T): T { // TODO: Delete this, see CORDA-858.
     val (property, env) = listOf(_contextSerializationEnv, _inheritableContextSerializationEnv).map { Pair(it, it.get()) }.single { it.second != null }
     property.set(null)
     try {
