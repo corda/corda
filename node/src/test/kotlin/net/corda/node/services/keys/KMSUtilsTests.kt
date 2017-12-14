6f59d3ddfdeef3a832b6716a14717f5703056842
Ross Nicoll <ross.nicoll@r3.com>
2017-12-14 15:44:18 +0000

    Update KMSUtilsTest after rebasing

--- node/src/test/kotlin/net/corda/node/services/keys/KMSUtilsTests.kt
+++ node/src/test/kotlin/net/corda/node/services/keys/KMSUtilsTests.kt
@@ -1,13 +1,11 @@
 package net.corda.node.services.keys
 
 import net.corda.core.CordaOID
+import net.corda.core.crypto.generateKeyPair
 import net.corda.core.internal.CertRole
-import net.corda.testing.MEGA_CORP
-import net.corda.testing.MEGA_CORP_IDENTITY
-import net.corda.testing.MEGA_CORP_KEY
+import net.corda.testing.*
 import net.corda.testing.node.MockServices
 import net.corda.testing.node.makeTestIdentityService
-import net.corda.testing.singleIdentityAndCert
 import org.bouncycastle.asn1.DEROctetString
 import org.junit.Test
 import kotlin.test.assertEquals
@@ -15,9 +13,11 @@ import kotlin.test.assertEquals
 class KMSUtilsTests {
     @Test
     fun `should generate certificates with the correct role`() {
+        val aliceKey = generateKeyPair()
+        val alice = getTestPartyAndCertificate(ALICE_NAME, aliceKey.public)
         val cordappPackages = emptyList<String>()
-        val ledgerIdentityService = makeTestIdentityService(listOf(MEGA_CORP_IDENTITY))
-        val mockServices = MockServices(cordappPackages, ledgerIdentityService, MEGA_CORP.name, MEGA_CORP_KEY)
+        val ledgerIdentityService = makeTestIdentityService(listOf(alice))
+        val mockServices = MockServices(cordappPackages, ledgerIdentityService, alice.name, aliceKey)
         val wellKnownIdentity = mockServices.myInfo.singleIdentityAndCert()
         val confidentialIdentity = mockServices.keyManagementService.freshKeyAndCert(wellKnownIdentity, false)
         val cert = confidentialIdentity.certificate
