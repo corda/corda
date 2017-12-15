037673abe43f7b9fbe568f0cca31aafb7ebcdd51
Christian Sailer <christian.sailer@r3.com>
2017-12-15 16:15:11 +0000

    perftest scripts (#172)
    * SQL scripts to set up SQL server for perftest cluster
    
    * Fix notary name in jmx files

---
+++ perftestcordapp/src/main/sql/01_create_logins.sql
@@ -0,0 +1,7 @@
+USE master
+
+CREATE LOGIN perfnode1 with PASSWORD = 'yourStrong(!)Password';
+CREATE LOGIN perfnode2 with PASSWORD = 'yourStrong(!)Password';
+CREATE LOGIN perfnode3 with PASSWORD = 'yourStrong(!)Password';
+CREATE LOGIN perfnode4 with PASSWORD = 'yourStrong(!)Password';
+CREATE LOGIN perfnotary with PASSWORD = 'yourStrong(!)Password';
