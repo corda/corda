cb703476a0e25e0a0b599c07ce739cbc59eb89b8
Anthony Keenan <34482776+anthonykr3@users.noreply.github.com>
2017-12-21 09:42:09 +0000

    CORDA-880 Make contractsdsl java interop functions behave same as inline functions (#2275)
    * Make java interop functions same as inline functions and add tests
    
    * Fixing docs
    
    * Move unspecifiedCountry to internal. (#2274)
    
    * Review changes
    
    * Call java interop functions from inline functions
    
    * Use correct test assertion

--- core/src/main/kotlin/net/corda/core/contracts/ContractsDSL.kt
+++ core/src/main/kotlin/net/corda/core/contracts/ContractsDSL.kt
@@ -12,7 +12,6 @@ import java.util.*
  * Defines a simple domain specific language for the specification of financial contracts. Currently covers:
  *
  *  - Some utilities for working with commands.
- *  - An Amount type that represents a positive quantity of a specific token.
  *  - A simple language extension for specifying requirements in English, along with logic to enforce them.
  */
 
@@ -30,36 +29,43 @@ inline fun <R> requireThat(body: Requirements.() -> R) = Requirements.body()
 
 //// Authenticated commands ///////////////////////////////////////////////////////////////////////////////////////////
 
-// TODO: Provide a version of select that interops with Java
-
 /** Filters the command list by type, party and public key all at once. */
 inline fun <reified T : CommandData> Collection<CommandWithParties<CommandData>>.select(signer: PublicKey? = null,
                                                                                         party: AbstractParty? = null) =
-        filter { it.value is T }.
+        select(T::class.java, signer, party)
+
+/** Filters the command list by type, party and public key all at once. */
+fun <C : CommandData> Collection<CommandWithParties<CommandData>>.select(klass: Class<C>,
+                                                                         signer: PublicKey? = null,
+                                                                         party: AbstractParty? = null) =
+        mapNotNull { if (klass.isInstance(it.value)) uncheckedCast<CommandWithParties<CommandData>, CommandWithParties<C>>(it) else null }.
                 filter { if (signer == null) true else signer in it.signers }.
                 filter { if (party == null) true else party in it.signingParties }.
-                map { CommandWithParties(it.signers, it.signingParties, it.value as T) }
-
-// TODO: Provide a version of select that interops with Java
+                map { CommandWithParties(it.signers, it.signingParties, it.value) }
 
 /** Filters the command list by type, parties and public keys all at once. */
 inline fun <reified T : CommandData> Collection<CommandWithParties<CommandData>>.select(signers: Collection<PublicKey>?,
                                                                                         parties: Collection<Party>?) =
-        filter { it.value is T }.
+        select(T::class.java, signers, parties)
+
+/** Filters the command list by type, parties and public keys all at once. */
+fun <C : CommandData> Collection<CommandWithParties<CommandData>>.select(klass: Class<C>,
+                                                                         signers: Collection<PublicKey>?,
+                                                                         parties: Collection<Party>?) =
+        mapNotNull { if (klass.isInstance(it.value)) uncheckedCast<CommandWithParties<CommandData>, CommandWithParties<C>>(it) else null }.
                 filter { if (signers == null) true else it.signers.containsAll(signers) }.
                 filter { if (parties == null) true else it.signingParties.containsAll(parties) }.
-                map { CommandWithParties(it.signers, it.signingParties, it.value as T) }
+                map { CommandWithParties(it.signers, it.signingParties, it.value) }
 
 /** Ensures that a transaction has only one command that is of the given type, otherwise throws an exception. */
-inline fun <reified T : CommandData> Collection<CommandWithParties<CommandData>>.requireSingleCommand() = try {
-    select<T>().single()
+inline fun <reified T : CommandData> Collection<CommandWithParties<CommandData>>.requireSingleCommand() = requireSingleCommand(T::class.java)
+
+/** Ensures that a transaction has only one command that is of the given type, otherwise throws an exception. */
+fun <C : CommandData> Collection<CommandWithParties<CommandData>>.requireSingleCommand(klass: Class<C>) = try {
+    select(klass).single()
 } catch (e: NoSuchElementException) {
-    throw IllegalStateException("Required ${T::class.qualifiedName} command")   // Better error message.
+    throw IllegalStateException("Required ${klass.kotlin.qualifiedName} command")   // Better error message.
 }
-
-/** Ensures that a transaction has only one command that is of the given type, otherwise throws an exception. */
-fun <C : CommandData> Collection<CommandWithParties<CommandData>>.requireSingleCommand(klass: Class<C>) =
-        mapNotNull { if (klass.isInstance(it.value)) uncheckedCast<CommandWithParties<CommandData>, CommandWithParties<C>>(it) else null }.single()
 
 /**
  * Simple functionality for verifying a move command. Verifies that each input has a signature from its owning key.
