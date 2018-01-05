694721e8ba693d83419feaad2582d3cf6b17d517
Katelyn Baker <katelyn.baker@r3.com>
2018-01-05 14:07:55 +0000

    TEMP removal of SSH tests till they're made determinsitic (#2325)

--- node/src/integration-test/kotlin/net/corda/node/SSHServerTest.kt
+++ node/src/integration-test/kotlin/net/corda/node/SSHServerTest.kt
@@ -19,10 +19,12 @@ import java.net.ConnectException
 import kotlin.test.assertTrue
 import kotlin.test.fail
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.Ignore
 import java.util.regex.Pattern
 
 class SSHServerTest {
 
+    @Ignore("Test has undeterministic capacity to hang, ignore till fixed")
     @Test()
     fun `ssh server does not start be default`() {
         val user = User("u", "p", setOf())
@@ -44,6 +46,7 @@ class SSHServerTest {
         }
     }
 
+    @Ignore("Test has undeterministic capacity to hang, ignore till fixed")
     @Test
     fun `ssh server starts when configured`() {
         val user = User("u", "p", setOf())
@@ -64,6 +67,7 @@ class SSHServerTest {
     }
 
 
+    @Ignore("Test has undeterministic capacity to hang, ignore till fixed")
     @Test
     fun `ssh server verify credentials`() {
         val user = User("u", "p", setOf())
@@ -87,6 +91,7 @@ class SSHServerTest {
         }
     }
 
+    @Ignore("Test has undeterministic capacity to hang, ignore till fixed")
     @Test
     fun `ssh respects permissions`() {
         val user = User("u", "p", setOf(startFlow<FlowICanRun>()))
@@ -117,6 +122,7 @@ class SSHServerTest {
         }
     }
 
+    @Ignore("Test has undeterministic capacity to hang, ignore till fixed")
     @Test
     fun `ssh runs flows`() {
         val user = User("u", "p", setOf(startFlow<FlowICanRun>()))
