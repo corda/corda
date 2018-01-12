9423ca4ed526fc682b982d174215544a43f14fee
Anthony Keenan <anthony.keenan@r3.com>
2018-01-12 17:34:17 +0000

    Update kdocs

--- testing/node-driver/src/main/kotlin/net/corda/testing/node/User.kt
+++ testing/node-driver/src/main/kotlin/net/corda/testing/node/User.kt
@@ -1,5 +1,6 @@
 package net.corda.testing.node
 
+/** Object encapsulating a node rpc user and their associated permissions for use when testing */
 data class User(
         val username: String,
         val password: String,
