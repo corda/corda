1367cd4adb24fe00702ab8985b7fcb328f81c55a
Anthony Keenan <34482776+anthonykr3@users.noreply.github.com>
2018-01-15 19:30:33 +0000

    CORDA-912 Stop exposing internal node user, create user in testing infrastructu… (#2361)
    * Stop exposing internal node user, create user in testing infrastructure instead
    
    * Update kdocs

--- testing/node-driver/src/main/kotlin/net/corda/testing/driver/Driver.kt
+++ testing/node-driver/src/main/kotlin/net/corda/testing/driver/Driver.kt
@@ -14,8 +14,8 @@ import net.corda.node.internal.Node
 import net.corda.node.internal.StartedNode
 import net.corda.node.services.config.NodeConfiguration
 import net.corda.node.services.config.VerifierType
-import net.corda.nodeapi.internal.config.User
 import net.corda.testing.DUMMY_NOTARY_NAME
+import net.corda.testing.node.User
 import net.corda.testing.node.NotarySpec
 import net.corda.testing.node.internal.DriverDSLImpl
 import net.corda.testing.node.internal.genericDriver
--- testing/node-driver/src/main/kotlin/net/corda/testing/node/NotarySpec.kt
+++ testing/node-driver/src/main/kotlin/net/corda/testing/node/NotarySpec.kt
@@ -3,7 +3,6 @@ package net.corda.testing.node
 import net.corda.core.DoNotImplement
 import net.corda.core.identity.CordaX500Name
 import net.corda.node.services.config.VerifierType
-import net.corda.nodeapi.internal.config.User
 
 data class NotarySpec(
         val name: CordaX500Name,
