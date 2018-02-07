4ece77bca4c5d2fa4ce737f8f37d088908517cac
Anthony Keenan <anthony.keenan@r3.com>
2018-02-07 10:32:51 +0000

    Updated docs and removed pointless attribute

--- core/src/main/kotlin/net/corda/core/CordaInternal.kt
+++ core/src/main/kotlin/net/corda/core/CordaInternal.kt
@@ -1,15 +1,12 @@
 package net.corda.core
 
-import java.lang.annotation.Inherited
-
 /**
- * These fields are only meant to be used by Corda internally, and are not intended to be part of the public API.
- *
  * These methods are not part of Corda's API compatibility guarantee and applications should not use them.
+ *
+ * These fields are only meant to be used by Corda internally, and are not intended to be part of the public API.
  */
 @Retention(AnnotationRetention.BINARY)
 @Target(AnnotationTarget.PROPERTY_GETTER, AnnotationTarget.PROPERTY_SETTER)
 @MustBeDocumented
-@Inherited
 annotation class CordaInternal
 
