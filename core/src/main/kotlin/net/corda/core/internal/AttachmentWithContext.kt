54561ba6bf9c188fbf932b8ecc2f048f9cd17ae5
Andrius Dagys <andrius.dagys@r3.com>
2018-02-20 15:31:12 +0000

    CORDA-941: Instead of storing the network map contract implementation whitelist along with the constraint, pass it in during verification on LedgerTransaction

---
+++ core/src/main/kotlin/net/corda/core/internal/AttachmentWithContext.kt
@@ -0,0 +1,22 @@
+package net.corda.core.internal
+
+import net.corda.core.contracts.Attachment
+import net.corda.core.contracts.ContractAttachment
+import net.corda.core.contracts.ContractClassName
+import net.corda.core.node.services.AttachmentId
+
+/**
+ * Used only for passing to the Attachment constraint verification.
+ */
+class AttachmentWithContext(
+        val contractAttachment: ContractAttachment,
+        val stateContract: ContractClassName,
+        /** Required for verifying [WhitelistedByZoneAttachmentConstraint] */
+        val whitelistedContractImplementations: Map<String, List<AttachmentId>>?
+) : Attachment by contractAttachment {
+    init {
+        require(stateContract in contractAttachment.allContracts) {
+            "This AttachmentWithContext was not initialised properly"
+        }
+    }
+}
