1c67654b71a6b1863f7a493282e1ef52a2c6051a
Mike Hearn <mike@r3.com>
2018-03-02 12:22:52 +0100

    Fix another non-serializable exception, add docs, fix a possible security issue.

--- core/src/main/kotlin/net/corda/core/CordaInternal.kt
+++ core/src/main/kotlin/net/corda/core/CordaInternal.kt
@@ -6,6 +6,6 @@ package net.corda.core
  * These fields are only meant to be used by Corda internally, and are not intended to be part of the public API.
  */
 @Retention(AnnotationRetention.BINARY)
-@Target(AnnotationTarget.PROPERTY_GETTER, AnnotationTarget.PROPERTY_SETTER)
+@Target(AnnotationTarget.PROPERTY_GETTER, AnnotationTarget.PROPERTY_SETTER, AnnotationTarget.FUNCTION)
 @MustBeDocumented
 annotation class CordaInternal
