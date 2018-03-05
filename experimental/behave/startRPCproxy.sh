4eb31e33a946613751b559a9e9bb5335245e7cc8
josecoll <jose.coll@r3cev.com>
2018-03-05 09:40:00 +0000

    RPCProxy port number configuration.

--- experimental/behave/startRPCproxy.sh
+++ experimental/behave/startRPCproxy.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 DISTRO_DIR=$1
+PORT="${2:-13000}"
 
 if [ ! -d "$DISTRO_DIR" ]; then
     echo "Must specify location of Corda distribution (directory does not exist: $DISTRO_DIR)"
@@ -28,11 +29,11 @@ echo "Launching RPC proxy ..."
 echo "/usr/bin/java -cp $DISTRO_DIR/corda-rpcProxy.jar:\
 \n\t$(ls $DISTRO_DIR/proxy/*.jar | tr '\n' ':'):\
 \n\t$(ls $DISTRO_DIR/apps/*.jar | tr '\n' ':') \
-\n\tnet.corda.behave.service.proxy.RPCProxyServerKt
+\n\tnet.corda.behave.service.proxy.RPCProxyServerKt ${PORT}
 "
 
 /usr/bin/java -cp $DISTRO_DIR/corda-rpcProxy.jar:\
 $(ls $DISTRO_DIR/proxy/*.jar | tr '\n' ':'):\
 $(ls $DISTRO_DIR/apps/*.jar | tr '\n' ':') \
-net.corda.behave.service.proxy.RPCProxyServerKt & echo $! > /tmp/rpcProxy-pid &
+net.corda.behave.service.proxy.RPCProxyServerKt ${PORT} & echo $! > /tmp/rpcProxy-pid &
 echo "RPCProxyServer PID: $(cat /tmp/rpcProxy-pid)"
