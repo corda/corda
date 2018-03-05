59d88324cc17259bc9c64945af7d9b797ab753bb
Stefano Franz <stefano.franz@r3.com>
2018-03-05 13:43:39 +0000

    @OnNodeStartup kDocs

--- core/src/main/kotlin/net/corda/core/node/startup/OnNodeStartup.kt
+++ core/src/main/kotlin/net/corda/core/node/startup/OnNodeStartup.kt
@@ -2,6 +2,13 @@ package net.corda.core.node.startup
 
 import kotlin.annotation.AnnotationTarget.CLASS
 
+/**
+ * Annotate any flow that needs to execute on node startup
+ * The flow must have a no-args constructor
+ * The flow may be run multiple times in a failure scenario - it must be able to detect the condition where it has already run
+ *
+ * The annotated class must be a SubClass of [net.corda.core.flows.FlowLogic]
+ */
 
 @Target(CLASS)
 annotation class OnNodeStartup
