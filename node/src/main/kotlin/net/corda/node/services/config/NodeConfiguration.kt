72074c76c71fb49de258878604beb17da05f1a71
szymonsztuka <szymon.sztuka@yahoo.com>
2018-03-07 09:57:32 +0000

    [CORDA-792] Standalone Shell (#2663)
    - Existing embedded Shell connects via RPC including checking RPC user credentials (before was a direct use of CordaRPCOps):  in dev mode when console terminal is enabled, node created `shell` user.
    - New Standalone Shell app with the same functionalities as Shell: connects to a node via RPC Client,  can use SSL and run SSH server.

--- node/src/main/kotlin/net/corda/node/services/config/NodeConfiguration.kt
+++ node/src/main/kotlin/net/corda/node/services/config/NodeConfiguration.kt
@@ -14,6 +14,7 @@ import net.corda.nodeapi.internal.config.SSLConfiguration
 import net.corda.nodeapi.internal.config.User
 import net.corda.nodeapi.internal.config.parseAs
 import net.corda.nodeapi.internal.persistence.DatabaseConfig
+import net.corda.tools.shell.SSHDConfiguration
 import java.net.URL
 import java.nio.file.Path
 import java.time.Duration
@@ -253,8 +254,6 @@ data class CertChainPolicyConfig(val role: String, private val policy: CertChain
         }
 }
 
-data class SSHDConfiguration(val port: Int)
-
 // Supported types of authentication/authorization data providers
 enum class AuthDataSourceType {
     // External RDBMS
@@ -290,6 +289,8 @@ data class SecurityConfiguration(val authService: SecurityConfiguration.AuthServ
             }
         }
 
+        fun copyWithAdditionalUser(user: User) = AuthService(dataSource.copyWithAdditionalUser(user), id, options)
+
         // Optional components: cache
         data class Options(val cache: Options.Cache?) {
 
@@ -316,6 +317,12 @@ data class SecurityConfiguration(val authService: SecurityConfiguration.AuthServ
                     AuthDataSourceType.INMEMORY -> require(users != null && connection == null)
                     AuthDataSourceType.DB -> require(users == null && connection != null)
                 }
+            }
+
+            fun copyWithAdditionalUser(user: User) : DataSource{
+                val extendedList = this.users?.toMutableList()?: mutableListOf()
+                extendedList.add(user)
+                return DataSource(this.type, this.passwordEncryption, this.connection, listOf(*extendedList.toTypedArray()))
             }
         }
 
---
+++ node/src/main/kotlin/net/corda/node/services/config/shell/ShellConfig.kt
@@ -0,0 +1,44 @@
+package net.corda.node.services.config.shell
+
+import net.corda.core.internal.div
+import net.corda.core.utilities.NetworkHostAndPort
+import net.corda.node.services.Permissions
+import net.corda.node.services.config.NodeConfiguration
+import net.corda.node.services.config.shouldInitCrashShell
+import net.corda.nodeapi.internal.config.User
+import net.corda.tools.shell.ShellConfiguration
+import net.corda.tools.shell.ShellConfiguration.Companion.COMMANDS_DIR
+import net.corda.tools.shell.ShellConfiguration.Companion.CORDAPPS_DIR
+import net.corda.tools.shell.ShellConfiguration.Companion.SSHD_HOSTKEY_DIR
+import net.corda.tools.shell.ShellConfiguration.Companion.SSH_PORT
+import net.corda.tools.shell.ShellSslOptions
+
+
+//re-packs data to Shell specific classes
+fun NodeConfiguration.toShellConfig(): ShellConfiguration {
+
+    val sslConfiguration = if (this.rpcOptions.useSsl) {
+        with(this.rpcOptions.sslConfig) {
+            ShellSslOptions(sslKeystore,
+                    keyStorePassword,
+                    trustStoreFile,
+                    trustStorePassword)
+        }
+    } else {
+        null
+    }
+    val localShellUser: User = localShellUser()
+    return ShellConfiguration(
+            commandsDirectory = this.baseDirectory / COMMANDS_DIR,
+            cordappsDirectory = this.baseDirectory.toString() / CORDAPPS_DIR,
+            user = localShellUser.username,
+            password = localShellUser.password,
+            hostAndPort = this.rpcOptions.address ?: NetworkHostAndPort("localhost", SSH_PORT),
+            ssl = sslConfiguration,
+            sshdPort = this.sshd?.port,
+            sshHostKeyDirectory = this.baseDirectory / SSHD_HOSTKEY_DIR,
+            noLocalShell = this.noLocalShell)
+}
+
+private fun localShellUser() = User("shell", "shell", setOf(Permissions.all()))
+fun NodeConfiguration.shellUser() = shouldInitCrashShell()?.let { localShellUser() }
