da74263f428cf8f445caf12a8d2b931fc6ccb7e9
Andrius Dagys <andrius.dagys@r3.com>
2018-03-08 17:59:25 +0000

    CORDA-696 - Ensure deterministic transaction id calculation for contra… (#2676)
    * CORDA-696: Ensure deterministic transaction id calculation for contract upgrade and notary change transactions.
    
    The problem with the previous implementation is that the transaction would be deserialized with the schema specified
    in the serialized form, but the calculation of the id would involve re-serializing properties using a local serialization context
    which might produce a different result.
    
    * Support forwards compatibility for new hidden or visible properties
    
    * Some refactoring and updating api docs
    
    * Fix tests & add custom serializer in case the transaction is captured in a checkpoint
    
    * Update id calculation for notary change transactions as well - no filtering is involved
    
    * Use computeNonce
    
    * More refactoring
    
    * Use helper for computing component hashes
    
    * Optimise id calculation

--- core/src/main/kotlin/net/corda/core/crypto/CryptoUtils.kt
+++ core/src/main/kotlin/net/corda/core/crypto/CryptoUtils.kt
@@ -222,7 +222,11 @@ fun componentHash(opaqueBytes: OpaqueBytes, privacySalt: PrivacySalt, componentG
 /** Return the SHA256(SHA256(nonce || serializedComponent)). */
 fun componentHash(nonce: SecureHash, opaqueBytes: OpaqueBytes): SecureHash = SecureHash.sha256Twice(nonce.bytes + opaqueBytes.bytes)
 
-/** Serialise the object and return the hash of the serialized bytes. */
+/**
+ * Serialise the object and return the hash of the serialized bytes. Note that the resulting hash may not be deterministic
+ * across platform versions: serialization can produce different values if any of the types being serialized have changed,
+ * or if the version of serialization specified by the context changes.
+ */
 fun <T : Any> serializedHash(x: T): SecureHash = x.serialize(context = SerializationDefaults.P2P_CONTEXT.withoutReferences()).bytes.sha256()
 
 /**
