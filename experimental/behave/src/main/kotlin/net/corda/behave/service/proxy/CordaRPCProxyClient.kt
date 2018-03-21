0ab747a53d4f4aa6b6aa37b07cf6e8cc79e8a54b
josecoll <jose.coll@r3cev.com>
2018-03-21 10:51:05 +0000

    Updated code to reflect API changes on master (CordaRPCClientConfiguration). Fixed return type on doPost(). Added additional Unit test for Multi-party cash flows.

--- experimental/behave/src/main/kotlin/net/corda/behave/service/proxy/CordaRPCProxyClient.kt
+++ experimental/behave/src/main/kotlin/net/corda/behave/service/proxy/CordaRPCProxyClient.kt
@@ -111,10 +111,7 @@ class CordaRPCProxyClient(private val targetHostAndPort: NetworkHostAndPort) : C
         val argList = listOf(flowName, *args)
 
         println("Corda RPC Proxy client calling: $flowName with values: $argList")
-        // TODO: Fix me to return an arbitrary result type <T>
-//        val response = doPost<AbstractCashFlow.Result>(targetHostAndPort, "start-flow", argList.serialize().bytes)
-//        val response = doPost<ContractState>(targetHostAndPort, "start-flow", argList.serialize().bytes)
-        val response = doPost<SignedTransaction>(targetHostAndPort, "start-flow", argList.serialize().bytes)
+        val response = doPost<Any>(targetHostAndPort, "start-flow", argList.serialize().bytes)
         val result = doneFuture(response)
         return FlowHandleImpl(StateMachineRunId.createRandom(), result) as FlowHandle<T>
     }
@@ -204,6 +201,10 @@ class CordaRPCProxyClient(private val targetHostAndPort: NetworkHostAndPort) : C
     }
 
     override fun isFlowsDrainingModeEnabled(): Boolean {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun shutdown() {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
