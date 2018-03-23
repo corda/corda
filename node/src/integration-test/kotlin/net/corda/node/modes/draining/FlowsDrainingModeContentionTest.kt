4ce69e3aced9c10c26c4b89d077ec75628b7ead7
sollecitom <michele.sollecito@gmail.com>
2018-03-23 12:17:34 +0000

    Merge remote-tracking branch 'remotes/open/master' into merges/march-23-11-47
    # Conflicts:
    #	node/src/integration-test/kotlin/net/corda/node/NodeStartAndStopTest.kt
    #	node/src/integration-test/kotlin/net/corda/node/NodeStatePersistenceTests.kt
    #	node/src/main/kotlin/net/corda/node/services/statemachine/StateMachineManagerImpl.kt

--- node/src/integration-test/kotlin/net/corda/node/modes/draining/FlowsDrainingModeContentionTest.kt
+++ node/src/integration-test/kotlin/net/corda/node/modes/draining/FlowsDrainingModeContentionTest.kt
@@ -5,9 +5,18 @@ import net.corda.MESSAGE_CONTRACT_PROGRAM_ID
 import net.corda.Message
 import net.corda.MessageContract
 import net.corda.MessageState
+import net.corda.RpcInfo
+import net.corda.client.rpc.CordaRPCClient
 import net.corda.core.contracts.Command
 import net.corda.core.contracts.StateAndContract
-import net.corda.core.flows.*
+import net.corda.core.flows.FinalityFlow
+import net.corda.core.flows.FlowLogic
+import net.corda.core.flows.FlowSession
+import net.corda.core.flows.InitiatedBy
+import net.corda.core.flows.InitiatingFlow
+import net.corda.core.flows.ReceiveTransactionFlow
+import net.corda.core.flows.SendTransactionFlow
+import net.corda.core.flows.StartableByRPC
 import net.corda.core.identity.Party
 import net.corda.core.internal.packageName
 import net.corda.core.messaging.startFlow
@@ -15,14 +24,11 @@ import net.corda.core.transactions.SignedTransaction
 import net.corda.core.transactions.TransactionBuilder
 import net.corda.core.utilities.getOrThrow
 import net.corda.core.utilities.unwrap
-import net.corda.RpcInfo
-import net.corda.client.rpc.CordaRPCClient
 import net.corda.node.services.Permissions.Companion.all
 import net.corda.testing.core.singleIdentity
 import net.corda.testing.driver.DriverParameters
 import net.corda.testing.driver.PortAllocation
 import net.corda.testing.driver.driver
-import net.corda.testing.internal.IntegrationTest
 import net.corda.testing.node.User
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.After
@@ -31,7 +37,7 @@ import org.junit.Test
 import java.util.concurrent.Executors
 import java.util.concurrent.ScheduledExecutorService
 
-class FlowsDrainingModeContentionTest : IntegrationTest() {
+class FlowsDrainingModeContentionTest {
 
     private val portAllocation = PortAllocation.Incremental(10000)
     private val user = User("mark", "dadada", setOf(all()))
