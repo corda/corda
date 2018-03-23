5e36af60e6a3bcb16dcc73931c1b3690485cc242
josecoll <jose.coll@r3cev.com>
2018-03-23 14:37:20 +0000

    Should not copy cordapps to proxy directory.

--- experimental/behave/src/qa-scenarios/resources/scripts/update-cordapps-cts.sh
+++ experimental/behave/src/qa-scenarios/resources/scripts/update-cordapps-cts.sh
@@ -4,21 +4,20 @@
 # For example:
 #   corda-master   => git clone https://github.com/corda/corda
 #   r3corda-master => git clone https://github.com/corda/enterprise
-VERSION=corda-master
-BUILD_DIR=/Users/josecoll/IdeaProjects/corda
+VERSION=master
+BUILD_DIR=/Users/josecoll/IdeaProjects/r3corda
 SAMPLES_ROOT=/Users/josecoll/IdeaProjects
-STAGING_DIR=/Users/josecoll/IdeaProjects/corda-reviews/experimental/behave/deps/corda/corda-master
+STAGING_DIR=/Users/josecoll/IdeaProjects/corda-reviews/experimental/behave/deps/corda/r3corda-${VERSION}
 
 # Set up directories
-rm -rf ${STAGING_DIR}
 mkdir -p ${STAGING_DIR}/apps
+mkdir -p ${STAGING_DIR}/proxy
 
 # Corda repository pinned cordapps
 cd $BUILD_DIR
 
 ./gradlew samples:simm-valuation-demo:jar
-cp -v $(ls samples/simm-valuation-demo/build/libs/simm-valuation-demo-*.jar | tail -n1) ${STAGING_DIR}/apps/simm-valuation-demo.jar
-cp -v $(ls samples/simm-valuation-demo/build/libs/simm-valuation-demo-*.jar | tail -n1) ${STAGING_DIR}/proxy/simm-valuation-demo.jar
+cp -v $(ls samples/simm-valuation-demo/build/libs/simm-valuation-demo-*.jar | tail -n1) ${STAGING_DIR}/apps
 
 # Independent cordapps
 cd $SAMPLES_ROOT
