e57958330243ece6ead4db2c2274871db0d39ee1
josecoll <jose.coll@r3cev.com>
2018-03-28 12:25:57 +0100

    Modularisation completed including scripts to execute scenarios with new ScenarioRunner.

--- experimental/behave/src/scenario/resources/features/cash/currencies.feature
+++ experimental/behave/src/scenario/resources/features/cash/currencies.feature
@@ -3,19 +3,23 @@ Feature: Cash - Issuable Currencies
   To have cash on ledger, certain nodes must have the ability to issue cash of various currencies.
 
   Scenario: Node can issue no currencies by default
-    Given a node PartyA of version master
+    Given a node PartyA of version <Node-Version>
     And node PartyA has the finance app installed
     When the network is ready
     Then node PartyA has 0 issuable currencies
 
-  Scenario: Node can issue a currency
-    Given a node PartyA of version master
-    And node PartyA can issue USD
+  Scenario: Node has an issuable currency
+    Given a node PartyA of version <Node-Version>
+    And node PartyA has the finance app installed
     When the network is ready
     Then node PartyA has 1 issuable currency
 
   Scenario: Node can issue a currency
-    Given a node PartyA of version master
-    And node PartyA can issue USD
+    Given a node PartyA of version <Node-Version>
+    And node PartyA has the finance app installed
     When the network is ready
     Then node PartyA can issue 100 USD
+
+  Examples:
+  | Node-Version    |
+  | master          |
--- samples/simm-valuation-demo/src/system-test/scenario/kotlin/net/corda/vega/scenarios/SIMMValuationStepsProvider.kt
+++ samples/simm-valuation-demo/src/system-test/scenario/kotlin/net/corda/vega/scenarios/SIMMValuationStepsProvider.kt
@@ -1,31 +1,14 @@
 package net.corda.vega.scenarios
 
-import net.corda.behave.scenarios.StepsContainer
-import net.corda.behave.scenarios.StepsProvider
-import net.corda.vega.scenarios.helpers.SIMMValuation
+import net.corda.behave.scenarios.api.StepsBlock
+import net.corda.behave.scenarios.api.StepsProvider
+import net.corda.vega.scenarios.steps.SimmValuationSteps
 
-//class SIMMValuationStepsProvider : StepsProvider {
-//
-//    override val name: String
-//        get() = SIMMValuationStepsProvider::javaClass.name
-//
-//    override val stepsDefinition: (StepsBlock) -> Unit
-//        get() = ::simmValuationSteps
-//}
-//
-//fun simmValuationSteps(steps: StepsBlock) = steps {
-//
-//    val simmValuation = SIMMValuation(state)
-//
-//    Then<String,String>("^node (\\w+) can trade with node (\\w+)$") { nodeA, nodeB ->
-//        simmValuation.trade(nodeA, nodeB)
-//    }
-//
-//    Then<String>("^node (\\w+) can run portfolio valuation$") { node ->
-//        simmValuation.runValuation(node)
-//    }
-//
-//    Then<String,Long>("^node (\\w+) portfolio valuation is (\\d+)$") { _, value ->
-//        simmValuation.checkValuation(value)
-//    }
-//}
+class SIMMValuationStepsProvider : StepsProvider {
+
+    override val name: String
+        get() = SIMMValuationStepsProvider::javaClass.name
+
+    override val stepsDefinition: StepsBlock
+        get() = SimmValuationSteps()
+}
