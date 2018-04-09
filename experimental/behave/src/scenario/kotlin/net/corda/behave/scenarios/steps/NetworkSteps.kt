81246bfe6448822833668c2211e75b2816fc4742
Maksymilian Pawlak <120831+m4ksio@users.noreply.github.com>
2018-04-09 11:05:11 +0100

    Additional fixes

--- experimental/behave/src/scenario/kotlin/net/corda/behave/scenarios/steps/NetworkSteps.kt
+++ experimental/behave/src/scenario/kotlin/net/corda/behave/scenarios/steps/NetworkSteps.kt
@@ -2,12 +2,17 @@ package net.corda.behave.scenarios.steps
 
 import net.corda.behave.scenarios.ScenarioState
 import net.corda.behave.scenarios.api.StepsBlock
+import net.corda.core.utilities.minutes
 
 class NetworkSteps : StepsBlock {
 
     override fun initialize(state: ScenarioState) {
         When("^the network is ready$") {
             state.ensureNetworkIsRunning()
+        }
+
+        When<Int>("^the network is ready within (\\d+) minutes$") { minutes ->
+            state.ensureNetworkIsRunning(minutes.minutes)
         }
     }
 }
