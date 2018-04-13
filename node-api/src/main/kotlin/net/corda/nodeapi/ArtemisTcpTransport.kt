1775d60989a8240ee208c0e8b27435aabe04d066
Christian Sailer <christian.sailer@r3.com>
2018-04-13 15:19:29 +0100

    CORDA-1336: put in a comment explaining the config setting.

--- node-api/src/main/kotlin/net/corda/nodeapi/ArtemisTcpTransport.kt
+++ node-api/src/main/kotlin/net/corda/nodeapi/ArtemisTcpTransport.kt
@@ -66,6 +66,8 @@ class ArtemisTcpTransport {
                     TransportConstants.PROTOCOLS_PROP_NAME to "CORE,AMQP",
                     TransportConstants.USE_GLOBAL_WORKER_POOL_PROP_NAME to (nodeSerializationEnv != null),
                     TransportConstants.REMOTING_THREADS_PROPNAME to (if (nodeSerializationEnv != null) -1 else 1),
+                    // turn off direct delivery in Artemis - this is latency optimisation that can lead to
+                    //hick-ups under high load (CORDA-1336)
                     TransportConstants.DIRECT_DELIVER to false
             )
 
