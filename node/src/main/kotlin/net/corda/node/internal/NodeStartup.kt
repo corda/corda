07e03832709a32041a174c0bdb7f845a00107634
Shams Asari <shams.asari@r3.com>
2018-04-30 15:53:21 +0100

    Introducing AbstractArgsParser which removes the boilerplate of printing help and exiting the process on cmd line errors.

--- node/src/main/kotlin/net/corda/node/internal/NodeStartup.kt
+++ node/src/main/kotlin/net/corda/node/internal/NodeStartup.kt
@@ -1,7 +1,6 @@
 package net.corda.node.internal
 
 import com.jcabi.manifests.Manifests
-import joptsimple.OptionException
 import net.corda.core.internal.Emoji
 import net.corda.core.internal.concurrent.thenMatch
 import net.corda.core.internal.createDirectories
@@ -29,14 +28,13 @@ import java.net.InetAddress
 import java.nio.file.Path
 import java.nio.file.Paths
 import java.util.*
-import kotlin.system.exitProcess
 
 /** This class is responsible for starting a Node from command line arguments. */
 open class NodeStartup(val args: Array<String>) {
     companion object {
         private val logger by lazy { loggerFor<Node>() } // I guess this is lazy to allow for logging init, but why Node?
-        val LOGS_DIRECTORY_NAME = "logs"
-        val LOGS_CAN_BE_FOUND_IN_STRING = "Logs can be found in"
+        const val LOGS_DIRECTORY_NAME = "logs"
+        const val LOGS_CAN_BE_FOUND_IN_STRING = "Logs can be found in"
     }
 
     /**
@@ -49,7 +47,7 @@ open class NodeStartup(val args: Array<String>) {
             println("Corda will now exit...")
             return false
         }
-        val (argsParser, cmdlineOptions) = parseArguments()
+        val cmdlineOptions = NodeArgsParser().parseOrExit(*args)
 
         // We do the single node check before we initialise logging so that in case of a double-node start it
         // doesn't mess with the running node's logs.
@@ -63,12 +61,6 @@ open class NodeStartup(val args: Array<String>) {
             println("${versionInfo.vendor} ${versionInfo.releaseVersion}")
             println("Revision ${versionInfo.revision}")
             println("Platform Version ${versionInfo.platformVersion}")
-            return true
-        }
-
-        // Maybe render command line help.
-        if (cmdlineOptions.help) {
-            argsParser.printHelp(System.out)
             return true
         }
 
@@ -244,18 +236,6 @@ open class NodeStartup(val args: Array<String>) {
         val ourProcessID: String = ManagementFactory.getRuntimeMXBean().name.split("@")[0]
         pidFileRw.setLength(0)
         pidFileRw.write(ourProcessID.toByteArray())
-    }
-
-    private fun parseArguments(): Pair<ArgsParser, CmdLineOptions> {
-        val argsParser = ArgsParser()
-        val cmdlineOptions = try {
-            argsParser.parse(*args)
-        } catch (ex: OptionException) {
-            println("Invalid command line arguments: ${ex.message}")
-            argsParser.printHelp(System.out)
-            exitProcess(1)
-        }
-        return Pair(argsParser, cmdlineOptions)
     }
 
     open protected fun initLogging(cmdlineOptions: CmdLineOptions) {
