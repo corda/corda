00119265a07c7f0fc00bf16de55bbb0fb0e93e08
Viktor Kolomeyko <viktor.kolomeyko@r3.com>
2018-05-02 11:33:46 +0100

    CORDA-1416: Add additional check validate serialized form.

--- finance/src/integration-test/kotlin/net/corda/finance/compat/CompatibilityTest.kt
+++ finance/src/integration-test/kotlin/net/corda/finance/compat/CompatibilityTest.kt
@@ -2,6 +2,7 @@ package net.corda.finance.compat
 
 import net.corda.core.serialization.SerializationDefaults
 import net.corda.core.serialization.deserialize
+import net.corda.core.serialization.serialize
 import net.corda.core.transactions.SignedTransaction
 import net.corda.finance.contracts.asset.Cash
 import net.corda.testing.core.SerializationEnvironmentRule
@@ -12,7 +13,7 @@ import kotlin.test.assertNotNull
 import kotlin.test.assertTrue
 
 // TODO: If this type of testing gets momentum, we can create a mini-framework that rides through list of files
-// and performs necessary validation on all of them
+// and performs necessary validation on all of them.
 class CompatibilityTest {
 
     @Rule
@@ -23,11 +24,15 @@ class CompatibilityTest {
     fun issueCashTansactionReadTest() {
         val inputStream = javaClass.classLoader.getResourceAsStream("compatibilityData/v3/node_transaction.dat")
         assertNotNull(inputStream)
-        val byteArray: ByteArray = inputStream.readBytes()
-        val transaction = byteArray.deserialize<SignedTransaction>(context = SerializationDefaults.STORAGE_CONTEXT)
+        val inByteArray: ByteArray = inputStream.readBytes()
+        val transaction = inByteArray.deserialize<SignedTransaction>(context = SerializationDefaults.STORAGE_CONTEXT)
         assertNotNull(transaction)
         val commands = transaction.tx.commands
         assertEquals(1, commands.size)
         assertTrue(commands.first().value is Cash.Commands.Issue)
+
+        // Serialize back and check that representation is byte-to-byte identical to what it was originally.
+        val serializedForm = transaction.serialize(context = SerializationDefaults.STORAGE_CONTEXT)
+        assertTrue(inByteArray.contentEquals(serializedForm.bytes))
     }
 }
