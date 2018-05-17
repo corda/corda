53110ad63caf6f668f1f36f8397bef259614f918
sollecitom <michele.sollecito@r3.com>
2018-05-17 15:59:46 +0100

    Self-review changes.

--- client/rpc/src/main/kotlin/net/corda/client/rpc/PermissionException.kt
+++ client/rpc/src/main/kotlin/net/corda/client/rpc/PermissionException.kt
@@ -2,10 +2,9 @@ package net.corda.client.rpc
 
 import net.corda.core.CordaRuntimeException
 import net.corda.core.flows.ClientRelevantError
-import net.corda.nodeapi.exceptions.RpcSerializableError
 
 /**
  * Thrown to indicate that the calling user does not have permission for something they have requested (for example
  * calling a method).
  */
-class PermissionException(message: String) : CordaRuntimeException(message), RpcSerializableError, ClientRelevantError
+class PermissionException(message: String) : CordaRuntimeException(message), ClientRelevantError
