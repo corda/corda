5fb6508c20e96b6692df94961a65b9fcbf6457a3
rick.parker <rick.parker@r3cev.com>
2018-05-24 18:17:37 +0100

    AppendOnlyPersistentMap comments.

--- node/src/main/kotlin/net/corda/node/utilities/AppendOnlyPersistentMap.kt
+++ node/src/main/kotlin/net/corda/node/utilities/AppendOnlyPersistentMap.kt
@@ -14,9 +14,10 @@ import java.util.concurrent.atomic.AtomicReference
 
 
 /**
- * Implements a caching layer on top of an *append-only* table accessed via Hibernate mapping. Note that if the same key is [set] twice the
- * behaviour is unpredictable! There is a best-effort check for double inserts, but this should *not* be relied on, so
- * ONLY USE THIS IF YOUR TABLE IS APPEND-ONLY
+ * Implements a caching layer on top of an *append-only* table accessed via Hibernate mapping. Note that if the same key is [set] twice,
+ * typically this will result in a duplicate insert if this is racing with another transaction.  The flow framework will then retry.
+ *
+ * This class relies heavily on the fact that compute operations in the cache are atomic for a particular key.
  */
 abstract class AppendOnlyPersistentMapBase<K, V, E, out EK>(
         val toPersistentEntityKey: (K) -> EK,
@@ -55,21 +56,26 @@ abstract class AppendOnlyPersistentMapBase<K, V, E, out EK>(
     }
 
     private fun set(key: K, value: V, logWarning: Boolean, store: (K, V) -> V?): Boolean {
+        // Will be set to true if store says it isn't in the database.
         var isUnique = false
         cache.asMap().compute(key) { _, oldValue ->
+            // Always write to the database, unless we can see it's already committed.
             when (oldValue) {
                 is Transactional.InFlight<*, V> -> {
+                    // Someone else is writing, so store away!
                     // TODO: we can do collision detection here and prevent it happening in the database.  But we also have to do deadlock detection, so a bit of work.
                     isUnique = (store(key, value) == null)
                     oldValue.apply { alsoWrite(value) }
                 }
-                is Transactional.Committed<V> -> oldValue
+                is Transactional.Committed<V> -> oldValue // The value is already globally visible and cached.  So do nothing since the values are always the same.
                 else -> {
-                    // Null or Missing
+                    // Null or Missing.  Store away!
                     isUnique = (store(key, value) == null)
                     if (!isUnique && !weAreWriting(key)) {
+                        // If we found a value already in the database, and we were not already writing, then it's already committed but got evicted.
                         Transactional.Committed(value)
                     } else {
+                        // Some database transactions, including us, writing, with readers seeing whatever is in the database and writers seeing the (in memory) value.
                         Transactional.InFlight(this, key, { loadValue(key) }).apply { alsoWrite(value) }
                     }
                 }
@@ -94,7 +100,8 @@ abstract class AppendOnlyPersistentMapBase<K, V, E, out EK>(
 
     /**
      * Associates the specified value with the specified key in this map and persists it.
-     * If the map previously contained a mapping for the key, the old value is not replaced.
+     * If the map previously contained a committed mapping for the key, the old value is not replaced.  It may throw an error from the
+     * underlying storage if this races with another database transaction to store a value for the same key.
      * @return true if added key was unique, otherwise false
      */
     fun addWithDuplicatesAllowed(key: K, value: V, logWarning: Boolean = true): Boolean =
@@ -134,9 +141,11 @@ abstract class AppendOnlyPersistentMapBase<K, V, E, out EK>(
         cache.invalidateAll()
     }
 
+    // Helpers to know if transaction(s) are currently writing the given key.
     protected fun weAreWriting(key: K): Boolean = pendingKeys.get(key)?.contains(contextTransaction) ?: false
     protected fun anyoneWriting(key: K): Boolean = pendingKeys.get(key)?.isNotEmpty() ?: false
 
+    // Indicate this database transaction is a writer of this key.
     private fun addPendingKey(key: K, databaseTransaction: DatabaseTransaction): Boolean {
         var added = true
         pendingKeys.compute(key) { k, oldSet ->
@@ -152,6 +161,7 @@ abstract class AppendOnlyPersistentMapBase<K, V, E, out EK>(
         return added
     }
 
+    // Remove this database transaction as a writer of this key, because the transaction committed or rolled back.
     private fun removePendingKey(key: K, databaseTransaction: DatabaseTransaction) {
         pendingKeys.compute(key) { k, oldSet ->
             if (oldSet == null) {
@@ -163,6 +173,12 @@ abstract class AppendOnlyPersistentMapBase<K, V, E, out EK>(
         }
     }
 
+    /**
+     * Represents a value in the cache, with transaction isolation semantics.
+     *
+     * There are 3 states.  Globally missing, globally visible, and being written in a transaction somewhere now or in
+     * the past (and it rolled back).
+     */
     sealed class Transactional<T> {
         abstract val value: T
         abstract val isPresent: Boolean
@@ -188,16 +204,26 @@ abstract class AppendOnlyPersistentMapBase<K, V, E, out EK>(
                 get() = null
         }
 
-        // Written in a transaction (uncommitted) somewhere.
+        // Written in a transaction (uncommitted) somewhere, but there's a small window when this might be seen after commit,
+        // hence the committed flag.
         class InFlight<K, T>(private val map: AppendOnlyPersistentMapBase<K, T, *, *>,
                              private val key: K,
                              private val _readerValueLoader: () -> T?,
                              private val _writerValueLoader: () -> T = { throw IllegalAccessException("No value loader provided") }) : Transactional<T>() {
+
+            // A flag to indicate this has now been committed, but hasn't yet been replaced with Committed.  This also
+            // de-duplicates writes of the Committed value to the cache.
             private val committed = AtomicBoolean(false)
+
+            // What to do if a non-writer needs to see the value and it hasn't yet been committed to the database.
+            // Can be updated into a no-op once evaluated.
             private val readerValueLoader = AtomicReference<() -> T?>(_readerValueLoader)
+            // What to do if a writer needs to see the value and it hasn't yet been committed to the database.
+            // Can be updated into a no-op once evaluated.
             private val writerValueLoader = AtomicReference<() -> T>(_writerValueLoader)
 
             fun alsoWrite(_value: T) {
+                // Make the lazy loader the writers see actually just return the value that has been set.
                 writerValueLoader.set({ _value })
                 // We make all these vals so that the lambdas do not need a reference to this, and so the onCommit only has a weak ref to the value.
                 // We want this so that the cache could evict the value (due to memory constraints etc) without the onCommit callback
@@ -208,6 +234,8 @@ abstract class AppendOnlyPersistentMapBase<K, V, E, out EK>(
                 val strongComitted = committed
                 val strongMap = map
                 if (map.addPendingKey(key, tx)) {
+                    // If the transaction commits, update cache to make globally visible if we're first for this key,
+                    // and then stop saying the transaction is writing the key.
                     tx.onCommit {
                         if (strongComitted.compareAndSet(false, true)) {
                             val dereferencedKey = strongKey
@@ -218,12 +246,15 @@ abstract class AppendOnlyPersistentMapBase<K, V, E, out EK>(
                         }
                         strongMap.removePendingKey(strongKey, tx)
                     }
+                    // If the transaction rolls back, stop saying this transaction is writing the key.
                     tx.onRollback {
                         strongMap.removePendingKey(strongKey, tx)
                     }
                 }
             }
 
+            // Lazy load the value a "writer" would see.  If the original loader hasn't been replaced, replace it
+            // with one that just returns the value once evaluated.
             private fun loadAsWriter(): T {
                 val _value = writerValueLoader.get()()
                 if (writerValueLoader.get() == _writerValueLoader) {
@@ -232,6 +263,8 @@ abstract class AppendOnlyPersistentMapBase<K, V, E, out EK>(
                 return _value
             }
 
+            // Lazy load the value a "reader" would see.  If the original loader hasn't been replaced, replace it
+            // with one that just returns the value once evaluated.
             private fun loadAsReader(): T? {
                 val _value = readerValueLoader.get()()
                 if (readerValueLoader.get() == _readerValueLoader) {
@@ -240,15 +273,20 @@ abstract class AppendOnlyPersistentMapBase<K, V, E, out EK>(
                 return _value
             }
 
+            // Whether someone reading (only) can see the entry.
             private val isPresentAsReader: Boolean get() = (loadAsReader() != null)
+            // Whether the entry is already written and committed, or we are writing it (and thus it can be seen).
             private val isPresentAsWriter: Boolean get() = committed.get() || map.weAreWriting(key)
 
             override val isPresent: Boolean
                 get() = isPresentAsWriter || isPresentAsReader
 
+            // If it is committed or we are writing, reveal the value, potentially lazy loading from the database.
+            // If none of the above, see what was already in the database, potentially lazily.
             override val value: T
                 get() = if (isPresentAsWriter) loadAsWriter() else if (isPresentAsReader) loadAsReader()!! else throw NoSuchElementException("Not present")
 
+            // The value from the perspective of the eviction algorithm of the cache.  i.e. we want to reveal memory footprint to it etc.
             override val valueWithoutIsolation: T?
                 get() = if (writerValueLoader.get() != _writerValueLoader) writerValueLoader.get()() else if (readerValueLoader.get() != _writerValueLoader) readerValueLoader.get()() else null
         }
@@ -271,23 +309,35 @@ open class AppendOnlyPersistentMap<K, V, E, out EK>(
     override val cache = NonInvalidatingCache<K, Transactional<V>>(
             bound = cacheBound,
             loadFunction = { key: K ->
+                // This gets called if a value is read and the cache has no Transactional for this key yet.
                 val value: V? = loadValue(key)
                 if (value == null) {
+                    // No visible value
                     if (anyoneWriting(key)) {
+                        // If someone is writing (but not us)
+                        // For those not writing, the value cannot be seen.
+                        // For those writing, they need to re-load the value from the database (which their database transaction CAN see).
                         Transactional.InFlight<K, V>(this, key, { null }, { loadValue(key)!! })
                     } else {
+                        // If no one is writing, then the value does not exist.
                         Transactional.Missing<V>()
                     }
                 } else {
+                    // A value was found
                     if (weAreWriting(key)) {
+                        // If we are writing, it might not be globally visible, and was evicted from the cache.
+                        // For those not writing, they need to check the database again.
+                        // For those writing, they can see the value found.
                         Transactional.InFlight<K, V>(this, key, { loadValue(key) }, { value })
                     } else {
+                        // If no one is writing, then make it globally visible.
                         Transactional.Committed<V>(value)
                     }
                 }
             })
 }
 
+// Same as above, but with weighted values (e.g. memory footprint sensitive).
 class WeightBasedAppendOnlyPersistentMap<K, V, E, out EK>(
         toPersistentEntityKey: (K) -> EK,
         fromPersistentEntity: (E) -> Pair<K, V>,
