ab43344d9f0dd60e7bfcbbf0aec237d69601c269
Tudor Malene <tudor.malene@r3.com>
2018-05-31 14:59:26 +0100

    CORDA-1477 attempt to fix tests

--- node/src/main/kotlin/net/corda/node/internal/cordapp/CordappLoader.kt
+++ node/src/main/kotlin/net/corda/node/internal/cordapp/CordappLoader.kt
@@ -135,7 +135,7 @@ class CordappLoader private constructor(private val cordappJarPaths: List<Restri
             return this::class.java.classLoader.getResources(resource)
                     .asSequence()
                     // This is to only scan classes from test folders.
-                    .filter { url -> listOf("main", "production").none { url.toString().contains("$it/$resource") } || listOf("net.corda.node", "net.corda.core").none { scanPackage.startsWith(it) } }
+                    .filter { url -> listOf("main", "production").none { url.toString().contains("$it/$resource") } || listOf("net.corda.core", "net.corda.node", "net.corda.finance").none { scanPackage.startsWith(it) } }
                     .map { url ->
                         if (url.protocol == "jar") {
                             // When running tests from gradle this may be a corda module jar, so restrict to scanPackage:
