de46b61a8032aeb817235314011100843a768443
Michal Kit <michal.kit@r3.com>
2018-06-06 15:51:19 +0100

    CORDA-1539 Extracting the whitelist logic and making it public

---
+++ node-api/src/main/kotlin/net/corda/nodeapi/internal/network/WhitelisGenerator.kt
@@ -0,0 +1,41 @@
+package net.corda.nodeapi.internal.network
+
+import net.corda.core.contracts.ContractClassName
+import net.corda.core.internal.div
+import net.corda.core.internal.exists
+import net.corda.core.internal.readAllLines
+import net.corda.core.internal.toMultiMap
+import net.corda.core.node.NetworkParameters
+import net.corda.core.node.services.AttachmentId
+import net.corda.nodeapi.internal.ContractsJar
+import java.nio.file.Path
+
+private const val EXCLUDE_WHITELIST_FILE_NAME = "exclude_whitelist.txt"
+
+fun generateWhitelist(networkParameters: NetworkParameters?,
+                      excludeContracts: List<ContractClassName>,
+                      cordappJars: List<ContractsJar>): Map<ContractClassName, List<AttachmentId>> {
+    val existingWhitelist = networkParameters?.whitelistedContractImplementations ?: emptyMap()
+
+    if (excludeContracts.isNotEmpty()) {
+        println("Exclude contracts from whitelist: ${excludeContracts.joinToString()}")
+        existingWhitelist.keys.forEach {
+            require(it !in excludeContracts) { "$it is already part of the existing whitelist and cannot be excluded." }
+        }
+    }
+
+    val newWhiteList = cordappJars
+            .flatMap { jar -> (jar.scan() - excludeContracts).map { it to jar.hash } }
+            .toMultiMap()
+
+    return (newWhiteList.keys + existingWhitelist.keys).associateBy({ it }) {
+        val existingHashes = existingWhitelist[it] ?: emptyList()
+        val newHashes = newWhiteList[it] ?: emptyList()
+        (existingHashes + newHashes).distinct()
+    }
+}
+
+fun readExcludeWhitelist(directory: Path): List<String> {
+    val file = directory / EXCLUDE_WHITELIST_FILE_NAME
+    return if (file.exists()) file.readAllLines().map(String::trim) else emptyList()
+}
