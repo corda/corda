5af3b1b8f91b15fdab7d8d431560a581c2665c50
Tudor Malene <tudor.malene@r3.com>
2018-06-07 11:14:49 +0100

    ENT-2036 Handle ClosedChannelException during SSL handshake

--- node-api/src/main/kotlin/net/corda/nodeapi/internal/persistence/CordaPersistence.kt
+++ node-api/src/main/kotlin/net/corda/nodeapi/internal/persistence/CordaPersistence.kt
@@ -265,3 +265,5 @@ private fun Throwable.hasSQLExceptionCause(): Boolean =
             is SQLException -> true
             else -> cause?.hasSQLExceptionCause() ?: false
         }
+
+class CouldNotCreateDatasource(override val message: String?, override val cause: Throwable?) : Exception()
