999ee4980494f5012fa0f15d145bf667142cc0fc
Rick Parker <rick.parker@r3.com>
2018-06-15 16:57:49 +0100

    ENT-2053 Fix messaging TTL related settings causing disconnects (#3384)
    * ENT-2053 Revert RPC TTL due to hanging RPC clients.
    
    * ENT-2053 Set TTL correctly.

--- node/src/main/kotlin/net/corda/node/services/messaging/P2PMessagingClient.kt
+++ node/src/main/kotlin/net/corda/node/services/messaging/P2PMessagingClient.kt
@@ -142,7 +142,7 @@ class P2PMessagingClient(val config: NodeConfiguration,
                 // Never time out on our loopback Artemis connections. If we switch back to using the InVM transport this
                 // would be the default and the two lines below can be deleted.
                 connectionTTL = 60000
-                clientFailureCheckPeriod = -1
+                clientFailureCheckPeriod = 30000
                 minLargeMessageSize = maxMessageSize + JOURNAL_HEADER_SIZE
                 isUseGlobalPools = nodeSerializationEnv != null
             }
