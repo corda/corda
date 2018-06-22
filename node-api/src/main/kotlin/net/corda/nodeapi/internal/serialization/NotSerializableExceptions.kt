5be8c9a102c493de128a6a4cd7f9fda99aef76d4
Anthony Keenan <anthony.keenan@r3.com>
2018-06-22 15:53:18 +0100

    CORDA-1498 - serialization multiple transform bug (#3391)
    * CORDA-1498: serialization multiple transform bug (#3216)
    
    * Fix issue when evolving enums with transformation chains
    
    * Regenerate test data for deserializeWithRename test and unignore
    
    * Further tweaks / remove debugging
    
    * Formatting tweaks
    
    * Address review comments
    
    * Remove debug
    
    * Add classname to serialization tranform exceptions
    
    * Use direct node links instead of indexes to improve readability
    
    * More readability tweaks
    
    * More readability improvements
    
    * rename require to requireThat to resolve conflict with kotlin libraries
    
    * Add logging of error message
    
    * Change requireThat helper to inline function
    
    * remove unneeded toString
    
    * Further tweaks
    
    * Change NotSerializableException to more generic IOException
    
    * Make exception context clearer
    
    # Conflicts:
    #	node-api/src/test/kotlin/net/corda/nodeapi/internal/serialization/amqp/EnumEvolvabilityTests.kt
    #	serialization/src/test/resources/net/corda/serialization/internal/amqp/EnumEvolveTests.deserializeWithRename.1.C
    #	serialization/src/test/resources/net/corda/serialization/internal/amqp/EnumEvolveTests.deserializeWithRename.2.C
    #	serialization/src/test/resources/net/corda/serialization/internal/amqp/EnumEvolveTests.deserializeWithRename.3.C
    
    * Fix merge conflicts
    
    * Fix broken test
    
    * Revert changes to serialized classes

---
+++ node-api/src/main/kotlin/net/corda/nodeapi/internal/serialization/NotSerializableExceptions.kt
@@ -0,0 +1,14 @@
+package net.corda.nodeapi.internal.serialization
+
+import java.io.IOException
+import java.io.NotSerializableException
+
+class NotSerializableDetailedException(classname: String?, val reason: String) : NotSerializableException(classname) {
+    override fun toString(): String {
+        return "Unable to serialize/deserialize $message: $reason"
+    }
+}
+
+// This exception is thrown when serialization isn't possible but at the point the exception
+// is thrown the classname isn't known. It's caught and rethrown as a [NotSerializableDetailedException]
+class NotSerializableWithReasonException(message: String?): IOException(message)
