547677483c99428e40e09d681c1ee340c895dfbe
Stefano Franz <stefano.franz@r3.com>
2018-06-22 14:50:55 +0100

    add ability to filter checked counterparties to specific x500

--- samples/network-verifier/src/main/kotlin/net/corda/verification/TestCommsFlow.kt
+++ samples/network-verifier/src/main/kotlin/net/corda/verification/TestCommsFlow.kt
@@ -1,4 +1,4 @@
-package net.corda.configsample
+package net.corda.verification
 
 import co.paralleluniverse.fibers.Suspendable
 import net.corda.core.contracts.CommandData
@@ -6,6 +6,8 @@ import net.corda.core.contracts.Contract
 import net.corda.core.contracts.ContractState
 import net.corda.core.flows.*
 import net.corda.core.identity.AbstractParty
+import net.corda.core.identity.CordaX500Name
+import net.corda.core.identity.Party
 import net.corda.core.serialization.CordaSerializable
 import net.corda.core.transactions.LedgerTransaction
 import net.corda.core.transactions.TransactionBuilder
@@ -15,7 +17,7 @@ import net.corda.core.utilities.unwrap
 
 @StartableByRPC
 @InitiatingFlow
-class TestCommsFlowInitiator : FlowLogic<List<String>>() {
+class TestCommsFlowInitiator(val x500Name: CordaX500Name? = null) : FlowLogic<List<String>>() {
 
     object SENDING : ProgressTracker.Step("SENDING")
     object RECIEVED_ALL : ProgressTracker.Step("RECIEVED_ALL")
@@ -26,28 +28,29 @@ class TestCommsFlowInitiator : FlowLogic<List<String>>() {
     @Suspendable
     override fun call(): List<String> {
         progressTracker.currentStep = SENDING
-        val responses = serviceHub.networkMapCache.allNodes.filter {
-            it.legalIdentities.first() != serviceHub.myInfo.legalIdentities.first()
-        }.filterNot {
-            it.legalIdentities.first() in serviceHub.networkMapCache.notaryIdentities
-        }.map {
+        val responses = serviceHub.networkMapCache.allNodes.map {
             it.legalIdentities.first()
-        }.map {
-            val initiateFlow = initiateFlow(it)
-            initiateFlow.receive<String>().unwrap { it }
-        }.toList().also {
-            progressTracker.currentStep = RECIEVED_ALL
-        }
-
+        }.filterNot {
+            it in serviceHub.myInfo.legalIdentities
+        }.filterNot {
+            it in serviceHub.networkMapCache.notaryIdentities
+        }.filter(::matchesX500)
+                .map {
+                    val initiateFlow = initiateFlow(it)
+                    initiateFlow.receive<String>().unwrap { it }
+                }.toList().also {
+                    progressTracker.currentStep = RECIEVED_ALL
+                }
         val tx = TransactionBuilder(notary = serviceHub.networkMapCache.notaryIdentities.first())
         tx.addOutputState(CommsTestState(responses, serviceHub.myInfo.legalIdentities.first()), CommsTestContract::class.qualifiedName!!)
         tx.addCommand(CommsTestCommand, serviceHub.myInfo.legalIdentities.first().owningKey)
-
         val signedTx = serviceHub.signInitialTransaction(tx)
-
         subFlow(FinalityFlow(signedTx))
-
         return responses
+    }
+
+    fun matchesX500(it: Party): Boolean {
+        return x500Name?.equals(it.name) ?: true
     }
 }
 
