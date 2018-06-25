e5d0b13231c771aa9eab4c4918e1273c2f8070c7
Sean Zhang <sean.zhang@wellsfargo.com>
2018-06-24 23:06:18 -0700

    IdentitySyncFlowWrapper

---
+++ confidential-identities/src/main/kotlin/net/corda/confidential/IdentitySyncFlowWrapper.kt
@@ -0,0 +1,36 @@
+package net.corda.confidential
+
+import co.paralleluniverse.fibers.Suspendable
+import net.corda.core.flows.FlowLogic
+import net.corda.core.flows.FlowSession
+import net.corda.core.flows.InitiatedBy
+import net.corda.core.flows.InitiatingFlow
+import net.corda.core.identity.Party
+import net.corda.core.transactions.WireTransaction
+import net.corda.core.utilities.unwrap
+
+/**
+ * A simple wrapper of IdentitySyncFlow to make it standalone with its own flow session.
+ * It turns out to be the same as the wrapper in IdentitySyncFlowTests.
+ */
+object IdentitySyncFlowWrapper {
+    @InitiatingFlow
+    class Initiator(val otherParty: Party,
+                    val tx: WireTransaction) : FlowLogic<Boolean>() {
+        @Suspendable
+        override fun call(): Boolean {
+            val otherSideSession = initiateFlow(otherParty)
+            subFlow(IdentitySyncFlow.Send(otherSideSession, tx))
+            return otherSideSession.receive<Boolean>().unwrap { it }
+        }
+    }
+
+    @InitiatedBy(Initiator::class)
+    class Receive(val otherSideSession: FlowSession) : FlowLogic<Unit>() {
+        @Suspendable
+        override fun call() {
+            subFlow(IdentitySyncFlow.Receive(otherSideSession))
+            otherSideSession.send(true)
+        }
+    }
+}
