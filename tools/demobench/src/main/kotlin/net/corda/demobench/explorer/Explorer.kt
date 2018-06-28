a75f4c25bf68d1add3ded1d402a3e12044d655bf
Chris Rankin <chris.rankin@r3.com>
2018-06-28 14:21:00 +0100

    Ensure that DemoBench's RPC connections terminate on shutdown.

--- tools/demobench/src/main/kotlin/net/corda/demobench/explorer/Explorer.kt
+++ tools/demobench/src/main/kotlin/net/corda/demobench/explorer/Explorer.kt
@@ -12,6 +12,7 @@ import net.corda.demobench.readErrorLines
 import tornadofx.*
 import java.io.IOException
 import java.nio.file.Files
+import java.nio.file.Path
 import java.nio.file.StandardCopyOption.REPLACE_EXISTING
 import java.util.concurrent.Executors
 
@@ -123,7 +124,7 @@ class Explorer internal constructor(private val explorerController: ExplorerCont
 
 class ExplorerController : Controller() {
     private val jvm by inject<JVMConfig>()
-    private val explorerPath = jvm.applicationDir.resolve("explorer").resolve("node-explorer.jar")
+    private val explorerPath: Path = jvm.applicationDir.resolve("explorer").resolve("node-explorer.jar")
 
     init {
         log.info("Explorer JAR: $explorerPath")
