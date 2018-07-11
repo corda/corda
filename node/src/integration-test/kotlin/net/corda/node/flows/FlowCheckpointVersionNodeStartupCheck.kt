aa3e5d4899678f902b469638ce59c5b74d76f20e
szymonsztuka <szymon.sztuka@yahoo.com>
2018-07-11 13:19:00 +0100

    Comments, tiding up.

--- node/src/integration-test/kotlin/net/corda/node/flows/FlowCheckpointVersionNodeStartupCheck.kt
+++ node/src/integration-test/kotlin/net/corda/node/flows/FlowCheckpointVersionNodeStartupCheck.kt
@@ -33,7 +33,9 @@ class FlowCheckpointVersionNodeStartupCheck {
     fun `restart nodes with incompatible version of sunspended flow - different jar name`() {
 
         val user = User("mark", "dadada", setOf(Permissions.startFlow<SendMessageFlow>(), Permissions.invokeRpc("vaultQuery"), Permissions.invokeRpc("vaultTrack")))
-        return driver(DriverParameters(isDebug = true, startNodesInProcess = false,
+        return driver(DriverParameters(isDebug = true,
+                startNodesInProcess = false, // start nodes in separate processes to ensure CordappLoader is not shared between restarts
+                inMemoryDB = false, // ensure database is persisted between node restarts so we can keep suspended flow in Bob's node
                 portAllocation = RandomFree, extraCordappPackagesToScan = listOf(MessageState::class.packageName, "net.test.cordapp.v1"))) {
             val logFolder = {
                 val alice = startNode(rpcUsers = listOf(user), providedName = ALICE_NAME).getOrThrow()
@@ -41,29 +43,26 @@ class FlowCheckpointVersionNodeStartupCheck {
                 alice.stop()
                 CordaRPCClient(bob.rpcAddress).start(user.username, user.password).use {
                     val flowTracker = it.proxy.startTrackedFlow(::SendMessageFlow, message, defaultNotaryIdentity, alice.nodeInfo.singleIdentity()).progress
-                    //wait until Bob progresses as far as possible because alice node is offline
+                    // wait until Bob progresses as far as possible because Alice node is offline
                     flowTracker.takeFirst { it == SendMessageFlow.Companion.FINALISING_TRANSACTION.label }.toBlocking().single()
                 }
-
                 val logFolder = bob.baseDirectory / NodeStartup.LOGS_DIRECTORY_NAME
-
+                // SendMessageFlow suspends in Bob node
                 bob.stop()
                 logFolder
             }()
 
+            //after nodes restart the package with the flow is loaded bvia a JAR filw with different name (a random UUID is added)
             startNode(rpcUsers = listOf(user), providedName = ALICE_NAME, customOverrides = mapOf("devMode" to false)).getOrThrow()
-            //assertFailsWith(CheckpointIncompatibleException.FlowNotInstalledException::class) {
+            assertFailsWith(net.corda.testing.node.internal.ListenProcessDeathException::class) {
+                //ALic node is up so Bob's suspended flow should restart, however this time is loaded from different JAR file
                 startNode(providedName = BOB_NAME, rpcUsers = listOf(user), customOverrides = mapOf("devMode" to false)).getOrThrow()
-            //}
+            }
 
             val logFile = logFolder.list { it.filter { it.fileName.toString().endsWith(".log") }.findAny().get() }
-            // We count the number of nodes that wrote into the logfile by counting "Logs can be found in"
-            //val numberOfNodesThatLogged = logFile.readLines { it.filter { "that is incompatible with the current installed version of" in it }.count() }
+            val expectedLogMessage = CheckpointIncompatibleException.FlowNotInstalledException(SendMessageFlow::class.java)
 
-            //val expectedLogMessage = CheckpointIncompatibleException.FlowNotInstalledException(SendMessageFlow::class)
-            val numberOfNodesThatLogged = logFile.readLines { it.filter {"Found checkpoint"/*"that is no longer installed"*/ in it }
-                    .map{ a-> a}
-                    .count() }
+            val numberOfNodesThatLogged = logFile.readLines { it.filter { expectedLogMessage.message in it }.count() }
             assertEquals(1, numberOfNodesThatLogged)
         }
     }
