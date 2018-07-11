0ceac42dae914243ca82b51e7999fd8b4645e4ae
szymonsztuka <szymon.sztuka@r3.com>
2018-07-11 17:27:57 +0100

    Revert removing db integration test support and update db schema names for BootTest.

--- node/src/integration-test/kotlin/net/corda/node/BootTests.kt
+++ node/src/integration-test/kotlin/net/corda/node/BootTests.kt
@@ -25,7 +25,7 @@ import net.corda.node.services.Permissions.Companion.startFlow
 import net.corda.nodeapi.exceptions.InternalNodeException
 import net.corda.testing.core.ALICE_NAME
 import net.corda.testing.core.BOB_NAME
-import net.corda.testing.core.DUMMY_NOTARY_NAME
+import net.corda.testing.core.DUMMY_BANK_A_NAME
 import net.corda.testing.driver.DriverParameters
 import net.corda.testing.driver.NodeHandle
 import net.corda.testing.driver.NodeParameters
@@ -44,7 +44,12 @@ import java.io.ObjectOutputStream
 import java.io.Serializable
 import kotlin.test.assertEquals
 
-class BootTests {
+class BootTests : IntegrationTest() {
+    companion object {
+        @ClassRule
+        @JvmField
+        val databaseSchemas = IntegrationTestSchemas(ALICE_NAME.toDatabaseSchemaName(), BOB_NAME.toDatabaseSchemaName(), DUMMY_BANK_A_NAME.toDatabaseSchemaName())
+    }
     @Test
     fun `java deserialization is disabled`() {
         driver(DriverParameters(notarySpecs = emptyList())) {
