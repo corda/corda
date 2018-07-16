2e3087e17a2ab3439019cccc5e4d05948c019e9d
Michele Sollecito <michele.sollecito@r3.com>
2018-07-16 16:44:05 +0100

    [CORDA-1799]: Fixed an issue with generated test cordapps.

--- node/src/integration-test/kotlin/net/corda/node/flows/AsymmetricCorDappsTests.kt
+++ node/src/integration-test/kotlin/net/corda/node/flows/AsymmetricCorDappsTests.kt
@@ -14,6 +14,7 @@ import net.corda.testing.driver.driver
 import org.junit.Test
 import kotlin.test.assertEquals
 
+// TODO sollecitom refactor or remove
 class AsymmetricCorDappsTests {
 
     @StartableByRPC
