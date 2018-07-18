2f2e5a71683e350857d09e4dc1f4e82fdf4c7d72
szymonsztuka <szymon.sztuka@yahoo.com>
2018-07-18 11:06:21 +0100

    DriverDSL startNode can remove content of it's specific cordapps folder (useful for when restarting with different jars), tests for CORDA-1543 Create an integration test for the flow checkpoint version safety feature

---
+++ node/src/integration-test/kotlin/net/corda/node/flows/FlowCheckpointVersionNodeStartupCheck.kt
@@ -0,0 +1,153 @@
+package net.corda.node.flows
+
+import net.corda.client.rpc.CordaRPCClient
+import net.corda.core.internal.div
+import net.corda.core.internal.list
+import net.corda.core.internal.readLines
+import net.corda.core.messaging.startTrackedFlow
+import net.corda.core.utilities.getOrThrow
+import net.corda.node.internal.CheckpointIncompatibleException
+import net.corda.node.internal.NodeStartup
+import net.corda.node.services.Permissions
+import net.corda.testMessage.Message
+import net.corda.testMessage.MessageState
+import net.corda.testing.core.ALICE_NAME
+import net.corda.testing.core.BOB_NAME
+import net.corda.testing.core.singleIdentity
+import net.corda.testing.driver.DriverParameters
+import net.corda.testing.driver.TestCorDapp
+import net.corda.testing.driver.driver
+import net.corda.testing.driver.internal.RandomFree
+import net.corda.testing.node.User
+import net.corda.testing.node.internal.ListenProcessDeathException
+import net.test.cordapp.v1.SendMessageFlow
+import org.junit.Test
+import java.util.concurrent.TimeUnit
+import kotlin.test.assertEquals
+import kotlin.test.assertFailsWith
+import kotlin.test.assertNotNull
+
+class FlowCheckpointVersionNodeStartupCheck {
+    companion object {
+        val message = Message("Hello world!")
+        val classes = setOf(net.corda.testMessage.MessageState::class.java,
+                net.corda.testMessage.MessageContract::class.java,
+                net.test.cordapp.v1.SendMessageFlow::class.java,
+                net.test.cordapp.v1.Record::class.java)
+        val user = User("mark", "dadada", setOf(Permissions.startFlow<SendMessageFlow>(), Permissions.invokeRpc("vaultQuery"), Permissions.invokeRpc("vaultTrack")))
+    }
+
+    @Test
+    fun `restart node sucesfully with sunspended flow`() {
+
+        val cordapps = setOf(TestCorDapp.Factory.create("testJar", "1.0", classes = classes))
+
+        return driver(DriverParameters(isDebug = true,  startNodesInProcess = false, inMemoryDB = false, portAllocation = RandomFree, corDappsForAllNodes = cordapps)) {
+            {
+                val alice = startNode(rpcUsers = listOf(user), providedName = ALICE_NAME).getOrThrow()
+                val bob = startNode(rpcUsers = listOf(user), providedName = BOB_NAME).getOrThrow()
+                alice.stop()
+                CordaRPCClient(bob.rpcAddress).start(user.username, user.password).use {
+                    val flowTracker = it.proxy.startTrackedFlow(::SendMessageFlow, message, defaultNotaryIdentity, alice.nodeInfo.singleIdentity()).progress
+                    //wait until Bob progresses as far as possible because alice node is off
+                    flowTracker.takeFirst { it == SendMessageFlow.Companion.FINALISING_TRANSACTION.label }.toBlocking().single()
+                }
+                bob.stop()
+            }()
+            val result = {
+                //Bob will resume the flow
+                val alice = startNode(rpcUsers = listOf(user), providedName = ALICE_NAME, customOverrides = mapOf("devMode" to false)).getOrThrow()
+                startNode(providedName = BOB_NAME, rpcUsers = listOf(user), customOverrides = mapOf("devMode" to false)).getOrThrow()
+                CordaRPCClient(alice.rpcAddress).start(user.username, user.password).use {
+                    val page = it.proxy.vaultTrack(MessageState::class.java)
+                    if (page.snapshot.states.isNotEmpty()) {
+                        page.snapshot.states.first()
+                    } else {
+                        val r = page.updates.timeout(5, TimeUnit.SECONDS).take(1).toBlocking().single()
+                        if (r.consumed.isNotEmpty()) r.consumed.first() else r.produced.first()
+                    }
+                }
+            }()
+            assertNotNull(result)
+            assertEquals(message, result.state.data.message)
+        }
+    }
+
+    private fun assertNodeRestartFailure(
+            cordapps:Set<TestCorDapp>?,
+            cordappsVersionAtStartup: Set<TestCorDapp>,
+            cordappsVersionAtRestart: Set<TestCorDapp>,
+            reuseAdditionalCorDappsAtRestart: Boolean,
+            assertNodeLogs: String
+    ) {
+
+        return driver(DriverParameters(
+                startNodesInProcess = false, // start nodes in separate processes to ensure CordappLoader is not shared between restarts
+                inMemoryDB = false, // ensure database is persisted between node restarts so we can keep suspended flow in Bob's node
+                portAllocation = RandomFree,
+                corDappsForAllNodes = cordapps)
+        ) {
+            val bobLogFolder = {
+                val alice = startNode(rpcUsers = listOf(user), providedName = ALICE_NAME, additionalCorDapps = cordappsVersionAtStartup).getOrThrow()
+                val bob = startNode(rpcUsers = listOf(user), providedName = BOB_NAME, additionalCorDapps = cordappsVersionAtStartup).getOrThrow()
+                alice.stop()
+                CordaRPCClient(bob.rpcAddress).start(user.username, user.password).use {
+                    val flowTracker = it.proxy.startTrackedFlow(::SendMessageFlow, message, defaultNotaryIdentity, alice.nodeInfo.singleIdentity()).progress
+                    // wait until Bob progresses as far as possible because Alice node is offline
+                    flowTracker.takeFirst { it == SendMessageFlow.Companion.FINALISING_TRANSACTION.label }.toBlocking().single()
+                }
+                val logFolder = bob.baseDirectory / NodeStartup.LOGS_DIRECTORY_NAME
+                // SendMessageFlow suspends in Bob node
+                bob.stop()
+                logFolder
+            }()
+
+            startNode(rpcUsers = listOf(user), providedName = ALICE_NAME, customOverrides = mapOf("devMode" to false),
+                    additionalCorDapps = cordappsVersionAtRestart, reuseAdditionalCorDapps = reuseAdditionalCorDappsAtRestart).getOrThrow()
+
+            assertFailsWith(ListenProcessDeathException::class) {
+                startNode(providedName = BOB_NAME, rpcUsers = listOf(user), customOverrides = mapOf("devMode" to false),
+                        additionalCorDapps = cordappsVersionAtRestart, reuseAdditionalCorDapps = reuseAdditionalCorDappsAtRestart).getOrThrow()
+            }
+
+            val logFile = bobLogFolder.list { it.filter { it.fileName.toString().endsWith(".log") }.findAny().get() }
+            val numberOfNodesThatLogged = logFile.readLines { it.filter { assertNodeLogs in it }.count() }
+            assertEquals(1, numberOfNodesThatLogged)
+        }
+    }
+
+    @Test
+    fun `restart nodes with incompatible version of sunspended flow due to different jar name`() {
+
+        assertNodeRestartFailure(
+                null,
+                setOf(TestCorDapp.Factory.create("testJar", "1.0", classes = classes)),
+                setOf(TestCorDapp.Factory.create("testJar2", "1.0", classes = classes)),
+                false,
+                 CheckpointIncompatibleException.FlowNotInstalledException(SendMessageFlow::class.java).message)
+    }
+
+    @Test
+    fun `restart nodes with incompatible version of sunspended flow`() {
+
+        assertNodeRestartFailure(
+                null,
+                setOf(TestCorDapp.Factory.create("testJar", "1.0", classes = classes)),
+                setOf(TestCorDapp.Factory.create("testJar", "1.0", classes = classes + net.test.cordapp.v1.SendMessageFlow::class.java)),
+                false,
+                // the part of the log message generated by CheckpointIncompatibleException.FlowVersionIncompatibleException
+                "that is incompatible with the current installed version of")
+    }
+
+    @Test
+    fun `restart nodes with incompatible version of sunspended flow due to different timestamps only`() {
+
+        assertNodeRestartFailure(
+                null,
+                setOf(TestCorDapp.Factory.create("testJar", "1.0", classes = classes)),
+                setOf(TestCorDapp.Factory.create("testJar", "1.0", classes = classes)),
+                false,
+                // the part of the log message generated by CheckpointIncompatibleException.FlowVersionIncompatibleException
+                "that is incompatible with the current installed version of")
+    }
+}
