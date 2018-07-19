fe9b89369f4c62255e83a6f3a24c0c0ae2354206
Chris Rankin <chris.rankin@r3.com>
2018-07-19 16:35:36 +0100

    CORDA-1816: Migrate JarFilter plugin to corda-gradle-plugins. (#3651)

--- buildSrc/jarfilter/src/main/kotlin/net/corda/gradle/jarfilter/Repeatable.kt
+++
@@ -1,8 +0,0 @@
-package net.corda.gradle.jarfilter
-
-import org.objectweb.asm.ClassVisitor
-
-interface Repeatable<T : ClassVisitor> {
-    fun recreate(visitor: ClassVisitor): T
-    val hasUnwantedElements: Boolean
-}
