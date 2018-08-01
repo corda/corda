a2feb49a04bbe9894faf3a0632dd4e767fe8110d
josecoll <jose.coll@r3cev.com>
2018-08-01 14:07:44 +0100

    Added CI smoke tests batch script.

---
+++ .ci/ci-gradle-build-cache-smokeTest.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -x
+
+USE_GRADLE_DAEMON="${USE_GRADLE_DAEMON:-false}"
+GRADLE_CACHE_DEBUG="${GRADLE_CACHE_DEBUG:-false}"
+PERFORM_GRADLE_SCAN="${PERFORM_GRADLE_SCAN:---scan}"
+
+echo ":core:smokeTest"
+cd ../core
+time ../gradlew --stacktrace -Dorg.gradle.daemon=${USE_GRADLE_DAEMON} -Dorg.gradle.caching.debug=${GRADLE_CACHE_DEBUG} clean smokeTest --build-cache ${PERFORM_GRADLE_SCAN}
+
+echo ":client:rpc:smokeTest"
+cd ../client/rpc
+time ../../gradlew --stacktrace -Dorg.gradle.daemon=${USE_GRADLE_DAEMON} -Dorg.gradle.caching.debug=${GRADLE_CACHE_DEBUG} clean smokeTest --build-cache ${PERFORM_GRADLE_SCAN}
