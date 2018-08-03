412ad9ca48fd5278718e2eb40f88cb110b13d3bc
josecoll <jose.coll@r3cev.com>
2018-08-03 13:50:52 +0100

    Minor updates to test scope.

--- .ci/ci-gradle-build-cache-smokeTest.sh
+++ .ci/ci-gradle-build-cache-smokeTest.sh
@@ -2,10 +2,12 @@
 
 source .ci/ci-gradle-build-cache-init.sh
 
+# This test is consistently re-run (cache entry miss)
 echo ":core:smokeTest"
 cd core
 time ../gradlew --stacktrace -Dorg.gradle.daemon=${USE_GRADLE_DAEMON} -Dorg.gradle.caching.debug=${GRADLE_CACHE_DEBUG} clean smokeTest --build-cache ${PERFORM_GRADLE_SCAN}
 
+# This test is consistently re-run (cache entry miss)
 echo ":client:rpc:smokeTest"
 cd ../client/rpc
 time ../../gradlew --stacktrace -Dorg.gradle.daemon=${USE_GRADLE_DAEMON} -Dorg.gradle.caching.debug=${GRADLE_CACHE_DEBUG} clean smokeTest --build-cache ${PERFORM_GRADLE_SCAN}
