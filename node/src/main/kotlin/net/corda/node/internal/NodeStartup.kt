68bfb7ff66423134ec6b9fa3f75010ddf1767099
Mike Hearn <mike@r3.com>
2018-08-04 23:56:09 -0400

    New jokes.

--- node/src/main/kotlin/net/corda/node/internal/NodeStartup.kt
+++ node/src/main/kotlin/net/corda/node/internal/NodeStartup.kt
@@ -42,6 +42,8 @@ import java.lang.management.ManagementFactory
 import java.net.InetAddress
 import java.nio.file.Path
 import java.nio.file.Paths
+import java.time.DayOfWeek
+import java.time.ZonedDateTime
 import java.util.*
 import kotlin.system.exitProcess
 
@@ -496,8 +498,8 @@ open class NodeStartup(val args: Array<String>) {
                     "It's kind of like a block chain but\ncords sounded healthier than chains.",
                     "Computer science and finance together.\nYou should see our crazy Christmas parties!",
                     "I met my bank manager yesterday and asked\nto check my balance ... he pushed me over!",
-                    "A banker with nobody around may find\nthemselves .... a-loan! <applause>",
-                    "Whenever I go near my bank I get\nwithdrawal symptoms ${Emoji.coolGuy}",
+                    "A banker left to their own devices may find\nthemselves .... a-loan! <applause>",
+                    "Whenever I go near my bank\nI get withdrawal symptoms ${Emoji.coolGuy}",
                     "There was an earthquake in California,\na local bank went into de-fault.",
                     "I asked for insurance if the nearby\nvolcano erupted. They said I'd be covered.",
                     "I had an account with a bank in the\nNorth Pole, but they froze all my assets ${Emoji.santaClaus}",
@@ -508,11 +510,48 @@ open class NodeStartup(val args: Array<String>) {
                     "I won $3M on the lottery so I donated a quarter\nof it to charity. Now I have $2,999,999.75.",
                     "There are two rules for financial success:\n1) Don't tell everything you know.",
                     "Top tip: never say \"oops\", instead\nalways say \"Ah, Interesting!\"",
-                    "Computers are useless. They can only\ngive you answers.  -- Picasso"
+                    "Computers are useless. They can only\ngive you answers.  -- Picasso",
+                    "Regular naps prevent old age, especially\nif you take them whilst driving.",
+                    "Always borrow money from a pessimist.\nHe won't expect it back.",
+                    "War does not determine who is right.\nIt determines who is left.",
+                    "A bus stops at a bus station. A train stops at a\ntrain station. What happens at a workstation?",
+                    "I got a universal remote control yesterday.\nI thought, this changes everything.",
+                    "Did you ever walk into an office and\nthink, whiteboards are remarkable!",
+                    "The good thing about lending out your time machine\nis that you basically get it back immediately.",
+                    "I used to work in a shoe recycling\nshop. It was sole destroying.",
+                    "What did the fish say\nwhen he hit a wall? Dam.",
+                    "You should really try a seafood diet.\nIt's easy: you see food and eat it.",
+                    "I recently sold my vacuum cleaner,\nall it was doing was gathering dust.",
+                    "My professor accused me of plagiarism.\nHis words, not mine!",
+                    "Change is inevitable, except\nfrom a vending machine.",
+                    "If at first you don't succeed, destroy\nall the evidence that you tried.",
+                    "If at first you don't succeed, \nthen we have something in common!",
+                    "Moses had the first tablet that\ncould connect to the cloud.",
+                    "How did my parents fight boredom before the internet?\nI asked my 17 siblings and they didn't know either.",
+                    "Cats spend two thirds of their lives sleeping\nand the other third making viral videos.",
+                    "The problem with troubleshooting\nis that trouble shoots back.",
+                    "I named my dog 'Six Miles' so I can tell\npeople I walk Six Miles every day.",
+                    "People used to laugh at me when I said I wanted\nto be a comedian. Well they're not laughing now!",
+                    "My wife just found out I replaced our bed\nwith a trampoline; she hit the roof.",
+                    "My boss asked me who is the stupid one, me or him?\nI said everyone knows he doesn't hire stupid people.",
+                    "Don't trust atoms.\nThey make up everything.",
+                    "Keep the dream alive:\nhit the snooze button.",
+                    "Rest in peace, boiled water.\nYou will be mist.",
+                    "When I discovered my toaster wasn't\nwaterproof, I was shocked.",
+                    "Where do cryptographers go for\nentertainment? The security theatre.",
+                    "How did the Java programmer get rich?\nThey inherited a factory.",
+                    "Why did the developer quit his job?\nHe didn't get ar-rays."
             )
 
             if (Emoji.hasEmojiTerminal)
                 messages += "Kind of like a regular database but\nwith emojis, colours and ascii art. ${Emoji.coolGuy}"
+
+
+            if (ZonedDateTime.now().dayOfWeek == DayOfWeek.FRIDAY) {
+                // Make it quite likely people see it.
+                repeat(20) { messages += "Ah, Friday.\nMy second favourite F-word." }
+            }
+
             val (msg1, msg2) = messages.randomOrNull()!!.split('\n')
 
             println(Ansi.ansi().newline().fgBrightRed().a(
@@ -520,7 +559,8 @@ open class NodeStartup(val args: Array<String>) {
                     """  / ____/     _________/ /___ _""").newline().a(
                     """ / /     __  / ___/ __  / __ `/         """).fgBrightBlue().a(msg1).newline().fgBrightRed().a(
                     """/ /___  /_/ / /  / /_/ / /_/ /          """).fgBrightBlue().a(msg2).newline().fgBrightRed().a(
-                    """\____/     /_/   \__,_/\__,_/""").reset().newline().newline().fgBrightDefault().bold().a("--- ${versionInfo.vendor} ${versionInfo.releaseVersion} (${versionInfo.revision.take(7)}) -----------------------------------------------").newline().newline().reset())
+                    """\____/     /_/   \__,_/\__,_/""").reset().newline().newline().fgBrightDefault().bold().a("--- ${versionInfo.vendor} ${versionInfo.releaseVersion} (${versionInfo.revision.take(7)}) -------------------------------------------------------------").newline().newline().reset())
+
         }
     }
 }
