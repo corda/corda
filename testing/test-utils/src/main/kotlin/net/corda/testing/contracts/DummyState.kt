2a1824d3cefb32f18a23b9cb65efed398b625feb
josecoll <jose.coll@r3cev.com>
2018-08-13 15:18:05 +0100

    Fix failing Java Unit test.

--- testing/test-utils/src/main/kotlin/net/corda/testing/contracts/DummyState.kt
+++ testing/test-utils/src/main/kotlin/net/corda/testing/contracts/DummyState.kt
@@ -6,7 +6,7 @@ import net.corda.core.identity.AbstractParty
 /**
  * Dummy state for use in testing. Not part of any contract, not even the [DummyContract].
  */
-data class DummyState(
+data class DummyState @JvmOverloads constructor (
         /** Some information that the state represents for test purposes. **/
         val magicNumber: Int = 0,
         override val participants: List<AbstractParty> = listOf()) : ContractState {
