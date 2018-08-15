55eb21a8a7bd4f1aa5e0d0e6cb6719bfaf64b791
Chris Rankin <chris.rankin@r3.com>
2018-08-15 01:18:05 +0100

    CORDA-1905: Extend JSON deserialisation to handle Amount<T> for any T.

--- client/jackson/src/main/kotlin/net/corda/client/jackson/JacksonSupport.kt
+++ client/jackson/src/main/kotlin/net/corda/client/jackson/JacksonSupport.kt
@@ -413,10 +413,11 @@ object JacksonSupport {
         }
     }
 
-    @Deprecated("This is an internal class, do not use")
+    @Deprecated("No longer used",
+            replaceWith = ReplaceWith("Corda's internal AmountDeserializer and TokenDeserializer classes"))
     object AmountDeserializer : JsonDeserializer<Amount<*>>() {
         override fun deserialize(parser: JsonParser, context: DeserializationContext): Amount<*> {
-            return if (parser.currentToken == JsonToken.VALUE_STRING) {
+            return if (parser.currentToken() == JsonToken.VALUE_STRING) {
                 Amount.parseCurrency(parser.text)
             } else {
                 val wrapper = parser.readValueAs<CurrencyAmountWrapper>()
