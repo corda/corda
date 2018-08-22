d12e5dc032189b633aa468da891aa34a751a41e5
Dominic Fox <dominic.fox@r3.com>
2018-08-22 14:14:10 +0100

    Extract some helper functions in test

--- core/src/test/kotlin/net/corda/core/internal/JarSignatureCollectorTest.kt
+++ core/src/test/kotlin/net/corda/core/internal/JarSignatureCollectorTest.kt
@@ -1,5 +1,6 @@
 package net.corda.core.internal
 
+import net.corda.core.identity.CordaX500Name
 import net.corda.core.identity.Party
 import net.corda.testing.core.ALICE_NAME
 import net.corda.testing.core.BOB_NAME
@@ -21,6 +22,7 @@ class JarSignatureCollectorTest {
         private val dir = Files.createTempDirectory(JarSignatureCollectorTest::class.simpleName)
         private val bin = Paths.get(System.getProperty("java.home")).let { if (it.endsWith("jre")) it.parent else it } / "bin"
         private val shredder = (dir / "_shredder").toFile() // No need to delete after each test.
+
         fun execute(vararg command: String) {
             assertEquals(0, ProcessBuilder()
                     .inheritIO()
@@ -31,18 +33,25 @@ class JarSignatureCollectorTest {
                     .waitFor())
         }
 
+        private const val FILENAME = "attachment.jar"
+        private const val ALICE = "alice"
+        private const val ALICE_PASS = "alicepass"
+        private const val BOB = "bob"
+        private const val BOB_PASS = "bobpass"
+
+        private fun generateKey(alias: String, password: String, name: CordaX500Name) =
+                execute("keytool", "-genkey", "-keystore", "_teststore", "-storepass", "storepass", "-keyalg", "RSA", "-alias", alias, "-keypass", password, "-dname", name.toString())
+
         @BeforeClass
         @JvmStatic
         fun beforeClass() {
-            execute("keytool", "-genkey", "-keystore", "_teststore", "-storepass", "storepass", "-keyalg", "RSA", "-alias", "alice", "-keypass", "alicepass", "-dname", ALICE_NAME.toString())
-            execute("keytool", "-genkey", "-keystore", "_teststore", "-storepass", "storepass", "-keyalg", "RSA", "-alias", "bob", "-keypass", "bobpass", "-dname", BOB_NAME.toString())
+            generateKey(ALICE, ALICE_PASS, ALICE_NAME)
+            generateKey(BOB, BOB_PASS, BOB_NAME)
+
             (dir / "_signable1").writeLines(listOf("signable1"))
             (dir / "_signable2").writeLines(listOf("signable2"))
             (dir / "_signable3").writeLines(listOf("signable3"))
         }
-
-        private fun getSigners(name: String) =
-            JarInputStream(FileInputStream((dir / name).toFile())).use(JarSignatureCollector::collectSigningParties)
 
         @AfterClass
         @JvmStatic
@@ -64,63 +73,87 @@ class JarSignatureCollectorTest {
     @Test
     fun `empty jar has no signers`() {
         (dir / "META-INF").createDirectory() // At least one arg is required, and jar cvf conveniently ignores this.
-        execute("jar", "cvf", "attachment.jar", "META-INF")
-        assertEquals(emptyList(), getSigners("attachment.jar"))
-        execute("jarsigner", "-keystore", "_teststore", "-storepass", "storepass", "-keypass", "alicepass", "attachment.jar", "alice")
-        assertEquals(emptyList(), getSigners("attachment.jar")) // There needs to have been a file for ALICE to sign.
+        createJar("META-INF")
+        assertEquals(emptyList(), getJarSigners())
+
+        signAsAlice()
+        assertEquals(emptyList(), getJarSigners()) // There needs to have been a file for ALICE to sign.
     }
 
     @Test
     fun `unsigned jar has no signers`() {
-        execute("jar", "cvf", "attachment.jar", "_signable1")
-        assertEquals(emptyList(), getSigners("attachment.jar"))
-        execute("jar", "uvf", "attachment.jar", "_signable2")
-        assertEquals(emptyList(), getSigners("attachment.jar"))
+        createJar("_signable1")
+        assertEquals(emptyList(), getJarSigners())
+
+        updateJar("_signable2")
+        assertEquals(emptyList(), getJarSigners())
     }
 
     @Test
     fun `one signer`() {
-        execute("jar", "cvf", "attachment.jar", "_signable1", "_signable2")
-        execute("jarsigner", "-keystore", "_teststore", "-storepass", "storepass", "-keypass", "alicepass", "attachment.jar", "alice")
-        assertEquals(listOf(ALICE_NAME), getSigners("attachment.jar").names) // We only reused ALICE's distinguished name, so the keys will be different.
+        createJar("_signable1", "_signable2")
+        signAsAlice()
+        assertEquals(listOf(ALICE_NAME), getJarSigners().names) // We only reused ALICE's distinguished name, so the keys will be different.
+
         (dir / "my-dir").createDirectory()
-        execute("jar", "uvf", "attachment.jar", "my-dir")
-        assertEquals(listOf(ALICE_NAME), getSigners("attachment.jar").names) // Unsigned directory is irrelevant.
+        updateJar("my-dir")
+        assertEquals(listOf(ALICE_NAME), getJarSigners().names) // Unsigned directory is irrelevant.
     }
 
     @Test
     fun `two signers`() {
-        execute("jar", "cvf", "attachment.jar", "_signable1", "_signable2")
-        execute("jarsigner", "-keystore", "_teststore", "-storepass", "storepass", "-keypass", "alicepass", "attachment.jar", "alice")
-        execute("jarsigner", "-keystore", "_teststore", "-storepass", "storepass", "-keypass", "bobpass", "attachment.jar", "bob")
-        assertEquals(listOf(ALICE_NAME, BOB_NAME), getSigners("attachment.jar").names)
+        createJar("_signable1", "_signable2")
+        signAsAlice()
+        signAsBob()
+
+        assertEquals(listOf(ALICE_NAME, BOB_NAME), getJarSigners().names)
     }
 
     @Test
     fun `all files must be signed by the same set of signers`() {
-        execute("jar", "cvf", "attachment.jar", "_signable1")
-        execute("jarsigner", "-keystore", "_teststore", "-storepass", "storepass", "-keypass", "alicepass", "attachment.jar", "alice")
-        assertEquals(listOf(ALICE_NAME), getSigners("attachment.jar").names)
-        execute("jar", "uvf", "attachment.jar", "_signable2")
-        execute("jarsigner", "-keystore", "_teststore", "-storepass", "storepass", "-keypass", "bobpass", "attachment.jar", "bob")
+        createJar("_signable1")
+        signAsAlice()
+        assertEquals(listOf(ALICE_NAME), getJarSigners().names)
+
+        updateJar("_signable2")
+        signAsBob()
         assertFailsWith<InvalidJarSignersException>(
             """
             Mismatch between signers [O=Alice Corp, L=Madrid, C=ES, O=Bob Plc, L=Rome, C=IT] for file _signable1
             and signers [O=Bob Plc, L=Rome, C=IT] for file _signable2
             """.trimIndent().replace('\n', ' ')
-        ) { getSigners("attachment.jar") }
+        ) { getJarSigners() }
     }
 
     @Test
     fun `bad signature is caught even if the party would not qualify as a signer`() {
         (dir / "volatile").writeLines(listOf("volatile"))
-        execute("jar", "cvf", "attachment.jar", "volatile")
-        execute("jarsigner", "-keystore", "_teststore", "-storepass", "storepass", "-keypass", "alicepass", "attachment.jar", "alice")
-        assertEquals(listOf(ALICE_NAME), getSigners("attachment.jar").names)
+        createJar("volatile")
+        signAsAlice()
+        assertEquals(listOf(ALICE_NAME), getJarSigners().names)
+
         (dir / "volatile").writeLines(listOf("garbage"))
-        execute("jar", "uvf", "attachment.jar", "volatile", "_signable1") // ALICE's signature on volatile is now bad.
-        execute("jarsigner", "-keystore", "_teststore", "-storepass", "storepass", "-keypass", "bobpass", "attachment.jar", "bob")
+        updateJar("volatile", "_signable1") // ALICE's signature on volatile is now bad.
+        signAsBob()
         // The JDK doesn't care that BOB has correctly signed the whole thing, it won't let us process the entry with ALICE's bad signature:
-        assertFailsWith<SecurityException> { getSigners("attachment.jar") }
+        assertFailsWith<SecurityException> { getJarSigners() }
     }
+
+    //region Helper functions
+    private fun createJar(vararg contents: String) =
+            execute(*(arrayOf("jar", "cvf", FILENAME) + contents))
+
+    private fun updateJar(vararg contents: String) =
+            execute(*(arrayOf("jar", "uvf", FILENAME) + contents))
+
+    private fun signJar(alias: String, password: String) =
+            execute("jarsigner", "-keystore", "_teststore", "-storepass", "storepass", "-keypass", password, FILENAME, alias)
+
+    private fun signAsAlice() = signJar(ALICE, ALICE_PASS)
+    private fun signAsBob() = signJar(BOB, BOB_PASS)
+
+    private fun getJarSigners() =
+            JarInputStream(FileInputStream((dir / FILENAME).toFile())).use(JarSignatureCollector::collectSigningParties)
+    //endregion
+    
 }
