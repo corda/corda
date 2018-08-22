b24bd1a622eceff4c8bc09a86ff95824f1840770
Michele Sollecito <michele.sollecito@r3.com>
2018-08-22 10:32:00 +0100

    [CORDA-1778]: Updated intentions.

--- node/src/test/kotlin/net/corda/node/services/config/parsing/ConfigDefinitionTest.kt
+++ node/src/test/kotlin/net/corda/node/services/config/parsing/ConfigDefinitionTest.kt
@@ -14,20 +14,32 @@ class ConfigDefinitionTest {
 
         val prop1 = "prop1"
         val prop1Value = "value1"
+
         val prop2 = "prop2"
         val prop2Value = 3
 
-        val configuration = configOf(prop1 to prop1Value, prop2 to prop2Value).toConfig()
+        val prop3 = "prop3"
+        val prop4 = "prop4"
+        val prop4Value = true
+        val prop5 = "prop5"
+        val prop5Value = -17.3
+        val prop3Value = configOf(prop4 to prop4Value, prop5 to prop5Value)
 
-        val properties = setOf(ConfigProperty.string(prop1), ConfigProperty.int(prop2))
+        val configuration = configOf(prop1 to prop1Value, prop2 to prop2Value, prop3 to prop3Value).toConfig()
 
-        val config: BlahConfig = proxyConfig(configuration, properties)
+        val fooProperties = setOf(ConfigProperty.boolean("prop4"), ConfigProperty.double("prop5"))
+        val blahProperties = setOf(ConfigProperty.string(prop1), ConfigProperty.int(prop2), ConfigProperty.value("prop3").map("FooConfig") { value -> proxyConfig<FooConfig>(value.toConfig(), fooProperties) })
 
-        assertThat(config.prop1).isEqualTo(prop1Value)
-        assertThat(config.prop2).isEqualTo(prop2Value)
+        val blahConfig: BlahConfig = proxyConfig(configuration, blahProperties)
+
+        assertThat(blahConfig.prop1).isEqualTo(prop1Value)
+        assertThat(blahConfig.prop2).isEqualTo(prop2Value)
+
+        val fooConfig = blahConfig.prop3
+        assertThat(fooConfig.prop4).isEqualTo(prop4Value)
+        assertThat(fooConfig.prop5).isEqualTo(prop5Value)
     }
 
-    // TODO sollecitom add a more complex test, involving nested objects using ObjectConfigProperty.
     // TODO sollecitom add an even more complex test, involving nested objects using nested ConfigSchema.
 }
 
@@ -35,7 +47,20 @@ private interface BlahConfig {
 
     val prop1: String
     val prop2: Int
+    val prop3: FooConfig
 }
+
+private interface FooConfig {
+
+    val prop4: Boolean
+    val prop5: Double
+}
+
+//interface Validatable {
+//
+//    fun validate():
+//}
+
 
 // TODO sollecitom introduce a ConfigSchema type able to output the structure of an expected config, to validate against a `Config` type, and to proxy it. Try to make this composite an ObjectConfigProperty. Also, it'd be great to have it as a DelegatedProperty as well.
 
@@ -53,7 +78,7 @@ private class PropertiesInvocationHandler(private val configuration: Config, pro
             throw IllegalStateException("Unmapped key ${method.name}. Known keys are: ${propertyByGetterName.values.map(ConfigProperty<*>::key)}.")
         }
         val property = propertyByGetterName[method.name]!!
-        // TODO sollecitom turn this into proper validation
+        // TODO sollecitom turn this into proper validation, rejecting unknown keys when specified.
         if (property.mandatory && !property.isSpecifiedBy(configuration)) {
             throw IllegalStateException("Unspecified value for mandatory property key ${property.key}.")
         }
