d950cb12b0289b689a109dbef56d21e02940214d
Clinton Alexander <admin@clintonio.com>
2018-08-23 14:20:57 +0100

    Addressed review comments

--- node-api/src/main/kotlin/net/corda/nodeapi/internal/NodeInfoConstants.kt
+++ node-api/src/main/kotlin/net/corda/nodeapi/internal/NodeInfoConstants.kt
@@ -1,4 +1,4 @@
 package net.corda.nodeapi.internal
 
 // TODO: Add to Corda node.conf to allow customisation
-val NODE_INFO_DIRECTORY = "additional-node-infos"
+const val NODE_INFO_DIRECTORY = "additional-node-infos"
