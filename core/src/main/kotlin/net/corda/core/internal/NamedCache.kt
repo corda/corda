875f359e9c7c4d1aa9b4231d47f596c81db9d351
Christian Sailer <christian.sailer@r3.com>
2018-08-24 11:58:12 +0100

    Suppressed 'name not used' warning, added comment, and fixed generic parameters on the buildNamed functions.

--- core/src/main/kotlin/net/corda/core/internal/NamedCache.kt
+++ core/src/main/kotlin/net/corda/core/internal/NamedCache.kt
@@ -5,15 +5,26 @@ import com.github.benmanes.caffeine.cache.CacheLoader
 import com.github.benmanes.caffeine.cache.Caffeine
 import com.github.benmanes.caffeine.cache.LoadingCache
 
-fun <K, V> Caffeine<Any, Any>.buildNamed(name: String): Cache<K, V> {
+/* buildNamed is the central helper method to build caffeine caches in Corda.
+ * This allows to easily add tweaks to all caches build in Corda, and also forces
+ * cache users to give their cache a (meaningful) name that can be used e.g. for
+ * capturing cache traces etc.
+ *
+ * Currently it is not used in this version of CORDA, but there are plans to do so
+ */
+
+@Suppress("UNUSED_PARAMETER")
+fun <K, V> Caffeine<in K, in V>.buildNamed(name: String): Cache<K, V> {
     return this.build<K, V>()
 }
 
-fun <K, V> Caffeine<Any, Any>.buildNamed(name: String, loadFunc: (K) -> V): LoadingCache<K, V> {
+@Suppress("UNUSED_PARAMETER")
+fun <K, V> Caffeine<in K, in V>.buildNamed(name: String, loadFunc: (K) -> V): LoadingCache<K, V> {
     return this.build<K, V>(loadFunc)
 }
 
 
+@Suppress("UNUSED_PARAMETER")
 fun <K, V> Caffeine<in K, in V>.buildNamed(name: String, loader: CacheLoader<K, V>): LoadingCache<K, V> {
     return this.build<K, V>(loader)
 }
