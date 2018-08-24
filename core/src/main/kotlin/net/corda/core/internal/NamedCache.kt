bb8b6754e59321ece5789c4a640492de138c966b
Christian Sailer <christian.sailer@r3.com>
2018-08-24 14:44:34 +0100

    Add checks for the cache name

--- core/src/main/kotlin/net/corda/core/internal/NamedCache.kt
+++ core/src/main/kotlin/net/corda/core/internal/NamedCache.kt
@@ -5,6 +5,19 @@ import com.github.benmanes.caffeine.cache.CacheLoader
 import com.github.benmanes.caffeine.cache.Caffeine
 import com.github.benmanes.caffeine.cache.LoadingCache
 
+
+/**
+ * Restrict the allowed characters of a cache name - this ensures that each cache has a name, and that
+ * the name can be used to create a file name or a metric name.
+ */
+fun checkCacheName(name: String) {
+    require(!name.isBlank())
+    require(allowedChars.matches(name))
+}
+
+val allowedChars = Regex("^[0-9A-Za-z_.]*\$")
+
+
 /* buildNamed is the central helper method to build caffeine caches in Corda.
  * This allows to easily add tweaks to all caches build in Corda, and also forces
  * cache users to give their cache a (meaningful) name that can be used e.g. for
@@ -13,18 +26,18 @@ import com.github.benmanes.caffeine.cache.LoadingCache
  * Currently it is not used in this version of CORDA, but there are plans to do so
  */
 
-@Suppress("UNUSED_PARAMETER")
 fun <K, V> Caffeine<in K, in V>.buildNamed(name: String): Cache<K, V> {
+    checkCacheName(name)
     return this.build<K, V>()
 }
 
-@Suppress("UNUSED_PARAMETER")
 fun <K, V> Caffeine<in K, in V>.buildNamed(name: String, loadFunc: (K) -> V): LoadingCache<K, V> {
+    checkCacheName(name)
     return this.build<K, V>(loadFunc)
 }
 
 
-@Suppress("UNUSED_PARAMETER")
 fun <K, V> Caffeine<in K, in V>.buildNamed(name: String, loader: CacheLoader<K, V>): LoadingCache<K, V> {
+    checkCacheName(name)
     return this.build<K, V>(loader)
 }
