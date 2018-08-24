3594ccd89b24c50f51805de5f57eb8a43212337e
Clinton <clintonxa@gmail.com>
2018-08-24 16:29:14 +0100

    CORDA-1924: Remove references to Cordform definition and cordform-common (#3842)

---
+++ node-api/src/main/kotlin/net/corda/nodeapi/internal/NodeInfoConstants.kt
@@ -0,0 +1,4 @@
+package net.corda.nodeapi.internal
+
+// TODO: Add to Corda node.conf to allow customisation
+const val NODE_INFO_DIRECTORY = "additional-node-infos"
--- node/src/test/kotlin/net/corda/node/services/network/NetworkMapUpdaterTest.kt
+++ node/src/test/kotlin/net/corda/node/services/network/NetworkMapUpdaterTest.kt
@@ -3,7 +3,6 @@ package net.corda.node.services.network
 import com.google.common.jimfs.Configuration.unix
 import com.google.common.jimfs.Jimfs
 import com.nhaarman.mockito_kotlin.*
-import net.corda.cordform.CordformNode.NODE_INFO_DIRECTORY
 import net.corda.core.crypto.Crypto
 import net.corda.core.crypto.SecureHash
 import net.corda.core.crypto.sign
@@ -15,6 +14,7 @@ import net.corda.core.node.NodeInfo
 import net.corda.core.serialization.serialize
 import net.corda.core.utilities.millis
 import net.corda.node.services.api.NetworkMapCacheInternal
+import net.corda.nodeapi.internal.NODE_INFO_DIRECTORY
 import net.corda.nodeapi.internal.NodeInfoAndSigned
 import net.corda.nodeapi.internal.network.NETWORK_PARAMS_UPDATE_FILE_NAME
 import net.corda.nodeapi.internal.network.NodeInfoFilesCopier
