874b0ac9d9d60a53909c8c97003b8779b86d39e6
Anthony Keenan <anthony.keenan@r3.com>
2018-08-29 17:53:50 +0100

    Add return code to network bootstrapper

--- tools/bootstrapper/src/main/kotlin/net/corda/bootstrapper/Main.kt
+++ tools/bootstrapper/src/main/kotlin/net/corda/bootstrapper/Main.kt
@@ -24,7 +24,8 @@ class NetworkBootstrapperRunner : CordaCliWrapper("bootstrapper", "Bootstrap a l
     @Option(names = ["--no-copy"], description = ["""Don't copy the CorDapp JARs into the nodes' "cordapps" directories."""])
     private var noCopy: Boolean = false
 
-    override fun runProgram() {
+    override fun runProgram(): Int {
         NetworkBootstrapper().bootstrap(dir.toAbsolutePath().normalize(), copyCordapps = !noCopy)
+        return 0 //exit code
     }
 }
