6e2eae4f5d5785776ba2436249e0c98e4536b56e
Michal Kit <michal.kit@r3.com>
2018-08-31 09:34:42 +0100

    CORDA-1938 Adding version info to the node info submission request (#3854)

--- node/src/main/kotlin/net/corda/node/services/network/NetworkMapClient.kt
+++ node/src/main/kotlin/net/corda/node/services/network/NetworkMapClient.kt
@@ -3,7 +3,6 @@ package net.corda.node.services.network
 import net.corda.core.crypto.SecureHash
 import net.corda.core.crypto.SignedData
 import net.corda.core.internal.openHttpConnection
-import net.corda.core.internal.post
 import net.corda.core.internal.responseAs
 import net.corda.core.node.NodeInfo
 import net.corda.core.serialization.deserialize
@@ -11,18 +10,22 @@ import net.corda.core.serialization.serialize
 import net.corda.core.utilities.contextLogger
 import net.corda.core.utilities.seconds
 import net.corda.core.utilities.trace
+import net.corda.node.VersionInfo
 import net.corda.node.utilities.registration.cacheControl
 import net.corda.nodeapi.internal.SignedNodeInfo
 import net.corda.nodeapi.internal.network.NetworkMap
 import net.corda.nodeapi.internal.network.SignedNetworkMap
 import net.corda.nodeapi.internal.network.SignedNetworkParameters
 import net.corda.nodeapi.internal.network.verifiedNetworkMapCert
+import org.apache.commons.io.IOUtils
 import java.io.BufferedReader
+import java.io.IOException
+import java.net.HttpURLConnection
 import java.net.URL
 import java.security.cert.X509Certificate
 import java.time.Duration
 
-class NetworkMapClient(compatibilityZoneURL: URL, val trustedRoot: X509Certificate) {
+class NetworkMapClient(compatibilityZoneURL: URL, val trustedRoot: X509Certificate, private val versionInfo: VersionInfo) {
     companion object {
         private val logger = contextLogger()
     }
@@ -32,15 +35,40 @@ class NetworkMapClient(compatibilityZoneURL: URL, val trustedRoot: X509Certifica
     fun publish(signedNodeInfo: SignedNodeInfo) {
         val publishURL = URL("$networkMapUrl/publish")
         logger.trace { "Publishing NodeInfo to $publishURL." }
-        publishURL.post(signedNodeInfo.serialize())
-        logger.trace { "Published NodeInfo to $publishURL successfully." }
+        val conn = publishURL.openHttpConnection()
+        conn.doOutput = true
+        conn.requestMethod = "POST"
+        conn.setRequestProperty("Content-Type", "application/octet-stream")
+        conn.setRequestProperty("Client-Version", versionInfo.releaseVersion)
+        conn.setRequestProperty("Platform-Version", versionInfo.platformVersion.toString())
+        conn.outputStream.write(signedNodeInfo.serialize().bytes)
+        when (conn.responseCode) {
+            HttpURLConnection.HTTP_OK -> {
+                logger.trace { "Published NodeInfo to $publishURL successfully." }
+            }
+            else -> throw IOException("Response Code ${conn.responseCode}: ${IOUtils.toString(conn.errorStream)}")
+        }
     }
 
     fun ackNetworkParametersUpdate(signedParametersHash: SignedData<SecureHash>) {
         val ackURL = URL("$networkMapUrl/ack-parameters")
+
+        // Post request to certificate signing server via http.
+        val conn = ackURL.openHttpConnection()
         logger.trace { "Sending network parameters with hash ${signedParametersHash.raw.deserialize()} approval to $ackURL." }
-        ackURL.post(signedParametersHash.serialize())
-        logger.trace { "Sent network parameters approval to $ackURL successfully." }
+        conn.doOutput = true
+        conn.requestMethod = "POST"
+        conn.setRequestProperty("Content-Type", "application/octet-stream")
+        conn.setRequestProperty("Client-Version", versionInfo.releaseVersion)
+        conn.setRequestProperty("Platform-Version", versionInfo.platformVersion.toString())
+        conn.outputStream.write(signedParametersHash.serialize().bytes)
+
+        when (conn.responseCode) {
+            HttpURLConnection.HTTP_OK -> {
+                logger.trace { "Sent network parameters approval to $ackURL successfully." }
+            }
+            else -> throw IOException("Response Code ${conn.responseCode}: ${IOUtils.toString(conn.errorStream)}")
+        }
     }
 
     fun getNetworkMap(): NetworkMapResponse {
