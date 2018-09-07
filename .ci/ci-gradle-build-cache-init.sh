2fe3952c2adfaa39ce23cddf2fb5e9ecc3749492
josecoll <jose.coll@r3cev.com>
2018-09-07 14:03:59 +0100

    Update init script.

--- .ci/ci-gradle-build-cache-init.sh
+++ .ci/ci-gradle-build-cache-init.sh
@@ -1,20 +1,15 @@
 #!/bin/bash
 
-set -x
-
 export GRADLE_BUILD_CACHE_URL="${GRADLE_BUILD_CACHE_URL:-http://localhost:5071/cache/}"
 export USE_GRADLE_DAEMON="${USE_GRADLE_DAEMON:-false}"
 export GRADLE_CACHE_DEBUG="${GRADLE_CACHE_DEBUG:-false}"
 export PERFORM_GRADLE_SCAN="${PERFORM_GRADLE_SCAN:---scan}"
 
-# cd %teamcity.build.checkoutDir%
-echo "Using Gradle Build Cache: $GRADLE_BUILD_CACHE_URL"
-
 # GRADLE HOME
 # Required to use custom patched Gradle 4.9 with KEEP-ALIVE timeout fix
 # Currently built on Azure "gradle-build-client" machine at:
 # export GRADLE_HOME=~/gradle/install
-if [[ -v name_of_var ]]; then
+if [[ ! ${GRADLE_HOME} ]]; then
     echo "Please set GRADLE_HOME variable"
     exit;
 else
@@ -22,4 +17,7 @@ else
 fi
 
 echo "Gradle version:"
-${GRADLE_EXE} --version
+${GRADLE_HOME}/bin/gradle --version
+
+# cd %teamcity.build.checkoutDir%
+echo "Using Gradle Build Cache: $GRADLE_BUILD_CACHE_URL"
