f77b5d5e88389d8d74e30969481bf4cc14216112
Stefano Franz <stefano.franz@r3.com>
2018-09-13 16:18:14 +0100

    enforce backwards compatibility of new CLI's

---
+++ tools/bootstrapper/src/test/kotlin/net/corda/bootstrapper/NetworkBootstrapperRunnerTest.kt
@@ -0,0 +1,5 @@
+package net.corda.bootstrapper
+
+import net.corda.testing.CliBackwardsCompatibleTest
+
+class NetworkBootstrapperRunnerTest : CliBackwardsCompatibleTest(NetworkBootstrapperRunner::class.java)
