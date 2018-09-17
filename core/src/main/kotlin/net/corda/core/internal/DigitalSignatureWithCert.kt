f7876c13dd6dbaa41d17c98272cb261667515ba4
Michal Kit <michal.kit@r3.com>
2018-09-17 16:42:01 +0100

    CORDA-1995 removing DigitalSignatureWithCertPath

--- core/src/main/kotlin/net/corda/core/internal/DigitalSignatureWithCert.kt
+++ core/src/main/kotlin/net/corda/core/internal/DigitalSignatureWithCert.kt
@@ -4,26 +4,32 @@ import net.corda.core.crypto.DigitalSignature
 import net.corda.core.crypto.SignedData
 import net.corda.core.crypto.verify
 import net.corda.core.serialization.CordaSerializable
+import net.corda.core.serialization.DeprecatedConstructorForDeserialization
 import net.corda.core.serialization.SerializedBytes
 import net.corda.core.serialization.deserialize
 import net.corda.core.utilities.OpaqueBytes
-import java.security.cert.CertPath
-import java.security.cert.X509Certificate
+import java.security.cert.*
 
 // TODO: Rename this to DigitalSignature.WithCert once we're happy for it to be public API. The methods will need documentation
 // and the correct exceptions will be need to be annotated
 /** A digital signature with attached certificate of the public key. */
-open class DigitalSignatureWithCert(val by: X509Certificate, bytes: ByteArray) : DigitalSignature(bytes) {
+class DigitalSignatureWithCert(val by: X509Certificate, val remainingChain: List<X509Certificate>, bytes: ByteArray) : DigitalSignature(bytes) {
+    @DeprecatedConstructorForDeserialization(1)
+    constructor(by: X509Certificate, bytes: ByteArray) : this(by, emptyList(), bytes)
+
+    val fullCertChain: List<X509Certificate> get() = listOf(by) + remainingChain
+    val fullCertPath: CertPath get() = CertificateFactory.getInstance("X.509").generateCertPath(fullCertChain)
+
     fun verify(content: ByteArray): Boolean = by.publicKey.verify(content, this)
     fun verify(content: OpaqueBytes): Boolean = verify(content.bytes)
-}
 
-/**
- * A digital signature with attached certificate path. The first certificate in the path corresponds to the data signer key.
- * @param path certificate path associated with this signature
- * @param bytes signature bytes
- */
-class DigitalSignatureWithCertPath(val path: List<X509Certificate>, bytes: ByteArray): DigitalSignatureWithCert(path.first(), bytes)
+    init {
+        if (remainingChain.isNotEmpty()) {
+            val parameters = PKIXParameters(setOf(TrustAnchor(remainingChain.last(), null))).apply { isRevocationEnabled = false }
+            CertPathValidator.getInstance("PKIX").validate(fullCertPath, parameters)
+        }
+    }
+}
 
 /** Similar to [SignedData] but instead of just attaching the public key, the certificate for the key is attached instead. */
 @CordaSerializable
