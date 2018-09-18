fa2b909cdc1a57578a7fca5b35edbde37bc98245
Michele Sollecito <michele.sollecito@r3.com>
2018-09-18 11:17:00 +0100

    [CORDA-1971]: Refactored the way fields are selected for error code generation. Fixed a bug highlighted by the refactoring.

--- node/src/main/kotlin/net/corda/node/internal/ExceptionsHashFunction.kt
+++ node/src/main/kotlin/net/corda/node/internal/ExceptionsHashFunction.kt
@@ -1,17 +1,20 @@
 package net.corda.node.internal
 
+import net.corda.core.CordaRuntimeException
+import java.lang.IllegalStateException
 import java.util.*
 
-fun Exception.errorCode(): String {
-    val hash = staticLocationBasedHash()
+fun Exception.errorCode(hashedFields: (Throwable) -> Array<out Any?> = Throwable::defaultHashedFields): String {
+    val hash = staticLocationBasedHash(hashedFields)
     return hash.toBase(36)
 }
 
-private fun Throwable.staticLocationBasedHash(visited: Set<Throwable> = setOf(this)): Int {
+private fun Throwable.staticLocationBasedHash(hashedFields: (Throwable) -> Array<out Any?>, visited: Set<Throwable> = setOf(this)): Int {
     val cause = this.cause
+    val fields = hashedFields.invoke(this)
     return when {
-        cause != null && !visited.contains(cause) -> Objects.hash(this::class.java.name, stackTrace.customHashCode(), cause.staticLocationBasedHash(visited +  cause))
-        else -> Objects.hash(this::class.java.name, stackTrace.customHashCode())
+        cause != null && !visited.contains(cause) -> Objects.hash(*fields, cause.staticLocationBasedHash(hashedFields,visited +  cause))
+        else -> Objects.hash(*fields)
     }
 }
 
@@ -25,7 +28,26 @@ private fun Array<StackTraceElement?>?.customHashCode(): Int {
     return Arrays.hashCode(map { it?.customHashCode() ?: 0 }.toIntArray())
 }
 
-private fun StackTraceElement.customHashCode(): Int {
+private fun StackTraceElement.customHashCode(hashedFields: (StackTraceElement) -> Array<out Any?> = StackTraceElement::defaultHashedFields): Int {
 
-    return Objects.hash(StackTraceElement::class.java.name, methodName, lineNumber)
+    return Objects.hash(*hashedFields.invoke(this))
+}
+
+private fun Throwable.defaultHashedFields(): Array<out Any?> {
+
+    return arrayOf(this::class.java.name, stackTrace.customHashCode())
+}
+
+private fun StackTraceElement.defaultHashedFields(): Array<out Any?> {
+
+    return arrayOf(className, methodName, lineNumber)
+}
+
+// TODO sollecitom remove when done
+fun main(args: Array<String>) {
+
+    val exception = CordaRuntimeException("Boom!", IllegalStateException("Oops!"))
+    val exception2 = CordaRuntimeException("Boom!", IllegalStateException("Oops!"))
+    println(exception.errorCode())
+    println(exception2.errorCode())
 }
