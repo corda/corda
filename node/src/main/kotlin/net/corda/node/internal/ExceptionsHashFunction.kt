89c1663e7326562b3b4d6c95f1fef11d917c0339
Michele Sollecito <michele.sollecito@r3.com>
2018-09-18 11:35:54 +0100

    [CORDA-1971]: Modified stacktrace hash function to only take the first 3 elements as part of error code derivation from exceptions.

--- node/src/main/kotlin/net/corda/node/internal/ExceptionsHashFunction.kt
+++ node/src/main/kotlin/net/corda/node/internal/ExceptionsHashFunction.kt
@@ -1,7 +1,5 @@
 package net.corda.node.internal
 
-import net.corda.core.CordaRuntimeException
-import java.lang.IllegalStateException
 import java.util.*
 
 fun Exception.errorCode(hashedFields: (Throwable) -> Array<out Any?> = Throwable::defaultHashedFields): String {
@@ -13,19 +11,16 @@ private fun Throwable.staticLocationBasedHash(hashedFields: (Throwable) -> Array
     val cause = this.cause
     val fields = hashedFields.invoke(this)
     return when {
-        cause != null && !visited.contains(cause) -> Objects.hash(*fields, cause.staticLocationBasedHash(hashedFields,visited +  cause))
+        cause != null && !visited.contains(cause) -> Objects.hash(*fields, cause.staticLocationBasedHash(hashedFields, visited + cause))
         else -> Objects.hash(*fields)
     }
 }
 
 private fun Int.toBase(base: Int): String = Integer.toUnsignedString(this, base)
 
-private fun Array<StackTraceElement?>?.customHashCode(): Int {
+private fun Array<StackTraceElement?>.customHashCode(maxElementsToConsider: Int = this.size): Int {
 
-    if (this == null) {
-        return 0
-    }
-    return Arrays.hashCode(map { it?.customHashCode() ?: 0 }.toIntArray())
+    return Arrays.hashCode(take(maxElementsToConsider).map { it?.customHashCode() ?: 0 }.toIntArray())
 }
 
 private fun StackTraceElement.customHashCode(hashedFields: (StackTraceElement) -> Array<out Any?> = StackTraceElement::defaultHashedFields): Int {
@@ -35,19 +30,10 @@ private fun StackTraceElement.customHashCode(hashedFields: (StackTraceElement) -
 
 private fun Throwable.defaultHashedFields(): Array<out Any?> {
 
-    return arrayOf(this::class.java.name, stackTrace.customHashCode())
+    return arrayOf(this::class.java.name, stackTrace?.customHashCode(3) ?: 0)
 }
 
 private fun StackTraceElement.defaultHashedFields(): Array<out Any?> {
 
     return arrayOf(className, methodName)
-}
-
-// TODO sollecitom remove when done
-fun main(args: Array<String>) {
-
-    val exception = CordaRuntimeException("Boom!", IllegalStateException("Oops!"))
-    val exception2 = CordaRuntimeException("Boom!", IllegalStateException("Oops!"))
-    println(exception.errorCode())
-    println(exception2.errorCode())
 }
