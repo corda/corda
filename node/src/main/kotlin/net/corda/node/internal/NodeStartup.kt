4fd8250cda396710855feae08668e6e878c29bf0
LankyDan <danknewton@hotmail.com>
2018-09-19 14:57:06 +0100

    fix typo for java 8 and ext.java8_minUpdateVersion relation

--- node/src/main/kotlin/net/corda/node/internal/NodeStartup.kt
+++ node/src/main/kotlin/net/corda/node/internal/NodeStartup.kt
@@ -140,7 +140,7 @@ open class NodeStartup : CordaCliWrapper("corda", "Runs a Corda Node") {
     }
 
     private fun hasMinimumJavaVersion(): Boolean {
-        // when the ext.java8_minUpdateVersion gradle constant changes, so much this check
+        // when the ext.java8_minUpdateVersion gradle constant changes, so must this check
         val major = SystemUtils.JAVA_VERSION_FLOAT
         val update = SystemUtils.JAVA_VERSION.substringAfter("_").toLong()
         return major == 1.8F && update >= 171
