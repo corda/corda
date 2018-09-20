b6a677558c68e9a9f98a5b88ed51912f63521e4d
Michele Sollecito <michele.sollecito@r3.com>
2018-09-20 16:20:17 +0100

    [CORDA-1970]: Endpoints exception handling skeleton.

--- tools/error-codes-server/src/main/kotlin/net/corda/tools/error/codes/server/web/endpoints/HealthCheckEndpoint.kt
+++ tools/error-codes-server/src/main/kotlin/net/corda/tools/error/codes/server/web/endpoints/HealthCheckEndpoint.kt
@@ -2,10 +2,15 @@ package net.corda.tools.error.codes.server.web.endpoints
 
 import com.uchuhimo.konf.Config
 import com.uchuhimo.konf.ConfigSpec
+import io.netty.handler.codec.http.HttpHeaderNames
+import io.netty.handler.codec.http.HttpHeaderValues
 import io.netty.handler.codec.http.HttpResponseStatus
 import io.vertx.core.http.HttpMethod
 import io.vertx.core.http.HttpServerResponse
+import io.vertx.core.json.JsonArray
+import io.vertx.core.json.JsonObject
 import io.vertx.ext.web.Router
+import io.vertx.ext.web.RoutingContext
 import net.corda.tools.error.codes.server.commons.web.vertx.Endpoint
 import javax.inject.Inject
 import javax.inject.Named
@@ -16,7 +21,7 @@ internal class HealthCheckEndpoint @Inject constructor(configuration: HealthChec
     // TODO sollecitom introduce a map-based functional Endpoint template
     override fun install(router: Router) {
 
-        router.get(path).failureHandler { ctx -> ctx.failure().writeTo(ctx.response()) }.handler { ctx -> ctx.response().setStatusCode(HttpResponseStatus.OK.code()).end() }
+        router.get(path).failureHandler(::handleFailure).handler { ctx -> ctx.response().setStatusCode(HttpResponseStatus.OK.code()).end() }
     }
 
     override val path = configuration.path
@@ -32,13 +37,26 @@ internal class HealthCheckEndpoint @Inject constructor(configuration: HealthChec
         val path: String
     }
 
-    private fun Throwable.writeTo(response: HttpServerResponse) {
+    // TODO sollecitom move to supertype
+    private fun handleFailure(ctx: RoutingContext) {
 
-        // TODO sollecitom move to supertype
-        response.statusCode = HttpResponseStatus.INTERNAL_SERVER_ERROR.code()
-        message?.let { response.write(it) }
-        response.end()
+        // TODO sollecitom publish event and log
+        val json = JsonObject()
+        json["error"] = "An unexpected error occurred."
+        ctx.response().setStatusCode(HttpResponseStatus.INTERNAL_SERVER_ERROR.code()).end(json)
     }
+
+    // TODO sollecitom move to supertype
+    private operator fun JsonObject.set(key: String, value: Any?) {
+
+        put(key, value)
+    }
+
+    // TODO sollecitom move to supertype
+    private fun HttpServerResponse.end(json: JsonObject) = putHeader(HttpHeaderNames.CONTENT_TYPE, HttpHeaderValues.APPLICATION_JSON).end(json.toBuffer())
+
+    // TODO sollecitom move to supertype
+    private fun HttpServerResponse.end(json: JsonArray) = putHeader(HttpHeaderNames.CONTENT_TYPE, HttpHeaderValues.APPLICATION_JSON).end(json.toBuffer())
 
     @Named
     internal class ConfigProvider @Inject constructor(applyConfigStandards: (Config) -> Config) : HealthCheckEndpoint.Configuration {
