6fa94f34caa1fe21a903a628834e6fb4d1fa4728
Michele Sollecito <michele.sollecito@r3.com>
2018-09-24 15:50:19 +0100

    [CORDA-1970]: Intentions.

--- tools/error-codes-server/src/main/kotlin/net/corda/tools/error/codes/server/ErrorCodesWebApplication.kt
+++ tools/error-codes-server/src/main/kotlin/net/corda/tools/error/codes/server/ErrorCodesWebApplication.kt
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication
 import org.springframework.boot.WebApplicationType
 import org.springframework.boot.autoconfigure.SpringBootApplication
 
+// TODO sollecitom refactor the module to use sub-modules instead of packages.
 // This is the default, but it's better explicit.
 @SpringBootApplication(scanBasePackageClasses = [ErrorCodesWebApplication::class])
 private open class ErrorCodesWebApplication
