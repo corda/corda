a016fb697fe6d5a4be2c773dd48baac5bd3cb6a7
Michele Sollecito <michele.sollecito@r3.com>
2018-09-24 10:29:05 +0100

    [CORDA-1970]: Added a test.

--- tools/error-codes-server/src/test/kotlin/net/corda/tools/error/codes/server/application/CachingErrorDescriptionServiceTest.kt
+++ tools/error-codes-server/src/test/kotlin/net/corda/tools/error/codes/server/application/CachingErrorDescriptionServiceTest.kt
@@ -6,40 +6,49 @@ import net.corda.tools.error.codes.server.domain.InvocationContext
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import reactor.core.publisher.Mono
+import reactor.core.publisher.Mono.empty
 import reactor.core.publisher.Mono.just
 import java.net.URI
 import java.util.*
 
 internal class CachingErrorDescriptionServiceTest {
 
-    // TODO sollecitom add assertion to check params are passed correctly
     @Test
     fun cache_hit_does_not_cause_a_lookup() {
 
         var lookupCalled = false
-        val locationForCode: (ErrorCode) -> Mono<Optional<out ErrorDescriptionLocation>> = { errorCode: ErrorCode ->
-            val location = ErrorDescriptionLocation.External(URI.create("https://stackoverflow.com/questions/3591291/spring-jackson-and-customization-e-g-customdeserializer"), errorCode)
-            just(Optional.of(location))
-        }
-        val retrieveCached: (ErrorCode) -> Mono<Optional<out ErrorDescriptionLocation>> = { errorCode -> locationForCode(errorCode) }
+        val code = ErrorCode("1jwqa1d")
+        val retrieveCached: (ErrorCode) -> Mono<Optional<out ErrorDescriptionLocation>> = { errorCode -> locationForCode(errorCode).also { assertThat(errorCode).isEqualTo(code) } }
         val lookup: (ErrorCode, InvocationContext) -> Mono<Optional<out ErrorDescriptionLocation>> = { errorCode, _ -> locationForCode(errorCode).also { lookupCalled = true } }
         val addToCache: (ErrorCode, ErrorDescriptionLocation) -> Mono<Unit> = { _, _ -> Mono.empty() }
 
         val service = CachingErrorDescriptionService(lookup, retrieveCached, addToCache)
 
-        service.descriptionLocationFor(ErrorCode("1aoj"), InvocationContext.newInstance()).block()
+        service.descriptionLocationFor(code, InvocationContext.newInstance()).block()
 
         assertThat(lookupCalled).isFalse()
+    }
+
+    @Test
+    fun cache_miss_causes_a_lookup() {
+
+        var lookupCalled = false
+        val code = ErrorCode("2kawqa1d")
+        val retrieveCached: (ErrorCode) -> Mono<Optional<out ErrorDescriptionLocation>> = { _ -> Mono.empty() }
+        val lookup: (ErrorCode, InvocationContext) -> Mono<Optional<out ErrorDescriptionLocation>> = { errorCode, _ -> locationForCode(errorCode).also { lookupCalled = true }.also { assertThat(errorCode).isEqualTo(code) } }
+        val addToCache: (ErrorCode, ErrorDescriptionLocation) -> Mono<Unit> = { _, _ -> Mono.empty() }
+
+        val service = CachingErrorDescriptionService(lookup, retrieveCached, addToCache)
+
+        service.descriptionLocationFor(code, InvocationContext.newInstance()).block()
+
+        assertThat(lookupCalled).isTrue()
     }
 
     @Test
     fun looked_up_value_is_cached() {
 
         var addToCacheCalled = false
-        val locationForCode: (ErrorCode) -> Mono<Optional<out ErrorDescriptionLocation>> = { errorCode: ErrorCode ->
-            val location = ErrorDescriptionLocation.External(URI.create("https://stackoverflow.com/questions/3591291/spring-jackson-and-customization-e-g-customdeserializer"), errorCode)
-            just(Optional.of(location))
-        }
         val errorCode = ErrorCode("1jwqa")
         var lookedUpLocation: ErrorDescriptionLocation? = null
 
@@ -58,5 +67,14 @@ internal class CachingErrorDescriptionServiceTest {
         service.descriptionLocationFor(errorCode, InvocationContext.newInstance()).block()
 
         assertThat(addToCacheCalled).isTrue()
+    }
+
+    private fun locationForCode(errorCode: ErrorCode, url: String = "https://stackoverflow.com/questions/3591291/spring-jackson-and-customization-e-g-customdeserializer", location: ErrorDescriptionLocation? = ErrorDescriptionLocation.External(URI.create(url), errorCode)): Mono<Optional<out ErrorDescriptionLocation>> {
+
+        return if (location != null) {
+            just(Optional.of(location))
+        } else {
+            empty()
+        }
     }
 }
