c13ceee210737baec0d507875d3b80d04dc46689
Michele Sollecito <michele.sollecito@r3.com>
2018-09-24 11:01:26 +0100

    [CORDA-1970]: Made Gradle work with jUnit5.

--- tools/error-codes-server/src/test/kotlin/net/corda/tools/error/codes/server/application/CachingErrorDescriptionServiceTest.kt
+++ tools/error-codes-server/src/test/kotlin/net/corda/tools/error/codes/server/application/CachingErrorDescriptionServiceTest.kt
@@ -4,6 +4,7 @@ import net.corda.tools.error.codes.server.domain.ErrorCode
 import net.corda.tools.error.codes.server.domain.ErrorDescriptionLocation
 import net.corda.tools.error.codes.server.domain.InvocationContext
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import reactor.core.publisher.Mono
 import reactor.core.publisher.Mono.empty
@@ -13,54 +14,58 @@ import java.util.*
 
 internal class CachingErrorDescriptionServiceTest {
 
-    @Test
-    fun cache_hit_does_not_cause_a_lookup() {
+    @Nested
+    internal inner class DescriptionLocationFor {
 
-        var lookupCalled = false
-        val code = ErrorCode("1jwqa1d")
-        val retrieveCached = { errorCode: ErrorCode -> locationForCode(errorCode).also { assertThat(errorCode).isEqualTo(code) } }
-        val lookup = { errorCode: ErrorCode, _: InvocationContext -> locationForCode(errorCode).also { lookupCalled = true } }
-        val addToCache = { _: ErrorCode, _: ErrorDescriptionLocation -> empty<Unit>() }
+        @Test
+        fun cache_hit_does_not_cause_a_lookup() {
 
-        val service = CachingErrorDescriptionService(lookup, retrieveCached, addToCache)
+            var lookupCalled = false
+            val code = ErrorCode("1jwqa1d")
+            val retrieveCached = { errorCode: ErrorCode -> locationForCode(errorCode).also { assertThat(errorCode).isEqualTo(code) } }
+            val lookup = { errorCode: ErrorCode, _: InvocationContext -> locationForCode(errorCode).also { lookupCalled = true } }
+            val addToCache = { _: ErrorCode, _: ErrorDescriptionLocation -> empty<Unit>() }
 
-        service.descriptionLocationFor(code, InvocationContext.newInstance()).block()
+            val service = CachingErrorDescriptionService(lookup, retrieveCached, addToCache)
 
-        assertThat(lookupCalled).isFalse()
-    }
+            service.descriptionLocationFor(code, InvocationContext.newInstance()).block()
 
-    @Test
-    fun cache_miss_causes_a_lookup() {
+            assertThat(lookupCalled).isFalse()
+        }
 
-        var lookupCalled = false
-        val code = ErrorCode("2kawqa1d")
-        val retrieveCached = { _: ErrorCode -> empty<Optional<out ErrorDescriptionLocation>>() }
-        val lookup = { errorCode: ErrorCode, _: InvocationContext -> locationForCode(errorCode).also { lookupCalled = true }.also { assertThat(errorCode).isEqualTo(code) } }
-        val addToCache = { _: ErrorCode, _: ErrorDescriptionLocation -> empty<Unit>() }
+        @Test
+        fun cache_miss_causes_a_lookup() {
 
-        val service = CachingErrorDescriptionService(lookup, retrieveCached, addToCache)
+            var lookupCalled = false
+            val code = ErrorCode("2kawqa1d")
+            val retrieveCached = { _: ErrorCode -> empty<Optional<out ErrorDescriptionLocation>>() }
+            val lookup = { errorCode: ErrorCode, _: InvocationContext -> locationForCode(errorCode).also { lookupCalled = true }.also { assertThat(errorCode).isEqualTo(code) } }
+            val addToCache = { _: ErrorCode, _: ErrorDescriptionLocation -> empty<Unit>() }
 
-        service.descriptionLocationFor(code, InvocationContext.newInstance()).block()
+            val service = CachingErrorDescriptionService(lookup, retrieveCached, addToCache)
 
-        assertThat(lookupCalled).isTrue()
-    }
+            service.descriptionLocationFor(code, InvocationContext.newInstance()).block()
 
-    @Test
-    fun looked_up_value_is_cached() {
+            assertThat(lookupCalled).isTrue()
+        }
 
-        var addToCacheCalled = false
-        val errorCode = ErrorCode("1jwqa")
-        var lookedUpLocation: ErrorDescriptionLocation? = null
+        @Test
+        fun looked_up_value_is_cached() {
 
-        val retrieveCached = { _: ErrorCode -> just<Optional<out ErrorDescriptionLocation>>(Optional.empty()) }
-        val lookup = { code: ErrorCode, _: InvocationContext -> locationForCode(code).doOnNext { location -> location.ifPresent { lookedUpLocation = it } } }
-        val addToCache = { code: ErrorCode, location: ErrorDescriptionLocation -> empty<Unit>().also { addToCacheCalled = true }.also { assertThat(code).isEqualTo(errorCode) }.also { assertThat(location).isEqualTo(lookedUpLocation) } }
+            var addToCacheCalled = false
+            val errorCode = ErrorCode("1jwqa")
+            var lookedUpLocation: ErrorDescriptionLocation? = null
 
-        val service = CachingErrorDescriptionService(lookup, retrieveCached, addToCache)
+            val retrieveCached = { _: ErrorCode -> just<Optional<out ErrorDescriptionLocation>>(Optional.empty()) }
+            val lookup = { code: ErrorCode, _: InvocationContext -> locationForCode(code).doOnNext { location -> location.ifPresent { lookedUpLocation = it } } }
+            val addToCache = { code: ErrorCode, location: ErrorDescriptionLocation -> empty<Unit>().also { addToCacheCalled = true }.also { assertThat(code).isEqualTo(errorCode) }.also { assertThat(location).isEqualTo(lookedUpLocation) } }
 
-        service.descriptionLocationFor(errorCode, InvocationContext.newInstance()).block()
+            val service = CachingErrorDescriptionService(lookup, retrieveCached, addToCache)
 
-        assertThat(addToCacheCalled).isTrue()
+            service.descriptionLocationFor(errorCode, InvocationContext.newInstance()).block()
+
+            assertThat(addToCacheCalled).isTrue()
+        }
     }
 
     private fun locationForCode(errorCode: ErrorCode, url: String = "https://stackoverflow.com/questions/3591291/spring-jackson-and-customization-e-g-customdeserializer", location: ErrorDescriptionLocation? = ErrorDescriptionLocation.External(URI.create(url), errorCode)): Mono<Optional<out ErrorDescriptionLocation>> {
