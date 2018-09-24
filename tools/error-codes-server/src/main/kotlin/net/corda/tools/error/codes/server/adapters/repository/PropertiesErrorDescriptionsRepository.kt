4f6193db2746db3db9c9ec3e45bd45bb3d00d14f
Michele Sollecito <michele.sollecito@r3.com>
2018-09-24 11:55:04 +0100

    [CORDA-1970]: Crude property-based error descriptions repository.

--- tools/error-codes-server/src/main/kotlin/net/corda/tools/error/codes/server/adapters/repository/PropertiesErrorDescriptionsRepository.kt
+++ tools/error-codes-server/src/main/kotlin/net/corda/tools/error/codes/server/adapters/repository/PropertiesErrorDescriptionsRepository.kt
@@ -1,29 +1,43 @@
 package net.corda.tools.error.codes.server.adapters.repository
 
 import net.corda.tools.error.codes.server.commons.di.FunctionQualifier
+import net.corda.tools.error.codes.server.commons.lifecycle.Startable
 import net.corda.tools.error.codes.server.domain.ErrorCode
 import net.corda.tools.error.codes.server.domain.ErrorDescriptionLocation
 import net.corda.tools.error.codes.server.domain.ErrorDescriptionsRepository
 import net.corda.tools.error.codes.server.domain.InvocationContext
+import net.corda.tools.error.codes.server.domain.loggerFor
 import reactor.core.publisher.Mono
 import reactor.core.publisher.Mono.empty
 import reactor.core.publisher.Mono.just
 import java.net.URI
 import java.util.*
+import javax.annotation.PostConstruct
 import javax.inject.Inject
 import javax.inject.Named
 
 @Named
-internal class PropertiesErrorDescriptionsRepository : ErrorDescriptionsRepository {
+internal class PropertiesErrorDescriptionsRepository : ErrorDescriptionsRepository, Startable {
+
+    private companion object {
+
+        private val logger = loggerFor<PropertiesErrorDescriptionsRepository>()
+    }
+
+    // TODO sollecitom pass Properties already loaded through constructor
+    private val properties = Properties()
+
+    @PostConstruct
+    override fun start() {
+
+        this.javaClass.classLoader.getResourceAsStream("error_code.properties").use {
+            properties.load(it)
+        }
+    }
 
     override operator fun get(errorCode: ErrorCode, invocationContext: InvocationContext): Mono<Optional<out ErrorDescriptionLocation>> {
 
-        // TODO sollecitom change to read from a properties file
-        if (errorCode.value.length % 2 == 0) {
-            return empty()
-        }
-        val location = ErrorDescriptionLocation.External(URI.create("https://stackoverflow.com/questions/3591291/spring-jackson-and-customization-e-g-customdeserializer"), errorCode)
-        return just(Optional.of(location))
+        return properties.getProperty(errorCode.value)?.let { url -> just<Optional<out ErrorDescriptionLocation>>(Optional.of(ErrorDescriptionLocation.External(URI.create(url), errorCode))) } ?: empty()
     }
 }
 
