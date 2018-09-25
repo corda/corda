7d41092e1fcd56e8449986e12ceadbb425b05dfa
josecoll <jose.coll@r3cev.com>
2018-09-25 17:35:31 +0100

    Remove GRADLE_HOME as no longer needed - everything is driven via gradle wrapper.

--- .ci/ci-gradle-build-cache-init.sh
+++ .ci/ci-gradle-build-cache-init.sh
@@ -5,19 +5,5 @@ export USE_GRADLE_DAEMON="${USE_GRADLE_DAEMON:-false}"
 export GRADLE_CACHE_DEBUG="${GRADLE_CACHE_DEBUG:-false}"
 export PERFORM_GRADLE_SCAN="${PERFORM_GRADLE_SCAN:---scan}"
 
-# GRADLE HOME
-# Required to use custom patched Gradle 4.9 with KEEP-ALIVE timeout fix
-# Currently built on Azure "gradle-build-client" machine at:
-# export GRADLE_HOME=~/gradle/install
-if [[ ! ${GRADLE_HOME} ]]; then
-    echo "Please set GRADLE_HOME variable"
-    exit;
-else
-    echo "GRADLE_HOME is set to '$GRADLE_HOME'"
-fi
-
-echo "Gradle version:"
-${GRADLE_HOME}/bin/gradle --version
-
 # cd %teamcity.build.checkoutDir%
 echo "Using Gradle Build Cache: $GRADLE_BUILD_CACHE_URL"
