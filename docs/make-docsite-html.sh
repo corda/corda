957537547f2bf801953f68e3ff1d8b6f988ccb23
Stefano Franz <stefano.franz@r3.com>
2018-09-26 13:38:49 +0100

    disable dependency on pdf docs for building enterprise

--- docs/make-docsite-html.sh
+++ docs/make-docsite-html.sh
@@ -11,11 +11,7 @@ else
     source virtualenv/Scripts/activate
 fi
 
-echo "Generating PDF document ..."
-make latexpdf
 
 echo "Generating HTML pages ..."
 make html
 
-echo "Moving PDF file from $(eval echo $PWD/build/pdf/corda-developer-site.pdf) to $(eval echo $PWD/build/html/_static/corda-developer-site.pdf)"
-mv $PWD/build/latex/corda-developer-site.pdf $PWD/build/html/_static/corda-developer-site.pdf
