244f8f4b7fd4ca9a753c9e309d27d09126dce951
Andrius Dagys <andrius.dagys@r3.com>
2018-10-05 16:04:21 +0100

    Undo CordaCaplet change

--- node/src/main/java/CordaCaplet.java
+++ node/src/main/java/CordaCaplet.java
@@ -84,7 +84,6 @@ public class CordaCaplet extends Capsule {
             requireCordappsDirExists(cordappsDir);
             // Add additional directories of JARs to the classpath (at the end), e.g., for JDBC drivers.
             augmentClasspath((List<Path>) cp, cordappsDir);
-            augmentClasspath((List<Path>) cp, new File(baseDir, "drivers"));
             try {
                 List<String> jarDirs = nodeConfig.getStringList("jarDirs");
                 log(LOG_VERBOSE, "Configured JAR directories = " + jarDirs);
