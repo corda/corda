b05d31042284d8c3ec6eadf546f8712c466cada1
Michele Sollecito <michele.sollecito@r3.com>
2018-10-17 15:39:46 +0100

    [CORDA-1778]: Extracted common validation and config parsing to separate modules.

--- node/src/main/kotlin/net/corda/common/validation/internal/Validator.kt
+++
@@ -1,6 +0,0 @@
-package net.corda.common.validation.internal
-
-interface Validator<TARGET : Any, ERROR : Any, OPTIONS> {
-
-    fun validate(target: TARGET, options: OPTIONS? = null): Validated<TARGET, ERROR>
-}
