d4f71eedabe28160fe6238ee4127757095df5f68
Stefano Franz <stefano.franz@r3.com>
2018-10-18 16:32:41 +0100

    add some comments about how flow manager weights flows

--- node/src/main/kotlin/net/corda/node/internal/FlowManager.kt
+++ node/src/main/kotlin/net/corda/node/internal/FlowManager.kt
@@ -12,9 +12,27 @@ import net.corda.node.internal.classloading.requireAnnotation
 import net.corda.node.services.config.FlowOverrideConfig
 import net.corda.node.services.statemachine.appName
 import net.corda.node.services.statemachine.flowVersionAndInitiatingClass
-import java.util.concurrent.ConcurrentHashMap
+import javax.annotation.concurrent.ThreadSafe
 import kotlin.reflect.KClass
+/*
 
+This class is responsible for organising which flow should respond to a specific @InitiatingFlow
+
+There are two main ways to modify the behaviour of a cordapp with regards to responding with a different flow
+
+    1.) implementing a new subclass. For example, if we have a ResponderFlow similar to  @InitiatedBy(Sender) MyBaseResponder : FlowLogic
+        If we subclassed a new Flow with specific logic for DB2, it would be similar to IBMB2Responder() : MyBaseResponder
+        When these two flows are encountered by the classpath scan for @InitiatedBy, they will both be selected for responding to Sender
+        This implementation will sort them for responding in order of their "depth" from FlowLogic - see: FlowWeightComparator
+        So IBMB2Responder would win and it would be selected for responding
+
+    2.) It is possible to specify a flowOverride key in the node configuration. Say we configure a node to have
+        flowOverrides{
+          "Sender" = "MyBaseResponder"
+        }
+        In this case, FlowWeightComparator would detect that there is an override in action, and it will assign MyBaseResponder a maximum weight
+        This will result in MyBaseResponder being selected for responding to Sender
+ */
 interface FlowManager {
 
     fun registerInitiatedCoreFlowFactory(initiatingFlowClass: KClass<out FlowLogic<*>>, flowFactory: (FlowSession) -> FlowLogic<*>)
@@ -29,27 +47,29 @@ interface FlowManager {
     fun validateRegistrations()
 }
 
-
+@ThreadSafe
 open class NodeFlowManager(flowOverrides: FlowOverrideConfig? = null) : FlowManager {
 
-
-    val flowOverrides = (flowOverrides
+    private val flowFactories = HashMap<Class<out FlowLogic<*>>, MutableList<RegisteredFlowContainer>>()
+    private val flowOverrides = (flowOverrides
             ?: FlowOverrideConfig()).overrides.map { it.initiator to it.responder }.toMutableMap()
 
     companion object {
         private val log = contextLogger()
+
     }
 
-    private val flowFactories = ConcurrentHashMap<Class<out FlowLogic<*>>, MutableList<RegisteredFlowContainer>>()
-
+    @Synchronized
     override fun getFlowFactoryForInitiatingFlow(initiatedFlowClass: Class<out FlowLogic<*>>): InitiatedFlowFactory<*>? {
         return flowFactories[initiatedFlowClass]?.firstOrNull()?.flowFactory
     }
 
+    @Synchronized
     override fun <F : FlowLogic<*>> registerInitiatedFlow(responder: Class<F>) {
         return registerInitiatedFlow(responder.requireAnnotation<InitiatedBy>().value.java, responder)
     }
 
+    @Synchronized
     override fun <F : FlowLogic<*>> registerInitiatedFlow(initiator: Class<out FlowLogic<*>>, responder: Class<F>) {
         val constructors = responder.declaredConstructors.associateBy { it.parameterTypes.toList() }
         val flowSessionCtor = constructors[listOf(FlowSession::class.java)]?.apply { isAccessible = true }
@@ -75,12 +95,11 @@ open class NodeFlowManager(flowOverrides: FlowOverrideConfig? = null) : FlowMana
         log.info("Registered ${initiator.name} to initiate ${responder.name} (version $version)")
     }
 
-
     private fun <F : FlowLogic<*>> registerInitiatedFlowFactory(initiatingFlowClass: Class<out FlowLogic<*>>,
                                                                 flowFactory: InitiatedFlowFactory<F>,
                                                                 initiatedFlowClass: Class<F>?) {
 
-        check(flowFactory !is InitiatedFlowFactory.Core) { "This should only be used for CORDAPP flows" }
+        check(flowFactory !is InitiatedFlowFactory.Core) { "This should only be used for Cordapp flows" }
         val listOfFlowsForInitiator = flowFactories.computeIfAbsent(initiatingFlowClass) { mutableListOf() }
         if (listOfFlowsForInitiator.isNotEmpty() && listOfFlowsForInitiator.first().type == FlowType.CORE) {
             throw IllegalStateException("Attempting to register over an existing platform flow: $initiatingFlowClass")
@@ -91,21 +110,24 @@ open class NodeFlowManager(flowOverrides: FlowOverrideConfig? = null) : FlowMana
             listOfFlowsForInitiator.add(flowToAdd)
             listOfFlowsForInitiator.sortWith(flowWeightComparator)
             if (listOfFlowsForInitiator.size > 1) {
-                log.info("Mutiliple flows are registered for InitiatingFlow: $initiatingFlowClass, currently using: ${listOfFlowsForInitiator.first().initiatedFlowClass}")
+                log.warn("Multiple flows are registered for InitiatingFlow: $initiatingFlowClass, currently using: ${listOfFlowsForInitiator.first().initiatedFlowClass}")
             }
         }
 
     }
 
     //TODO Harmonise use of these methods - 99% of invocations come from tests.
+    @Synchronized
     override fun registerInitiatedCoreFlowFactory(initiatingFlowClass: KClass<out FlowLogic<*>>, initiatedFlowClass: KClass<out FlowLogic<*>>?, flowFactory: (FlowSession) -> FlowLogic<*>) {
         registerInitiatedCoreFlowFactory(initiatingFlowClass, initiatedFlowClass, InitiatedFlowFactory.Core(flowFactory))
     }
 
+    @Synchronized
     override fun registerInitiatedCoreFlowFactory(initiatingFlowClass: KClass<out FlowLogic<*>>, flowFactory: (FlowSession) -> FlowLogic<*>) {
         registerInitiatedCoreFlowFactory(initiatingFlowClass, null, InitiatedFlowFactory.Core(flowFactory))
     }
 
+    @Synchronized
     override fun registerInitiatedCoreFlowFactory(initiatingFlowClass: KClass<out FlowLogic<*>>, initiatedFlowClass: KClass<out FlowLogic<*>>?, flowFactory: InitiatedFlowFactory.Core<FlowLogic<*>>) {
         require(initiatingFlowClass.java.flowVersionAndInitiatingClass.first == 1) {
             "${InitiatingFlow::class.java.name}.version not applicable for core flows; their version is the node's platform version"
@@ -120,6 +142,8 @@ open class NodeFlowManager(flowOverrides: FlowOverrideConfig? = null) : FlowMana
         log.debug { "Installed core flow ${initiatingFlowClass.java.name}" }
     }
 
+    //To verify the integrity of the current state, it is important that the tip of the responders is a unique weight
+    //if there are multiple flows with the same weight as the tip, it means that it is impossible to reliably pick one as the responder
     private fun validateInvariants(toValidate: List<RegisteredFlowContainer>) {
         val currentTip = toValidate.first()
         val flowWeightComparator = FlowWeightComparator(currentTip.initiatingFlowClass, flowOverrides)
@@ -130,23 +154,28 @@ open class NodeFlowManager(flowOverrides: FlowOverrideConfig? = null) : FlowMana
         }
     }
 
+    @Synchronized
     override fun validateRegistrations() {
         flowFactories.values.forEach {
             validateInvariants(it)
         }
     }
 
-
-    enum class FlowType {
+    private enum class FlowType {
         CORE, CORDAPP
     }
 
-    data class RegisteredFlowContainer(val initiatingFlowClass: Class<out FlowLogic<*>>,
+    private data class RegisteredFlowContainer(val initiatingFlowClass: Class<out FlowLogic<*>>,
                                        val initiatedFlowClass: Class<out FlowLogic<*>>?,
                                        val flowFactory: InitiatedFlowFactory<FlowLogic<*>>,
                                        val type: FlowType)
 
-    open class FlowWeightComparator(val initiatingFlowClass: Class<out FlowLogic<*>>, val flowOverrides: Map<String, String>) : Comparator<NodeFlowManager.RegisteredFlowContainer> {
+    //this is used to sort the responding flows in order of "importance"
+    // the logic is as follows
+    // IF responder is a specific lambda (like for notary implementations / testing code) always return that responder
+    // ELSE IF responder is present in the overrides list, always return that responder
+    // ELSE compare responding flows by their depth from FlowLogic, always return the flow which is most specific (IE, has the most hops to FlowLogic)
+    private open class FlowWeightComparator(val initiatingFlowClass: Class<out FlowLogic<*>>, val flowOverrides: Map<String, String>) : Comparator<NodeFlowManager.RegisteredFlowContainer> {
 
         override fun compare(o1: NodeFlowManager.RegisteredFlowContainer, o2: NodeFlowManager.RegisteredFlowContainer): Int {
             if (o1.initiatedFlowClass == null && o2.initiatedFlowClass != null) {
@@ -183,9 +212,8 @@ open class NodeFlowManager(flowOverrides: FlowOverrideConfig? = null) : FlowMana
         }
 
     }
-
 }
 
 private fun <X, Y> Iterable<Pair<X, Y>>.toMutableMap(): MutableMap<X, Y> {
-    return this.toMap(kotlin.collections.HashMap())
+    return this.toMap(HashMap())
 }
