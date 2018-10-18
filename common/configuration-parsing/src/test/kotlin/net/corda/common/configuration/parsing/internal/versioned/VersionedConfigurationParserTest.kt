dd18dd316c40f9063890b935cf5228243d05c1f5
Michele Sollecito <michele.sollecito@r3.com>
2018-10-18 14:28:35 +0100

    [CORDA-1778]: VersionedConfigurationParserTest is good to go.

--- common/configuration-parsing/src/test/kotlin/net/corda/common/configuration/parsing/internal/versioned/VersionedConfigurationParserTest.kt
+++ common/configuration-parsing/src/test/kotlin/net/corda/common/configuration/parsing/internal/versioned/VersionedConfigurationParserTest.kt
@@ -1,11 +1,6 @@
 package net.corda.common.configuration.parsing.internal.versioned
 
-import com.typesafe.config.Config
-import net.corda.common.configuration.parsing.internal.Configuration
-import net.corda.common.configuration.parsing.internal.configObject
-import net.corda.common.validation.internal.Validated
-import net.corda.common.validation.internal.Validated.Companion.invalid
-import net.corda.common.validation.internal.Validated.Companion.valid
+import net.corda.common.configuration.parsing.internal.*
 import org.assertj.core.api.Assertions.assertThat
 import org.assertj.core.api.Assertions.assertThatThrownBy
 import org.junit.Test
@@ -15,17 +10,27 @@ class VersionedConfigurationParserTest {
     @Test
     fun correct_matching_function_is_used_for_supported_version() {
 
+        val defaultVersion = 1
+        val version = 5
+        val valueForVersion = "Five"
 
+        val configuration = configObject().toConfig()
+        val parser = VersionedConfigurationParser.mapping(extractVersion(version), defaultVersion, version to extractValidValue(valueForVersion), 1 to extractValidValue("One"))
+
+        val result = parser.parse(configuration, Configuration.Validation.Options(strict = false))
+
+        assertThat(result.isValid).isTrue()
+        assertThat(result.valueIfValid).isEqualTo(valueForVersion)
     }
 
     @Test
     fun unsupported_version_raises_exception() {
 
         val defaultVersion = 1
-        val unsupportedVersion: Int? = 5
+        val unsupportedVersion = 5
 
         val configuration = configObject().toConfig()
-        val parser = VersionedConfigurationParser.mapping(extractValidValue(unsupportedVersion), defaultVersion, 1 to extractValidValue("One"), 2 to extractValidValue("Two"))
+        val parser = VersionedConfigurationParser.mapping(extractVersion(unsupportedVersion), defaultVersion, 1 to extractValidValue("One"), 2 to extractValidValue("Two"))
 
         assertThatThrownBy { parser.parse(configuration, Configuration.Validation.Options(strict = false)) }.isInstanceOf(VersionedConfigurationParser.Exception.UnsupportedVersion::class.java)
     }
@@ -68,19 +73,5 @@ class VersionedConfigurationParserTest {
         val result = parser.parse(configuration, Configuration.Validation.Options(strict = false)).valueOrThrow()
 
         assertThat(result).isEqualTo(expectedResult)
-    }
-
-    private val extractMissingVersion: Configuration.Value.Parser<Int?> = extractValidValue(null)
-
-    // TODO sollecitom move to common
-    private fun <VALUE> extractValidValue(value: VALUE) = extractValue(valid(value))
-
-    // TODO sollecitom move to common
-    private fun <VALUE> extractValueWithErrors(errors: Set<Configuration.Validation.Error>) = extractValue<VALUE>(invalid(errors))
-
-    // TODO sollecitom move to common
-    private fun <VALUE> extractValue(value: Validated<VALUE, Configuration.Validation.Error>) = object : Configuration.Value.Parser<VALUE> {
-
-        override fun parse(configuration: Config, options: Configuration.Validation.Options): Validated<VALUE, Configuration.Validation.Error> = value
     }
 }
