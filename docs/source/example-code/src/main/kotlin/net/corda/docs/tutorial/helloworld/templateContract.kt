c3ba304ed6eda5f9a5597fae89e8e7c027128793
Joel Dudley <joel.dudley@r3cev.com>
2018-10-22 07:59:40 +0200

    Updates tutorial code to match template changes.

--- docs/source/example-code/src/main/kotlin/net/corda/docs/tutorial/helloworld/templateContract.kt
+++ docs/source/example-code/src/main/kotlin/net/corda/docs/tutorial/helloworld/templateContract.kt
@@ -4,9 +4,12 @@ import net.corda.core.contracts.CommandData
 import net.corda.core.contracts.Contract
 import net.corda.core.transactions.LedgerTransaction
 
-val TEMPLATE_CONTRACT_ID = "com.template.TemplateContract"
-
 open class TemplateContract : Contract {
+    // This is used to identify our contract when building a transaction.
+    companion object {
+        val ID = "com.template.TemplateContract"
+    }
+    
     // A transaction is considered valid if the verify() function of the contract of each of the transaction's input
     // and output states does not throw an exception.
     override fun verify(tx: LedgerTransaction) {
