8da8fb0a5cfb60ff04e900071f227f8307dab91f
rick.parker <rick.parker@r3cev.com>
2018-11-06 16:09:22 +0000

    Replace error code generation technique with custom event re-writer. Switch to RandomAccessFile appenders because they supposedly give higher throughput.

---
+++ node/src/test/kotlin/net/corda/node/utilities/logging/AsyncLoggingTest.kt
@@ -0,0 +1,13 @@
+package net.corda.node.utilities.logging
+
+import org.junit.Test
+import kotlin.test.assertTrue
+
+
+class AsyncLoggingTest {
+
+    @Test
+    fun `async logging is configured`() {
+        assertTrue(AsyncLoggerContextSelectorNoThreadLocal.isSelected())
+    }
+}
