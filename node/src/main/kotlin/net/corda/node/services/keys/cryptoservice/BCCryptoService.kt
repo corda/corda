6ac5439de0d168dbdb05eff4304bb4c90feb65b8
Kostas Chalkias <konstantinos.chalkias@r3.com>
2018-11-07 17:31:13 +0000

    Throw CryptoServiceException in BCCryptoService

--- node/src/main/kotlin/net/corda/node/services/keys/cryptoservice/BCCryptoService.kt
+++ node/src/main/kotlin/net/corda/node/services/keys/cryptoservice/BCCryptoService.kt
@@ -2,11 +2,13 @@ package net.corda.node.services.keys.cryptoservice
 
 import net.corda.core.crypto.Crypto
 import net.corda.core.crypto.newSecureRandom
+import net.corda.core.crypto.sha256
 import net.corda.node.services.config.NodeConfiguration
 import net.corda.nodeapi.internal.config.CertificateStore
 import net.corda.nodeapi.internal.crypto.ContentSignerBuilder
 import net.corda.nodeapi.internal.crypto.X509Utilities
 import net.corda.nodeapi.internal.cryptoservice.CryptoService
+import net.corda.nodeapi.internal.cryptoservice.CryptoServiceException
 import org.bouncycastle.operator.ContentSigner
 import java.security.KeyPair
 import java.security.KeyStore
@@ -24,9 +26,13 @@ class BCCryptoService(private val nodeConf: NodeConfiguration) : CryptoService {
     internal var certificateStore: CertificateStore = nodeConf.signingCertificateStore.get(true)
 
     override fun generateKeyPair(alias: String, schemeNumberID: Int): PublicKey {
-        val keyPair = Crypto.generateKeyPair(Crypto.findSignatureScheme(schemeNumberID))
-        importKey(alias, keyPair)
-        return keyPair.public
+        try {
+            val keyPair = Crypto.generateKeyPair(Crypto.findSignatureScheme(schemeNumberID))
+            importKey(alias, keyPair)
+            return keyPair.public
+        } catch (e: Exception) {
+            throw CryptoServiceException("Cannot generate key for alias $alias and signature scheme with id $schemeNumberID -- (${e.message})", e.cause)
+        }
     }
 
     override fun containsKey(alias: String): Boolean {
@@ -34,17 +40,29 @@ class BCCryptoService(private val nodeConf: NodeConfiguration) : CryptoService {
     }
 
     override fun getPublicKey(alias: String): PublicKey {
-        return certificateStore.query { getPublicKey(alias) }
+        try {
+            return certificateStore.query { getPublicKey(alias) }
+        } catch (e: Exception) {
+            throw CryptoServiceException("Cannot get public key for alias $alias -- (${e.message})", e.cause)
+        }
     }
 
     override fun sign(alias: String, data: ByteArray): ByteArray {
-        return Crypto.doSign(certificateStore.query { getPrivateKey(alias, certificateStore.entryPassword) } , data)
+        try {
+            return Crypto.doSign(certificateStore.query { getPrivateKey(alias, certificateStore.entryPassword) }, data)
+        } catch (e: Exception) {
+            throw CryptoServiceException("Cannot sign using the key with alias $alias. SHA256 of data to be signed: ${data.sha256()} -- (${e.message})", e.cause)
+        }
     }
 
     override fun getSigner(alias: String): ContentSigner {
-        val privateKey = certificateStore.query { getPrivateKey(alias, certificateStore.entryPassword) }
-        val signatureScheme = Crypto.findSignatureScheme(privateKey)
-        return ContentSignerBuilder.build(signatureScheme, privateKey, Crypto.findProvider(signatureScheme.providerName), newSecureRandom())
+        try {
+            val privateKey = certificateStore.query { getPrivateKey(alias, certificateStore.entryPassword) }
+            val signatureScheme = Crypto.findSignatureScheme(privateKey)
+            return ContentSignerBuilder.build(signatureScheme, privateKey, Crypto.findProvider(signatureScheme.providerName), newSecureRandom())
+        } catch (e: Exception) {
+            throw CryptoServiceException("Cannot get Signer for key with alias $alias -- (${e.message})", e.cause)
+        }
     }
 
     /**
@@ -58,9 +76,13 @@ class BCCryptoService(private val nodeConf: NodeConfiguration) : CryptoService {
 
     /** Import an already existing [KeyPair] to this [CryptoService]. */
     fun importKey(alias: String, keyPair: KeyPair) {
-        // Store a self-signed certificate, as Keystore requires to store certificates instead of public keys.
-        // We could probably add a null cert, but we store a self-signed cert that will be used to retrieve the public key.
-        val cert = X509Utilities.createSelfSignedCACertificate(nodeConf.myLegalName.x500Principal, keyPair)
-        certificateStore.query { setPrivateKey(alias, keyPair.private, listOf(cert), certificateStore.entryPassword) }
+        try {
+            // Store a self-signed certificate, as Keystore requires to store certificates instead of public keys.
+            // We could probably add a null cert, but we store a self-signed cert that will be used to retrieve the public key.
+            val cert = X509Utilities.createSelfSignedCACertificate(nodeConf.myLegalName.x500Principal, keyPair)
+            certificateStore.query { setPrivateKey(alias, keyPair.private, listOf(cert), certificateStore.entryPassword) }
+        } catch (e: Exception) {
+            throw CryptoServiceException("Cannot import key with alias $alias -- (${e.message})", e.cause)
+        }
     }
 }
