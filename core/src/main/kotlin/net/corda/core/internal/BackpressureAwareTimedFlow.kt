5629d486f5bac37b1f82c1925ad3c87029688ff3
Christian Sailer <christian.sailer@r3.com>
2018-11-14 16:13:02 +0000

    Add missing annotation

--- core/src/main/kotlin/net/corda/core/internal/BackpressureAwareTimedFlow.kt
+++ core/src/main/kotlin/net/corda/core/internal/BackpressureAwareTimedFlow.kt
@@ -1,11 +1,13 @@
 package net.corda.core.internal
 
+import co.paralleluniverse.fibers.Suspendable
 import net.corda.core.flows.FlowLogic
 import net.corda.core.flows.FlowSession
 import net.corda.core.flows.WaitTimeUpdate
 import net.corda.core.utilities.UntrustworthyData
 
 abstract class BackpressureAwareTimedFlow<ResultType, ReceiveType>(val receiveType: Class<ReceiveType>) : FlowLogic<ResultType>(), TimedFlow {
+    @Suspendable
     fun <T> receiveResultOrTiming(session: FlowSession): UntrustworthyData<ReceiveType> {
         while (true) {
             val wrappedResult = session.receive<Any>()
