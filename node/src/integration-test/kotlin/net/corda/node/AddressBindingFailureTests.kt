48f19f5f141fbd168eb9a4c3b66d30dab2e7c860
Michele Sollecito <michele.sollecito@r3.com>
2018-11-26 14:30:29 +0000

    Reverted my changes to notary handles.

--- node/src/integration-test/kotlin/net/corda/node/AddressBindingFailureTests.kt
+++ node/src/integration-test/kotlin/net/corda/node/AddressBindingFailureTests.kt
@@ -1,13 +1,11 @@
 package net.corda.node
 
-import net.corda.core.identity.CordaX500Name
+import net.corda.core.internal.errors.AddressBindingException
 import net.corda.core.utilities.NetworkHostAndPort
 import net.corda.core.utilities.getOrThrow
-import net.corda.core.internal.errors.AddressBindingException
 import net.corda.testing.driver.DriverParameters
 import net.corda.testing.driver.PortAllocation
 import net.corda.testing.driver.driver
-import net.corda.testing.node.NotarySpec
 import org.assertj.core.api.Assertions.assertThat
 import org.assertj.core.api.Assertions.assertThatThrownBy
 import org.junit.Test
@@ -31,25 +29,6 @@ class AddressBindingFailureTests {
 
     @Test
     fun `H2 address`() = assertBindExceptionForOverrides { address -> mapOf("h2Settings" to mapOf("address" to address.toString()), "dataSourceProperties.dataSource.password" to "password") }
-
-    @Test
-    fun `notary P2P address`() {
-        ServerSocket(0).use { socket ->
-
-            val notaryName = CordaX500Name.parse("O=Notary Cleaning Service, L=Zurich, C=CH")
-            val address = InetSocketAddress(socket.localPort).toNetworkHostAndPort()
-
-            assertThatThrownBy {
-                driver(DriverParameters(startNodesInProcess = false,
-                    notarySpecs = listOf(NotarySpec(notaryName)),
-                    notaryCustomOverrides = mapOf("p2pAddress" to address.toString()),
-                        portAllocation = portAllocation)
-                ) {} }.isInstanceOfSatisfying(IllegalStateException::class.java) { error ->
-
-                assertThat(error.message).contains("Unable to start notaries")
-            }
-        }
-    }
 
     private fun assertBindExceptionForOverrides(overrides: (NetworkHostAndPort) -> Map<String, Any?>) {
 
