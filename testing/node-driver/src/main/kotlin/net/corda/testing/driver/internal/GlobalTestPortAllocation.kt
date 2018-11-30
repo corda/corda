0a7d200261e43431a7cb98d1e1e3de733b3c0eef
Michele Sollecito <michele.sollecito@r3.com>
2018-11-30 15:53:45 +0000

    Enabled system property / env variable cascade for global test port allocation toggles.

--- testing/node-driver/src/main/kotlin/net/corda/testing/driver/internal/GlobalTestPortAllocation.kt
+++ testing/node-driver/src/main/kotlin/net/corda/testing/driver/internal/GlobalTestPortAllocation.kt
@@ -1,11 +1,15 @@
 package net.corda.testing.driver.internal
 
 import net.corda.testing.driver.PortAllocation
+import net.corda.testing.driver.internal.GlobalTestPortAllocation.enablingEnvVar
+import net.corda.testing.driver.internal.GlobalTestPortAllocation.enablingSystemProperty
+import net.corda.testing.driver.internal.GlobalTestPortAllocation.startingPortEnvVariable
+import net.corda.testing.driver.internal.GlobalTestPortAllocation.startingPortSystemProperty
 
 fun incrementalPortAllocation(startingPortIfNoEnv: Int): PortAllocation {
 
     return when {
-        System.getenv(GlobalTestPortAllocation.enablingEnvVar)?.toBoolean() == true -> GlobalTestPortAllocation
+        System.getProperty(enablingSystemProperty)?.toBoolean() ?: System.getenv(enablingEnvVar)?.toBoolean() == true -> GlobalTestPortAllocation
         else -> PortAllocation.Incremental(startingPortIfNoEnv)
     }
 }
@@ -14,7 +18,9 @@ private object GlobalTestPortAllocation : PortAllocation.Incremental(startingPor
 
     const val enablingEnvVar = "CORDA_TEST_GLOBAL_PORT_ALLOCATION_ENABLED"
     const val startingPortEnvVariable = "CORDA_TEST_GLOBAL_PORT_ALLOCATION_STARTING_PORT"
+    val enablingSystemProperty = enablingEnvVar.toLowerCase().replace("_", ".")
+    val startingPortSystemProperty = startingPortEnvVariable.toLowerCase().replace("_", ".")
     const val startingPortDefaultValue = 5000
 }
 
-private val startingPort: Int = System.getenv(GlobalTestPortAllocation.startingPortEnvVariable)?.toIntOrNull() ?: GlobalTestPortAllocation.startingPortDefaultValue
+private val startingPort: Int = System.getProperty(startingPortSystemProperty)?.toIntOrNull() ?: System.getenv(startingPortEnvVariable)?.toIntOrNull() ?: GlobalTestPortAllocation.startingPortDefaultValue
