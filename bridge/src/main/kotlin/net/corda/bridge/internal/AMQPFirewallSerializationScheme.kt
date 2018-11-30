b5e5f8a5ac4951d6b1e5bde611342cea7db382fc
Tudor Malene <tudor.malene@r3.com>
2018-11-30 18:12:54 +0000

    Fix merge

--- bridge/src/main/kotlin/net/corda/bridge/internal/AMQPFirewallSerializationScheme.kt
+++ bridge/src/main/kotlin/net/corda/bridge/internal/AMQPFirewallSerializationScheme.kt
@@ -5,15 +5,12 @@ import net.corda.core.serialization.ClassWhitelist
 import net.corda.core.serialization.SerializationContext
 import net.corda.core.serialization.SerializationCustomSerializer
 import net.corda.serialization.internal.CordaSerializationMagic
-import net.corda.serialization.internal.amqp.AbstractAMQPSerializationScheme
-import net.corda.serialization.internal.amqp.AccessOrderLinkedHashMap
-import net.corda.serialization.internal.amqp.SerializerFactory
-import net.corda.serialization.internal.amqp.amqpMagic
+import net.corda.serialization.internal.amqp.*
 import java.util.concurrent.ConcurrentHashMap
 
 class AMQPFirewallSerializationScheme(
         cordappCustomSerializers: Set<SerializationCustomSerializer<*, *>>,
-        serializerFactoriesForContexts: AccessOrderLinkedHashMap<Pair<ClassWhitelist, ClassLoader>, SerializerFactory>
+        serializerFactoriesForContexts: AccessOrderLinkedHashMap<SerializationFactoryCacheKey, SerializerFactory>
 ) : AbstractAMQPSerializationScheme(cordappCustomSerializers, serializerFactoriesForContexts) {
     constructor(cordapps: List<Cordapp>) : this(cordapps.customSerializers, AccessOrderLinkedHashMap { 128 })
 
