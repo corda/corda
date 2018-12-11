af7bcfb809c268106b4fde4d2d028400015bebc7
Stefano Franz <stefano.franz@r3.com>
2018-12-11 14:20:50 +0000

    fix issue due to missing Dockerfile with AlpineZulu build

--- docker/container-scan/docker_up.sh
+++ docker/container-scan/docker_up.sh
@@ -16,7 +16,7 @@ docker run --network="clair-scanning" -d --name clair arminc/clair-local-scan:v2
 
 ( docker rm -f scanner || true )  && docker run --network="clair-scanning" -ti -v /var/run/docker.sock:/var/run/docker.sock -v $(pwd):/output --name scanner \
     roastario/clair-local-scanner:latest \
-    clair-scanner --ip scanner --clair="http://clair:6060" --threshold="Low" --report="/output/corretto.json" entdocker.corda.r3cev.com/corda-enterprise-alpine-zulu-4.0-snapshot:latest
+    clair-scanner --ip scanner --clair="http://clair:6060" --threshold="Low" --report="/output/alpine-zulu.json" entdocker.corda.r3cev.com/corda-enterprise-alpine-zulu-4.0-snapshot:latest
 
 docker rm -f postgres
 docker rm -f clair
