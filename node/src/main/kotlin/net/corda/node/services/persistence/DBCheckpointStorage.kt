c133974109b3f4357f3b3401029858e79af88866
Emanuel Russo <emanuelrusso93@gmail.com>
2018-12-11 17:31:27 +0100

    Update DBCheckpointStorage.kt
    Fix for Code Style Guideline

--- node/src/main/kotlin/net/corda/node/services/persistence/DBCheckpointStorage.kt
+++ node/src/main/kotlin/net/corda/node/services/persistence/DBCheckpointStorage.kt
@@ -34,14 +34,10 @@ class DBCheckpointStorage : CheckpointStorage {
 
             @Type(type = "corda-blob")
             @Column(name = "checkpoint_value", nullable = false)
-            var checkpoint: ByteArray = EMPTY_BYTE_ARRAY
-            
-    ){
-        override fun toString(): String{
-		    val DBCheckpoint2Str = "{checkpointId = ${checkpointId}, checkpointSize = ${checkpoint.size}}"
-		    return DBCheckpoint2Str
-	    }
-    }
+            var checkpoint: ByteArray = EMPTY_BYTE_ARRAY          
+    ) {
+	    override fun toString() = "DBCheckpoint(checkpointId = ${checkpointId}, checkpointSize = ${checkpoint.size})"
+      }
 
     override fun addCheckpoint(id: StateMachineRunId, checkpoint: SerializedBytes<Checkpoint>) {
         currentDBSession().save(DBCheckpoint().apply {
