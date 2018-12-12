62fcc5a82e57786e3cc3e19c0d3d9b7d5d2063c2
Stefano Franz <stefano.franz@r3.com>
2018-12-12 16:50:09 +0000

    add corda-firewall image

---
+++ docker/src/bash/run-firewall.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+: ${JVM_ARGS='-XX:+UseG1GC'}
+
+JVM_ARGS="-XX:+UnlockExperimentalVMOptions -XX:+UseCGroupMemoryLimitForHeap "${JVM_ARGS}
+
+if [[ ${JVM_ARGS} == *"Xmx"* ]]; then
+  echo "WARNING: the use of the -Xmx flag is not recommended within docker containers. Use the --memory option passed to the container to limit heap size"
+fi
+
+java -Djava.security.egd=file:/dev/./urandom -Dcapsule.jvm.args="${JVM_ARGS}" -jar /opt/corda/bin/corda-firewall.jar -b=/opt/corda --config-file=/etc/corda/firewall.conf
