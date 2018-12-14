06a82d22384d1e8fcc7c16f345299077201c871e
rick.parker <rick.parker@r3cev.com>
2018-12-14 16:49:53 +0000

    Review feedback

--- core/src/main/kotlin/net/corda/core/node/services/AttachmentStorage.kt
+++ core/src/main/kotlin/net/corda/core/node/services/AttachmentStorage.kt
@@ -3,7 +3,8 @@ package net.corda.core.node.services
 import net.corda.core.DoNotImplement
 import net.corda.core.contracts.Attachment
 import net.corda.core.crypto.SecureHash
-import net.corda.core.node.services.vault.*
+import net.corda.core.node.services.vault.AttachmentQueryCriteria
+import net.corda.core.node.services.vault.AttachmentSort
 import java.io.IOException
 import java.io.InputStream
 import java.nio.file.FileAlreadyExistsException
@@ -76,7 +77,8 @@ interface AttachmentStorage {
     }
 
     /**
-     * Find the Attachment Id of the contract attachment with the highest version for a given contract class name.
+     * Find the Attachment Id of the contract attachment with the highest version for a given contract class name
+     * from trusted upload sources.
      *
      * @param contractClassName The fully qualified name of the contract class.
      * @param minContractVersion The minimum contract version that should be returned.
