3e87e2b365adb1c525f4e9ce5607d8bb6caafee7
Florian Friemel <florian.friemel@r3.com>
2018-12-17 13:55:25 +0000

    Rename and clean up.

--- serialization/src/test/kotlin/net/corda/serialization/internal/amqp/DeserializeQueryableStateTest.kt
+++ serialization/src/test/kotlin/net/corda/serialization/internal/amqp/DeserializeQueryableStateTest.kt
@@ -4,7 +4,10 @@ import net.corda.core.identity.AbstractParty
 import net.corda.core.schemas.MappedSchema
 import net.corda.core.schemas.PersistentState
 import net.corda.core.schemas.QueryableState
+import net.corda.core.serialization.SerializationContext
+import net.corda.core.serialization.SerializedBytes
 import net.corda.serialization.internal.AllWhitelist
+import net.corda.serialization.internal.SerializationContextImpl
 import net.corda.serialization.internal.amqp.testutils.*
 import net.corda.serialization.internal.carpenter.ClassCarpenterImpl
 import org.junit.Test
@@ -30,19 +33,31 @@ class ClassLoaderWithoutTestState(classLoader: ClassLoader): ClassLoader(classLo
     }
 }
 
-class DeserializeQueyableStateTest {
+class DeserializeQueryableStateTest {
 
     /**
-     * Reproduce CORDA-2330
+     * Set `lenient` to false to reproduce CORDA-2330
      * https://r3-cev.atlassian.net/browse/CORDA-2330
      */
     @Test
-    fun queryableState() {
+    fun `should deserialize subclass of QueryableState that is not present in the class loader`() {
 
-        val sf = SerializerFactoryBuilder.build(AllWhitelist, ClassCarpenterImpl(AllWhitelist, ClassLoaderWithoutTestState(ClassLoader.getSystemClassLoader())))
+        val lenient = true
         val instance = TestState(listOf())
         val serialized = TestSerializationOutput(false, testDefaultFactory()).serialize(instance)
 
-        DeserializationInput(sf).deserialize(serialized)
+        val sf = SerializerFactoryBuilder.build(AllWhitelist, ClassCarpenterImpl(AllWhitelist, ClassLoaderWithoutTestState(ClassLoader.getSystemClassLoader()), lenient))
+
+        val context = SerializationContextImpl(
+                preferredSerializationVersion = amqpMagic,
+                deserializationClassLoader = ClassLoaderWithoutTestState(ClassLoader.getSystemClassLoader()),
+                whitelist = AllWhitelist,
+                properties = serializationProperties,
+                objectReferencesEnabled = false,
+                useCase = SerializationContext.UseCase.Testing,
+                encoding = null)
+        val bytes = SerializedBytes<QueryableState>(serialized.bytes)
+        // will throw when lenient is false
+        DeserializationInput(sf).deserialize(bytes, context)
     }
 }
