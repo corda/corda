c08f65a92cd76395833a25f723eec4d61b538ae2
Shams Asari <shams.asari@r3.com>
2018-12-27 14:21:37 +0000

    CORDA-2128: Moved all the helper methods added to NetworkParameters.kt into internal (#4466)

--- core/src/main/kotlin/net/corda/core/internal/CordaUtils.kt
+++ core/src/main/kotlin/net/corda/core/internal/CordaUtils.kt
@@ -1,12 +1,14 @@
 package net.corda.core.internal
 
 import net.corda.core.DeleteForDJVM
+import net.corda.core.contracts.ContractClassName
 import net.corda.core.cordapp.Cordapp
 import net.corda.core.cordapp.CordappConfig
 import net.corda.core.cordapp.CordappContext
 import net.corda.core.crypto.SecureHash
 import net.corda.core.flows.DataVendingFlow
 import net.corda.core.flows.FlowLogic
+import net.corda.core.node.NetworkParameters
 import net.corda.core.node.ServicesForResolution
 import net.corda.core.node.ZoneVersionTooLowException
 import net.corda.core.serialization.CordaSerializable
@@ -16,6 +18,7 @@ import net.corda.core.transactions.SignedTransaction
 import net.corda.core.transactions.TransactionBuilder
 import net.corda.core.transactions.WireTransaction
 import org.slf4j.MDC
+import java.security.PublicKey
 
 // *Internal* Corda-specific utilities.
 
@@ -71,9 +74,7 @@ private fun isPackageValid(packageName: String): Boolean {
             }
 }
 
-/**
- * Check if a string is a legal Java package name.
- */
+/** Check if a string is a legal Java package name. */
 fun requirePackageValid(name: String) {
     require(isPackageValid(name)) { "Invalid Java package name: `$name`." }
 }
@@ -85,3 +86,33 @@ fun requirePackageValid(name: String) {
  */
 @CordaSerializable
 object RetrieveAnyTransactionPayload : ArrayList<Any>()
+
+/**
+ * Returns true if the [fullClassName] is in a subpackage of [packageName].
+ * E.g.: "com.megacorp" owns "com.megacorp.tokens.MegaToken"
+ *
+ * Note: The ownership check is ignoring case to prevent people from just releasing a jar with: "com.megaCorp.megatoken" and pretend they are MegaCorp.
+ * By making the check case insensitive, the node will require that the jar is signed by MegaCorp, so the attack fails.
+ */
+private fun owns(packageName: String, fullClassName: String): Boolean = fullClassName.startsWith("$packageName.", ignoreCase = true)
+
+/** Returns the public key of the package owner of the [contractClassName], or null if not owned. */
+internal fun NetworkParameters.getPackageOwnerOf(contractClassName: ContractClassName): PublicKey? {
+    return packageOwnership.entries.singleOrNull { owns(it.key, contractClassName) }?.value
+}
+
+/**
+ * Returns the public key of the package owner if any of [contractClassNames] match, or null if not owned.
+ */
+internal fun NetworkParameters.getPackageOwnerOf(contractClassNames: Set<ContractClassName>): PublicKey? {
+    for (contractClassName in contractClassNames) {
+        val owner = getPackageOwnerOf(contractClassName)
+        if (owner != null) return owner
+    }
+    return null
+}
+
+// Make sure that packages don't overlap so that ownership is clear.
+fun noPackageOverlap(packages: Collection<String>): Boolean {
+    return packages.all { outer -> packages.none { inner -> inner != outer && inner.startsWith("$outer.") } }
+}
