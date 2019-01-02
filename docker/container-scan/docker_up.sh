f99d92c91efdcae4c3a66f16791facf6a215b717
Stefano Franz <stefano.franz@r3.com>
2019-01-02 17:30:33 +0000

    refactor code to use common build file for all docker images
    add corda-firewall image
    
    refactor code to use common build file for all docker images

--- docker/container-scan/docker_up.sh
+++ docker/container-scan/docker_up.sh
@@ -1,24 +1,30 @@
 #!/usr/bin/env bash
 
-#docker build . -f DockerfileScanner -t roastario/clair-local-scanner:latest
+#docker build . -f DockerfileScanner -t corda/clair-local-scanner:latest
+SCANNER_CONTAINER_NAME=scanner
+NETWORK_NAME=clair-scanning
+mkdir scanning_output
+
+function tidy_up()
+{
+    (docker rm -f postgres || true) && \
+    (docker rm -f clair || true) && \
+    (docker rm -f ${SCANNER_CONTAINER_NAME} || true) && \
+    (docker network rm clair-scanning || true)
+}
+
+tidy_up
 
 docker network create --attachable clair-scanning
-docker run --network="clair-scanning" -d --name postgres arminc/clair-db:latest
-docker run --network="clair-scanning" -d --name clair arminc/clair-local-scan:v2.0.6
+docker run --network="${NETWORK_NAME}" -d --name postgres arminc/clair-db:latest
+docker run --network="${NETWORK_NAME}" -d --name clair arminc/clair-local-scan:v2.0.6
 
-( docker rm -f scanner || true )  && docker run --network="clair-scanning" -v /var/run/docker.sock:/var/run/docker.sock -v $(pwd):/output --name scanner \
-    roastario/clair-local-scanner:latest \
-    clair-scanner --ip scanner --clair="http://clair:6060" --threshold="Low" --report="/output/zulu.json" entdocker.corda.r3cev.com/corda-enterprise-zulu-4.0-snapshot:latest
 
-( docker rm -f scanner || true )  && docker run --network="clair-scanning" -ti -v /var/run/docker.sock:/var/run/docker.sock -v $(pwd):/output --name scanner \
-    roastario/clair-local-scanner:latest \
-    clair-scanner --ip scanner --clair="http://clair:6060" --threshold="Low" --report="/output/corretto.json" entdocker.corda.r3cev.com/corda-enterprise-corretto-4.0-snapshot:latest
+for image ; do
+    image_base=$(basename ${image})
+    ( docker rm -f ${SCANNER_CONTAINER_NAME} || true )  && docker run --network="${NETWORK_NAME}" -v /var/run/docker.sock:/var/run/docker.sock -v $(pwd)/scanning_output:/output --name ${SCANNER_CONTAINER_NAME} \
+        corda/clair-local-scanner:latest \
+        clair-scanner --ip scanner --clair="http://clair:6060" --threshold="Low" --report="/output/${image_base}.json" ${image}
+done
 
-( docker rm -f scanner || true )  && docker run --network="clair-scanning" -ti -v /var/run/docker.sock:/var/run/docker.sock -v $(pwd):/output --name scanner \
-    roastario/clair-local-scanner:latest \
-    clair-scanner --ip scanner --clair="http://clair:6060" --threshold="Low" --report="/output/alpine-zulu.json" entdocker.corda.r3cev.com/corda-enterprise-alpine-zulu-4.0-snapshot:latest
-
-docker rm -f postgres
-docker rm -f clair
-docker rm -f scanner
-docker network rm clair-scanning
+tidy_up
--- docker/src/bash/example-generate-testnet.sh
+++ docker/src/bash/example-generate-testnet.sh
@@ -1,27 +1,32 @@
 #!/usr/bin/env bash
 
-docker run -ti \
-        -e MY_PUBLIC_ADDRESS="corda-node.example.com" \
-        -e ONE_TIME_DOWNLOAD_KEY="6bbd155f-6fc9-46c5-821a-d5b3835488b3" \
-        -e LOCALITY="London" -e COUNTRY="GB" \
-        -v $(pwd)/docker/config:/etc/corda \
-        -v $(pwd)/docker/certificates:/opt/corda/certificates \
-        entdocker.corda.r3cev.com/corda-enterprise-corretto-4.0-snapshot:latest config-generator --testnet
+mkdir -p $(pwd)/config
+mkdir -p $(pwd)/certificates
+mkdir -p $(pwd)/persistence
+mkdir -p $(pwd)/logs
 
 docker run -ti \
-        -v $(pwd)/docker/config:/etc/corda \
-        -v $(pwd)/docker/certificates:/opt/corda/certificates \
-        -v $(pwd)/docker/persistence:/opt/corda/persistence \
-        -v $(pwd)/docker/logs:/opt/corda/logs \
+        -e MY_PUBLIC_ADDRESS="corda-node.example.com" \
+        -e ONE_TIME_DOWNLOAD_KEY="7776c00a-0298-4d27-8cfa-30aa1a664e40" \
+        -e LOCALITY="London" -e COUNTRY="GB" \
+        -v $(pwd)/config:/etc/corda \
+        -v $(pwd)/certificates:/opt/corda/certificates \
+        entdocker.corda.r3cev.com/corda-utils-4.0-snapshot:latest config-generator --testnet && \
+
+docker run -ti \
+        -v $(pwd)/config:/etc/corda \
+        -v $(pwd)/certificates:/opt/corda/certificates \
+        -v $(pwd)/persistence:/opt/corda/persistence \
+        -v $(pwd)/logs:/opt/corda/logs \
         -v $(pwd)/samples/bank-of-corda-demo/build/nodes/BankOfCorda/cordapps:/opt/corda/cordapps \
-        entdocker.corda.r3cev.com/corda-enterprise-corretto-4.0-snapshot:latest db-migrate-create-jars
+        entdocker.corda.r3cev.com/corda-utils-4.0-snapshot:latest db-migrate-create-jars && \
 
 docker run --memory=2048m \
         --cpus=2 \
-        -v $(pwd)/docker/config:/etc/corda \
-        -v $(pwd)/docker/certificates:/opt/corda/certificates \
-        -v $(pwd)/docker/persistence:/opt/corda/persistence \
-        -v $(pwd)/docker/logs:/opt/corda/logs \
+        -v $(pwd)/config:/etc/corda \
+        -v $(pwd)/certificates:/opt/corda/certificates \
+        -v $(pwd)/persistence:/opt/corda/persistence \
+        -v $(pwd)/logs:/opt/corda/logs \
         -v $(pwd)/samples/bank-of-corda-demo/build/nodes/BankOfCorda/cordapps:/opt/corda/cordapps \
         -p 10200:10200 \
         -p 10201:10201 \
