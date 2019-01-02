9ccf5b827d56cfabebd09406d0e557ac6ed8a955
Florian Friemel <florian.friemel@r3.com>
2019-01-02 13:26:49 +0000

    Improve test.

--- serialization/src/test/kotlin/net/corda/serialization/internal/amqp/DeserializeQueryableStateTest.kt
+++ serialization/src/test/kotlin/net/corda/serialization/internal/amqp/DeserializeQueryableStateTest.kt
@@ -1,24 +1,31 @@
 package net.corda.serialization.internal.amqp
 
+import net.corda.client.rpc.internal.serialization.amqp.AMQPClientSerializationScheme
 import net.corda.core.identity.AbstractParty
 import net.corda.core.schemas.MappedSchema
 import net.corda.core.schemas.PersistentState
 import net.corda.core.schemas.QueryableState
 import net.corda.core.serialization.SerializedBytes
+import net.corda.node.serialization.amqp.AMQPServerSerializationScheme
 import net.corda.serialization.internal.AMQP_RPC_CLIENT_CONTEXT
+import net.corda.serialization.internal.AMQP_RPC_SERVER_CONTEXT
 import net.corda.serialization.internal.AllWhitelist
-import net.corda.serialization.internal.amqp.testutils.*
-import net.corda.serialization.internal.carpenter.ClassCarpenterImpl
+import net.corda.testing.core.DUMMY_BANK_A_NAME
+import net.corda.testing.core.TestIdentity
 import org.junit.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertFailsWith
 
 data class TestState(override val participants: List<AbstractParty>): QueryableState {
     override fun generateMappedObject(schema: MappedSchema): PersistentState {
         throw NotImplementedError()
     }
     override fun supportedSchemas(): Iterable<MappedSchema> {
-        throw NotImplementedError()
+        // return dummy value
+        return listOf(MappedSchema(TestState::class.java, 432, listOf()))
     }
 }
+
 
 /**
  * A class loader that excludes [TestState].
@@ -39,12 +46,19 @@ class DeserializeQueryableStateTest {
      */
     @Test
     fun `should deserialize subclass of QueryableState that is not present in the class loader`() {
-        val instance = TestState(listOf())
-        val serialized = TestSerializationOutput(false, testDefaultFactory()).serialize(instance)
-        val context = AMQP_RPC_CLIENT_CONTEXT.copy(whitelist = AllWhitelist, deserializationClassLoader = ClassLoaderWithoutTestState(ClassLoader.getSystemClassLoader()))
-        val sf = SerializerFactoryBuilder.build(context.whitelist, context.deserializationClassLoader, context.lenientCarpenterEnabled)
-        val bytes = SerializedBytes<QueryableState>(serialized.bytes)
-        // doesn't throw
-        DeserializationInput(sf).deserialize(bytes, context)
+        val testParty = TestIdentity(DUMMY_BANK_A_NAME).identity.party
+        val instance = TestState(listOf(testParty))
+
+        val scheme = AMQPServerSerializationScheme(emptyList())
+        val serialized = scheme.serialize(instance, AMQP_RPC_SERVER_CONTEXT)
+
+        val clientContext = AMQP_RPC_CLIENT_CONTEXT.copy(whitelist = AllWhitelist, deserializationClassLoader = ClassLoaderWithoutTestState(ClassLoader.getSystemClassLoader()))
+        val serializedBytes = SerializedBytes<QueryableState>(serialized.bytes)
+        val clientScheme = AMQPClientSerializationScheme(emptyList())
+
+        // this operation used to fail because AMQP_RPC_CLIENT_CONTEXT.lenientCarpenterEnabled was false
+        val deserialized = clientScheme.deserialize(serializedBytes, QueryableState::class.java, clientContext)
+        assertFailsWith<AbstractMethodError> { deserialized.supportedSchemas() }
+        assertEquals(deserialized.participants.first(), testParty)
     }
 }
