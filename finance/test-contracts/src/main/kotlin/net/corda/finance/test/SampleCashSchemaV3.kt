d021e6ad3e153abb0796d48c434cd32587591152
Stefano Franz <stefano.franz@r3.com>
2019-01-03 16:11:40 +0000

    add magic words to publish the test-contracts jar

--- finance/test-contracts/src/main/kotlin/net/corda/finance/test/SampleCashSchemaV3.kt
+++ finance/test-contracts/src/main/kotlin/net/corda/finance/test/SampleCashSchemaV3.kt
@@ -1,30 +1,33 @@
-package net.corda.finance.schemas.test
+package net.corda.finance.test
 
 import net.corda.core.contracts.MAX_ISSUER_REF_SIZE
+import net.corda.core.identity.AbstractParty
 import net.corda.core.schemas.MappedSchema
 import net.corda.core.schemas.PersistentState
-import net.corda.core.utilities.MAX_HASH_HEX_SIZE
 import org.hibernate.annotations.Type
 import javax.persistence.Column
+import javax.persistence.ElementCollection
 import javax.persistence.Entity
-import javax.persistence.Index
 import javax.persistence.Table
-
-/**
- * An object used to fully qualify the [CashSchema] family name (i.e. independent of version).
- */
-object CashSchema
 
 /**
  * First version of a cash contract ORM schema that maps all fields of the [Cash] contract state as it stood
  * at the time of writing.
  */
-object SampleCashSchemaV1 : MappedSchema(schemaFamily = CashSchema.javaClass, version = 1, mappedTypes = listOf(PersistentCashState::class.java)) {
+object SampleCashSchemaV3 : MappedSchema(schemaFamily = CashSchema.javaClass, version = 3,
+        mappedTypes = listOf(PersistentCashState::class.java)) {
     @Entity
-    @Table(name = "contract_cash_states_v1", indexes = [Index(name = "ccy_code_idx1", columnList = "ccy_code"), Index(name = "pennies_idx1", columnList = "pennies")])
+    @Table(name = "cash_states_v3")
     class PersistentCashState(
-            @Column(name = "owner_key_hash", length = MAX_HASH_HEX_SIZE, nullable = false)
-            var ownerHash: String,
+            /** [ContractState] attributes */
+
+            /** X500Name of participant parties **/
+            @ElementCollection
+            var participants: MutableSet<AbstractParty?>? = null,
+
+            /** X500Name of owner party **/
+            @Column(name = "owner_name", nullable = true)
+            var owner: AbstractParty?,
 
             @Column(name = "pennies", nullable = false)
             var pennies: Long,
@@ -32,8 +35,9 @@ object SampleCashSchemaV1 : MappedSchema(schemaFamily = CashSchema.javaClass, ve
             @Column(name = "ccy_code", length = 3, nullable = false)
             var currency: String,
 
-            @Column(name = "issuer_key_hash", length = MAX_HASH_HEX_SIZE, nullable = false)
-            var issuerPartyHash: String,
+            /** X500Name of issuer party **/
+            @Column(name = "issuer_name", nullable = true)
+            var issuer: AbstractParty?,
 
             @Column(name = "issuer_ref", length = MAX_ISSUER_REF_SIZE, nullable = false)
             @Type(type = "corda-wrapper-binary")
