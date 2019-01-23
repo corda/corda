77f7b2ef8cb9c50450b09a0f340868d5dbc7cbe6
Shams Asari <shams.asari@r3.com>
2019-01-23 16:24:49 +0000

    CORDA-2228: Exceptions emanating from ReceiveFinalityFlow are sent to the flow hospital (#4621)
    (cherry picked from commit 5bb5244e5571f9f7f73e708de6abb68c39493e5b)

--- testing/node-driver/src/main/kotlin/net/corda/testing/node/internal/InternalTestUtils.kt
+++ testing/node-driver/src/main/kotlin/net/corda/testing/node/internal/InternalTestUtils.kt
@@ -63,7 +63,7 @@ val FINANCE_CONTRACTS_CORDAPP: TestCordappImpl = findCordapp("net.corda.finance.
 val FINANCE_WORKFLOWS_CORDAPP: TestCordappImpl = findCordapp("net.corda.finance.flows")
 
 @JvmField
-val FINANCE_CORDAPPS: Set<TestCordappInternal> = setOf(FINANCE_CONTRACTS_CORDAPP, FINANCE_WORKFLOWS_CORDAPP)
+val FINANCE_CORDAPPS: Set<TestCordappImpl> = setOf(FINANCE_CONTRACTS_CORDAPP, FINANCE_WORKFLOWS_CORDAPP)
 
 fun cordappsForPackages(vararg packageNames: String): Set<CustomCordapp> = cordappsForPackages(packageNames.asList())
 
