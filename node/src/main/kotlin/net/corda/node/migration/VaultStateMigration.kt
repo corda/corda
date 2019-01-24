cb219a0a34724c623f99849708019ffee9f3b8d7
James Higgs <james.higgs@r3.com>
2019-01-24 15:52:22 +0000

    Use pagination when querying vault states

--- node/src/main/kotlin/net/corda/node/migration/VaultStateMigration.kt
+++ node/src/main/kotlin/net/corda/node/migration/VaultStateMigration.kt
@@ -6,6 +6,7 @@ import net.corda.core.contracts.StateAndRef
 import net.corda.core.contracts.StateRef
 import net.corda.core.crypto.SecureHash
 import net.corda.core.node.services.Vault
+import net.corda.core.node.services.vault.DEFAULT_PAGE_SIZE
 import net.corda.core.schemas.MappedSchema
 import net.corda.core.schemas.PersistentStateRef
 import net.corda.core.utilities.contextLogger
@@ -14,25 +15,13 @@ import net.corda.node.services.keys.BasicHSMKeyManagementService
 import net.corda.node.services.persistence.DBTransactionStorage
 import net.corda.node.services.vault.NodeVaultService
 import net.corda.node.services.vault.VaultSchemaV1
+import net.corda.nodeapi.internal.persistence.CordaPersistence
 import net.corda.nodeapi.internal.persistence.currentDBSession
 import org.hibernate.Session
 
 class VaultStateMigration : CordaMigration() {
     companion object {
         private val logger = contextLogger()
-    }
-
-    private fun getVaultStates(session: Session): List<VaultSchemaV1.VaultStates> {
-        return cordaDB.transaction {
-            val criteriaQuery = cordaDB.entityManagerFactory.criteriaBuilder.createQuery(VaultSchemaV1.VaultStates::class.java)
-            val queryRootStates = criteriaQuery.from(VaultSchemaV1.VaultStates::class.java)
-            criteriaQuery.select(queryRootStates)
-            val query = session.createQuery(criteriaQuery)
-
-            //TODO: Paging?
-
-            query.resultList
-        }
     }
 
     private fun addStateParties(session: Session, stateAndRef: StateAndRef<ContractState>) {
@@ -52,29 +41,24 @@ class VaultStateMigration : CordaMigration() {
         return StateAndRef(state, stateRef)
     }
 
-    private fun persistentStateToStateRef(persistentState: VaultSchemaV1.VaultStates): StateRef {
-        return StateRef(SecureHash.parse(persistentState.stateRef!!.txId), persistentState.stateRef!!.index)
-    }
-
     override fun execute(database: Database?) {
         logger.info("Migrating vault state data to V4 tables")
         initialiseNodeServices(database!!, setOf(VaultMigrationSchemaV1, VaultSchemaV1))
 
         cordaDB.transaction {
+            val persistentStates = VaultStateIterator(cordaDB)
             val session = currentDBSession()
-            val persistentStates = getVaultStates(session)
-            val refPersistentMapping = persistentStates.map { Pair(persistentStateToStateRef(it), it) }.toMap()
-            val states = persistentStates.map { getStateAndRef(it)}
+            persistentStates.forEach {
+                val stateAndRef = getStateAndRef(it)
 
-            logger.debug("Adding ${states.size} states to state party table")
-            states.forEach { addStateParties(session, it) }
+                addStateParties(session, stateAndRef)
 
-            logger.debug("Updating not relevant states in the vault")
-            states.filter {
-                val myKeys = identityService.stripNotOurKeys(it.state.data.participants.map { it.owningKey }).toSet()
-                !NodeVaultService.isRelevant(it.state.data, myKeys)
-            }.forEach {
-                refPersistentMapping[it.ref]?.relevancyStatus = Vault.RelevancyStatus.NOT_RELEVANT
+                val myKeys = identityService.stripNotOurKeys(stateAndRef.state.data.participants.map{ participant ->
+                    participant.owningKey
+                }).toSet()
+                if (!NodeVaultService.isRelevant(stateAndRef.state.data, myKeys)) {
+                    it.relevancyStatus = Vault.RelevancyStatus.NOT_RELEVANT
+                }
             }
         }
     }
@@ -95,4 +79,65 @@ object VaultMigrationSchemaV1 : MappedSchema(schemaFamily = VaultMigrationSchema
                 PersistentIdentityService.PersistentIdentity::class.java,
                 PersistentIdentityService.PersistentIdentityNames::class.java,
                 BasicHSMKeyManagementService.PersistentKey::class.java
-        ))
+        )
+)
+
+
+// Can this be generalised (add extra criteria etc)? May also want to move into own file
+class VaultStateIterator(private val database: CordaPersistence) : Iterator<VaultSchemaV1.VaultStates> {
+
+    companion object {
+        val logger = contextLogger()
+    }
+    private val criteriaBuilder = database.entityManagerFactory.criteriaBuilder
+    private val numStates = getTotalStates()
+
+    private fun getTotalStates(): Long {
+        return database.transaction {
+            val session = currentDBSession()
+            val criteriaQuery = criteriaBuilder.createQuery(Long::class.java)
+            val queryRootStates = criteriaQuery.from(VaultSchemaV1.VaultStates::class.java)
+            criteriaQuery.select(criteriaBuilder.count(queryRootStates))
+            val query = session.createQuery(criteriaQuery)
+            val result = query.singleResult
+            logger.debug("Found $result total states in the vault")
+            result
+        }
+    }
+
+    private val pageSize = DEFAULT_PAGE_SIZE
+    private var pageNumber = 0
+    private var currentPage = getNextPage()
+
+    private fun getNextPage(): List<VaultSchemaV1.VaultStates> {
+        return database.transaction {
+            val session = currentDBSession()
+            val criteriaQuery = criteriaBuilder.createQuery(VaultSchemaV1.VaultStates::class.java)
+            val queryRootStates = criteriaQuery.from(VaultSchemaV1.VaultStates::class.java)
+            criteriaQuery.select(queryRootStates)
+            val query = session.createQuery(criteriaQuery)
+            query.firstResult = (pageNumber * pageSize)
+            query.maxResults = pageSize
+            pageNumber++
+            val result = query.resultList
+            logger.debug("Current page has ${result.size} vault states")
+            result
+        }
+    }
+
+    private var currentIndex = 0
+
+    override fun hasNext(): Boolean {
+        return currentIndex + ((pageNumber - 1) * pageSize) < numStates
+    }
+
+    override fun next(): VaultSchemaV1.VaultStates {
+        if (currentIndex == pageSize) {
+            currentPage = getNextPage()
+            currentIndex = 0
+        }
+        val stateToReturn = currentPage[currentIndex]
+        currentIndex++
+        return stateToReturn
+    }
+}
