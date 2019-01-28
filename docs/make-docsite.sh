1dcc1c6e0501962f80325c0ee0bae79ce41de43d
Stefano Franz <stefano.franz@r3.com>
2019-01-28 17:32:29 +0000

    fix issue with api docs

--- docs/make-docsite.sh
+++ docs/make-docsite.sh
@@ -1,16 +1,4 @@
 #!/usr/bin/env bash
-# The purpose of this file is to make the docsite in a python virtualenv
-# You can call it manually if running make manually, otherwise gradle will run it for you
-
-# Activate the virtualenv
-if [ -d "virtualenv/bin" ]
-then
-    # it's a Unix system
-    source virtualenv/bin/activate
-else
-    source virtualenv/Scripts/activate
-fi
-
 echo "Generating PDF document ..."
 make latexpdf
 
