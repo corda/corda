c436c454d97bef0eade7a4b78624e51140a7d03c
Rick Parker <rick.parker@r3.com>
2019-01-29 14:34:24 +0000

    ENT-2768 Restore at least V3 equivalence in SecureRandom contention (#1742)

--- core/src/main/kotlin/net/corda/core/crypto/internal/PlatformSecureRandom.kt
+++ core/src/main/kotlin/net/corda/core/crypto/internal/PlatformSecureRandom.kt
@@ -6,7 +6,9 @@ import io.netty.util.concurrent.FastThreadLocal
 import net.corda.core.DeleteForDJVM
 import net.corda.core.crypto.DummySecureRandom
 import net.corda.core.utilities.SgxSupport
+import net.corda.core.utilities.loggerFor
 import org.apache.commons.lang.SystemUtils
+import java.io.*
 import java.security.Provider
 import java.security.SecureRandom
 import java.security.SecureRandomSpi
@@ -17,10 +19,9 @@ import java.security.SecureRandomSpi
  */
 internal val platformSecureRandom: () -> SecureRandom = when {
     SgxSupport.isInsideEnclave -> { { DummySecureRandom } }
-    SystemUtils.IS_OS_LINUX -> {
-        { SunSecureRandom() }
+    else -> {
+        { sharedSecureRandom }
     }
-    else -> SecureRandom::getInstanceStrong
 }
 
 @DeleteForDJVM
@@ -29,25 +30,60 @@ class PlatformSecureRandomService(provider: Provider)
 
     companion object {
         const val algorithm = "CordaPRNG"
+        private val logger = loggerFor<PlatformSecureRandomService>()
     }
 
-    private val instance: SecureRandomSpi = PlatformSecureRandomSpi()
+    private val instance: SecureRandomSpi = if (SystemUtils.IS_OS_LINUX) tryAndUseLinuxSecureRandomSpi() else PlatformSecureRandomSpi()
+
+    private fun tryAndUseLinuxSecureRandomSpi(): SecureRandomSpi = try {
+        LinuxSecureRandomSpi()
+    } catch (e: Exception) {
+        logger.error("Unable to initialise LinuxSecureRandomSpi. The exception logged with this message might assist with diagnosis.  The process will now exit.", e)
+        System.exit(1)
+        throw RuntimeException("Never reached, but calms the compiler.")
+    }
+
     override fun newInstance(constructorParameter: Any?) = instance
 }
 
 @DeleteForDJVM
 private class PlatformSecureRandomSpi : SecureRandomSpi() {
     private val threadLocalSecureRandom = object : FastThreadLocal<SecureRandom>() {
-        override fun initialValue() = platformSecureRandom()
+        override fun initialValue() = SecureRandom.getInstanceStrong()
     }
 
-    private val secureRandom: SecureRandom = threadLocalSecureRandom.get()
+    private val secureRandom: SecureRandom get() = threadLocalSecureRandom.get()
 
     override fun engineSetSeed(seed: ByteArray) = secureRandom.setSeed(seed)
     override fun engineNextBytes(bytes: ByteArray) = secureRandom.nextBytes(bytes)
     override fun engineGenerateSeed(numBytes: Int): ByteArray = secureRandom.generateSeed(numBytes)
 }
 
-// Enterprise performance tweak: Unlike all the NativePRNG algorithms, this doesn't use a global lock:
-// TODO: This is using private Java API. Just replace this with an implementation that always reads /dev/urandom on Linux.
-private class SunSecureRandom : SecureRandom(sun.security.provider.SecureRandom(), null)
+@DeleteForDJVM
+private class LinuxSecureRandomSpi : SecureRandomSpi() {
+    private fun openURandom(): InputStream {
+        try {
+            val file = File("/dev/urandom")
+            val stream = FileInputStream(file)
+            if (stream.read() == -1)
+                throw RuntimeException("/dev/urandom not readable?")
+            return stream
+        } catch (e: Exception) {
+            throw RuntimeException(e)
+        }
+    }
+
+    private var urandom = DataInputStream(openURandom())
+
+    override fun engineSetSeed(seed: ByteArray) {}
+    override fun engineNextBytes(bytes: ByteArray) = try {
+        urandom.readFully(bytes)
+    } catch (e: IOException) {
+        throw RuntimeException(e)
+    }
+
+    override fun engineGenerateSeed(numBytes: Int): ByteArray = ByteArray(numBytes).apply { engineNextBytes(this) }
+}
+
+// Enterprise optimisation: This is safe to share because of the underlying implementation of SecureRandomSpi
+private val sharedSecureRandom: SecureRandom by lazy(LazyThreadSafetyMode.PUBLICATION) { SecureRandom.getInstance(PlatformSecureRandomService.algorithm) }
