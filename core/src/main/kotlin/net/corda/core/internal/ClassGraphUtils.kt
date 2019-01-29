39ec38b3348fd0539ec66de7ab5ade04c726c592
rick.parker <rick.parker@r3cev.com>
2019-01-29 14:03:05 +0000

    Daemonize the threads

--- core/src/main/kotlin/net/corda/core/internal/ClassGraphUtils.kt
+++ core/src/main/kotlin/net/corda/core/internal/ClassGraphUtils.kt
@@ -5,11 +5,24 @@ package net.corda.core.internal
 import io.github.classgraph.ClassGraph
 import io.github.classgraph.ScanResult
 import net.corda.core.DeleteForDJVM
-import java.util.concurrent.Executors
+import java.util.concurrent.LinkedBlockingQueue
+import java.util.concurrent.ThreadFactory
+import java.util.concurrent.ThreadPoolExecutor
+import java.util.concurrent.TimeUnit
+import java.util.concurrent.atomic.AtomicInteger
 
 // ClassGraph seems to default to a minimum of 2, so we follow suit.
 private val poolSize = Math.max(2, Runtime.getRuntime().availableProcessors())
-private val pooledScanExecutorService = Executors.newFixedThreadPool(poolSize)
+private val pooledScanExecutorService = ThreadPoolExecutor(poolSize, poolSize, 0L, TimeUnit.MILLISECONDS,
+        LinkedBlockingQueue<Runnable>(), object : ThreadFactory {
+
+    private val counter = AtomicInteger(0)
+    override fun newThread(runnable: Runnable?): Thread {
+        val thread = Thread(runnable, "ClassGraph-pooled-${counter.getAndIncrement()}")
+        thread.isDaemon = true
+        return thread
+    }
+})
 
 /**
  * Use this rather than the built in implementation of [scan] on [ClassGraph].  The built in implementation of [scan] creates
