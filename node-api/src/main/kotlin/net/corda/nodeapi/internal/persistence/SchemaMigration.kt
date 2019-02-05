1d713bc43dbd297572493f4a51a5fa7fe7bec8b3
szymonsztuka <szymon.sztuka@yahoo.com>
2019-02-05 16:40:43 +0000

    Change 3.3 baseline before Liquibase.

--- node-api/src/main/kotlin/net/corda/nodeapi/internal/persistence/SchemaMigration.kt
+++ node-api/src/main/kotlin/net/corda/nodeapi/internal/persistence/SchemaMigration.kt
@@ -163,8 +163,7 @@ class SchemaMigration(
                     "migration/vault-schema.changelog-pkey.xml"))
 
             if (schemas.any { schema -> schema.migrationResource == "node-notary.changelog-master" })
-                preV4Baseline.addAll(listOf("migration/node-notary.changelog-init.xml",
-                        "migration/node-notary.changelog-v1.xml"))
+                preV4Baseline.addAll(listOf("migration/node-notary.changelog-init.xml"))
 
             if (schemas.any { schema -> schema.migrationResource == "notary-raft.changelog-master" })
                 preV4Baseline.addAll(listOf("migration/notary-raft.changelog-init.xml",
