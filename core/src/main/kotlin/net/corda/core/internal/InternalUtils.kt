28f6c3fccd771713c55f87f3ad6e1579a8603164
rick.parker <rick.parker@r3cev.com>
2019-03-07 09:36:37 +0000

    ENT-3256 Compilation error fix and extra comment.

--- core/src/main/kotlin/net/corda/core/internal/InternalUtils.kt
+++ core/src/main/kotlin/net/corda/core/internal/InternalUtils.kt
@@ -388,7 +388,7 @@ val Class<*>.location: URL get() = protectionDomain.codeSource.location
 /** Convenience method to get the package name of a class literal. */
 val KClass<*>.packageName: String get() = java.packageName
 val Class<*>.packageName: String get() = requireNotNull(this.packageNameOrNull) { "$this not defined inside a package" }
-val Class<*>.packageNameOrNull: String?
+val Class<*>.packageNameOrNull: String? // This intentionally does not go via `package` as that code path is slow and contended and just ends up doing this.
     get() {
         val name = this.getName()
         val i = name.lastIndexOf('.')
