c181c625511304e8a27972bb2e5053f785824a75
josecoll <jose.coll@r3cev.com>
2019-03-11 14:52:37 +0000

    RPC Client using HA addresses.

--- samples/bank-of-corda-demo/src/main/kotlin/net/corda/bank/IssueCash.kt
+++ samples/bank-of-corda-demo/src/main/kotlin/net/corda/bank/IssueCash.kt
@@ -5,7 +5,6 @@ import net.corda.bank.api.BankOfCordaClientApi
 import net.corda.bank.api.BankOfCordaWebApi
 import net.corda.core.contracts.Amount
 import net.corda.core.identity.CordaX500Name
-import net.corda.core.internal.VisibleForTesting
 import net.corda.core.transactions.SignedTransaction
 import net.corda.core.utilities.NetworkHostAndPort
 import net.corda.testing.core.BOC_NAME
@@ -16,7 +15,12 @@ object IssueCash {
     private val NOTARY_NAME = CordaX500Name(organisation = "Notary Service", locality = "Zurich", country = "CH")
     private val BIGCORP_NAME = CordaX500Name(organisation = "BigCorporation", locality = "New York", country = "US")
     private const val BOC_RPC_PORT = 10006
+    private const val BOC2_RPC_PORT = 10012
     private const val BOC_WEB_PORT = 10007
+
+    private val availableRpcServersBOC = listOf(
+            NetworkHostAndPort("localhost", BOC_RPC_PORT),
+            NetworkHostAndPort("localhost", BOC2_RPC_PORT))
 
     @JvmStatic
     fun main(args: Array<String>) {
@@ -45,12 +49,20 @@ object IssueCash {
                 requestWebIssue(amount)
                 println("Successfully processed Cash Issue request")
             }
+            Role.ISSUE_CASH_RPC_HA -> {
+                println("Issuing Cash via RPC using first available node from list of [$availableRpcServersBOC] available servers ...")
+                val result = requestRPCIssueHA(amount)
+                println("Success!! Your transaction receipt is ${result.tx.id}")
+            }
         }
     }
 
-    @VisibleForTesting
-    fun requestRpcIssue(amount: Amount<Currency>): SignedTransaction {
+    private fun requestRpcIssue(amount: Amount<Currency>): SignedTransaction {
         return BankOfCordaClientApi.requestRPCIssue(NetworkHostAndPort("localhost", BOC_RPC_PORT), createParams(amount, NOTARY_NAME))
+    }
+
+    private fun requestRPCIssueHA(amount: Amount<Currency>): SignedTransaction {
+        return BankOfCordaClientApi.requestRPCIssueHA(availableRpcServersBOC, createParams(amount, NOTARY_NAME))
     }
 
     private fun requestWebIssue(amount: Amount<Currency>) {
@@ -73,5 +85,6 @@ object IssueCash {
     enum class Role {
         ISSUE_CASH_RPC,
         ISSUE_CASH_WEB,
+        ISSUE_CASH_RPC_HA
     }
 }
--- samples/bank-of-corda-demo/src/main/kotlin/net/corda/bank/api/BankOfCordaClientApi.kt
+++ samples/bank-of-corda-demo/src/main/kotlin/net/corda/bank/api/BankOfCordaClientApi.kt
@@ -2,13 +2,15 @@ package net.corda.bank.api
 
 import net.corda.bank.api.BankOfCordaWebApi.IssueRequestParams
 import net.corda.client.rpc.CordaRPCClient
+import net.corda.client.rpc.CordaRPCClientConfiguration
+import net.corda.client.rpc.CordaRPCConnection
+import net.corda.client.rpc.RPCException
 import net.corda.core.messaging.startFlow
 import net.corda.core.transactions.SignedTransaction
-import net.corda.core.utilities.NetworkHostAndPort
-import net.corda.core.utilities.OpaqueBytes
-import net.corda.core.utilities.getOrThrow
+import net.corda.core.utilities.*
 import net.corda.finance.flows.CashIssueAndPaymentFlow
 import net.corda.testing.http.HttpApi
+import org.apache.activemq.artemis.api.core.ActiveMQSecurityException
 
 /**
  * Interface for communicating with Bank of Corda node
@@ -16,6 +18,8 @@ import net.corda.testing.http.HttpApi
 object BankOfCordaClientApi {
     const val BOC_RPC_USER = "bankUser"
     const val BOC_RPC_PWD = "test"
+
+    private val logger = loggerFor<BankOfCordaClientApi>()
 
     /**
      * HTTP API
@@ -29,12 +33,12 @@ object BankOfCordaClientApi {
     /**
      * RPC API
      *
-     * @return a pair of the issuing and payment transactions.
+     * @return a payment transaction (following successful issuance of cash to self).
      */
     fun requestRPCIssue(rpcAddress: NetworkHostAndPort, params: IssueRequestParams): SignedTransaction {
-        val client = CordaRPCClient(rpcAddress)
+        val client = establishConnectionWithRetry(listOf(rpcAddress), BOC_RPC_USER, BOC_RPC_PWD)
         // TODO: privileged security controls required
-        client.start(BOC_RPC_USER, BOC_RPC_PWD).use { connection ->
+        client.use { connection ->
             val rpc = connection.proxy
             rpc.waitUntilNetworkReady().getOrThrow()
 
@@ -50,5 +54,63 @@ object BankOfCordaClientApi {
             return rpc.startFlow(::CashIssueAndPaymentFlow, params.amount, issuerBankPartyRef, issueToParty, anonymous, notaryLegalIdentity)
                     .returnValue.getOrThrow().stx
         }
+    }
+
+    /**
+     * RPC API
+     *
+     * @return a cash issue transaction.
+     */
+    fun requestRPCIssueHA(availableRpcServers: List<NetworkHostAndPort>, params: IssueRequestParams): SignedTransaction {
+        val client = establishConnectionWithRetry(availableRpcServers, BOC_RPC_USER, BOC_RPC_PWD)
+        // TODO: privileged security controls required
+        client.use { connection ->
+            val rpc = connection.proxy
+            rpc.waitUntilNetworkReady().getOrThrow()
+
+            // Resolve parties via RPC
+            val issueToParty = rpc.wellKnownPartyFromX500Name(params.issueToPartyName)
+                    ?: throw IllegalStateException("Unable to locate ${params.issueToPartyName} in Network Map Service")
+            val notaryLegalIdentity = rpc.notaryIdentities().firstOrNull { it.name == params.notaryName }
+                    ?: throw IllegalStateException("Couldn't locate notary ${params.notaryName} in NetworkMapCache")
+
+            val anonymous = true
+            val issuerBankPartyRef = OpaqueBytes.of(params.issuerBankPartyRef.toByte())
+
+            logger.info("${rpc.nodeInfo()} issuing ${params.amount} to transfer to $issueToParty ...")
+            return rpc.startFlow(::CashIssueAndPaymentFlow, params.amount, issuerBankPartyRef, issueToParty, anonymous, notaryLegalIdentity)
+                    .returnValue.getOrThrow().stx
+        }
+    }
+
+    private fun establishConnectionWithRetry(nodeHostAndPorts: List<NetworkHostAndPort>, username: String, password: String): CordaRPCConnection {
+        val retryInterval = 5.seconds
+        var connection: CordaRPCConnection?
+        do {
+            connection = try {
+                logger.info("Connecting to: $nodeHostAndPorts")
+                val client = CordaRPCClient(
+                        nodeHostAndPorts,
+                        CordaRPCClientConfiguration(connectionMaxRetryInterval = retryInterval)
+                )
+                val _connection = client.start(username, password)
+                // Check connection is truly operational before returning it.
+                val nodeInfo = _connection.proxy.nodeInfo()
+                require(nodeInfo.legalIdentitiesAndCerts.isNotEmpty())
+                _connection
+            } catch (secEx: ActiveMQSecurityException) {
+                // Happens when incorrect credentials provided - no point retrying connection
+                logger.info("Security exception upon attempt to establish connection: " + secEx.message)
+                throw secEx
+            } catch (ex: RPCException) {
+                logger.info("Exception upon attempt to establish connection: " + ex.message)
+                null    // force retry are sleep
+            }
+            // Could not connect this time round - pause before giving another try.
+            Thread.sleep(retryInterval.toMillis())
+        } while (connection == null)
+
+        logger.info("Connection successfully established with: ${connection.proxy.nodeInfo()}")
+        return connection
     }
 }
