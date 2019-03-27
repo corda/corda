58ec884bb65a22965aaef225d76e0581532b160c
stefano <stefano.franz@r3.com>
2019-03-27 16:03:51 +0000

    new IdentityServiceInternal API to register anonymous parties

--- core/src/main/kotlin/net/corda/core/flows/CollectSignaturesFlow.kt
+++ core/src/main/kotlin/net/corda/core/flows/CollectSignaturesFlow.kt
@@ -105,14 +105,16 @@ class CollectSignaturesFlow @JvmOverloads constructor(val partiallySignedTx: Sig
         // If the unsigned counterparties list is empty then we don't need to collect any more signatures here.
         if (unsigned.isEmpty()) return partiallySignedTx
 
+        val partyToSessionMap = sessionsToCollectFrom.groupBy { it.counterparty }
+
         val partyToKeysMap = groupPublicKeysByWellKnownParty(serviceHub, unsigned)
         // Check that we have a session for all parties.  No more, no less.
         require(sessionsToCollectFrom.map { it.counterparty }.toSet() == partyToKeysMap.keys) {
             "The Initiator of CollectSignaturesFlow must pass in exactly the sessions required to sign the transaction."
         }
         // Collect signatures from all counterparties and append them to the partially signed transaction.
-        val counterpartySignatures = sessionsToCollectFrom.flatMap { session ->
-            subFlow(CollectSignatureFlow(partiallySignedTx, session, partyToKeysMap[session.counterparty]!!))
+        val counterpartySignatures = partyToSessionMap.flatMap { (party, sessions)->
+            subFlow(CollectSignatureFlow(partiallySignedTx, sessions.first(), partyToKeysMap[party]!!))
         }
         val stx = partiallySignedTx + counterpartySignatures
 
