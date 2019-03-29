945445d8fb962b3e3e5be8d2c48f9c9f52b81afc
Christian Sailer <christian.sailer@r3.com>
2019-03-29 14:00:18 +0000

    CORDA-2813 Address threading/blocking concerns raised during review

--- node/src/main/kotlin/net/corda/node/utilities/errorAndTerminate.kt
+++ node/src/main/kotlin/net/corda/node/utilities/errorAndTerminate.kt
@@ -2,14 +2,18 @@ package net.corda.node.utilities
 
 import net.corda.core.utilities.seconds
 import org.slf4j.LoggerFactory
+import kotlin.concurrent.thread
 
 /**
  * Log error message and terminate the process. This might not clean up resources and could leave
  * the system in a messy state.
  */
+@Synchronized
 fun errorAndTerminate(message: String, e: Throwable?) {
-    val log = LoggerFactory.getLogger("errorAndTerminate")
-    log.error(message, e)
+    thread {
+        val log = LoggerFactory.getLogger("errorAndTerminate")
+        log.error(message, e)
+    }
 
     // give the logger a chance to flush the error message before killing the node
     Thread.sleep(10.seconds.toMillis())
