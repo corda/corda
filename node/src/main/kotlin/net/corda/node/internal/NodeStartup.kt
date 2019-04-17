0c37e1fe32d204a63812827e3dd9c4197a7735a8
manojkrgupta <manoj.gupta@r3.com>
2019-04-17 14:11:59 +0800

    CORDA-2862: Fix to allow softlinks of logs directory

--- node/src/main/kotlin/net/corda/node/internal/NodeStartup.kt
+++ node/src/main/kotlin/net/corda/node/internal/NodeStartup.kt
@@ -486,7 +486,7 @@ fun CliWrapperBase.initLogging(baseDirectory: Path): Boolean {
     //Test for access to the logging path and shutdown if we are unable to reach it.
     val logPath = baseDirectory / NodeCliCommand.LOGS_DIRECTORY_NAME
     try {
-        logPath.createDirectories()
+        (logPath / "/").createDirectories()
     } catch (e: IOException) {
         printError("Unable to create logging directory ${logPath.toString()}. Node will now shutdown.")
         return false
