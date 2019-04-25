630194c4077650475ba5c17c07ed06dde077e71f
Richard Crook <richard@wishstream.org>
2019-04-25 11:18:25 +0100

    CORDA-4954 : Improvements to docker image : compatible with v3.3; image size; truststore (#4965)
    * CORDA-4954
    
    * test-docker
    
    * test fix

--- docker/src/bash/run-corda.sh
+++ docker/src/bash/run-corda.sh
@@ -1,10 +1,7 @@
 #!/usr/bin/env bash
-: ${JVM_ARGS='-XX:+UseG1GC'}
-
-JVM_ARGS="-XX:+UnlockExperimentalVMOptions -XX:+UseCGroupMemoryLimitForHeap "${JVM_ARGS}
 
 if [[ ${JVM_ARGS} == *"Xmx"* ]]; then
   echo "WARNING: the use of the -Xmx flag is not recommended within docker containers. Use the --memory option passed to the container to limit heap size"
 fi
 
-java -Djava.security.egd=file:/dev/./urandom -Dcapsule.jvm.args="${JVM_ARGS}" -jar /opt/corda/bin/corda.jar --base-directory=/opt/corda --config-file=/etc/corda/node.conf ${CORDA_ARGS}
+java -Djava.security.egd=file:/dev/./urandom -Dcapsule.jvm.args="${JVM_ARGS}" -jar /opt/corda/bin/corda.jar --base-directory /opt/corda --config-file ${CONFIG_FOLDER}/node.conf ${CORDA_ARGS}
