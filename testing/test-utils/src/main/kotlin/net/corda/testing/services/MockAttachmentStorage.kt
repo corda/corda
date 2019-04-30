7da545fed6882cc27680f33464aa3fbe99c1847c
James Higgs <james.higgs@r3.com>
2019-04-30 09:29:26 +0100

    Update the MockAttachmentStorage query attachments method

--- testing/test-utils/src/main/kotlin/net/corda/testing/services/MockAttachmentStorage.kt
+++ testing/test-utils/src/main/kotlin/net/corda/testing/services/MockAttachmentStorage.kt
@@ -5,20 +5,19 @@ import net.corda.core.contracts.ContractAttachment
 import net.corda.core.contracts.ContractClassName
 import net.corda.core.crypto.SecureHash
 import net.corda.core.crypto.sha256
-import net.corda.core.internal.*
+import net.corda.core.internal.AbstractAttachment
+import net.corda.core.internal.TRUSTED_UPLOADERS
+import net.corda.core.internal.UNKNOWN_UPLOADER
 import net.corda.core.internal.cordapp.CordappImpl.Companion.DEFAULT_CORDAPP_VERSION
+import net.corda.core.internal.readFully
 import net.corda.core.node.services.AttachmentId
 import net.corda.core.node.services.AttachmentStorage
-import net.corda.core.node.services.vault.AttachmentQueryCriteria
-import net.corda.core.node.services.vault.AttachmentSort
-import net.corda.core.node.services.vault.Builder
-import net.corda.core.node.services.vault.ColumnPredicate
-import net.corda.core.node.services.vault.Sort
+import net.corda.core.node.services.vault.*
 import net.corda.core.serialization.SingletonSerializeAsToken
 import net.corda.nodeapi.internal.withContractsInJar
 import java.io.InputStream
 import java.security.PublicKey
-import java.util.HashMap
+import java.util.*
 import java.util.jar.Attributes
 import java.util.jar.JarInputStream
 
@@ -26,7 +25,7 @@ import java.util.jar.JarInputStream
  * A mock implementation of [AttachmentStorage] for use within tests
  */
 class MockAttachmentStorage : AttachmentStorage, SingletonSerializeAsToken() {
-    private data class ContractAttachmentMetadata(val name: ContractClassName, val version: Int, val isSigned: Boolean)
+    private data class ContractAttachmentMetadata(val name: ContractClassName, val version: Int, val isSigned: Boolean, val signers: List<PublicKey>, val uploader: String)
 
     private val _files = HashMap<SecureHash, Pair<Attachment, ByteArray>>()
     private val _contractClasses = HashMap<ContractAttachmentMetadata, SecureHash>()
@@ -44,27 +43,27 @@ class MockAttachmentStorage : AttachmentStorage, SingletonSerializeAsToken() {
 
     override fun openAttachment(id: SecureHash): Attachment? = files[id]?.first
 
+    // This function only covers those possibilities currently used within tests. Each ColumnPredicate type can have multiple operators,
+    // and not all predicate types are covered here.
+    private fun <C>criteriaFilter(metadata: C, predicate: ColumnPredicate<C>?): Boolean {
+        return when (predicate) {
+            is ColumnPredicate.EqualityComparison -> predicate.rightLiteral == metadata
+            is ColumnPredicate.CollectionExpression -> predicate.rightLiteral.contains(metadata)
+            else -> true
+        }
+    }
+
     override fun queryAttachments(criteria: AttachmentQueryCriteria, sorting: AttachmentSort?): List<AttachmentId> {
         criteria as AttachmentQueryCriteria.AttachmentsQueryCriteria
-        val contractClassNames =
-                if (criteria.contractClassNamesCondition is ColumnPredicate.EqualityComparison)
-                    (criteria.contractClassNamesCondition as ColumnPredicate.EqualityComparison<List<ContractClassName>>).rightLiteral
-                else emptyList()
-        val contractMetadataList =
-            if (criteria.isSignedCondition != null) {
-                val isSigned = criteria.isSignedCondition == Builder.equal(true)
-                 contractClassNames.map {contractClassName ->
-                    ContractAttachmentMetadata(contractClassName, 1, isSigned)
-                }
-            }
-            else {
-                contractClassNames.flatMap { contractClassName ->
-                    listOf(ContractAttachmentMetadata(contractClassName, 1, false),
-                     ContractAttachmentMetadata(contractClassName, 1, true))
-                }
-            }
+        val metadataFilter = { metadata: ContractAttachmentMetadata ->
+            criteriaFilter(listOf(metadata.name), criteria.contractClassNamesCondition) &&
+                    criteriaFilter(metadata.signers, criteria.signersCondition) &&
+                    criteriaFilter(metadata.isSigned, criteria.isSignedCondition) &&
+                    criteriaFilter(metadata.version, criteria.versionCondition) &&
+                    criteriaFilter(metadata.uploader, criteria.uploaderCondition)
+        }
 
-        return _contractClasses.filterKeys { contractMetadataList.contains(it) }.values.toList()
+        return _contractClasses.filterKeys { metadataFilter(it) }.values.toList()
     }
 
     override fun hasAttachment(attachmentId: AttachmentId) = files.containsKey(attachmentId)
@@ -103,7 +102,7 @@ class MockAttachmentStorage : AttachmentStorage, SingletonSerializeAsToken() {
                     if (contractClassNames == null || contractClassNames.isEmpty()) baseAttachment
                     else {
                         contractClassNames.map {contractClassName ->
-                            val contractClassMetadata = ContractAttachmentMetadata(contractClassName, version, signers.isNotEmpty())
+                            val contractClassMetadata = ContractAttachmentMetadata(contractClassName, version, signers.isNotEmpty(), signers, uploader)
                             _contractClasses[contractClassMetadata] = sha256
                         }
                         ContractAttachment.create(baseAttachment, contractClassNames.first(), contractClassNames.toSet(), uploader, signers, version)
