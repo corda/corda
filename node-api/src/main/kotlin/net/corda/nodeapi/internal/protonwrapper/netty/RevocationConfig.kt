0fe63af73af22391ff09256ec8e3b5b4654d1f93
Viktor Kolomeyko <viktor.kolomeyko@r3.com>
2019-05-01 10:28:11 +0100

    ENT-3549: Custom parser for `RevocationConfig`

--- node-api/src/main/kotlin/net/corda/nodeapi/internal/protonwrapper/netty/RevocationConfig.kt
+++ node-api/src/main/kotlin/net/corda/nodeapi/internal/protonwrapper/netty/RevocationConfig.kt
@@ -1,8 +1,13 @@
 package net.corda.nodeapi.internal.protonwrapper.netty
 
+import com.typesafe.config.Config
+import net.corda.nodeapi.internal.config.ConfigParser
+import net.corda.nodeapi.internal.config.CustomConfigParser
+
 /**
  * Data structure for controlling the wy how Certificate Revocation Lists are handled.
  */
+@CustomConfigParser(RevocationConfigParser::class)
 interface RevocationConfig {
 
     enum class Mode {
@@ -32,3 +37,15 @@ interface RevocationConfig {
 fun Boolean.toRevocationConfig() = if(this) RevocationConfigImpl(RevocationConfig.Mode.SOFT_FAIL) else RevocationConfigImpl(RevocationConfig.Mode.HARD_FAIL)
 
 data class RevocationConfigImpl(override val mode: RevocationConfig.Mode) : RevocationConfig
+
+class RevocationConfigParser : ConfigParser<RevocationConfig> {
+    override fun parse(config: Config): RevocationConfig {
+        val mode = config.getString("mode")
+        return when (mode.toUpperCase()) {
+            "SOFT_FAIL" -> RevocationConfigImpl(RevocationConfig.Mode.SOFT_FAIL)
+            "HARD_FAIL" -> RevocationConfigImpl(RevocationConfig.Mode.HARD_FAIL)
+            "OFF" -> RevocationConfigImpl(RevocationConfig.Mode.OFF)
+            else -> throw IllegalArgumentException("Unsupported mode : '$mode'")
+        }
+    }
+}
