c422ed2c391f59cac13fd1402b57d77164507f55
Will Hester <will.hester@r3.com>
2019-05-08 15:14:06 +0100

    CO-2925 Renaming parameters in PersistentIdentitySerive and clean up tests.

--- confidential-identities/src/test/kotlin/net/corda/confidential/IdentitySyncFlowTests.kt
+++ confidential-identities/src/test/kotlin/net/corda/confidential/IdentitySyncFlowTests.kt
@@ -5,6 +5,8 @@ import net.corda.core.flows.FlowLogic
 import net.corda.core.flows.FlowSession
 import net.corda.core.flows.InitiatedBy
 import net.corda.core.flows.InitiatingFlow
+import net.corda.core.identity.AbstractParty
+import net.corda.core.identity.AnonymousParty
 import net.corda.core.identity.Party
 import net.corda.core.transactions.WireTransaction
 import net.corda.core.utilities.OpaqueBytes
