b8d17a756e8ff7342a3131ae883da3b52fdaa26d
Dominic Fox <dominic.fox@r3.com>
2019-05-13 10:48:28 +0100

    Limit exposure of gradle imports

--- testing/test-db/src/main/kotlin/net/corda/testing/internal/db/NoOpTestDatabaseContext.kt
+++ testing/test-db/src/main/kotlin/net/corda/testing/internal/db/NoOpTestDatabaseContext.kt
@@ -1,41 +1,20 @@
 package net.corda.testing.internal.db
 
-import org.slf4j.Logger
-import org.slf4j.LoggerFactory
-
 /**
- * An implementation of [TestDatabaseContext] which does nothing besides logging the calls made to it.
+ * An implementation of [TestDatabaseContext] which does nothing.
  */
 class NoOpTestDatabaseContext : TestDatabaseContext {
 
-    companion object {
-        private val logger: Logger = LoggerFactory.getLogger(NoOpTestDatabaseContext::class.java)
-    }
+    override fun initialize(groupName: String) {}
 
-    private lateinit var groupName: String
+    override fun beforeClass(setupSql: String) {}
 
-    override fun initialize(groupName: String) {
-        logger.trace("[NO-OP] Initializing database group $groupName")
-        this.groupName = groupName
-    }
+    override fun afterClass(teardownSql: String) {}
 
-    override fun beforeClass(setupSql: String) {
-        logger.trace("[NO-OP] Running SQL setup script $setupSql in group $groupName")
-    }
+    override fun beforeTest(setupSql: String) {}
 
-    override fun afterClass(teardownSql: String) {
-        logger.trace("[NO-OP] Running SQL teardown script $teardownSql in group $groupName")
-    }
+    override fun afterTest(teardownSql: String) {}
 
-    override fun beforeTest(setupSql: String) {
-        logger.trace("[NO-OP] Running SQL setup script $setupSql in group $groupName")
-    }
+    override fun close() {}
 
-    override fun afterTest(teardownSql: String) {
-        logger.trace("[NO-OP] Running SQL teardown script $teardownSql in group $groupName")
-    }
-
-    override fun close() {
-        logger.trace("[NO-OP] Cleaning up database group $groupName")
-    }
 }
