76a95f89a88af589101de1e35ac7efccbb3fcfea
josecoll <jose.coll@r3cev.com>
2019-05-20 14:24:45 +0100

    Fix attempt to access boot classpath from runtimeMXBean (in JDK9+)

--- node/src/main/kotlin/net/corda/node/internal/NodeStartup.kt
+++ node/src/main/kotlin/net/corda/node/internal/NodeStartup.kt
@@ -246,7 +246,8 @@ open class NodeStartup : NodeStartupLogging {
         logger.info("PID: ${info.name.split("@").firstOrNull()}")  // TODO Java 9 has better support for this
         logger.info("Main class: ${NodeConfiguration::class.java.location.toURI().path}")
         logger.info("CommandLine Args: ${info.inputArguments.joinToString(" ")}")
-        logger.info("bootclasspath: ${info.bootClassPath}")
+        // JDK 11 (bootclasspath no longer supported from JDK 9)
+        if (info.isBootClassPathSupported) logger.info("bootclasspath: ${info.bootClassPath}")
         logger.info("classpath: ${info.classPath}")
         logger.info("VM ${info.vmName} ${info.vmVendor} ${info.vmVersion}")
         logger.info("Machine: ${lookupMachineNameAndMaybeWarn()}")
