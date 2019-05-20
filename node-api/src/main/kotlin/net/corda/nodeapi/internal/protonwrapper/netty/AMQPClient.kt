6e59db620ace5613edb60e90a3bc6b7df49ba756
Viktor Kolomeyko <viktor.kolomeyko@r3.com>
2019-05-20 10:52:40 +0100

    Resolve independently introduced functionality for `sslHandshakeTimeout`

--- node-api/src/main/kotlin/net/corda/nodeapi/internal/protonwrapper/netty/AMQPClient.kt
+++ node-api/src/main/kotlin/net/corda/nodeapi/internal/protonwrapper/netty/AMQPClient.kt
@@ -183,7 +183,7 @@ class AMQPClient(val targets: List<NetworkHostAndPort>,
             } else {
                 createClientSslHelper(target, parent.allowedRemoteLegalNames, wrappedKeyManagerFactory, trustManagerFactory)
             }
-            handler.handshakeTimeoutMillis = Integer.getInteger(SSL_HANDSHAKE_TIMEOUT_PROP_NAME, 20000).toLong() // Aligned with sun.security.provider.certpath.URICertStore.DEFAULT_CRL_CONNECT_TIMEOUT
+            handler.handshakeTimeoutMillis = conf.sslHandshakeTimeout
             pipeline.addLast("sslHandler", handler)
             if (conf.trace) pipeline.addLast("logger", LoggingHandler(LogLevel.INFO))
             pipeline.addLast(AMQPChannelHandler(false,
