32a6282717c2fdf82ebefa1f8acc809083335434
bpaunescu <bogdan.paunescu@r3.com>
2019-06-05 14:17:05 +0100

    ENT-3642: add null check for BCC requirement

--- node-api/src/main/kotlin/net/corda/nodeapi/internal/cryptoservice/SupportedCryptoServices.kt
+++ node-api/src/main/kotlin/net/corda/nodeapi/internal/cryptoservice/SupportedCryptoServices.kt
@@ -13,8 +13,12 @@ enum class SupportedCryptoServices {
     // AZURE_KV // Azure key Vault.
 
     companion object {
-        fun makeCryptoService(legalName: CordaX500Name, signingCertificateStore: FileBasedCertificateStoreSupplier, cryptoServiceName: SupportedCryptoServices? = null, cryptoServiceConf: Path? = null): CryptoService {
-            return BCCryptoService(legalName.x500Principal, signingCertificateStore)
+        fun makeCryptoService(legalName: CordaX500Name, signingCertificateStore: FileBasedCertificateStoreSupplier? = null, cryptoServiceName: SupportedCryptoServices? = null, cryptoServiceConf: Path? = null): CryptoService {
+            // The signing certificate store can be null for other services as only BCC requires is at the moment.
+            if (signingCertificateStore == null) {
+                throw IllegalArgumentException("Currently only BouncyCastle is used as a crypto service. A valid signing certificate store is required.")
+            }
+            return BCCryptoService(legalName.x500Principal, signingCertificateStore!!)
         }
     }
 }
