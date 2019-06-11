daabe0e8695fd81f0fe923743768ed5ab460eb4d
LankyDan <danknewton@hotmail.com>
2019-06-11 09:20:36 +0100

    ENT-3496 Check in `InternalCordaRPCOps` that somehow got missed...
    (cherry picked from commit a60d657c7c4f58a08a30266364304ef1e2b2f7a8)

---
+++ core/src/main/kotlin/net/corda/core/internal/messaging/InternalCordaRPCOps.kt
@@ -0,0 +1,12 @@
+package net.corda.core.internal.messaging
+
+import net.corda.core.messaging.CordaRPCOps
+
+/**
+ * Contains internal RPC functions that should not be publicly exposed in [CordaRPCOps]
+ */
+interface InternalCordaRPCOps : CordaRPCOps {
+
+    /** Dump all the current flow checkpoints as JSON into a zip file in the node's log directory. */
+    fun dumpCheckpoints()
+}
