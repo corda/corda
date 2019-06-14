4ece576cab8836abf2ca4c0f1d23fbb566b55b1a
stefano <stefano.franz@r3.com>
2019-06-14 10:31:05 +0100

    make all portallocation requests use same starting point

--- testing/node-driver/src/main/kotlin/net/corda/testing/driver/internal/GlobalTestPortAllocation.kt
+++ testing/node-driver/src/main/kotlin/net/corda/testing/driver/internal/GlobalTestPortAllocation.kt
@@ -3,10 +3,7 @@ package net.corda.testing.driver.internal
 import net.corda.testing.driver.PortAllocation
 
 fun incrementalPortAllocation(startingPortIfNoEnv: Int): PortAllocation {
-    return when {
-        System.getProperty(enablingSystemProperty)?.toBoolean() ?: System.getenv(enablingEnvVar)?.toBoolean() == true -> PortAllocation(startingPoint = startingPort())
-        else -> PortAllocation(startingPoint = startingPortIfNoEnv)
-    }
+    return PORT_ALLOCATION_INSTANCE
 }
 
 private const val enablingEnvVar = "TESTING_GLOBAL_PORT_ALLOCATION_ENABLED"
@@ -15,8 +12,9 @@ private val enablingSystemProperty = enablingEnvVar.toLowerCase().replace("_", "
 private val startingPortSystemProperty = startingPortEnvVariable.toLowerCase().replace("_", ".")
 private const val startingPortDefaultValue = 5000
 
+private val PORT_ALLOCATION_INSTANCE = PortAllocation(startingPort())
+
 
 private fun startingPort(): Int {
-
     return System.getProperty(startingPortSystemProperty)?.toIntOrNull() ?: System.getenv(startingPortEnvVariable)?.toIntOrNull() ?: startingPortDefaultValue
 }
