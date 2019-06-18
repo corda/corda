24dea1d41b149b172fb967da7ac26a29ade9c959
Dominic Fox <40790090+r3domfox@users.noreply.github.com>
2019-06-18 10:57:20 +0100

    ENT-3444 create test-db module (#5093)
    * ENT-3444 define RequiresDB annotation and junit5 extension
    
    * Move to internal
    
    * info to trace
    
    * Limit exposure of gradle imports
    
    * Enable annotation inheritance, and multiple SQL scripts per class or method
    
    * Get the test context class globally for all groups
    
    * usingRemoteDatabase flag

---
+++ testing/test-db/src/test/kotlin/net/corda/testing/internal/db/GroupBTests.kt
@@ -0,0 +1,25 @@
+package net.corda.testing.internal.db
+
+import org.junit.jupiter.api.Test
+
+@GroupB
+class GroupBTests {
+
+    @Test
+    fun setExpectations() {
+        AssertingTestDatabaseContext.addExpectations("groupB",
+                "forClassGroupBTests-setup", "specialSql1-setup", "specialSql1-teardown", "forClassGroupBTests-teardown")
+    }
+
+    @Test
+    fun noSpecialSqlRequired() {
+
+    }
+
+    @Test
+    @SpecialSql1
+    fun someSpecialSqlRequired() {
+
+    }
+
+}
