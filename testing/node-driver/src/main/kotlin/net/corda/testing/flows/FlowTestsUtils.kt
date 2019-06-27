798321a3d297d814a77f9e8f6f8c8a1f44e99ec5
Christian Sailer <christian.sailer@r3.com>
2019-06-27 14:56:30 +0100

    Delete some cruft that flagged up on the API checker

--- testing/node-driver/src/main/kotlin/net/corda/testing/flows/FlowTestsUtils.kt
+++ testing/node-driver/src/main/kotlin/net/corda/testing/flows/FlowTestsUtils.kt
@@ -13,33 +13,6 @@ import rx.Observable
 import kotlin.reflect.KClass
 
 /**
- * Allows to simplify writing flows that simply rend a message back to an initiating flow.
- */
-class Answer<out R : Any>(session: FlowSession, override val answer: R, closure: (result: R) -> Unit = {}) : SimpleAnswer<R>(session, closure)
-
-/**
- * Allows to simplify writing flows that simply rend a message back to an initiating flow.
- */
-abstract class SimpleAnswer<out R : Any>(private val session: FlowSession, private val closure: (result: R) -> Unit = {}) : FlowLogic<Unit>() {
-    @Suspendable
-    override fun call() {
-        val tmp = answer
-        closure(tmp)
-        session.send(tmp)
-    }
-
-    protected abstract val answer: R
-}
-
-/**
- * A flow that does not do anything when triggered.
- */
-class NoAnswer(private val closure: () -> Unit = {}) : FlowLogic<Unit>() {
-    @Suspendable
-    override fun call() = closure()
-}
-
-/**
  * Extracts data from a [Map[FlowSession, UntrustworthyData<Any>]] without performing checks and casting to [R].
  */
 @Suppress("UNCHECKED_CAST")
