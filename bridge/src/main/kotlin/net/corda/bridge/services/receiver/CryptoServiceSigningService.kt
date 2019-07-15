39d783ea75fd02ba5ce8d9a1ea3c7c91eff0202b
Adel El-Beik <adelel-beik@19LDN-MAC108.local>
2019-07-15 14:38:43 +0100

    ENT-3410: Changed log level to warn for the CryptoServiceSigningService heartbeat message.

--- bridge/src/main/kotlin/net/corda/bridge/services/receiver/CryptoServiceSigningService.kt
+++ bridge/src/main/kotlin/net/corda/bridge/services/receiver/CryptoServiceSigningService.kt
@@ -55,7 +55,7 @@ class CryptoServiceSigningService(private val cryptoService: CryptoService,
                 log.trace { "Crypto service online." }
                 true
             } catch (e: Exception) {
-                log.error("Crypto service offline.", e)
+                log.warn("Crypto service offline.", e)
                 false
             }
         }
