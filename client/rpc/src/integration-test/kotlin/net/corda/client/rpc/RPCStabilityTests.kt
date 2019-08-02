64d6554d7ad66b4895315ed7153fcf06f909f0bd
Stefan Iliev <stefaniliev@19ldn-mac110.mynet>
2019-08-02 10:57:12 +0100

    Added ignore reason and link to jira ticket.

--- client/rpc/src/integration-test/kotlin/net/corda/client/rpc/RPCStabilityTests.kt
+++ client/rpc/src/integration-test/kotlin/net/corda/client/rpc/RPCStabilityTests.kt
@@ -69,7 +69,7 @@ class RPCStabilityTests {
     }
 
     @Test
-    @Ignore
+    @Ignore("Ignored as it became increasingly flaky. CORDA-3098")
     fun `client and server dont leak threads`() {
         fun startAndStop() {
             rpcDriver {
@@ -102,7 +102,7 @@ class RPCStabilityTests {
     }
 
     @Test
-    @Ignore
+    @Ignore("Ignored as it became increasingly flaky. CORDA-3098")
     fun `client doesnt leak threads when it fails to start`() {
         fun startAndStop() {
             rpcDriver {
