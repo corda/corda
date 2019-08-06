7f03fe1311615d2fc08811b56819297a165bc892
josecoll <jose.coll@r3cev.com>
2019-08-06 11:10:04 +0100

    Minor tweaks required for Java 8 compatibility.

--- node/src/main/kotlin/net/corda/node/utilities/JVMAgentUtil.kt
+++ node/src/main/kotlin/net/corda/node/utilities/JVMAgentUtil.kt
@@ -19,7 +19,7 @@ object JVMAgentUtil {
         } catch (e: Exception) {
             log.warn("Unable to determine whether checkpoint agent is running: ${e.message}.\n" +
                      "You may need to pass in -Djdk.attach.allowAttachSelf=true if running on a Java 9 or later VM")
-            Properties(0)
+            Properties()
         }
     }
 }
