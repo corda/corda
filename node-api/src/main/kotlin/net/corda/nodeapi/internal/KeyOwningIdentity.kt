77acf67d1f042c3081e0576370446a2adbaff293
James Higgs <james.higgs@r3.com>
2019-08-13 18:06:23 +0100

    [CORDA-3130] Further renaming and docs clarifications

--- node-api/src/main/kotlin/net/corda/nodeapi/internal/KeyOwningIdentity.kt
+++ node-api/src/main/kotlin/net/corda/nodeapi/internal/KeyOwningIdentity.kt
@@ -3,14 +3,16 @@ package net.corda.nodeapi.internal
 import java.util.*
 
 /**
- * A [KeyOwningIdentity] represents an entity that owns a public key.
+ * A [KeyOwningIdentity] represents an entity that owns a public key. In this case, the "owner" refers to either an identifier provided
+ * when the key was generated, or the node itself if no identifier was supplied.
  */
 sealed class KeyOwningIdentity {
 
     abstract val uuid: UUID?
 
     /**
-     * [NodeLegalIdentity] is used for keys that belong to the node identity.
+     * [NodeLegalIdentity] is used for keys that belong to the node identity. This is any key created on this node that did not have a UUID
+     * assigned to it on generation.
      */
     object NodeLegalIdentity : KeyOwningIdentity() {
         override fun toString(): String {
@@ -21,9 +23,10 @@ sealed class KeyOwningIdentity {
     }
 
     /**
-     * [ExternalIdentity] is used for keys that have an assigned external UUID.
+     * [MappedIdentity] is used for keys that have an assigned UUID. Keys belonging to a mapped identity were assigned a UUID at the point
+     * they were generated. This UUID may refer to something outside the core of the node, for example an account.
      */
-    data class ExternalIdentity(override val uuid: UUID) : KeyOwningIdentity() {
+    data class MappedIdentity(override val uuid: UUID) : KeyOwningIdentity() {
         override fun toString(): String {
             return uuid.toString()
         }
@@ -32,7 +35,7 @@ sealed class KeyOwningIdentity {
     companion object {
         fun fromUUID(uuid: UUID?): KeyOwningIdentity {
             return if (uuid != null) {
-                ExternalIdentity(uuid)
+                MappedIdentity(uuid)
             } else {
                 NodeLegalIdentity
             }
