213b1fbbe673a4fb185fb031644bce968c7c35d7
stefano <stefano.franz@r3.com>
2019-08-15 10:56:33 +0100

    simple attempt at running tests in docker container

--- testing/test-utils/src/main/java/net/corda/JunitPrinter.java
+++ testing/test-utils/src/main/java/net/corda/JunitPrinter.java
@@ -4,23 +4,36 @@ import io.github.classgraph.ClassGraph;
 import io.github.classgraph.ClassInfoList;
 import org.junit.Test;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 public class JunitPrinter {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+
+        String pathToWriteFileTo = args[0];
+
         ClassGraph classGraph = new ClassGraph();
         classGraph.enableAnnotationInfo();
         classGraph.enableMethodInfo();
         classGraph.enableClassInfo();
         ClassInfoList classesWithAnnotatedMethods = classGraph.scan().getClassesWithMethodAnnotation(Test.class.getCanonicalName());
+        try (PrintWriter bw = (new PrintWriter(new FileWriter(new File(new File(pathToWriteFileTo), "tests-print.out"))))) {
 
-        classesWithAnnotatedMethods.forEach(classInfo -> {
-            classInfo.getDeclaredMethodInfo().forEach(methodInfo -> {
-                if (methodInfo.hasAnnotation(Test.class.getCanonicalName())) {
-                    System.out.println(classInfo.getName() + "." + methodInfo.getName());
-                }
+            classesWithAnnotatedMethods.forEach(classInfo -> {
+                bw.println(classInfo.getName() + ".*");
             });
-        });
 
+//            classesWithAnnotatedMethods.forEach(classInfo -> {
+//                classInfo.getDeclaredMethodInfo().forEach(methodInfo -> {
+//                    if (methodInfo.hasAnnotation(Test.class.getCanonicalName())) {
+//                        bw.println(classInfo.getName() + "." + methodInfo.getName());
+//                    }
+//                });
+//            });
+        }
     }
 
 }
