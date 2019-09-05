b44ba031147729547e7c3dfa6d28395ac77db775
Schife <razvan.codreanu@r3.com>
2019-09-05 16:01:51 +0100

    TM-20 moved changedFiles method to a groovy class in buildSrc

---
+++ buildSrc/src/main/groovy/net/corda/buildtasks/DetektChangedFiles.groovy
@@ -0,0 +1,26 @@
+package net.corda.buildtasks
+
+class DetektChangedFiles {
+
+    static def getChangedFiles() {
+        String teamcityFile = System.getProperty("teamcity.build.changedFiles.file") ?: ""
+        String[] fileList = []
+
+        if (!teamcityFile.isEmpty()) {
+            String fileContents = new File(teamcityFile).text
+            fileList = fileContents.trim().split("\n")
+            for (int i = 0; i < fileList.length; i++) {
+                int endIndex = fileList[i].indexOf(":CHANGED")
+                fileList[i] = fileList[i].substring(0, endIndex)
+            }
+        } else {
+            def stOut = new StringBuilder()
+            def sysError = new StringBuilder()
+            def proc = 'git diff --name-only'.execute()
+            proc.consumeProcessOutput(stOut, sysError)
+            fileList = stOut.toString().trim().split("\n")
+        }
+
+        return fileList
+    }
+}
