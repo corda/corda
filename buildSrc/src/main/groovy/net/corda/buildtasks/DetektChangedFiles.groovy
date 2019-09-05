0b0d1dbe42b4eb0ee0e8504a5555678e2dde26b1
Schife <razvan.codreanu@r3.com>
2019-09-05 17:19:28 +0100

    TM-20 debugging TC build

--- buildSrc/src/main/groovy/net/corda/buildtasks/DetektChangedFiles.groovy
+++ buildSrc/src/main/groovy/net/corda/buildtasks/DetektChangedFiles.groovy
@@ -3,19 +3,15 @@ package net.corda.buildtasks
 class DetektChangedFiles {
 
     static def getChangedFiles() {
-        String teamcityFile = System.getProperty("teamcity.build.changedFiles.file") ?: ""
+        String teamcityFile = System.getProperty("system.teamcity.build.changedFiles.file") ?: ""
         String[] fileList = []
-        println(teamcityFile)
 //        if (!teamcityFile.isEmpty()) {
         String fileContents = new File(teamcityFile).text
-        println(fileContents)
         fileList = fileContents.trim().split("\n")
-        println(fileList)
         for (int i = 0; i < fileList.length; i++) {
             int endIndex = fileList[i].indexOf(":CHANGED")
             fileList[i] = fileList[i].substring(0, endIndex)
         }
-        println(fileList)
 //        } else {
 //            def stOut = new StringBuilder()
 //            def sysError = new StringBuilder()
