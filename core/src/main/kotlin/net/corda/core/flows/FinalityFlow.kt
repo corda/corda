327cbc20217d709c7c8097e836c7196f9d7b64d3
Nick Rogers <rogers.a.nick@gmail.com>
2019-09-15 17:13:05 -0400

    Adding param annotation with description of StatesToRecord for FinalityFlow

--- core/src/main/kotlin/net/corda/core/flows/FinalityFlow.kt
+++ core/src/main/kotlin/net/corda/core/flows/FinalityFlow.kt
@@ -69,6 +69,7 @@ class FinalityFlow private constructor(val transaction: SignedTransaction,
      * @param transaction What to commit.
      * @param sessions A collection of [FlowSession]s for each non-local participant of the transaction. Sessions to non-participants can
      * also be provided.
+     * @param statesToRecord Which transactions to commit to the vault. Defaults to [StatesToRecord.ONLY_RELEVANT].
      */
     @JvmOverloads
     constructor(
