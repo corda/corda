eea7fabe4d81ebf8c7fe127b4b110bc4f6284462
josecoll <jose.coll@r3cev.com>
2019-09-30 12:08:00 +0100

    Added general exception handler for capturing and exiting on all types of Virtual Machine errors.

---
+++ node/src/main/kotlin/net/corda/node/internal/GeneralExceptionHandler.kt
@@ -0,0 +1,32 @@
+package net.corda.node.internal
+
+import net.corda.node.utilities.errorAndTerminate
+import org.slf4j.LoggerFactory
+import java.sql.SQLException
+
+/**
+ *
+ * Cater for all type of unrecoverable [VirtualMachineError] in which the node may end up in an inconsistent state.
+ * Fail fast and hard.
+ */
+class GeneralExceptionHandler(private val parentHandler: Thread.UncaughtExceptionHandler? = null) : Thread.UncaughtExceptionHandler {
+    override fun uncaughtException(t: Thread?, e: Throwable?) {
+
+        // the error is a database connection issue - pull the rug
+        if (e is Error && e.cause is SQLException) {
+            errorAndTerminate("Thread ${t!!.name} failed due to database connection error. This is unrecoverable, terminating node.", e)
+        }
+        else if (e is VirtualMachineError) {
+            errorAndTerminate("Thread ${t!!.name} failed due to an unrecoverable Virtual Machine error. Terminating node.", e)
+        }
+
+        // replicate the default error handling from ThreadGroup for all other unhandled exceptions
+        if (parentHandler != null) {
+            parentHandler.uncaughtException(t, e)
+        } else if (e !is ThreadDeath) {
+            System.err.print("Exception in thread \"" + t!!.getName() + "\" ")
+            e!!.printStackTrace(System.err)
+            LoggerFactory.getLogger(this.javaClass.name).error("Exception in thread \"" + t.getName() + "\"", e)
+        }
+    }
+}
--- node/src/main/kotlin/net/corda/node/utilities/errorAndTerminate.kt
+++ node/src/main/kotlin/net/corda/node/utilities/errorAndTerminate.kt
@@ -1,8 +1,6 @@
 package net.corda.node.utilities
 
-import net.corda.core.utilities.seconds
 import org.slf4j.LoggerFactory
-import kotlin.concurrent.thread
 
 /**
  * Log error message and terminate the process. This might not clean up resources and could leave
@@ -10,12 +8,11 @@ import kotlin.concurrent.thread
  */
 @Synchronized
 fun errorAndTerminate(message: String, e: Throwable?) {
-    thread {
+    try {
         val log = LoggerFactory.getLogger("errorAndTerminate")
         log.error(message, e)
     }
-
-    // give the logger a chance to flush the error message before killing the node
-    Thread.sleep(10.seconds.toMillis())
-    Runtime.getRuntime().halt(1)
+    finally {
+        Runtime.getRuntime().halt(1)
+    }
 }
