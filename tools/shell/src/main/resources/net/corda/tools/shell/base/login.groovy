608fdb82f7259bfb4d11fc934b161b06fe7cf8d4
Dimos Raptis <dimos.raptis@r3.com>
2019-10-16 13:55:46 +0100

    [ENT-4119] Make welcome message generic

--- tools/shell/src/main/resources/net/corda/tools/shell/base/login.groovy
+++ tools/shell/src/main/resources/net/corda/tools/shell/base/login.groovy
@@ -3,12 +3,23 @@ package net.corda.tools.shell.base
 // Note that this file MUST be in a sub-directory called "base" relative to the path
 // given in the configuration code in InteractiveShell.
 
-welcome = """
-
-Welcome to the Corda interactive shell.
-Useful commands include 'help' to see what is available, and 'bye' to shut down the node.
-
-"""
+welcome = { ->
+    if (crash.context.attributes["crash.localShell"] == true) {
+        """
+        
+        Welcome to the Corda interactive shell.
+        Useful commands include 'help' to see what is available, and 'bye' to exit the shell.
+        
+        """.stripIndent()
+    } else {
+        """
+        
+        Welcome to the Corda interactive shell.
+        Useful commands include 'help' to see what is available, and 'bye' to shut down the node.
+        
+        """.stripIndent()
+    }
+}
 
 prompt = { ->
     return "${new Date()}>>> "
