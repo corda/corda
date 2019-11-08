3b61d4b7ebb02e1ae599a2877bc8ea4368fd75a2
Schife <razvan.codreanu@r3.com>
2019-11-08 15:43:53 +0000

    TM-84 more debugging

--- buildSrc/src/main/groovy/net/corda/testing/DistributedTesting.groovy
+++ buildSrc/src/main/groovy/net/corda/testing/DistributedTesting.groovy
@@ -220,7 +220,9 @@ class DistributedTesting implements Plugin<Project> {
                             fork,
                             task)
                     subProject.logger.info "got ${includes.size()} tests to include into testing task ${task.getPath()}"
+                    subProject.logger.info "INCLUDE: ${includes.toString()} "
                     subProject.logger.info "got ${executedTests.size()} tests to exclude from testing task ${task.getPath()}"
+                    subProject.logger.info "EXCLUDE: ${executedTests.toString()} "
                     if (includes.size() == 0) {
                         subProject.logger.info "Disabling test execution for testing task ${task.getPath()}"
                         excludeTestsMatching "*"
@@ -228,6 +230,7 @@ class DistributedTesting implements Plugin<Project> {
 
                     List<String> intersection = executedTests.stream().filter(includes.&contains).collect(Collectors.toList())
                     subProject.logger.info "got ${intersection.size()} tests in intersection"
+                    subProject.logger.info "INTERSECTION: ${intersection.toString()} "
                     includes.removeAll(intersection)
 
                     intersection.forEach { exclude ->
