e9284d0c862478c98d8c5c54337bc9367df43b93
Stefan Iliev <stefaniliev@19LDN-MAC110.local>
2019-11-18 10:38:25 +0000

    Resolved bad string parsing problems where the json structure could be broken if some symbols were included in the key or value.

--- node/src/main/kotlin/net/corda/node/services/config/ConfigUtilities.kt
+++ node/src/main/kotlin/net/corda/node/services/config/ConfigUtilities.kt
@@ -96,7 +96,7 @@ object ConfigHelper {
                     cordaPropOccurrences.add(newKey)
 
                     newKey.let { key ->
-                        val cfg = ConfigFactory.parseString("$key=${it.value}")
+                        val cfg = ConfigFactory.parseMap(mapOf(key to it.value))
                         val result = V1NodeConfigurationSpec.validate(cfg, Configuration.Validation.Options(strict = true))
 
                         val isInvalidProperty = result.errors.any { err -> err is Configuration.Validation.Error.Unknown }
