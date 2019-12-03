df80959345add16fbfd71d8315227ce95ea436fe
Schife <razvan.codreanu@r3.com>
2019-12-03 13:55:03 +0000

    TM-117 jenkins jira integration with fail test

--- core-tests/src/test/java/net/corda/coretests/contracts/AmountParsingTest.java
+++ core-tests/src/test/java/net/corda/coretests/contracts/AmountParsingTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 
 import static net.corda.finance.Currencies.POUNDS;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 public class AmountParsingTest {
 
@@ -12,5 +13,6 @@ public class AmountParsingTest {
     public void testGbpParse() {
         assertEquals(POUNDS(10), Amount.parseCurrency("10 GBP"));
         assertEquals(POUNDS(11), Amount.parseCurrency("£11"));
+        fail();
     }
 }
