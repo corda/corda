e631c7b970c459eb59ddcc1139935a072a0c336d
Viktor Kolomeyko <viktor.kolomeyko@r3.com>
2020-01-10 12:59:52 +0000

    CORDA-2942: `NodeLifecycleObserverService` and a set of integration tests.
    Public API violations are expected as well as integration tests failing.

---
+++ node/src/integration-test/kotlin/net/corda/node/services/CordaServiceLifecycleFatalTests.kt
@@ -0,0 +1,43 @@
+package net.corda.node.services
+
+import net.corda.core.node.AppServiceHub
+import net.corda.core.node.services.CordaService
+import net.corda.core.node.services.CordaServiceCriticalFailureException
+import net.corda.core.node.services.NodeLifecycleObserverService
+import net.corda.core.serialization.SingletonSerializeAsToken
+import net.corda.core.utilities.getOrThrow
+import net.corda.testing.core.ALICE_NAME
+import net.corda.testing.driver.DriverParameters
+import net.corda.testing.driver.driver
+import net.corda.testing.node.internal.enclosedCordapp
+import org.junit.Test
+import kotlin.test.assertFailsWith
+
+class CordaServiceLifecycleFatalTests {
+
+    @Test
+    fun `corda service receives events`() {
+        // Scenario terminates JVM - node should be running out of process
+        val result = driver(DriverParameters(startNodesInProcess = false, cordappsForAllNodes = listOf(enclosedCordapp()),
+                notarySpecs = emptyList())) {
+            val nodeHandle = startNode(providedName = ALICE_NAME)
+            assertFailsWith(CordaServiceCriticalFailureException::class) {
+                nodeHandle.getOrThrow()
+            }
+        }
+    }
+
+    @CordaService
+    @Suppress("unused")
+    class FatalService(private val services: AppServiceHub) : SingletonSerializeAsToken(), NodeLifecycleObserverService {
+
+        fun computeLength(text: String): Int {
+            require(text.isNotEmpty()) { "Length must be at least 1." }
+            return text.length
+        }
+
+        override fun onNodeStarted() {
+            throw CordaServiceCriticalFailureException("failure")
+        }
+    }
+}
--- node/src/integration-test/kotlin/net/corda/node/services/CordaServiceLifecycleTests.kt
+++ node/src/integration-test/kotlin/net/corda/node/services/CordaServiceLifecycleTests.kt
@@ -6,39 +6,40 @@ import net.corda.core.flows.StartableByRPC
 import net.corda.core.messaging.startFlow
 import net.corda.core.node.AppServiceHub
 import net.corda.core.node.services.CordaService
+import net.corda.core.node.services.NodeLifecycleObserverService
 import net.corda.core.serialization.SingletonSerializeAsToken
-import net.corda.core.utilities.Try
 import net.corda.core.utilities.getOrThrow
-import net.corda.nodeapi.internal.lifecycle.NodeLifecycleEvent
-import net.corda.nodeapi.internal.lifecycle.NodeLifecycleObserver
 import net.corda.testing.core.ALICE_NAME
 import net.corda.testing.driver.DriverParameters
 import net.corda.testing.driver.driver
 import net.corda.testing.node.internal.enclosedCordapp
 import org.junit.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertTrue
 
 class CordaServiceLifecycleTests {
 
     private companion object {
         const val TEST_PHRASE = "testPhrase"
 
-        private val eventsCaptured: MutableList<NodeLifecycleEvent> = mutableListOf()
+        @Volatile
+        private var nodeStartedReceived = false
+        @Volatile
+        private var nodeShuttingDownReceived = false
     }
 
     @Test
     fun `corda service receives events`() {
-        eventsCaptured.clear()
+        nodeStartedReceived = false
+        nodeShuttingDownReceived = false
         val result = driver(DriverParameters(startNodesInProcess = true, cordappsForAllNodes = listOf(enclosedCordapp()),
                 notarySpecs = emptyList())) {
             val node = startNode(providedName = ALICE_NAME).getOrThrow()
+            assertTrue(nodeStartedReceived)
             node.rpc.startFlow(::ComputeTextLengthThroughCordaService, TEST_PHRASE).returnValue.getOrThrow()
         }
         assertEquals(TEST_PHRASE.length, result)
-        // BeforeStart will not be delivered as it is dispatched even before the service is added.
-        assertEquals(3, eventsCaptured.size)
-        assertEquals(listOf(NodeLifecycleEvent.AfterStart::class.java, NodeLifecycleEvent.BeforeStop::class.java,
-                NodeLifecycleEvent.AfterStop::class.java), eventsCaptured.map { it.javaClass })
+        assertTrue(nodeShuttingDownReceived)
     }
 
     @StartableByRPC
@@ -52,18 +53,19 @@ class CordaServiceLifecycleTests {
 
     @CordaService
     @Suppress("unused")
-    class TextLengthComputingService(private val services: AppServiceHub) : SingletonSerializeAsToken(), NodeLifecycleObserver {
-
-        override val priority = NodeLifecycleObserver.SERVICE_PRIORITY_NORMAL
+    class TextLengthComputingService(private val services: AppServiceHub) : SingletonSerializeAsToken(), NodeLifecycleObserverService {
 
         fun computeLength(text: String): Int {
             require(text.isNotEmpty()) { "Length must be at least 1." }
             return text.length
         }
 
-        override fun update(nodeLifecycleEvent: NodeLifecycleEvent): Try<String> {
-            eventsCaptured.add(nodeLifecycleEvent)
-            return super.update(nodeLifecycleEvent)
+        override fun onNodeStarted() {
+            nodeStartedReceived = true
+        }
+
+        override fun onNodeShuttingDown() {
+            nodeShuttingDownReceived = true
         }
     }
 }
