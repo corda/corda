30282f1c1965488ffb66d1bda996785d3bf955a4
Kyriakos Tharrouniatis <kyriakos.tharrouniatis@r3.com>
2020-01-14 11:15:43 +0000

    Revert FinalityFlow to ef57e9909c92fc18ad568969fde5483d3f8cf893 - changing hasNotarySignature back to hasNoNotarySignature

--- core/src/main/kotlin/net/corda/core/flows/FinalityFlow.kt
+++ core/src/main/kotlin/net/corda/core/flows/FinalityFlow.kt
@@ -231,13 +231,13 @@ class FinalityFlow private constructor(val transaction: SignedTransaction,
     private fun needsNotarySignature(stx: SignedTransaction): Boolean {
         val wtx = stx.tx
         val needsNotarisation = wtx.inputs.isNotEmpty() || wtx.references.isNotEmpty() || wtx.timeWindow != null
-        return needsNotarisation && !hasNotarySignature(stx)
+        return needsNotarisation && hasNoNotarySignature(stx)
     }
 
-    private fun hasNotarySignature(stx: SignedTransaction): Boolean {
+    private fun hasNoNotarySignature(stx: SignedTransaction): Boolean {
         val notaryKey = stx.tx.notary?.owningKey
         val signers = stx.sigs.asSequence().map { it.by }.toSet()
-        return notaryKey?.isFulfilledBy(signers) == true
+        return notaryKey?.isFulfilledBy(signers) != true
     }
 
     private fun extractExternalParticipants(ltx: LedgerTransaction): Set<Party> {
