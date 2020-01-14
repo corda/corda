8573534a383b1cbe722fde59dfdb01022804f7f2
Kyriakos Tharrouniatis <kyriakos.tharrouniatis@r3.com>
2020-01-14 18:02:01 +0000

    Variable renaming

--- node/src/main/kotlin/net/corda/node/internal/AbstractNode.kt
+++ node/src/main/kotlin/net/corda/node/internal/AbstractNode.kt
@@ -1252,7 +1252,7 @@ fun createCordaPersistence(databaseConfig: DatabaseConfig,
             // "corrupting" a DatabaseTransaction only inside a flow state machine execution
             FlowStateMachineImpl.currentStateMachine()?.let {
                 // register only the very first exception thrown throughout a chain of logical transactions
-                if (logicalTxCorruptedBy == null) logicalTxCorruptedBy = t
+                if (databaseTxStatementBrokenBy == null) databaseTxStatementBrokenBy = t
             }
         })
 }
