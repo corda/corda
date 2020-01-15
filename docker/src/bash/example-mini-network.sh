8f3113df3be957a699e565e49211861011a64ac8
Jonathan Locke <lockathan@gmail.com>
2020-01-15 14:02:03 +0000

    First release candidate of Corda OS 4.4.

--- docker/src/bash/example-mini-network.sh
+++ docker/src/bash/example-mini-network.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 NODE_LIST=("dockerNode1" "dockerNode2" "dockerNode3")
 NETWORK_NAME=mininet
-CORDAPP_VERSION="4.4-SNAPSHOT"
-DOCKER_IMAGE_VERSION="corda-zulu-4.4-snapshot"
+CORDAPP_VERSION="4.4-RC01"
+DOCKER_IMAGE_VERSION="corda-zulu-4.4-rc01"
 
 mkdir cordapps
 rm -f cordapps/*
