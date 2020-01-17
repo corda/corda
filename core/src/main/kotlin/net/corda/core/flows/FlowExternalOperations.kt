1a7451e6062115c7a9a5b1019231f1c9c24369f6
LankyDan <danknewton@hotmail.com>
2020-01-17 16:44:26 +0000

    ENT-4601 Fix detekt

--- core/src/main/kotlin/net/corda/core/flows/FlowExternalOperations.kt
+++ core/src/main/kotlin/net/corda/core/flows/FlowExternalOperations.kt
@@ -4,18 +4,19 @@ import java.util.concurrent.CompletableFuture
 
 /**
  * [FlowExternalAsyncOperation] represents an external future that blocks a flow from continuing until the future returned by
- * [FlowExternalAsyncOperation.execute] has completed. Examples of external processes where [FlowExternalAsyncOperation] would be useful include,
- * triggering a long running process on an external system or retrieving information from a service that might be down.
+ * [FlowExternalAsyncOperation.execute] has completed. Examples of external processes where [FlowExternalAsyncOperation] would be useful
+ * include, triggering a long running process on an external system or retrieving information from a service that might be down.
  *
  * The flow will suspend while it is blocked to free up a flow worker thread, which allows other flows to continue processing while waiting
  * for the result of this process.
  *
- * Implementations of [FlowExternalAsyncOperation] should ideally hold references to any external values required by [execute]. These references
- * should be passed into the implementation's constructor. For example, an amount or a reference to a Corda Service could be passed in.
+ * Implementations of [FlowExternalAsyncOperation] should ideally hold references to any external values required by [execute]. These
+ * references should be passed into the implementation's constructor. For example, an amount or a reference to a Corda Service could be
+ * passed in.
  *
- * It is discouraged to insert into the node's database from a [FlowExternalAsyncOperation], except for keeping track of [deduplicationId]s that
- * have been processed. It is possible to interact with the database from inside a [FlowExternalAsyncOperation] but, for most operations, is not
- * currently supported.
+ * It is discouraged to insert into the node's database from a [FlowExternalAsyncOperation], except for keeping track of [deduplicationId]s
+ * that have been processed. It is possible to interact with the database from inside a [FlowExternalAsyncOperation] but, for most
+ * operations, is not currently supported.
  */
 interface FlowExternalAsyncOperation<R : Any> {
 
@@ -35,8 +36,8 @@ interface FlowExternalAsyncOperation<R : Any> {
 
 /**
  * [FlowExternalOperation] represents an external process that blocks a flow from continuing until the result of [execute]
- * has been retrieved. Examples of external processes where [FlowExternalOperation] would be useful include, triggering a long running process
- * on an external system or retrieving information from a service that might be down.
+ * has been retrieved. Examples of external processes where [FlowExternalOperation] would be useful include, triggering a long running
+ * process on an external system or retrieving information from a service that might be down.
  *
  * The flow will suspend while it is blocked to free up a flow worker thread, which allows other flows to continue processing while waiting
  * for the result of this process.
