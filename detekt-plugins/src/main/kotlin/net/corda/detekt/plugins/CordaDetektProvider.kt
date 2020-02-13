bce120282964ac39cfa90180e2e5d2f9accbf05a
James Higgs <james.higgs@r3.com>
2020-02-13 11:49:24 +0000

    [NOTICK] Add a test for custom detekt rules and tidying

--- detekt-plugins/src/main/kotlin/net/corda/detekt/plugins/CordaDetektProvider.kt
+++ detekt-plugins/src/main/kotlin/net/corda/detekt/plugins/CordaDetektProvider.kt
@@ -3,8 +3,9 @@ package net.corda.detekt.plugins
 import io.gitlab.arturbosch.detekt.api.Config
 import io.gitlab.arturbosch.detekt.api.RuleSet
 import io.gitlab.arturbosch.detekt.api.RuleSetProvider
-import net.corda.detekt.plugins.rules.UntimedTest
+import net.corda.detekt.plugins.rules.TestWithMissingTimeout
 
+// When adding new detekt rules, add the new rule to the list of instances below.
 class CordaDetektProvider : RuleSetProvider {
 
     override val ruleSetId: String = "corda-detekt"
@@ -12,7 +13,7 @@ class CordaDetektProvider : RuleSetProvider {
     override fun instance(config: Config): RuleSet = RuleSet(
             ruleSetId,
             listOf(
-                UntimedTest()
+                TestWithMissingTimeout()
             )
     )
 }
