d3ec97796856d531700f0457c7dba8c983bdb20a
Jakub Bielawa <jakubbielawa@16LHR-MAC26.local>
2020-02-28 11:18:09 +0000

    Address feedback

--- tools/shell/src/main/java/net/corda/tools/shell/StartShellCommand.java
+++ tools/shell/src/main/java/net/corda/tools/shell/StartShellCommand.java
@@ -18,7 +18,6 @@ public class StartShellCommand extends InteractiveShellCommand {
 
     @Command
     @Man("An alias for 'flow start'. Example: \"start Yo target: Some other company\"")
-    @Usage("An alias for 'flow start'. Example: \"start Yo target: Some other company\"")
     public void main(@Usage("The class name of the flow to run, or an unambiguous substring") @Argument String name,
                      @Usage("The data to pass as input") @Argument(unquote = false) List<String> input) {
 
