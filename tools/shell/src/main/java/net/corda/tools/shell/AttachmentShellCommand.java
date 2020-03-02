da3adb40b4e9d3d6cb5cf91ef2eec6b4c9a9585a
jakubbielawa <jakub.bielawa@gmail.com>
2020-03-02 11:14:55 +0000

    ENT-5039 Improved help text for commands (#6006)
    * Improved help text for commands
    
    * Address feedback

--- tools/shell/src/main/java/net/corda/tools/shell/AttachmentShellCommand.java
+++ tools/shell/src/main/java/net/corda/tools/shell/AttachmentShellCommand.java
@@ -2,6 +2,7 @@ package net.corda.tools.shell;
 
 import org.crsh.cli.Command;
 import org.crsh.cli.Man;
+import org.crsh.cli.Usage;
 
 import static net.corda.tools.shell.InteractiveShell.runAttachmentTrustInfoView;
 
@@ -9,6 +10,7 @@ public class AttachmentShellCommand extends InteractiveShellCommand {
 
     @Command
     @Man("Displays the trusted CorDapp attachments that have been manually installed or received over the network")
+    @Usage("Displays the trusted CorDapp attachments that have been manually installed or received over the network")
     public void trustInfo() {
         runAttachmentTrustInfoView(out, ops());
     }
--- tools/shell/src/main/java/net/corda/tools/shell/CheckpointShellCommand.java
+++ tools/shell/src/main/java/net/corda/tools/shell/CheckpointShellCommand.java
@@ -2,6 +2,7 @@ package net.corda.tools.shell;
 
 import org.crsh.cli.Command;
 import org.crsh.cli.Man;
+import org.crsh.cli.Usage;
 
 import static net.corda.tools.shell.InteractiveShell.*;
 
@@ -9,6 +10,7 @@ public class CheckpointShellCommand extends InteractiveShellCommand {
 
     @Command
     @Man("Outputs the contents of all checkpoints as json to be manually reviewed")
+    @Usage("Outputs the contents of all checkpoints as json to be manually reviewed")
     public void dump() {
         runDumpCheckpoints(ops());
     }
