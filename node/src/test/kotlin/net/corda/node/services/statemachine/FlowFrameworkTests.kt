dac6e1d4059a996fb36ef3f4a942a6e8003202cb
Will Vigor <william.vigor@r3.com>
2020-03-05 14:31:47 +0000

    Remove unnecessary non-null assertion

--- node/src/test/kotlin/net/corda/node/services/statemachine/FlowFrameworkTests.kt
+++ node/src/test/kotlin/net/corda/node/services/statemachine/FlowFrameworkTests.kt
@@ -328,7 +328,7 @@ class FlowFrameworkTests {
         aliceNode.database.transaction {
             val checkpoint = dbCheckpointStorage.getCheckpoint(flow.id)
             assertEquals(Checkpoint.FlowStatus.COMPLETED, checkpoint!!.status)
-            val result = checkpoint!!.result?.bytes?.deserialize<Any>()
+            val result = checkpoint.result?.bytes?.deserialize<Any>()
             assertTrue(result is Unit)
         }
     }
