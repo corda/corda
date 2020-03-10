3964c7c8b48e9c6b19f908b4221264580c5d6234
Will Vigor <william.vigor@r3.com>
2020-03-10 12:36:45 +0000

    Fix Detekt issue

--- node/src/main/kotlin/net/corda/node/services/persistence/DBCheckpointStorage.kt
+++ node/src/main/kotlin/net/corda/node/services/persistence/DBCheckpointStorage.kt
@@ -34,6 +34,7 @@ import javax.persistence.OneToOne
 /**
  * Simple checkpoint key value storage in DB.
  */
+@Suppress("TooManyFunctions")
 class DBCheckpointStorage(private val checkpointPerformanceRecorder: CheckpointPerformanceRecorder) : CheckpointStorage {
 
     companion object {
