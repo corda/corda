26689294b050b9ea6aaa0425b1a286b783efb69e
Kyriakos Tharrouniatis <kyriakos.tharrouniatis@r3.com>
2020-03-16 11:39:45 +0000

    Change test to reflect what can actually happen

--- node/src/test/kotlin/net/corda/node/services/statemachine/FlowFrameworkTests.kt
+++ node/src/test/kotlin/net/corda/node/services/statemachine/FlowFrameworkTests.kt
@@ -668,7 +668,7 @@ class FlowFrameworkTests {
 
             if (firstExecution) {
                 // the following manual persisting Checkpoint.status to FAILED should be removed when implementing CORDA-3604.
-                manuallyFailCheckpointInDB(aliceNode)
+                manuallyHospitalizeCheckpointInDB(aliceNode)
 
                 firstExecution = false
                 throw SQLException("deadlock") // will cause flow to retry
@@ -685,7 +685,7 @@ class FlowFrameworkTests {
 
         aliceNode.services.startFlow(SuspendingFlow()).resultFuture.getOrThrow()
 
-        assertEquals(Checkpoint.FlowStatus.FAILED, checkpointStatusInDBBeforeSuspension)
+        assertEquals(Checkpoint.FlowStatus.HOSPITALIZED, checkpointStatusInDBBeforeSuspension)
         assertEquals(Checkpoint.FlowStatus.RUNNABLE, checkpointStatusInMemoryBeforeSuspension)
         assertEquals(Checkpoint.FlowStatus.RUNNABLE, checkpointStatusInDBAfterSuspension)
 
@@ -705,7 +705,7 @@ class FlowFrameworkTests {
 
             if (firstExecution) {
                 // the following manual persisting Checkpoint.status to FAILED should be removed when implementing CORDA-3604.
-                manuallyFailCheckpointInDB(aliceNode)
+                manuallyHospitalizeCheckpointInDB(aliceNode)
 
                 firstExecution = false
                 throw SQLException("deadlock") // will cause flow to retry
@@ -717,17 +717,17 @@ class FlowFrameworkTests {
 
         aliceNode.services.startFlow(SuspendingFlow()).resultFuture.getOrThrow()
 
-        assertEquals(Checkpoint.FlowStatus.FAILED, checkpointStatusInDB)
+        assertEquals(Checkpoint.FlowStatus.HOSPITALIZED, checkpointStatusInDB)
         assertEquals(Checkpoint.FlowStatus.RUNNABLE, checkpointStatusInMemory)
 
         SuspendingFlow.hookAfterCheckpoint = {}
     }
 
     // the following method should be removed when implementing CORDA-3604.
-    private fun manuallyFailCheckpointInDB(node: TestStartedNode) {
-        val idCheckpoint = node.internals.checkpointStorage.getRunnableCheckpoints().toList().single()
+    private fun manuallyHospitalizeCheckpointInDB(node: TestStartedNode) {
+        val idCheckpoint = node.internals.checkpointStorage.getAllCheckpoints().toList().single()
         val checkpoint = idCheckpoint.second
-        val updatedCheckpoint = checkpoint.copy(status = Checkpoint.FlowStatus.FAILED)
+        val updatedCheckpoint = checkpoint.copy(status = Checkpoint.FlowStatus.HOSPITALIZED)
         node.internals.checkpointStorage.updateCheckpoint(idCheckpoint.first,
                 updatedCheckpoint.deserialize(CheckpointSerializationDefaults.CHECKPOINT_CONTEXT),
                 updatedCheckpoint.serializedFlowState)
