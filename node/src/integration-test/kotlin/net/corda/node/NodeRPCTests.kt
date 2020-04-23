80bdd2b06edd2d15d67e386df3642f706393226e
Dirkjan Ochtman <dirkjan.ochtman@ing.com>
2020-04-23 16:48:17 +0200

    Add integration test for RPC over AMQP

--- node/src/integration-test/kotlin/net/corda/node/NodeRPCTests.kt
+++ node/src/integration-test/kotlin/net/corda/node/NodeRPCTests.kt
@@ -1,13 +1,28 @@
 package net.corda.node
 
 import net.corda.core.internal.PLATFORM_VERSION
+import net.corda.core.serialization.serialize
+import net.corda.nodeapi.RPCApi
+import net.corda.nodeapi.internal.rpc.client.ObservableContext
 import net.corda.testing.driver.DriverParameters
 import net.corda.testing.driver.driver
 import net.corda.testing.node.internal.FINANCE_CONTRACTS_CORDAPP
 import net.corda.testing.node.internal.FINANCE_WORKFLOWS_CORDAPP
+import org.apache.activemq.artemis.jms.client.ActiveMQSession
 import org.junit.Test
+import javax.jms.MessageConsumer
+import javax.jms.MessageProducer
+import javax.jms.TextMessage
 import kotlin.test.assertEquals
 import kotlin.test.assertTrue
+import org.apache.qpid.jms.JmsConnectionFactory;
+import rx.Observable
+import java.util.*
+import javax.jms.BytesMessage
+import javax.jms.Connection
+import javax.jms.ConnectionFactory
+import javax.jms.Queue
+import javax.jms.Session
 
 class NodeRPCTests {
     private val CORDA_VERSION_REGEX = "\\d+(\\.\\d+)?(-\\w+)?".toRegex()
@@ -41,6 +56,63 @@ class NodeRPCTests {
             assertTrue(cordappInfo.minimumPlatformVersion <= PLATFORM_VERSION)
             assertTrue(cordappInfo.targetPlatformVersion <= PLATFORM_VERSION)
             assertTrue(cordappInfo.jarHash.toString().matches(HEXADECIMAL_REGEX))
+        }
+    }
+}
+
+class AMQPRPCTests {
+    private val CORDAPPS = listOf(FINANCE_CONTRACTS_CORDAPP, FINANCE_WORKFLOWS_CORDAPP)
+
+    private val observableContext = ObservableContext(
+            callSiteMap = null,
+            observableMap = createRpcObservableMap(),
+            hardReferenceStore = Collections.synchronizedSet(mutableSetOf<Observable<*>>())
+    )
+
+    @Test(timeout=300_000)
+    fun `RPC over AMQP`() {
+        driver(DriverParameters(notarySpecs = emptyList(), cordappsForAllNodes = CORDAPPS, extraCordappPackagesToScan = emptyList())) {
+            val node = startNode().get();
+            var connection: Connection? = null
+            val connectionFactory: ConnectionFactory = JmsConnectionFactory("amqp://${node.rpcAddress}")
+
+            try {
+                connection = connectionFactory.createConnection()
+
+                // Step 2. Create a session
+                val session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE)
+
+                val sendQueue = session.createQueue("exampleQueue")
+                val sender = session.createProducer(sendQueue)
+                val message = session.createBytesMessage()
+
+                val serialisedArguments = emptyList().serialize(context = serializationContextWithObservableContext)
+                val request = RPCApi.ClientToServer.RpcRequest(
+                        clientAddress,
+                        "currentNodeTime",
+                        serialisedArguments,
+                        replyId,
+                        sessionId,
+                        externalTrace,
+                        impersonatedActor
+                )
+
+
+
+                sender.send(session.createBytesMessage("Hello world "))
+                connection.start()
+
+                val receiveQueue = session.createQueue("")
+                val consumer: MessageConsumer = session.createConsumer(receiveQueue)
+
+                // Step 7. receive the simple message
+                val m = consumer.receive(5000) as BytesMessage
+            } finally {
+                if (connection != null) {
+                    // Step 9. close the connection
+                    connection.close()
+                }
+            }
         }
     }
 }
