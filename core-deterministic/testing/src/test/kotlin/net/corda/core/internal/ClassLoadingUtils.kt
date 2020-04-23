b9bab56f3f66f6937641a73f999d7c1912f52181
Adel El-Beik <adel.el-beik@r3.com>
2020-04-23 14:28:52 +0100

    CORDA-3715: Updating method to match non deterministic version.

--- core-deterministic/testing/src/test/kotlin/net/corda/core/internal/ClassLoadingUtils.kt
+++ core-deterministic/testing/src/test/kotlin/net/corda/core/internal/ClassLoadingUtils.kt
@@ -3,6 +3,7 @@ package net.corda.core.internal
 /**
  * Stubbing out non-deterministic method.
  */
-fun <T: Any> createInstancesOfClassesImplementing(@Suppress("UNUSED_PARAMETER") classloader: ClassLoader, @Suppress("UNUSED_PARAMETER") clazz: Class<T>): Set<T> {
+fun <T: Any> createInstancesOfClassesImplementing(@Suppress("UNUSED_PARAMETER") classloader: ClassLoader, @Suppress("UNUSED_PARAMETER") clazz: Class<T>,
+                                                  @Suppress("UNUSED_PARAMETER") classVersionRange: IntRange): Set<T> {
     return emptySet()
 }
