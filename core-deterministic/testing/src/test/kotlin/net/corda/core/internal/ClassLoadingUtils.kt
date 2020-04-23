aef2e946a383c0fbebc53aae31afd8de2d066c6f
Adel El-Beik <adel.el-beik@r3.com>
2020-04-23 14:34:05 +0100

    CORDA-3715: Added in default param.

--- core-deterministic/testing/src/test/kotlin/net/corda/core/internal/ClassLoadingUtils.kt
+++ core-deterministic/testing/src/test/kotlin/net/corda/core/internal/ClassLoadingUtils.kt
@@ -4,6 +4,6 @@ package net.corda.core.internal
  * Stubbing out non-deterministic method.
  */
 fun <T: Any> createInstancesOfClassesImplementing(@Suppress("UNUSED_PARAMETER") classloader: ClassLoader, @Suppress("UNUSED_PARAMETER") clazz: Class<T>,
-                                                  @Suppress("UNUSED_PARAMETER") classVersionRange: IntRange): Set<T> {
+                                                  @Suppress("UNUSED_PARAMETER") classVersionRange: IntRange = IntRange.EMPTY): Set<T> {
     return emptySet()
 }
