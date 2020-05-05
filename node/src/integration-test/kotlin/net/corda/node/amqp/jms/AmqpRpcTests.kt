c2864e05a7e04b0fd7990a20c826ea259c63ced4
Viktor Kolomeyko <viktor.kolomeyko@r3.com>
2020-05-05 10:28:16 +0100

    CORDA-3710: Trying to write a valid message and send it to server
        Currently blocked by:
        ```
        Identifier contains invalid JMS identifier character '-': 'rpc-id'
        java.lang.IllegalArgumentException: Identifier contains invalid JMS identifier character '-': 'rpc-id'
        	at org.apache.qpid.jms.message.JmsMessagePropertySupport.checkIdentifierLetterAndDigitRequirements(JmsMessagePropertySupport.java:101)
        	at org.apache.qpid.jms.message.JmsMessagePropertySupport.checkPropertyNameIsValid(JmsMessagePropertySupport.java:63)
        	at org.apache.qpid.jms.message.JmsMessagePropertyIntercepter.setProperty(JmsMessagePropertyIntercepter.java:724)
        	at org.apache.qpid.jms.message.JmsMessage.setObjectProperty(JmsMessage.java:348)
        	at org.apache.qpid.jms.message.JmsMessage.setStringProperty(JmsMessage.java:433)
        	at net.corda.node.amqp.jms.AmqpRpcTests$Companion.mapTo(AmqpRpcTests.kt:52)
        	at net.corda.node.amqp.jms.AmqpRpcTests$Companion.mapTo(AmqpRpcTests.kt:47)
        	at net.corda.node.amqp.jms.AmqpRpcTests$Companion.access$mapTo(AmqpRpcTests.kt:35)
        	at net.corda.node.amqp.jms.AmqpRpcTests$RPC over AMQP$1.invoke(AmqpRpcTests.kt:84)
        	at net.corda.node.amqp.jms.AmqpRpcTests$RPC over AMQP$1.invoke(AmqpRpcTests.kt:33)
        	at net.corda.testing.node.internal.DriverDSLImplKt.genericDriver(DriverDSLImpl.kt:1199)
        	at net.corda.testing.driver.Driver.driver(Driver.kt:185)
        	at net.corda.node.amqp.jms.AmqpRpcTests.RPC over AMQP(AmqpRpcTests.kt:59)
        	at sun.reflect.NativeMethodAccessorImpl.invoke0(Native Method)
        	at sun.reflect.NativeMethodAccessorImpl.invoke(NativeMethodAccessorImpl.java:62)
        	at sun.reflect.DelegatingMethodAccessorImpl.invoke(DelegatingMethodAccessorImpl.java:43)
        	at java.lang.reflect.Method.invoke(Method.java:498)
        	at org.junit.runners.model.FrameworkMethod$1.runReflectiveCall(FrameworkMethod.java:50)
        	at org.junit.internal.runners.model.ReflectiveCallable.run(ReflectiveCallable.java:12)
        	at org.junit.runners.model.FrameworkMethod.invokeExplosively(FrameworkMethod.java:47)
        	at org.junit.internal.runners.statements.InvokeMethod.evaluate(InvokeMethod.java:17)
        	at org.junit.internal.runners.statements.FailOnTimeout$CallableStatement.call(FailOnTimeout.java:298)
        	at org.junit.internal.runners.statements.FailOnTimeout$CallableStatement.call(FailOnTimeout.java:292)
        	at java.util.concurrent.FutureTask.run(FutureTask.java:266)
        	at java.lang.Thread.run(Thread.java:748)
        ```

--- node/src/integration-test/kotlin/net/corda/node/amqp/jms/AmqpRpcTests.kt
+++ node/src/integration-test/kotlin/net/corda/node/amqp/jms/AmqpRpcTests.kt
@@ -1,75 +1,128 @@
 package net.corda.node.amqp.jms
 
+import net.corda.core.context.Trace
+import net.corda.core.crypto.random63BitValue
 import net.corda.core.identity.CordaX500Name
+import net.corda.core.serialization.serialize
+import net.corda.core.utilities.Id
 import net.corda.core.utilities.NetworkHostAndPort
+import net.corda.core.utilities.contextLogger
 import net.corda.coretesting.internal.configureTestSSL
 import net.corda.nodeapi.RPCApi
 import net.corda.nodeapi.internal.config.MutualSslConfiguration
+import net.corda.serialization.internal.AMQP_RPC_CLIENT_CONTEXT
 import net.corda.services.messaging.SimpleMQClient
 import net.corda.testing.driver.DriverParameters
 import net.corda.testing.driver.NodeHandle
 import net.corda.testing.driver.driver
-import net.corda.testing.node.internal.FINANCE_CONTRACTS_CORDAPP
-import net.corda.testing.node.internal.FINANCE_WORKFLOWS_CORDAPP
 import org.apache.activemq.artemis.api.core.SimpleString
 import org.apache.qpid.jms.JmsConnectionFactory
 import org.apache.qpid.jms.JmsQueue
+import org.apache.qpid.jms.JmsTopic
 import org.junit.Ignore
 import org.junit.Test
-import javax.jms.BytesMessage
+import java.util.concurrent.CountDownLatch
+import java.util.concurrent.TimeUnit
+import javax.jms.CompletionListener
 import javax.jms.ConnectionFactory
-import javax.jms.MessageConsumer
+import javax.jms.DeliveryMode
+import javax.jms.ExceptionListener
+import javax.jms.Message
 import javax.jms.Session
+import kotlin.test.assertTrue
 
 @Ignore
 class AmqpRpcTests {
-    private val CORDAPPS = listOf(FINANCE_CONTRACTS_CORDAPP, FINANCE_WORKFLOWS_CORDAPP) // TODO: Do we need CordApps at all?
+
+    companion object {
+        private val logger = contextLogger()
+
+        const val TAG_FIELD_NAME = "tag" // RPCApi.TAG_FIELD_NAME
+        const val RPC_REQUEST = 0// RPCApi.ClientToServer.Tag.RPC_REQUEST
+
+        private const val RPC_ID_FIELD_NAME = "rpc-id" // RPCApi.*
+        private const val RPC_ID_TIMESTAMP_FIELD_NAME = "rpc-id-timestamp"
+        private const val RPC_SESSION_ID_FIELD_NAME = "rpc-session-id"
+        private const val RPC_SESSION_ID_TIMESTAMP_FIELD_NAME = "rpc-session-id-timestamp"
+        private const val METHOD_NAME_FIELD_NAME = "method-name"
+
+        private fun Trace.InvocationId.mapTo(message: Message) = mapTo(message, RPC_ID_FIELD_NAME, RPC_ID_TIMESTAMP_FIELD_NAME)
+
+        private fun Trace.SessionId.mapTo(message: Message) = mapTo(message, RPC_SESSION_ID_FIELD_NAME, RPC_SESSION_ID_TIMESTAMP_FIELD_NAME)
+
+        private fun Id<String>.mapTo(message: Message, valueProperty: String, timestampProperty: String) {
+            message.setStringProperty(valueProperty, value)
+            message.setLongProperty(timestampProperty, timestamp.toEpochMilli())
+        }
+    }
 
     @Test(timeout=300_000)
     fun `RPC over AMQP`() {
-        driver(DriverParameters(notarySpecs = emptyList(), cordappsForAllNodes = CORDAPPS, extraCordappPackagesToScan = emptyList())) {
+        driver(DriverParameters(startNodesInProcess = true, notarySpecs = emptyList())) {
             val node = startNode().get()
             val connectionFactory: ConnectionFactory = JmsConnectionFactory("amqp://${node.rpcAddress}")
             val rpcUser = node.rpcUsers.first()
             connectionFactory.createConnection(rpcUser.username, rpcUser.password).use { connection ->
 
-                // Create a session
-                val session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE)
-
-                val clientQueueName = loginToRPCAndGetClientQueue(node)
-                val sender = session.createProducer(JmsQueue(clientQueueName))
-                //val message = session.createBytesMessage()
-
-                /*
-                val serialisedArguments = emptyList().serialize(context = serializationContextWithObservableContext)
-                val request = RPCApi.ClientToServer.RpcRequest(
-                        clientAddress,
-                        "currentNodeTime",
-                        serialisedArguments,
-                        replyId,
-                        sessionId,
-                        externalTrace,
-                        impersonatedActor
-                )
-                 */
-
-                sender.send(session.createTextMessage("Hello world "))
+                connection.exceptionListener = ExceptionListener { exception -> logger.error("Exception on connection: $connection", exception) }
+                // Start connection
                 connection.start()
 
+                // Create a session
+                val session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE)
+                val rpcServerQueueName = RPCApi.RPC_SERVER_QUEUE_NAME // Note: Server side queue created with MULTICAST routing type,
+                // therefore for this "queue" we should use JmsTopic when publishing
+                val sender = session.createProducer(JmsTopic(rpcServerQueueName))
+
+                val replyId= Trace.InvocationId.newInstance()
+                val sessionId = Trace.SessionId.newInstance()
+                val replyQueue = JmsQueue("${RPCApi.RPC_CLIENT_QUEUE_NAME_PREFIX}.${rpcUser.username}.${random63BitValue()}")
+                //session.createTemporaryQueue()
+
+                val message = session.createBytesMessage()
+                message.apply {
+                    jmsReplyTo = replyQueue
+                    setIntProperty(TAG_FIELD_NAME, RPC_REQUEST)
+                    replyId.mapTo(this)
+                    sessionId.mapTo(this)
+                    message.setStringProperty(METHOD_NAME_FIELD_NAME, "currentNodeTime")
+                    val serialisedArguments = emptyList<Unit>().serialize(context = AMQP_RPC_CLIENT_CONTEXT)
+                    message.writeBytes(serialisedArguments.bytes)
+                }
+
+                val sentLatch = CountDownLatch(1)
+
+                logger.info("About to send")
+                sender.send(message, DeliveryMode.NON_PERSISTENT, Message.DEFAULT_PRIORITY, Message.DEFAULT_TIME_TO_LIVE, object : CompletionListener {
+                    override fun onException(message: Message, exception: Exception) {
+                        logger.error(message.toString(), exception)
+                    }
+
+                    override fun onCompletion(message: Message) {
+                        logger.info("Message successfully sent: $message")
+                        sentLatch.countDown()
+                    }
+                })
+
+                assertTrue(sentLatch.await(10, TimeUnit.SECONDS))
+                Thread.sleep(10000)
+                logger.info("Obtaining response")
+                /*
                 val receiveQueue = session.createQueue("")
                 val consumer: MessageConsumer = session.createConsumer(receiveQueue)
 
                 // Step 7. receive the simple message
                 /*val m = */consumer.receive(5000) as BytesMessage
+                */
             }
         }
     }
 
     private fun loginToRPCAndGetClientQueue(nodeHandle: NodeHandle): String {
         val rpcUser = nodeHandle.rpcUsers.first()
-        val clientQueueQuery = SimpleString("${RPCApi.RPC_CLIENT_QUEUE_NAME_PREFIX}.${rpcUser.username}.*")
         val client = clientTo(nodeHandle.rpcAddress)
         client.start(rpcUser.username, rpcUser.password, false)
+        val clientQueueQuery = SimpleString("${RPCApi.RPC_CLIENT_QUEUE_NAME_PREFIX}.${rpcUser.username}.*")
         return client.session.addressQuery(clientQueueQuery).queueNames.single().toString()
     }
 
