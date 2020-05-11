c9aeb209fd70841fa1763337049b9593fb59352a
Adel El-Beik <adel.el-beik@r3.com>
2020-05-11 18:33:02 +0100

    CORDA-3730: Remove unused feature.

--- core/src/main/kotlin/net/corda/core/internal/PlatformVersionSwitches.kt
+++ core/src/main/kotlin/net/corda/core/internal/PlatformVersionSwitches.kt
@@ -5,6 +5,5 @@ Constants for new features that can only be switched on at specific platform ver
 The text constant describes the feature and the numeric specifies the platform version the feature is enabled at.
  */
 object PlatformVersionSwitches {
-    const val REMOVE_NO_OVERLAP_RULE_FOR_REFERENCE_DATA_ATTACHMENTS = 7
     const val ENABLE_P2P_COMPRESSION = 7
 }
