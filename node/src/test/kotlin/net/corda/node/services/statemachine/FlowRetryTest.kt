0f4d9763bf7961a9eba4c1e58f91e9020ff884ff
Will Vigor <william.vigor@r3.com>
2020-05-11 17:38:57 +0100

    Add some more tests for FlowRetry

--- node/src/test/kotlin/net/corda/node/services/statemachine/FlowRetryTest.kt
+++ node/src/test/kotlin/net/corda/node/services/statemachine/FlowRetryTest.kt
@@ -1,14 +1,12 @@
 package net.corda.node.services.statemachine
 
 import co.paralleluniverse.fibers.Suspendable
+import net.corda.core.flows.FlowException
 import net.corda.core.flows.FlowLogic
 import net.corda.core.flows.HospitalizeFlowException
 import net.corda.core.flows.StateMachineRunId
-import net.corda.core.identity.Party
 import net.corda.core.utilities.getOrThrow
 import net.corda.testing.core.ALICE_NAME
-import net.corda.testing.core.BOB_NAME
-import net.corda.testing.core.singleIdentity
 import net.corda.testing.node.InMemoryMessagingNetwork
 import net.corda.testing.node.internal.DUMMY_CONTRACTS_CORDAPP
 import net.corda.testing.node.internal.InternalMockNetwork
@@ -24,10 +22,10 @@ import kotlin.test.assertEquals
 class FlowRetryTest {
     private lateinit var mockNet: InternalMockNetwork
     private lateinit var aliceNode: TestStartedNode
-    private lateinit var bobNode: TestStartedNode
-    private lateinit var alice: Party
-    private lateinit var bob: Party
-    private lateinit var notaryIdentity: Party
+
+    companion object {
+        val NUMBER_OF_RETRIES = 5
+    }
 
     @Before
     fun setUpMockNet() {
@@ -35,15 +33,7 @@ class FlowRetryTest {
                 cordappsForAllNodes = listOf(DUMMY_CONTRACTS_CORDAPP),
                 servicePeerAllocationStrategy = InMemoryMessagingNetwork.ServicePeerAllocationStrategy.RoundRobin()
         )
-
         aliceNode = mockNet.createNode(InternalMockNodeParameters(legalName = ALICE_NAME))
-        bobNode = mockNet.createNode(InternalMockNodeParameters(legalName = BOB_NAME))
-
-        // Extract identities
-        alice = aliceNode.info.singleIdentity()
-        bob = bobNode.info.singleIdentity()
-        notaryIdentity = mockNet.defaultNotaryIdentity
-
     }
 
     @After
@@ -51,12 +41,13 @@ class FlowRetryTest {
         mockNet.stopNodes()
     }
 
-    fun TestStartedNode.waitForFlowStatus(id: StateMachineRunId, flowStatus: Checkpoint.FlowStatus) : Boolean {
+    fun TestStartedNode.waitForFlowStatus(id: StateMachineRunId, flowStatus: Checkpoint.FlowStatus?) : Boolean {
         for (i in 0..100) {
             val checkpoint = this.database.transaction {
                 this@waitForFlowStatus.internals.checkpointStorage.getDBCheckpoint(id)
             }
             if (checkpoint?.status == flowStatus) return true
+            println("Status = ${checkpoint?.status}")
             Thread.sleep(10)
         }
         return false
@@ -66,7 +57,6 @@ class FlowRetryTest {
     fun `A hospitalized flow can be retried`() {
         val flow = aliceNode.services.startFlow(HospitalizingFlow())
         assertEquals(true, aliceNode.waitForFlowStatus(flow.id, Checkpoint.FlowStatus.HOSPITALIZED))
-
         val retry = aliceNode.internals.smm.retryFlow(flow.id)
         assertEquals(true, retry)
         val retriedFuture = aliceNode.smm.allStateMachines.single().stateMachine.resultFuture
@@ -80,7 +70,7 @@ class FlowRetryTest {
         val firstRetry = aliceNode.internals.smm.retryFlow(flow.id)
         assertEquals(true, firstRetry)
 
-        for (i in 0..5) {
+        for (i in 0..NUMBER_OF_RETRIES) {
             val retryAgain = aliceNode.internals.smm.retryFlow(flow.id)
             assertEquals(false, retryAgain)
         }
@@ -89,16 +79,53 @@ class FlowRetryTest {
     @Test(timeout = 300_000)
     fun `A running flow can not be retried`() {
         val flow = aliceNode.services.startFlow(LongPausingFlow())
-        for (i in 0..5) {
+        assertEquals(true, aliceNode.waitForFlowStatus(flow.id, Checkpoint.FlowStatus.RUNNABLE))
+        for (i in 0..NUMBER_OF_RETRIES) {
             val retryAgain = aliceNode.internals.smm.retryFlow(flow.id)
             assertEquals(false, retryAgain)
         }
-        assertEquals(true, aliceNode.waitForFlowStatus(flow.id, Checkpoint.FlowStatus.PAUSED))
+        assertEquals(1, aliceNode.internals.smm.allStateMachines.size)
 
         val flowKilled = aliceNode.internals.smm.killFlow(flow.id)
         assertEquals(true, flowKilled)
     }
 
+    @Test(timeout = 300_000)
+    fun `A completed flow can not be retried`() {
+        val flow = aliceNode.services.startFlow(FastCompletionFlow())
+        flow.resultFuture.getOrThrow()
+        //This assumes that the checkpoint gets removed from the database when 
+        assertEquals(true, aliceNode.waitForFlowStatus(flow.id, null))
+        for (i in 0..NUMBER_OF_RETRIES) {
+            val retryAgain = aliceNode.internals.smm.retryFlow(flow.id)
+            assertEquals(false, retryAgain)
+        }
+    }
+
+    @Test(timeout = 300_000)
+    fun `A failed flow can not be retried`() {
+        val flow = aliceNode.services.startFlow(FlowExceptionFlow())
+        aliceNode.waitForFlowStatus(flow.id, Checkpoint.FlowStatus.FAILED)
+        for (i in 0..NUMBER_OF_RETRIES) {
+            val retryAgain = aliceNode.internals.smm.retryFlow(flow.id)
+            assertEquals(false, retryAgain)
+        }
+    }
+
+    class FlowExceptionFlow : FlowLogic<Unit>() {
+        @Suspendable
+        override fun call() {
+            throw FlowException("FlowException")
+        }
+    }
+
+    class FastCompletionFlow : FlowLogic<Boolean>() {
+        @Suspendable
+        override fun call(): Boolean {
+            sleep(Duration.ofMillis(10))
+            return true
+        }
+    }
 
     class HospitalizingFlow : FlowLogic<Boolean>() {
         @Suspendable
