c2ac22a41ff2c929f7418c39e57daf3e8ebaa3a3
Dan Newton <danknewton@hotmail.com>
2020-05-13 15:17:51 +0100

    NOTICK Non-database error handling in `withEntityManager` (#6239)
    When a non-database exception is thrown out of a `withEntityManager`
    block, always check if the session needs to be rolled back.
    
    This means if a database error is caught and a new non-database error is
    thrown out of the `withEntityManager` block, the transaction is still
    rolled back. The flow can then continue progressing as normal.

--- node/src/main/kotlin/net/corda/node/internal/AbstractNode.kt
+++ node/src/main/kotlin/net/corda/node/internal/AbstractNode.kt
@@ -183,7 +183,7 @@ import java.sql.Savepoint
 import java.time.Clock
 import java.time.Duration
 import java.time.format.DateTimeParseException
-import java.util.*
+import java.util.Properties
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 import java.util.concurrent.LinkedBlockingQueue
@@ -193,8 +193,6 @@ import java.util.concurrent.TimeUnit.MINUTES
 import java.util.concurrent.TimeUnit.SECONDS
 import java.util.function.Consumer
 import javax.persistence.EntityManager
-import javax.persistence.PersistenceException
-import kotlin.collections.ArrayList
 
 /**
  * A base node implementation that can be customised either for production (with real implementations that do real
@@ -1196,6 +1194,7 @@ abstract class AbstractNode<S>(val configuration: NodeConfiguration,
          */
         override fun jdbcSession(): Connection = RestrictedConnection(database.createSession())
 
+        @Suppress("TooGenericExceptionCaught")
         override fun <T : Any?> withEntityManager(block: EntityManager.() -> T): T {
             return database.transaction(useErrorHandler = false) {
                 session.flush()
@@ -1210,7 +1209,7 @@ abstract class AbstractNode<S>(val configuration: NodeConfiguration,
                                 connection.rollback(savepoint)
                             }
                         }
-                    } catch (e: PersistenceException) {
+                    } catch (e: Exception) {
                         if (manager.transaction.rollbackOnly) {
                             connection.rollback(savepoint)
                         }
