9bfeb574ff548660ed21ed4894132408e9daba4f
Kyriakos Tharrouniatis <kyriakos.tharrouniatis@r3.com>
2020-06-12 14:47:19 +0100

    Remove uneeded locking

--- node/src/main/kotlin/net/corda/node/services/statemachine/SingleThreadedStateMachineManager.kt
+++ node/src/main/kotlin/net/corda/node/services/statemachine/SingleThreadedStateMachineManager.kt
@@ -271,9 +271,7 @@ internal class SingleThreadedStateMachineManager(
         ).also {
             if (clientID != null) {
                 // wire up this future to the clientIDsToFlowIds[clientID] future
-                mutex.locked {
-                    clientIDsToFlowIds[clientID]!!.captureLater(it)
-                }
+                mutex.content.clientIDsToFlowIds[clientID]!!.captureLater(it) // TODO: take care of this not checked null
             }
         }
     }
