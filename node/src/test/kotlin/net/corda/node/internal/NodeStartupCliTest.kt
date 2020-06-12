8ec2c6a107356fd0999dba4e03d4f9223aea1855
Andrei Palade <palade@users.noreply.github.com>
2020-06-12 14:21:21 +0100

    Updated picocli library and made logging level and verbose global options

--- node/src/test/kotlin/net/corda/node/internal/NodeStartupCliTest.kt
+++ node/src/test/kotlin/net/corda/node/internal/NodeStartupCliTest.kt
@@ -6,6 +6,7 @@ import net.corda.nodeapi.internal.config.UnknownConfigKeysPolicy
 import org.assertj.core.api.Assertions
 import org.junit.BeforeClass
 import org.junit.Test
+import org.junit.rules.TemporaryFolder
 import org.slf4j.event.Level
 import picocli.CommandLine
 import java.nio.file.Path
@@ -48,5 +49,32 @@ class NodeStartupCliTest {
         Assertions.assertThat(startup.cmdLineOptions.baseDirectory).isEqualTo(workingDirectory / "another-base-dir")
         Assertions.assertThat(startup.cmdLineOptions.configFile).isEqualTo(workingDirectory / "another-base-dir" / "node.conf")
         Assertions.assertThat(startup.cmdLineOptions.networkRootTrustStorePathParameter).isEqualTo(null)
+    }
+
+    @Test(timeout=300_000)
+    fun `--log-to-console and --logging-level are set correctly`() {
+        CommandLine.populateCommand(startup, "--log-to-console", "--logging-level=DEBUG")
+        Assertions.assertThat(startup.verbose).isEqualTo(true)
+        Assertions.assertThat(startup.loggingLevel).isEqualTo(Level.DEBUG)
+    }
+
+    @Test(timeout=300_000)
+    fun `assert that order of global options in command line arguments is not enforced`() {
+        val nodeCli = NodeStartupCli()
+
+        val temporaryFolder = TemporaryFolder()
+        temporaryFolder.create()
+
+        nodeCli.args = arrayOf()
+
+        nodeCli.cmd.parseArgs(
+                "--base-directory", temporaryFolder.root.toPath().toString(),
+                "initial-registration", "--network-root-truststore-password=password",
+                "--logging-level=DEBUG",  "--log-to-console"
+        )
+
+        Assertions.assertThat(nodeCli.loggingLevel).isEqualTo(Level.DEBUG)
+        Assertions.assertThat(nodeCli.verbose).isTrue()
+        Assertions.assertThat(nodeCli.cmdLineOptions.baseDirectory.toString()).isEqualTo(temporaryFolder.root.toPath().toString())
     }
 }
