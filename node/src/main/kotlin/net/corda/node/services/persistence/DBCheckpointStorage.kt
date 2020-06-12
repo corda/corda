abd80ed966ee6a45dfa813d6393fd6830cb28f00
Kyriakos Tharrouniatis <kyriakos.tharrouniatis@r3.com>
2020-06-12 14:59:52 +0100

    Replace wrong exception type

--- node/src/main/kotlin/net/corda/node/services/persistence/DBCheckpointStorage.kt
+++ node/src/main/kotlin/net/corda/node/services/persistence/DBCheckpointStorage.kt
@@ -22,7 +22,6 @@ import net.corda.nodeapi.internal.persistence.currentDBSession
 import org.apache.commons.lang3.ArrayUtils.EMPTY_BYTE_ARRAY
 import org.apache.commons.lang3.exception.ExceptionUtils
 import org.hibernate.annotations.Type
-import sun.plugin.dom.exception.InvalidStateException
 import java.sql.Connection
 import java.sql.SQLException
 import java.time.Clock
@@ -634,7 +633,7 @@ class DBCheckpointStorage(
             when {
                 arguments.size == 2 -> uncheckedCast<Any?, Array<Any?>>(arguments[1]).toList()
                 arguments.size == 3 -> uncheckedCast<Any?, Array<Any?>>(arguments[2]).toList()
-                else -> throw InvalidStateException("Unexpected argument number provided in rpc call")
+                else -> throw IllegalStateException("Unexpected argument number provided in rpc call")
             }
         } else {
             emptyList()
