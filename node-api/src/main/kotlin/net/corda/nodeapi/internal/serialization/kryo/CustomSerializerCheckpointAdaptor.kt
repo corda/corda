b43ec6a103b0f3c96c51365c6fea36a42a170192
Joseph Zuniga-Daly <joseph.zunigadaly@r3.com>
2020-06-24 12:31:30 +0100

    Remove unused constructors from exception

--- node-api/src/main/kotlin/net/corda/nodeapi/internal/serialization/kryo/CustomSerializerCheckpointAdaptor.kt
+++ node-api/src/main/kotlin/net/corda/nodeapi/internal/serialization/kryo/CustomSerializerCheckpointAdaptor.kt
@@ -39,10 +39,4 @@ class CustomSerializerCheckpointAdaptor<OBJ, PROXY>(private val userSerializer :
     }
 }
 
-class UnableToDetermineSerializerTypesException : java.lang.Exception {
-    constructor() : super()
-    constructor(message: String?) : super(message)
-    constructor(message: String?, cause: Throwable?) : super(message, cause)
-    constructor(cause: Throwable?) : super(cause)
-    constructor(message: String?, cause: Throwable?, enableSuppression: Boolean, writableStackTrace: Boolean) : super(message, cause, enableSuppression, writableStackTrace)
-}
+class UnableToDetermineSerializerTypesException(message: String) : java.lang.Exception(message)
