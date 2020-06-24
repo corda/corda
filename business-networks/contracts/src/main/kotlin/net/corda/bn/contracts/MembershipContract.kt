4097fd6b440a558626eda91ef686cde19cdfdec7
Ante Buterin <37048878+tubibuto@users.noreply.github.com>
2020-06-24 14:04:45 +0200

    EG-2293 Membership Contract for Business Networks (#6355)
    * EG-2293 Implemented contract for membership state and its test coverage
    
    * EG-2293 Added transaction signing related contract verification logic
    
    * EG-2293 Addressed redundant test helper method comments

---
+++ business-networks/contracts/src/main/kotlin/net/corda/bn/contracts/MembershipContract.kt
@@ -0,0 +1,108 @@
+package net.corda.bn.contracts
+
+import net.corda.bn.states.MembershipState
+import net.corda.core.contracts.CommandData
+import net.corda.core.contracts.CommandWithParties
+import net.corda.core.contracts.Contract
+import net.corda.core.contracts.requireSingleCommand
+import net.corda.core.contracts.requireThat
+import net.corda.core.transactions.LedgerTransaction
+import java.lang.IllegalArgumentException
+import java.security.PublicKey
+
+/**
+ * Contract that verifies an evolution of [MembershipState].
+ */
+open class MembershipContract : Contract {
+
+    companion object {
+        const val CONTRACT_NAME = "net.corda.bn.contracts.MembershipContract"
+    }
+
+    open class Commands(val requiredSigners: List<PublicKey>) : CommandData {
+        class Request(requiredSigners: List<PublicKey>) : Commands(requiredSigners)
+        class Activate(requiredSigners: List<PublicKey>) : Commands(requiredSigners)
+        class Suspend(requiredSigners: List<PublicKey>) : Commands(requiredSigners)
+        class Revoke(requiredSigners: List<PublicKey>) : Commands(requiredSigners)
+    }
+
+    @Suppress("ComplexMethod")
+    override fun verify(tx: LedgerTransaction) {
+        val command = tx.commands.requireSingleCommand<Commands>()
+        val input = if (tx.inputStates.isNotEmpty()) tx.inputs.single() else null
+        val inputState = input?.state?.data as? MembershipState
+        val output = if (tx.outputStates.isNotEmpty()) tx.outputs.single() else null
+        val outputState = output?.data as? MembershipState
+
+        requireThat {
+            input?.apply {
+                "Input state has to be validated by ${contractName()}" using (state.contract == contractName())
+            }
+            inputState?.apply {
+                "Input state's modified timestamp should be greater or equal to issued timestamp" using (issued <= modified)
+            }
+            output?.apply {
+                "Output state has to be validated by ${contractName()}" using (contract == contractName())
+            }
+            outputState?.apply {
+                "Output state's modified timestamp should be greater or equal to issued timestamp" using (issued <= modified)
+                "Required signers should be subset of all output state's participants" using (participants.map { it.owningKey }.containsAll(command.value.requiredSigners))
+            }
+            if (inputState != null && outputState != null) {
+                "Input and output state should have same Corda identity" using (inputState.identity == outputState.identity)
+                "Input and output state should have same network IDs" using (inputState.networkId == outputState.networkId)
+                "Input and output state should have same issued timestamps" using (inputState.issued == outputState.issued)
+                "Output state's modified timestamp should be greater or equal than input's" using (inputState.modified <= outputState.modified)
+                "Input and output state should have same linear IDs" using (inputState.linearId == outputState.linearId)
+                "Transaction must be signed by all signers specified inside command" using (command.signers.toSet() == command.value.requiredSigners.toSet())
+            }
+        }
+
+        when (command.value) {
+            is Commands.Request -> verifyRequest(tx, command, outputState!!)
+            is Commands.Activate -> verifyActivate(tx, command, inputState!!, outputState!!)
+            is Commands.Suspend -> verifySuspend(tx, command, inputState!!, outputState!!)
+            is Commands.Revoke -> verifyRevoke(tx, command, inputState!!)
+            else -> throw IllegalArgumentException("Unsupported command ${command.value}")
+        }
+    }
+
+    open fun contractName() = CONTRACT_NAME
+
+    open fun verifyRequest(tx: LedgerTransaction, command: CommandWithParties<Commands>, outputMembership: MembershipState) = requireThat {
+        "Membership request transaction shouldn't contain any inputs" using (tx.inputs.isEmpty())
+        "Membership request transaction should contain output state in PENDING status" using (outputMembership.isPending())
+        "Pending membership owner should be required signer of membership request transaction" using (outputMembership.identity.owningKey in command.value.requiredSigners)
+    }
+
+    open fun verifyActivate(
+            tx: LedgerTransaction,
+            command: CommandWithParties<Commands>,
+            inputMembership: MembershipState,
+            outputMembership: MembershipState
+    ) = requireThat {
+        "Input state of membership activation transaction shouldn't be already active" using (!inputMembership.isActive())
+        "Output state of membership activation transaction should be active" using (outputMembership.isActive())
+        "Input membership owner shouldn't be required signer of membership activation transaction" using (inputMembership.identity.owningKey !in command.value.requiredSigners)
+    }
+
+    open fun verifySuspend(
+            tx: LedgerTransaction,
+            command: CommandWithParties<Commands>,
+            inputMembership: MembershipState,
+            outputMembership: MembershipState
+    ) = requireThat {
+        "Input state of membership suspension transaction shouldn't be already suspended" using (!inputMembership.isSuspended())
+        "Output state of membership suspension transaction should be suspended" using (outputMembership.isSuspended())
+        "Input membership owner shouldn't be required signer of membership suspension transaction" using (inputMembership.identity.owningKey !in command.value.requiredSigners)
+    }
+
+    open fun verifyRevoke(
+            tx: LedgerTransaction,
+            command: CommandWithParties<Commands>,
+            inputMembership: MembershipState
+    ) = requireThat {
+        "Membership revocation transaction shouldn't contain any outputs" using (tx.outputs.isEmpty())
+        "Input membership owner shouldn't be required signer of membership revocation transaction" using (inputMembership.identity.owningKey !in command.value.requiredSigners)
+    }
+}
