e95757426fc1c5b3812c6096f2b131541de55df4
Joseph Zuniga-Daly <joseph.zunigadaly@r3.com>
2020-06-24 12:32:22 +0100

    Remove unused proxyType field

--- node-api/src/main/kotlin/net/corda/nodeapi/internal/serialization/kryo/CustomSerializerCheckpointAdaptor.kt
+++ node-api/src/main/kotlin/net/corda/nodeapi/internal/serialization/kryo/CustomSerializerCheckpointAdaptor.kt
@@ -6,7 +6,6 @@ import com.esotericsoftware.kryo.io.Input
 import com.esotericsoftware.kryo.io.Output
 import net.corda.core.serialization.SerializationCustomSerializer
 import net.corda.serialization.internal.amqp.CORDAPP_TYPE
-import net.corda.serialization.internal.amqp.PROXY_TYPE
 import java.lang.reflect.Type
 import kotlin.reflect.jvm.javaType
 import kotlin.reflect.jvm.jvmErasure
@@ -14,7 +13,6 @@ import kotlin.reflect.jvm.jvmErasure
 class CustomSerializerCheckpointAdaptor<OBJ, PROXY>(private val userSerializer : SerializationCustomSerializer<OBJ, PROXY>) : Serializer<OBJ>() {
 
     val type: Type
-    val proxyType: Type
 
     init {
         val types = userSerializer::class.supertypes.filter { it.jvmErasure == SerializationCustomSerializer::class }
@@ -24,7 +22,6 @@ class CustomSerializerCheckpointAdaptor<OBJ, PROXY>(private val userSerializer :
             throw UnableToDetermineSerializerTypesException("Unable to determine serializer parent types")
         }
         type = types[CORDAPP_TYPE]
-        proxyType = types[PROXY_TYPE]
     }
 
     override fun write(kryo: Kryo, output: Output, obj: OBJ) {
