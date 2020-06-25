680d8da581c142cd88cc94cf75f3d55643d1e857
Ante Buterin <37048878+tubibuto@users.noreply.github.com>
2020-06-25 11:09:49 +0200

    EG-2294 Create Business Network flow implementation (#6363)
    * EG-2293 Implemented contract for membership state and its test coverage
    
    * EG-2294 Implemented CreateBusinessNetworkFlow and added unit test coverage
    
    * EG-2294 Database service KDocs
    
    * EG-2294 Fixed failing tests due to missing test dependency
    
    * EG-2294 Addressed comments regarding network ID and notary flow arguments and rebased successfully
    
    * EG-2294 Created separate DatabaseService method to check whether BN with given ID exists

---
+++ business-networks/workflows/src/main/kotlin/net/corda/bn/flows/DatabaseService.kt
@@ -0,0 +1,51 @@
+package net.corda.bn.flows
+
+import net.corda.bn.schemas.MembershipStateSchemaV1
+import net.corda.bn.states.MembershipState
+import net.corda.bn.states.MembershipStatus
+import net.corda.core.contracts.StateAndRef
+import net.corda.core.node.ServiceHub
+import net.corda.core.node.services.CordaService
+import net.corda.core.node.services.Vault
+import net.corda.core.node.services.queryBy
+import net.corda.core.node.services.vault.QueryCriteria
+import net.corda.core.node.services.vault.builder
+import net.corda.core.serialization.SingletonSerializeAsToken
+
+/**
+ * Service which handles all Business Network related vault queries.
+ *
+ * Each method querying vault for Business Network information must be included here.
+ */
+@CordaService
+class DatabaseService(private val serviceHub: ServiceHub) : SingletonSerializeAsToken() {
+
+    /**
+     * Checks whether Business Network with [networkID] ID exists.
+     *
+     * @param networkID ID of the Business Network.
+     */
+    fun businessNetworkExists(networkID: String): Boolean {
+        val criteria = QueryCriteria.VaultQueryCriteria(Vault.StateStatus.ALL)
+                .and(networkIdCriteria(networkID))
+        return serviceHub.vaultService.queryBy<MembershipState>(criteria).states.isNotEmpty()
+    }
+
+    /**
+     * Queries for all the membership states inside Business Network with [networkId] with one of [statuses].
+     *
+     * @param networkId ID of the Business Network.
+     * @param statuses [MembershipStatus] of the memberships to be fetched.
+     *
+     * @return List of state and ref pairs of memberships matching the query.
+     */
+    fun getAllMembershipsWithStatus(networkId: String, vararg statuses: MembershipStatus): List<StateAndRef<MembershipState>> {
+        val criteria = QueryCriteria.VaultQueryCriteria(Vault.StateStatus.UNCONSUMED)
+                .and(networkIdCriteria(networkId))
+                .and(statusCriteria(statuses.toList()))
+        return serviceHub.vaultService.queryBy<MembershipState>(criteria).states
+    }
+
+    private fun networkIdCriteria(networkID: String) = QueryCriteria.VaultCustomQueryCriteria(builder { MembershipStateSchemaV1.PersistentMembershipState::networkId.equal(networkID) })
+    private fun statusCriteria(statuses: List<MembershipStatus>) = QueryCriteria.VaultCustomQueryCriteria(builder { MembershipStateSchemaV1.PersistentMembershipState::status.`in`(statuses) })
+}
---
+++ business-networks/workflows/src/test/kotlin/net/corda/bn/flows/CreateBusinessNetworkFlowTest.kt
@@ -0,0 +1,54 @@
+package net.corda.bn.flows
+
+import net.corda.bn.contracts.MembershipContract
+import net.corda.bn.states.MembershipState
+import net.corda.bn.states.MembershipStatus
+import net.corda.core.contracts.UniqueIdentifier
+import org.junit.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertFailsWith
+import kotlin.test.assertTrue
+
+class CreateBusinessNetworkFlowTest : MembershipManagementFlowTest(numberOfAuthorisedMembers = 1, numberOfRegularMembers = 0) {
+
+    @Test(timeout = 300_000)
+    fun `create business network flow should fail when trying to create business network with already existing network ID`() {
+        val authorisedMember = authorisedMembers.first()
+        val networkId = UniqueIdentifier()
+
+        runCreateBusinessNetworkFlow(authorisedMember, networkId = networkId)
+        assertFailsWith<DuplicateBusinessNetworkException> { runCreateBusinessNetworkFlow(authorisedMember, networkId = networkId) }
+    }
+
+    @Test(timeout = 300_000)
+    fun `create business network flow should fail when invalid notary argument is provided`() {
+        val authorisedMember = authorisedMembers.first()
+
+        assertFailsWith<IllegalStateException> { runCreateBusinessNetworkFlow(authorisedMember, notary = authorisedMember.identity()) }
+    }
+
+    @Test(timeout = 300_000)
+    fun `create business network flow happy path`() {
+        val authorisedMember = authorisedMembers.first()
+        val (membership, command) = runCreateBusinessNetworkFlow(authorisedMember).run {
+            verifyRequiredSignatures()
+            tx.outputs.single() to tx.commands.single()
+        }
+
+        val networkId = membership.run {
+            assertEquals(MembershipContract.CONTRACT_NAME, contract)
+            assertTrue(data is MembershipState)
+            val data = data as MembershipState
+            assertEquals(authorisedMember.identity(), data.identity)
+            assertEquals(MembershipStatus.ACTIVE, data.status)
+
+            data.networkId
+        }
+        assertTrue(command.value is MembershipContract.Commands.Activate)
+
+        // also check ledger
+        getAllMembershipsFromVault(authorisedMember, networkId).single().apply {
+            assertEquals(authorisedMember.identity(), identity)
+        }
+    }
+}
