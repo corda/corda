a6cfcc0eb47289cdb14f175b4734ef85f2fa32c5
nargas-ritu <ritu.gupta@r3.com>
2020-06-25 17:32:42 +0100

    NOTICK: Bump version to 4.4.3-PREVIEW to support a CE emergency patch for SIA

--- docker/src/bash/example-mini-network.sh
+++ docker/src/bash/example-mini-network.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 NODE_LIST=("dockerNode1" "dockerNode2" "dockerNode3")
 NETWORK_NAME=mininet
-CORDAPP_VERSION="4.4-SNAPSHOT"
-DOCKER_IMAGE_VERSION="corda-zulu-4.4-snapshot"
+CORDAPP_VERSION="4.4.3-PREVIEW"
+DOCKER_IMAGE_VERSION="corda-zulu-4.4.3-PREVIEW"
 
 mkdir cordapps
 rm -f cordapps/*
