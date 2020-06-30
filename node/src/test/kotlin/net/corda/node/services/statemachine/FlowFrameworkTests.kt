96712efb1d319c91f92ffec6b98b902a75427295
Kyriakos Tharrouniatis <kyriakos.tharrouniatis@r3.com>
2020-06-30 13:31:53 +0100

    Assert that only one flow is spawned upon multiple requests with same client id

--- node/src/test/kotlin/net/corda/node/services/statemachine/FlowFrameworkTests.kt
+++ node/src/test/kotlin/net/corda/node/services/statemachine/FlowFrameworkTests.kt
@@ -986,6 +986,7 @@ class FlowFrameworkTests {
         val clientID = UUID.randomUUID().toString()
 
         var run = 0
+        var noSecondFlowWasSpawned = 0
         val semaphore = Semaphore(0)
         ResultFlow.suspendableHook = object : FlowLogic<Unit>() {
             @Suspendable
@@ -994,6 +995,8 @@ class FlowFrameworkTests {
                     run++
                     semaphore.acquire() // make flow wait on first run so it remains in the system on shutdown
                 }
+
+                noSecondFlowWasSpawned++
             }
         }
 
@@ -1013,6 +1016,7 @@ class FlowFrameworkTests {
         assertEquals(flowHandle0.id, flowHandle1.id)
         assertEquals(clientID, flowHandle1.clientID)
         assertEquals(5, flowHandle1.resultFuture.getOrThrow(20.seconds))
+        assertEquals(1, noSecondFlowWasSpawned)
     }
 
     @Test
