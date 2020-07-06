ee7f221c4632d798dfe5525088679b976fe312ce
Kyriakos Tharrouniatis <kyriakos.tharrouniatis@r3.com>
2020-07-06 10:46:45 +0100

    Reduce exception_message and stack_trace lengths in table node_flow_exceptions from 4000 to 2000 to fix Oracle failing with: 'ORA-00910: specified length too long for its datatype'

--- node/src/main/kotlin/net/corda/node/services/persistence/DBCheckpointStorage.kt
+++ node/src/main/kotlin/net/corda/node/services/persistence/DBCheckpointStorage.kt
@@ -50,8 +50,8 @@ class DBCheckpointStorage(
         private const val HMAC_SIZE_BYTES = 16
 
         @VisibleForTesting
-        const val MAX_STACKTRACE_LENGTH = 4000
-        private const val MAX_EXC_MSG_LENGTH = 4000
+        const val MAX_STACKTRACE_LENGTH = 2000
+        private const val MAX_EXC_MSG_LENGTH = 2000
         private const val MAX_EXC_TYPE_LENGTH = 256
         private const val MAX_FLOW_NAME_LENGTH = 128
         private const val MAX_PROGRESS_STEP_LENGTH = 256
