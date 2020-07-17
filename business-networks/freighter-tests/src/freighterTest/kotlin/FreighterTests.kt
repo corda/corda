b14a3b732d22d958004826bb355082996b406005
jmacmahonr3 <james.macmahon@r3.com>
2020-07-17 10:01:37 +0100

    Initial Commit For WIP

---
+++ business-networks/freighter-tests/src/freighterTest/kotlin/FreighterTests.kt
@@ -0,0 +1,101 @@
+package `freighter-tests`.src.freighterTest.kotlin
+
+import freighter.deployments.DeploymentContext
+import freighter.deployments.NodeBuilder
+import freighter.deployments.SingleNodeDeployed
+import freighter.deployments.SingleNodeDeployment
+import freighter.installers.corda.ENTERPRISE
+import freighter.installers.corda.OPEN_SOURCE
+import freighter.machine.DeploymentMachineProvider
+import freighter.machine.generateRandomString
+import freighter.testing.DockerRemoteMachineBasedTest
+import net.corda.bn.flows.CreateBusinessNetworkFlow
+import net.corda.bn.flows.RequestMembershipFlow
+import net.corda.bn.states.BNIdentity
+import net.corda.core.contracts.UniqueIdentifier
+import net.corda.core.messaging.startFlow
+import net.corda.core.serialization.CordaSerializable
+import net.corda.core.utilities.getOrThrow
+import org.junit.jupiter.api.Test
+import utility.getOrThrow
+import java.util.concurrent.CompletableFuture
+import java.util.concurrent.atomic.AtomicInteger
+
+class FreighterTests : DockerRemoteMachineBasedTest() {
+
+    private val bnoContracts = NodeBuilder.DeployedCordapp.fromClassPath("corda-business-network-contracts")
+    private val bnoWorkflows = NodeBuilder.DeployedCordapp.fromClassPath("corda-business-network-workflows")
+    private val freighterHelperCordapp = NodeBuilder.DeployedCordapp.fromClassPath("freighter-cordapp-flows")
+
+
+    @Test
+    fun test() {
+        val deploymentContext = DeploymentContext(machineProvider, nms, artifactoryUsername, artifactoryPassword)
+
+        val nodeGenerator = createDeploymentGenerator(deploymentContext)
+        val bnoNode = nodeGenerator().getOrThrow()
+
+        val networkId = UniqueIdentifier()
+        val groupId = UniqueIdentifier()
+        val businessId = DummyIdentity("ImARealBNOY")
+        val groupName = "GroupOfBnoyos"
+
+        val listOfGroupMembers =  listOf(nodeGenerator(), nodeGenerator(), nodeGenerator()).map { it.getOrThrow() }
+
+        val output = bnoNode.rpc { startFlow(::CreateBusinessNetworkFlow,
+                networkId,
+                businessId,
+                groupId,
+                groupName,
+                null).returnValue.getOrThrow()
+        }
+
+        //Need to get a value from the state returned from teh CreateBusinessNetworkFlow
+
+        for( node in listOfGroupMembers){
+            node.rpc { startFlow(::RequestMembershipFlow,
+                    bnoNode.identity(),
+                    networkId.toString(),
+                    businessId,
+                    null).returnValue.getOrThrow() }
+        }
+
+
+    }
+
+//    private fun createNodeDeployment(randomString: String, deploymentContext: DeploymentContext): SingleNodeDeployed {
+//        val deploymentResult = SingleNodeDeployment(
+//                NodeBuilder().withX500("O=PartyB, C=GB, L=LONDON, CN=$randomString")
+//                        .withCordapp(bnoContracts)
+//                        .withCordapp(bnoWorkflows)
+//                        .withDatabase(machineProvider.requestDatabase(DeploymentMachineProvider.DatabaseType.PG_11_5))
+//        ).withVersion("4.5")
+//                .withDistribution(OPEN_SOURCE)
+//                .deploy(deploymentContext)
+//
+//        return deploymentResult.getOrThrow()
+//    }
+
+    private fun createDeploymentGenerator(
+            deploymentContext: DeploymentContext,
+            indexGenerator: AtomicInteger = AtomicInteger(0)
+    ): () -> CompletableFuture<SingleNodeDeployed> =
+            run {
+                ({ indexGen: AtomicInteger, deploymentContext: DeploymentContext, db: DeploymentMachineProvider.DatabaseType ->
+                    {
+                        SingleNodeDeployment(
+                                NodeBuilder().withX500("O=Party${indexGen.getAndIncrement()}, C=IE, L=DUBLIN CN=Corda")
+                                        .withCordapp(bnoContracts)
+                                        .withCordapp(bnoWorkflows)
+                                        .withCordapp(freighterHelperCordapp)
+                                        .withDatabase(machineProvider.requestDatabase(DeploymentMachineProvider.DatabaseType.PG_11_5))
+                        ).withVersion("4.4")
+                                .withDistribution(OPEN_SOURCE)
+                                .deploy(deploymentContext)
+                    }
+                })
+            }(indexGenerator, deploymentContext, DeploymentMachineProvider.DatabaseType.PG_11_5)
+}
+
+@CordaSerializable
+data class DummyIdentity(val name: String) : BNIdentity
