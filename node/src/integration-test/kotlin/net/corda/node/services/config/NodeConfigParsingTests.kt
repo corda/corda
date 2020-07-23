f63fb528bf128d99c5b43c20016ea9f3892acdcb
Ryan Fowler <ryan.fowler@r3.com>
2020-07-23 13:20:04 +0100

    Fix some rebased changes to log file handling

--- node/src/integration-test/kotlin/net/corda/node/services/config/NodeConfigParsingTests.kt
+++ node/src/integration-test/kotlin/net/corda/node/services/config/NodeConfigParsingTests.kt
@@ -1,16 +1,12 @@
 package net.corda.node.services.config
 
-import net.corda.core.internal.div
 import net.corda.core.utilities.getOrThrow
 import net.corda.testing.driver.DriverParameters
-import net.corda.testing.driver.NodeHandle
 import net.corda.testing.driver.driver
 import net.corda.testing.driver.internal.incrementalPortAllocation
+import net.corda.testing.driver.logFile
 import org.junit.Assert.assertTrue
 import org.junit.Test
-import java.io.File
-
-fun NodeHandle.logFile(): File = (baseDirectory / "logs").toFile().walk().filter { it.name.startsWith("node-") && it.extension == "log" }.single()
 
 class NodeConfigParsingTests {
     @Test(timeout = 300_000)
