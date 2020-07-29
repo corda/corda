3e025769e47b49e3d3558f34f42853246d0e93ac
nikinagy <nikolett.nagy@r3.com>
2020-07-29 16:40:31 +0100

    remove PartyIdentity

--- core/src/main/kotlin/net/corda/core/flows/PartyNotFoundException.kt
+++ core/src/main/kotlin/net/corda/core/flows/PartyNotFoundException.kt
@@ -2,16 +2,5 @@ package net.corda.core.flows
 
 import net.corda.core.CordaRuntimeException
 import net.corda.core.identity.CordaX500Name
-import net.corda.core.serialization.CordaSerializable
-import java.security.PublicKey
 
-@CordaSerializable
-data class PartyIdentity(
-        val partyKey: PublicKey? = null,
-        val partyName: CordaX500Name? = null
-)
-
-class PartyNotFoundException(
-        message: String,
-        val party: PartyIdentity
-) : CordaRuntimeException(message)
+class PartyNotFoundException(message: String, val party: CordaX500Name) : CordaRuntimeException(message)
