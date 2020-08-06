9bda5615658be16a9120319c1e3bf8f6bf95dc75
LankyDan <danknewton@hotmail.com>
2020-08-06 19:30:22 +0100

    NOTICK Save exception for hospitalized session init errors
    Save the exception for flows that fail during session init when they are
    kept for observation.
    
    Change the exception tidy up logic to only update the flow's status if
    the exception was removed.

--- node/src/main/kotlin/net/corda/node/services/statemachine/SingleThreadedStateMachineManager.kt
+++ node/src/main/kotlin/net/corda/node/services/statemachine/SingleThreadedStateMachineManager.kt
@@ -455,8 +455,12 @@ internal class SingleThreadedStateMachineManager(
             innerState.withLock { if (id in flows) return@Checkpoints }
             val checkpoint = tryDeserializeCheckpoint(serializedCheckpoint, id)?.also {
                 if (it.status == Checkpoint.FlowStatus.HOSPITALIZED) {
-                    checkpointStorage.updateStatus(id, Checkpoint.FlowStatus.RUNNABLE)
-                    checkpointStorage.removeFlowException(id)
+                    if (checkpointStorage.removeFlowException(id)) {
+                        checkpointStorage.updateStatus(id, Checkpoint.FlowStatus.RUNNABLE)
+                    } else {
+                        logger.error("Unable to remove database exception for flow $id. Something is very wrong. The flow will not be loaded and run.")
+                        return@Checkpoints
+                    }
                 }
             } ?: return@Checkpoints
             val flow = flowCreator.createFlowFromCheckpoint(id, checkpoint)
@@ -502,9 +506,10 @@ internal class SingleThreadedStateMachineManager(
 
                 tryDeserializeCheckpoint(serializedCheckpoint, flowId)?.also {
                     if (it.status == Checkpoint.FlowStatus.HOSPITALIZED) {
-                        checkpointStorage.updateStatus(flowId, Checkpoint.FlowStatus.RUNNABLE)
-                        if (!checkpointStorage.removeFlowException(flowId)) {
-                            logger.error("Unable to remove database exception for flow $flowId. Something is very wrong. The flow will not retry.")
+                        if (checkpointStorage.removeFlowException(flowId)) {
+                            checkpointStorage.updateStatus(flowId, Checkpoint.FlowStatus.RUNNABLE)
+                        } else {
+                            logger.error("Unable to remove database exception for flow $flowId. Something is very wrong. The flow will not be loaded and run.")
                             return@transaction null
                         }
                     }
