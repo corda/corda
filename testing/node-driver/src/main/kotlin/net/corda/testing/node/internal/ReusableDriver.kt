6d7b3c595700f375d9be4c9d189fde5ab625ef78
Yiftach Kaplan <yiftach.kaplan@r3.com>
2020-08-07 09:55:10 +0100

    INFRA-488: Thread safe, remove old nodes/drivers

--- testing/node-driver/src/main/kotlin/net/corda/testing/node/internal/ReusableDriver.kt
+++ testing/node-driver/src/main/kotlin/net/corda/testing/node/internal/ReusableDriver.kt
@@ -4,6 +4,7 @@ package net.corda.testing.node.internal
 
 import net.corda.core.concurrent.CordaFuture
 import net.corda.core.identity.CordaX500Name
+import net.corda.core.internal.ThreadBox
 import net.corda.core.internal.concurrent.doneFuture
 import net.corda.core.internal.div
 import net.corda.core.internal.uncheckedCast
@@ -20,10 +21,32 @@ import java.nio.file.Path
 import java.nio.file.Paths
 import java.time.Duration
 import java.time.Instant
+import java.util.concurrent.atomic.AtomicInteger
+import java.util.concurrent.atomic.AtomicLong
 
 class ReusableDriver private constructor(private val driver: InternalDriverDSL) : InternalDriverDSL, ReusableDriverDsl, AutoCloseable {
     companion object {
-        private val drivers = mutableMapOf<DriverParameters, ReusableDriver>()
+        private class Drivers {
+            val drivers = mutableMapOf<DriverParameters, ReusableDriver>()
+
+            fun cleanUpOldDrivers() {
+                while(drivers.size > MAX_NUMBER_OF_DRIVERS_TO_REUSE) {
+                    val driverToRemove = drivers.filter {
+                        it.value.usedCount.get() == 0
+                    }.minBy {
+                        it.value.lastUsed.get()
+                    }
+                    if(driverToRemove == null) {
+                        // Can't remove anything, will try again later
+                        return
+                    }
+                    drivers.remove(driverToRemove.key)
+                    driverToRemove.value.shutdown()
+                }
+            }
+        }
+        private val drivers = ThreadBox(Drivers())
+
 
         private val shutdownHook = addShutdownHook { closeAll() }
 
@@ -32,6 +55,9 @@ class ReusableDriver private constructor(private val driver: InternalDriverDSL) 
         private val startTime = Instant.now()
 
         internal val log = contextLogger()
+
+        private const val MAX_NUMBER_OF_DRIVERS_TO_REUSE = 1
+        private const val MAX_NUMBER_OF_NODES_TO_REUSE = 25
 
         private fun createDriver(parameters: DriverParameters): ReusableDriver {
             val driver = DriverDSLImpl(
@@ -63,8 +89,10 @@ class ReusableDriver private constructor(private val driver: InternalDriverDSL) 
 
         private fun closeAll() {
             shutdownHook.cancel()
-            drivers.values.forEach { it.shutdown() }
-            drivers.clear()
+            drivers.locked {
+                drivers.values.forEach { it.shutdown() }
+                drivers.clear()
+            }
             serializationEnabler?.close()
         }
 
@@ -73,32 +101,45 @@ class ReusableDriver private constructor(private val driver: InternalDriverDSL) 
                     .withDriverDirectory(Paths.get("build") / "node-driver" / "reusable-driver")
                     .withNetworkParameters(parameters.networkParameters.copy(modifiedTime = startTime))
 
-            return drivers.compute(parametersKey) { _, driver ->
-                driver?.recover() ?: createDriver(parameters)
-            }!!
+            return drivers.locked {
+                val driver = drivers.compute(parametersKey) { _, driver ->
+                    driver?.recover() ?: createDriver(parameters)
+                }!!
+
+                cleanUpOldDrivers()
+
+                driver
+            }
         }
+
     }
+
+    private val usedCount = AtomicInteger(1)
+    private val lastUsed = AtomicLong(System.currentTimeMillis())
 
     @Suppress("TooGenericExceptionCaught")
-    private fun recover(): ReusableDriver? {
-        return try {
-            recoveries.forEach { it.invoke() }
-            recoveries.clear()
-            this
-        } catch (e: Exception) {
-            log.warn("Can not recover driver, will use a new one", e)
-            shutdown()
-            null
+    private fun recover(): ReusableDriver? =
+        state.locked {
+            try {
+                recoveries.forEach { it.invoke() }
+                recoveries.clear()
+                usedCount.incrementAndGet()
+                lastUsed.set(System.currentTimeMillis())
+                this@ReusableDriver
+            } catch (e: Exception) {
+                log.warn("Can not recover driver, will use a new one", e)
+                shutdown()
+                null
+            }
         }
-    }
 
-    override fun close() {
+    override fun close(): Unit  = state.locked {
         val nodes = startedNodes.groupBy {
             it.second.isDone }
 
         nodes[false]?.forEach {
-            it.second.then {
-                it.get()?.stop()
+            it.second.then { future ->
+                future.get()?.stop()
             }
         }
 
@@ -108,16 +149,39 @@ class ReusableDriver private constructor(private val driver: InternalDriverDSL) 
                 ?.forEach {
                     if (it.second is NodeHandleInternal) {
                         reusableNodes.getOrPut(it.first, { mutableListOf() })
-                                .add(it.second as NodeHandleInternal)
+                                .add(ReusableNodeInfo(it.second as NodeHandleInternal))
                     }
                 }
 
+        cleanUpOldNodes()
+
         startedNodes.clear()
+        usedCount.decrementAndGet()
     }
 
-    private val startedNodes = mutableListOf<Pair<NodeKey, CordaFuture<NodeHandle>>>()
-    private val reusableNodes = mutableMapOf<NodeKey, MutableList<NodeHandleInternal>>()
-    private val runningNamedDriver = mutableMapOf<CordaX500Name, NodeKey>()
+
+    private fun cleanUpOldNodes()  = state.locked{
+        val allNodes = reusableNodes.flatMap {pair ->
+            pair.value.map {node->
+                node to pair.key } }.
+        sortedBy {
+            it.first.lastAccessTime }
+        allNodes.take(maxOf(0,allNodes.size - MAX_NUMBER_OF_NODES_TO_REUSE)).forEach { pair ->
+            if(reusableNodes.getOrDefault(pair.second, mutableListOf()).remove(pair.first)) {
+                pair.first.node.stop()
+            }
+        }
+    }
+
+    private data class ReusableNodeInfo(val node: NodeHandleInternal, val lastAccessTime: Long = System.currentTimeMillis())
+
+    private class ThreadSafeState {
+        val startedNodes = mutableListOf<Pair<NodeKey, CordaFuture<NodeHandle>>>()
+        val reusableNodes = mutableMapOf<NodeKey, MutableList<ReusableNodeInfo>>()
+        val runningNamedDriver = mutableMapOf<CordaX500Name, NodeKey>()
+        val recoveries = mutableListOf<() -> Unit>()
+    }
+    private val state = ThreadBox(ThreadSafeState())
 
     override val shutdownManager: ShutdownManager
         get() = driver.shutdownManager
@@ -136,36 +200,36 @@ class ReusableDriver private constructor(private val driver: InternalDriverDSL) 
     override fun shutdown() =
             driver.shutdown()
 
-    override fun startNode(parameters: NodeParameters, bytemanPort: Int?): CordaFuture<NodeHandle> {
-        val key = NodeKey(parameters, bytemanPort)
-        if (parameters.providedName != null) {
-            val runningKey = runningNamedDriver[parameters.providedName]
-            if ((runningKey != null) && (runningKey != key)) {
-                val runningNodes = reusableNodes.remove(runningKey)
-                runningNodes?.forEach { it.stop() }
+    override fun startNode(parameters: NodeParameters, bytemanPort: Int?): CordaFuture<NodeHandle> =
+            state.locked{
+
+                val key = NodeKey(parameters, bytemanPort)
+                if (parameters.providedName != null) {
+                    val runningKey = runningNamedDriver[parameters.providedName]
+                    if ((runningKey != null) && (runningKey != key)) {
+                        val runningNodes = reusableNodes.remove(runningKey)
+                        runningNodes?.forEach { it.node.stop() }
+                    }
+                    runningNamedDriver[parameters.providedName] = key
+                }
+                val reusableNodes = reusableNodes.getOrDefault(key, mutableListOf())
+                val size = reusableNodes.size
+                val nodeFuture = if (size > 0) {
+                    log.debug("Reusing node $parameters")
+                    val node = reusableNodes.removeAt(size - 1)
+                    doneFuture(node.node as NodeHandle)
+                } else {
+                    log.debug("Creating node $parameters")
+                    val node = driver.startNode(parameters, bytemanPort)
+                    node
+                }
+                startedNodes.add(key to nodeFuture)
+                return nodeFuture
             }
-            runningNamedDriver[parameters.providedName] = key
-        }
-        val reusableNodes = reusableNodes.getOrDefault(key, mutableListOf())
-        val size = reusableNodes.size
-        val nodeFuture = if (size > 0) {
-            log.debug("Reusing node $parameters")
-            val node = reusableNodes.removeAt(size - 1)
-            doneFuture(node as NodeHandle)
-        } else {
-            log.debug("Creating node $parameters")
-            val node = driver.startNode(parameters, bytemanPort)
-            node
-        }
-        startedNodes.add(key to nodeFuture)
-        return nodeFuture
-    }
 
     override fun startNode(parameters: NodeParameters): CordaFuture<NodeHandle> = startNode(parameters, bytemanPort = null)
 
-    private val recoveries = mutableListOf<() -> Unit>()
-
-    override fun recoverBy(recovery: () -> Unit) {
+    override fun recoverBy(recovery: () -> Unit): Unit = state.locked{
         recoveries.add(recovery)
     }
 
