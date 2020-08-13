60a3d87f8f71c0ed4a9bbb42b994c387a298deb2
Kyriakos Tharrouniatis <kyriakos.tharrouniatis@r3.com>
2020-08-13 13:53:09 +0100

    Minor comment update

--- client/rpc/src/integration-test/kotlin/net/corda/client/rpcreconnect/CordaRPCClientReconnectionTest.kt
+++ client/rpc/src/integration-test/kotlin/net/corda/client/rpcreconnect/CordaRPCClientReconnectionTest.kt
@@ -402,7 +402,7 @@ class CordaRPCClientReconnectionTest {
             client.start(rpcUser.username, rpcUser.password, gracefulReconnect = gracefulReconnect).use {
                 val rpcOps = it.proxy as ReconnectingCordaRPCOps
                 val clientId = UUID.randomUUID().toString()
-                // assert result reconnectable futures returned from both 'startFlowWithClientId' and 'reattachFlowWithClientId'
+                // assert result reconnectable futures returned work from both 'startFlowWithClientId' and 'reattachFlowWithClientId'
                 val flowHandle0 = rpcOps.startFlowWithClientId(clientId, ::SimpleFlow)
                 val flowHandle1 = rpcOps.reattachFlowWithClientId<Int>(clientId)
 
