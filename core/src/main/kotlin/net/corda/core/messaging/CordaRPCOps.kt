462da116db7f663c126c1fb7c1280f31cbab3d4f
Alexey Kadyrov <alexey.kadyrov@r3.com>
2020-08-21 17:58:28 +0100

    wip

--- core/src/main/kotlin/net/corda/core/messaging/CordaRPCOps.kt
+++ core/src/main/kotlin/net/corda/core/messaging/CordaRPCOps.kt
@@ -98,8 +98,58 @@ data class ParametersUpdateInfo(
 @CordaSerializable
 data class StateMachineTransactionMapping(val stateMachineRunId: StateMachineRunId, val transactionId: SecureHash)
 
+/** Operations that provide party information. */
+interface PartyInfoRpcOps {
+
+    /** Returns the [Party] corresponding to the given key, if found. */
+    fun partyFromKey(key: PublicKey): Party?
+
+    /** Returns the [Party] with the X.500 principal as it's [Party.name]. */
+    fun wellKnownPartyFromX500Name(x500Name: CordaX500Name): Party?
+
+    /**
+     * Returns a node's info from the network map cache, where known.
+     * Notice that when there are more than one node for a given name (in case of distributed services) first service node
+     * found will be returned.
+     *
+     * @return the node info if available.
+     */
+    fun nodeInfoFromParty(party: AbstractParty): NodeInfo?
+
+
+    // TODO These need rethinking. Instead of these direct calls we should have a way of replicating a subset of
+    // the node's state locally and query that directly.
+    /**
+     * Returns the well known identity from an abstract party. This is intended to resolve the well known identity
+     * from a confidential identity, however it transparently handles returning the well known identity back if
+     * a well known identity is passed in.
+     *
+     * @param party identity to determine well known identity for.
+     * @return well known identity, if found.
+     */
+    fun wellKnownPartyFromAnonymous(party: AbstractParty): Party?
+
+    /**
+     * Get a notary identity by name.
+     *
+     * @return the notary identity, or null if there is no notary by that name. Note that this will return null if there
+     * is a peer with that name but they are not a recognised notary service.
+     */
+    fun notaryPartyFromX500Name(x500Name: CordaX500Name): Party?
+
+    /**
+     * Returns a list of candidate matches for a given string, with optional fuzzy(ish) matching. Fuzzy matching may
+     * get smarter with time e.g. to correct spelling errors, so you should not hard-code indexes into the results
+     * but rather show them via a user interface and let the user pick the one they wanted.
+     *
+     * @param query The string to check against the X.500 name components
+     * @param exactMatch If true, a case sensitive match is done against each component of each X.500 name.
+     */
+    fun partiesFromName(query: String, exactMatch: Boolean): Set<Party>
+}
+
 /** RPC operations that the node exposes to clients. */
-interface CordaRPCOps : RPCOps {
+interface CordaRPCOps : RPCOps, PartyInfoRpcOps {
     /** Returns a list of currently in-progress state machine infos. */
     fun stateMachinesSnapshot(): List<StateMachineInfo>
 
@@ -374,53 +424,8 @@ interface CordaRPCOps : RPCOps {
     @RPCReturnsObservables
     fun waitUntilNetworkReady(): CordaFuture<Void?>
 
-    // TODO These need rethinking. Instead of these direct calls we should have a way of replicating a subset of
-    // the node's state locally and query that directly.
-    /**
-     * Returns the well known identity from an abstract party. This is intended to resolve the well known identity
-     * from a confidential identity, however it transparently handles returning the well known identity back if
-     * a well known identity is passed in.
-     *
-     * @param party identity to determine well known identity for.
-     * @return well known identity, if found.
-     */
-    fun wellKnownPartyFromAnonymous(party: AbstractParty): Party?
-
-    /** Returns the [Party] corresponding to the given key, if found. */
-    fun partyFromKey(key: PublicKey): Party?
-
-    /** Returns the [Party] with the X.500 principal as it's [Party.name]. */
-    fun wellKnownPartyFromX500Name(x500Name: CordaX500Name): Party?
-
-    /**
-     * Get a notary identity by name.
-     *
-     * @return the notary identity, or null if there is no notary by that name. Note that this will return null if there
-     * is a peer with that name but they are not a recognised notary service.
-     */
-    fun notaryPartyFromX500Name(x500Name: CordaX500Name): Party?
-
-    /**
-     * Returns a list of candidate matches for a given string, with optional fuzzy(ish) matching. Fuzzy matching may
-     * get smarter with time e.g. to correct spelling errors, so you should not hard-code indexes into the results
-     * but rather show them via a user interface and let the user pick the one they wanted.
-     *
-     * @param query The string to check against the X.500 name components
-     * @param exactMatch If true, a case sensitive match is done against each component of each X.500 name.
-     */
-    fun partiesFromName(query: String, exactMatch: Boolean): Set<Party>
-
     /** Enumerates the class names of the flows that this node knows about. */
     fun registeredFlows(): List<String>
-
-    /**
-     * Returns a node's info from the network map cache, where known.
-     * Notice that when there are more than one node for a given name (in case of distributed services) first service node
-     * found will be returned.
-     *
-     * @return the node info if available.
-     */
-    fun nodeInfoFromParty(party: AbstractParty): NodeInfo?
 
     /**
      * Clear all network map data from local node cache. Notice that after invoking this method your node will lose
