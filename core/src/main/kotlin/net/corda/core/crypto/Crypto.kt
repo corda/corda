43c75a5638eed56a82d968d78021a7668ea44b0f
Adel El-Beik <adel.el-beik@r3.com>
2020-08-25 16:37:44 +0100

    Upgraded DJVM to handle BC - latest version of BC is a multirelease JAR. When reading JKS keystore if a BC EdDSAPrivateKey is returned then swap for a net.i2p EdDSA private key.

--- core/src/main/kotlin/net/corda/core/crypto/Crypto.kt
+++ core/src/main/kotlin/net/corda/core/crypto/Crypto.kt
@@ -35,6 +35,7 @@ import org.bouncycastle.asn1.x9.X9ObjectIdentifiers
 import org.bouncycastle.crypto.CryptoServicesRegistrar
 import org.bouncycastle.jcajce.provider.asymmetric.ec.BCECPrivateKey
 import org.bouncycastle.jcajce.provider.asymmetric.ec.BCECPublicKey
+import org.bouncycastle.jcajce.provider.asymmetric.edec.BCEdDSAPrivateKey
 import org.bouncycastle.jcajce.provider.asymmetric.rsa.BCRSAPrivateKey
 import org.bouncycastle.jcajce.provider.asymmetric.rsa.BCRSAPublicKey
 import org.bouncycastle.jce.ECNamedCurveTable
@@ -1035,6 +1036,7 @@ object Crypto {
             is BCRSAPrivateKey -> key
             is BCSphincs256PrivateKey -> key
             is EdDSAPrivateKey -> key
+            is BCEdDSAPrivateKey -> EdDSAPrivateKey(PKCS8EncodedKeySpec(key.encoded))
             else -> decodePrivateKey(key.encoded)
         }
     }
