e7560f4976a976a73d12a5c684117777f361ed82
Jonathan Locke <jonathan.locke@r3.com>
2020-08-27 14:32:46 +0100

    Fixed unit test by undoing unnecessary build.gradle changes. Removed println and exception handling from DatabaseSnapshot as per review comments.

--- testing/node-driver/src/main/kotlin/net/corda/testing/node/DatabaseSnapshot.kt
+++ testing/node-driver/src/main/kotlin/net/corda/testing/node/DatabaseSnapshot.kt
@@ -6,36 +6,19 @@ import java.nio.file.Files
 import java.nio.file.Path
 
 object DatabaseSnapshot {
-    private val previousCordaVersion: String = "4.5.1"
+    private const val previousCordaVersion: String = "4.5.1"
+    private const val databaseName: String = "persistence.mv.db"
 
     private fun getDatabaseSnapshotStream(): InputStream {
-        var fileName = "/databasesnapshots/${previousCordaVersion}/persistence.mv.db"
-        println("Copying from resource $fileName")
+        val fileName = "/databasesnapshots/${previousCordaVersion}/$databaseName"
         val resourceUri = this::class.java.getResource(fileName)
-        var stream: InputStream? = null
-        try {
-            stream = resourceUri.openStream()
-        } catch (e: IOException) {
-            println("IOException occurred getting resource: ${e.message}")
-            println(e.toString())
-            e.printStackTrace()
-        }
-        return stream!!
+        return resourceUri.openStream()
     }
 
     fun copyDatabaseSnapshot(baseDirectory: Path) {
         val stream = getDatabaseSnapshotStream()
-        if (!Files.exists(baseDirectory)) {
-            Files.createDirectories(baseDirectory)
-        }
-        val path = baseDirectory.resolve("persistence.mv.db")
-        println("Copying stream to path: $path")
-        try {
-            Files.copy(stream, path)
-        } catch (e: IOException) {
-            println("IOException occurred copying snapshot: ${e.message}")
-            println(e.toString())
-            e.printStackTrace()
-        }
+        Files.createDirectories(baseDirectory)
+        val path = baseDirectory.resolve(databaseName)
+        Files.copy(stream, path)
     }
 }
