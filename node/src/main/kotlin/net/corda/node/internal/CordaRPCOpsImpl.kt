59cbbfa85bee0cd37a9aa1cda76836564519b189
LankyDan <danknewton@hotmail.com>
2020-09-16 14:39:52 +0100

    ENT-5768 Change after rebase

--- node/src/main/kotlin/net/corda/node/internal/CordaRPCOpsImpl.kt
+++ node/src/main/kotlin/net/corda/node/internal/CordaRPCOpsImpl.kt
@@ -281,9 +281,8 @@ internal class CordaRPCOpsImpl(
     private fun <T> startFlow(logicType: Class<out FlowLogic<T>>, context: InvocationContext, args: Array<out Any?>): FlowStateMachineHandle<T> {
         if (!logicType.isAnnotationPresent(StartableByRPC::class.java)) throw NonRpcFlowException(logicType)
         if (isFlowsDrainingModeEnabled()) {
-            return context.clientId?.let {
-                smm.reattachFlowWithClientId<T>(context.clientId!!)
-            } ?: throw RejectedCommandException("Node is draining before shutdown. Cannot start new flows through RPC.")
+            return context.clientId?.let { smm.reattachFlowWithClientId<T>(it, context.principal()) }
+                ?: throw RejectedCommandException("Node is draining before shutdown. Cannot start new flows through RPC.")
         }
         return flowStarter.invokeFlowAsync(logicType, context, *args).getOrThrow()
     }
