49bf7a9bb9c8c971dba361e005c23bee3e49d9c5
Ross Nicoll <ross.nicoll@r3.com>
2020-09-22 11:12:18 +0100

    INFRA-749 Shut down and delete test H2 databases

--- testing/node-driver/src/main/kotlin/net/corda/testing/node/DatabaseSnapshot.kt
+++ testing/node-driver/src/main/kotlin/net/corda/testing/node/DatabaseSnapshot.kt
@@ -6,18 +6,19 @@ import java.nio.file.Path
 
 object DatabaseSnapshot {
     private const val previousCordaVersion: String = "4.5.1"
-    private const val databaseName: String = "persistence.mv.db"
+    const val DATABASE_FILENAME: String = "persistence.mv.db"
 
     private fun getDatabaseSnapshotStream(): InputStream {
-        val resourceUri = this::class.java.getResource("/databasesnapshots/${previousCordaVersion}/$databaseName")
+        val resourceUri = this::class.java.getResource("/databasesnapshots/${previousCordaVersion}/$DATABASE_FILENAME")
         return resourceUri.openStream()
     }
+
+    fun databaseFilename(baseDirectory: Path) = baseDirectory.resolve(DATABASE_FILENAME)
 
     fun copyDatabaseSnapshot(baseDirectory: Path) {
         getDatabaseSnapshotStream().use { stream ->
             Files.createDirectories(baseDirectory)
-            val path = baseDirectory.resolve(databaseName)
-            Files.copy(stream, path)
+            Files.copy(stream, databaseFilename(baseDirectory))
         }
     }
 }
