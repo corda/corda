395cfe4c41a7a4582679d83abdf5f6cc92dcb4fc
Ross Nicoll <ross.nicoll@r3.com>
2020-09-22 13:56:23 +0100

    revert unnecessary changes

--- testing/node-driver/src/main/kotlin/net/corda/testing/node/DatabaseSnapshot.kt
+++ testing/node-driver/src/main/kotlin/net/corda/testing/node/DatabaseSnapshot.kt
@@ -6,14 +6,14 @@ import java.nio.file.Path
 
 object DatabaseSnapshot {
     private const val previousCordaVersion: String = "4.5.1"
-    const val DATABASE_FILENAME: String = "persistence.mv.db"
+    private const val databaseName: String = "persistence.mv.db"
 
     private fun getDatabaseSnapshotStream(): InputStream {
-        val resourceUri = this::class.java.getResource("/databasesnapshots/${previousCordaVersion}/$DATABASE_FILENAME")
+        val resourceUri = this::class.java.getResource("/databasesnapshots/${previousCordaVersion}/$databaseName")
         return resourceUri.openStream()
     }
 
-    fun databaseFilename(baseDirectory: Path) = baseDirectory.resolve(DATABASE_FILENAME)
+    fun databaseFilename(baseDirectory: Path) = baseDirectory.resolve(databaseName)
 
     fun copyDatabaseSnapshot(baseDirectory: Path) {
         getDatabaseSnapshotStream().use { stream ->
