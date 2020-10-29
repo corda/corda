2ec3a6bdd10899f8a25edd100d01d0d31e71e5fe
Kyriakos Tharrouniatis <kyriakos.tharrouniatis@r3.com>
2020-10-29 12:13:18 +0000

    Add test asserting hospital gives a 'Diagnosis_OVERNIGHT_OBSERVATION' when 'SedationNurse' is injected

--- node/src/test/kotlin/net/corda/node/services/statemachine/StaffedFlowHospitalTest.kt
+++ node/src/test/kotlin/net/corda/node/services/statemachine/StaffedFlowHospitalTest.kt
@@ -6,6 +6,7 @@ import com.nhaarman.mockito_kotlin.whenever
 import net.corda.core.flows.HospitalizeFlowException
 import net.corda.core.flows.StateMachineRunId
 import net.corda.core.utilities.seconds
+import net.corda.node.services.statemachine.hospital.SedationNurse
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
@@ -54,6 +55,24 @@ class StaffedFlowHospitalTest {
         )
 
         assertTrue(event is Event.StartErrorPropagation)
+        assertEquals(0.seconds, backOffForChronicCondition)
+    }
+
+    @Test(timeout = 300_000)
+    fun `Hospital gives a Diagnosis_OVERNIGHT_OBSERVATION for HospitalizeFlowException if injected with SedationNurse`() {
+        doReturn(id).whenever(fiber).id
+        doReturn(Instant.now()).whenever(clock).instant()
+        doReturn(1).whenever(checkpointState).numberOfSuspends
+
+        flowHospital = StaffedFlowHospital(flowMessaging, clock, ourSenderUUID, listOf(SedationNurse))
+
+        val (event, backOffForChronicCondition) = flowHospital!!.admit(
+            fiber,
+            currentState,
+            listOf(HospitalizeFlowException())
+        )
+
+        assertTrue(event is Event.OvernightObservation)
         assertEquals(0.seconds, backOffForChronicCondition)
     }
 
