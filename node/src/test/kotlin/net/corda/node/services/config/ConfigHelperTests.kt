3471a0b3dd13f9d6d27d85f720087c0ee4af8081
Conal Smith <conal.smith@r3.com>
2021-08-20 13:11:11 +0100

    NOTICK - removal tests for config helper

--- node/src/test/kotlin/net/corda/node/services/config/ConfigHelperTests.kt
+++ node/src/test/kotlin/net/corda/node/services/config/ConfigHelperTests.kt
@@ -8,7 +8,6 @@ import net.corda.core.internal.delete
 import net.corda.core.internal.div
 import net.corda.node.internal.Node
 import org.junit.After
-import org.junit.Assert
 import org.junit.Before
 import org.junit.Test
 import org.mockito.ArgumentMatchers.contains
@@ -30,40 +29,6 @@ class ConfigHelperTests {
     @After
     fun cleanup() {
         baseDir?.delete()
-    }
-
-    @Test(timeout = 300_000)
-    fun `config is overridden by underscore variable`() {
-        val sshPort: Long = 9000
-
-        // Verify the port isn't set when not provided
-        var config = loadConfig()
-        Assert.assertFalse("SSH port should not be configured when not provided", config!!.hasPath("sshd.port"))
-
-        config = loadConfig("corda_sshd_port" to sshPort)
-        Assert.assertEquals(sshPort, config?.getLong("sshd.port"))
-    }
-
-    @Test(timeout = 300_000)
-    fun `config is overridden by case insensitive underscore variable`() {
-        val sshPort: Long = 10000
-        val config = loadConfig("CORDA_sshd_port" to sshPort)
-        Assert.assertEquals(sshPort, config?.getLong("sshd.port"))
-    }
-
-    @Test(timeout = 300_000)
-    fun `config is overridden by case insensitive dot variable`() {
-        val sshPort: Long = 11000
-        val config = loadConfig("CORDA.sshd.port" to sshPort,
-                "corda.devMode" to true.toString())
-        Assert.assertEquals(sshPort, config?.getLong("sshd.port"))
-    }
-
-    @Test(timeout = 300_000, expected = ShadowingException::class)
-    fun `shadowing is forbidden`() {
-        val sshPort: Long = 12000
-        loadConfig("CORDA_sshd_port" to sshPort.toString(),
-                "corda.sshd.port" to sshPort.toString())
     }
 
     @Test(timeout = 300_000)
