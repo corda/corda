484d36e1cf1193f613a31d48352d8940e574ef1a
Ramzi El-Yafi <ramzi.el-yafi@r3.com>
2021-10-04 17:37:22 +0100

    NAAS-295 Fix notary flow retries after ETA message sent (#6965)

--- core/src/main/kotlin/net/corda/core/internal/IdempotentFlow.kt
+++ core/src/main/kotlin/net/corda/core/internal/IdempotentFlow.kt
@@ -17,7 +17,12 @@ interface IdempotentFlow
  * next available notary cluster member.
  *
  * Note that any sub-flows called by a [TimedFlow] are assumed to be [IdempotentFlow] and will NOT have checkpoints
- * persisted. Otherwise, it wouldn't be possible to correctly reset the [TimedFlow].
+ * persisted. Otherwise, it wouldn't be possible to correctly reset the [TimedFlow]. An implication of this is that
+ * idempotent flows must not only return the same final result of the flow, but if a flow returns multiple messages
+ * the full set of messages must be returned on subsequent attempts in the same order as the first flow.
+ *
+ * An example of this would be if a notary returns an ETA message at any point, then any subsequent retries of the
+ * flow must also send such a message before returning the actual notarisation result.
  */
 // TODO: allow specifying retry settings per flow
 interface TimedFlow : IdempotentFlow {
