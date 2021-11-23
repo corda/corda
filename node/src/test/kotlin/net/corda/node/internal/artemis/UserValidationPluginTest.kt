b74850185da4084e8d137bae01e56129fe650f48
Viktor Kolomeyko <Viktor.Kolomeyko@r3.com>
2021-11-23 14:40:23 +0000

    ENT-6286: Fix `UserValidationPluginTest`

--- node/src/test/kotlin/net/corda/node/internal/artemis/UserValidationPluginTest.kt
+++ node/src/test/kotlin/net/corda/node/internal/artemis/UserValidationPluginTest.kt
@@ -1,6 +1,8 @@
 package net.corda.node.internal.artemis
 
+import com.nhaarman.mockito_kotlin.any
 import com.nhaarman.mockito_kotlin.doReturn
+import com.nhaarman.mockito_kotlin.doThrow
 import com.nhaarman.mockito_kotlin.whenever
 import net.corda.coretesting.internal.rigorousMock
 import net.corda.nodeapi.internal.ArtemisMessagingComponent
@@ -10,17 +12,23 @@ import org.apache.activemq.artemis.api.core.ActiveMQSecurityException
 import org.apache.activemq.artemis.api.core.Message
 import org.apache.activemq.artemis.api.core.SimpleString
 import org.apache.activemq.artemis.core.client.impl.ClientMessageImpl
-import org.apache.activemq.artemis.core.persistence.impl.nullpm.NullStorageManager
 import org.apache.activemq.artemis.core.server.ServerSession
-import org.apache.activemq.artemis.protocol.amqp.converter.AMQPConverter
+import org.apache.activemq.artemis.protocol.amqp.broker.AMQPMessage
+import org.apache.activemq.artemis.protocol.amqp.broker.AMQPStandardMessage
 import org.assertj.core.api.Assertions
 import org.junit.Test
 
 class UserValidationPluginTest {
     private val plugin = UserValidationPlugin()
-    private val coreMessage = ClientMessageImpl(0, false, 0, System.currentTimeMillis(), 4.toByte(), 1024)
-    private val storageManager = NullStorageManager()
-    private val amqpMessage get() = AMQPConverter.getInstance().fromCore(coreMessage, storageManager)
+    private val coreMessage = ClientMessageImpl(0, false, 0, System.currentTimeMillis(),
+            4.toByte(), 1024)
+    private val amqpMessage: AMQPMessage
+        get() {
+            return rigorousMock<AMQPMessage>().also {
+                doReturn(coreMessage.validatedUserID).whenever(it).getStringProperty(Message.HDR_VALIDATED_USER)
+            }
+        }
+
     private val session = rigorousMock<ServerSession>().also {
         doReturn(ArtemisMessagingComponent.PEER_USER).whenever(it).username
         doReturn(ALICE_NAME.toString()).whenever(it).validatedUser
@@ -33,16 +41,17 @@ class UserValidationPluginTest {
 
     @Test(timeout = 300_000)
     fun `accept AMQP message with user`() {
-        coreMessage.putStringProperty("_AMQ_VALIDATED_USER", ALICE_NAME.toString())
+        coreMessage.validatedUserID = ALICE_NAME.toString()
         plugin.beforeSend(session, rigorousMock(), amqpMessage, direct = false, noAutoCreateQueue = false)
     }
 
     @Test(timeout = 300_000)
     fun `reject AMQP message with different user`() {
-        coreMessage.putStringProperty("_AMQ_VALIDATED_USER", BOB_NAME.toString())
+        coreMessage.validatedUserID = BOB_NAME.toString()
+        val localAmqpMessage = amqpMessage
         Assertions.assertThatExceptionOfType(ActiveMQSecurityException::class.java).isThrownBy {
-            plugin.beforeSend(session, rigorousMock(), amqpMessage, direct = false, noAutoCreateQueue = false)
-        }.withMessageContaining("_AMQ_VALIDATED_USER")
+            plugin.beforeSend(session, rigorousMock(), localAmqpMessage, direct = false, noAutoCreateQueue = false)
+        }.withMessageContaining(Message.HDR_VALIDATED_USER.toString())
     }
 
     @Test(timeout = 300_000)
@@ -51,7 +60,7 @@ class UserValidationPluginTest {
             doReturn(ArtemisMessagingComponent.NODE_P2P_USER).whenever(it).username
             doReturn(ALICE_NAME.toString()).whenever(it).validatedUser
         }
-        coreMessage.putStringProperty("_AMQ_VALIDATED_USER", BOB_NAME.toString())
+        coreMessage.validatedUserID = BOB_NAME.toString()
         plugin.beforeSend(internalSession, rigorousMock(), amqpMessage, direct = false, noAutoCreateQueue = false)
     }
 
@@ -64,11 +73,8 @@ class UserValidationPluginTest {
 
     @Test(timeout = 300_000)
     fun `reject message with exception`() {
-        coreMessage.putStringProperty("_AMQ_VALIDATED_USER", BOB_NAME.toString())
-        val messageWithException = object : Message by amqpMessage {
-            override fun getStringProperty(key: SimpleString?): String {
-                throw IllegalStateException("My exception")
-            }
+        val messageWithException = rigorousMock<AMQPMessage>().also {
+            doThrow(IllegalStateException("My exception")).whenever(it).getStringProperty(any<SimpleString>())
         }
         // Artemis swallows all exceptions except ActiveMQException, so making sure that proper exception is thrown
         Assertions.assertThatExceptionOfType(ActiveMQSecurityException::class.java).isThrownBy {
@@ -78,9 +84,8 @@ class UserValidationPluginTest {
 
     @Test(timeout = 300_000)
     fun `reject message with security exception`() {
-        coreMessage.putStringProperty("_AMQ_VALIDATED_USER", BOB_NAME.toString())
-        val messageWithException = object : Message by amqpMessage {
-            override fun getStringProperty(key: SimpleString?): String {
+        val messageWithException = object : AMQPStandardMessage(0, ByteArray(0), null) {
+            override fun getApplicationPropertiesMap(createIfAbsent: Boolean): MutableMap<String, Any> {
                 throw ActiveMQSecurityException("My security exception")
             }
         }
