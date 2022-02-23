db4d4edea2b45704b851c7bd55db9183d18740d3
Conal Smith <conal.smith@r3.com>
2022-02-23 14:22:01 +0000

    ENT-6508 - Add validation to prevent directory traversal with zip attachments

--- node/src/main/kotlin/net/corda/node/services/persistence/NodeAttachmentService.kt
+++ node/src/main/kotlin/net/corda/node/services/persistence/NodeAttachmentService.kt
@@ -90,6 +90,9 @@ class NodeAttachmentService @JvmOverloads constructor(
                 val cursor = jar.nextJarEntry ?: break
                 if (manifestHasEntries && !allManifestEntries!!.remove(cursor.name)) extraFilesNotFoundInEntries.add(cursor)
                 val entryPath = Paths.get(cursor.name)
+                // Security check to stop directory traversal from filename entry
+                require(!(cursor.name.contains("../"))) { "Bad character in $entryPath" }
+                require(!(cursor.name.contains("..\\"))) { "Bad character in $entryPath" }
                 // Security check to stop zips trying to escape their rightful place.
                 require(!entryPath.isAbsolute) { "Path $entryPath is absolute" }
                 require(entryPath.normalize() == entryPath) { "Path $entryPath is not normalised" }
