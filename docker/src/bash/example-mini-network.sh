d777d29fa36b030c85a69ae6090583eb92035602
nargas-ritu <ritu.gupta@r3.com>
2022-05-31 14:37:21 +0100

    NOTICK: Bump version to CE4.7.8-RC01

--- docker/src/bash/example-mini-network.sh
+++ docker/src/bash/example-mini-network.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 NODE_LIST=("dockerNode1" "dockerNode2" "dockerNode3")
 NETWORK_NAME=mininet
-CORDAPP_VERSION="4.6-SNAPSHOT"
-DOCKER_IMAGE_VERSION="corda-zulu-4.6-snapshot"
+CORDAPP_VERSION="4.7.8-RC01"
+DOCKER_IMAGE_VERSION="corda-zulu-4.7.8-RC01"
 
 mkdir cordapps
 rm -f cordapps/*
