51783b4bc7644dc345adc3eb8dc5e3ffb6afbb9f
rick.parker <rick.parker@r3cev.com>
2022-09-08 09:47:36 +0100

    ENT-6947: Remove magic numbers from PrivateInterner.  Make configurable via system properties

--- core/src/main/kotlin/net/corda/core/internal/utilities/PrivateInterner.kt
+++ core/src/main/kotlin/net/corda/core/internal/utilities/PrivateInterner.kt
@@ -1,10 +1,26 @@
 package net.corda.core.internal.utilities
 
 import com.google.common.collect.Interners
+import net.corda.core.internal.uncheckedCast
 
+/**
+ * This class converts instances supplied to [intern] to a common instance within the JVM, amongst all those
+ * instances that have been submitted.  It uses weak references to avoid memory leaks.
+ *
+ * NOTE: the Guava interners are Beta, so upgrading Guava may result in us having to adapt this code.
+ *
+ * System properties allow disabling, in the event an issue is uncovered in a live environment.  The
+ * correct default for the concurrency setting is the result of performance evaluation.
+ */
 class PrivateInterner<T> {
-    private val interner = Interners.newBuilder().weak().concurrencyLevel(32).build<T>()
+    companion object {
+        private const val DEFAULT_CONCURRENCY_LEVEL = 32
+        private val CONCURRENCY_LEVEL = Integer.getInteger("net.corda.core.intern.concurrency", DEFAULT_CONCURRENCY_LEVEL).toInt()
+        private val DISABLE = java.lang.Boolean.getBoolean("net.corda.core.intern.disable")
+    }
 
-    fun <S : T> intern(sample: S): S = interner.intern(sample) as S
+    private val interner = Interners.newBuilder().weak().concurrencyLevel(CONCURRENCY_LEVEL).build<T>()
+
+    fun <S : T> intern(sample: S): S = if (DISABLE) sample else uncheckedCast(interner.intern(sample))
 }
 
