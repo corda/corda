a2aa8a31613138dc67694698c9829691c3fa5bf8
rick.parker <rick.parker@r3cev.com>
2022-09-15 16:56:56 +0100

    ENT-6947: Revised default as result of performance testing

--- core/src/main/kotlin/net/corda/core/internal/utilities/PrivateInterner.kt
+++ core/src/main/kotlin/net/corda/core/internal/utilities/PrivateInterner.kt
@@ -19,7 +19,8 @@ import kotlin.reflect.full.companionObjectInstance
 @CordaInternal
 class PrivateInterner<T>(val verifier: IternabilityVerifier<T> = AlwaysInternableVerifier()) {
     companion object {
-        private const val DEFAULT_CONCURRENCY_LEVEL = 32
+        // This value is the default in Guava, and performance testing did reveal a need to change
+        private const val DEFAULT_CONCURRENCY_LEVEL = 4
         private val CONCURRENCY_LEVEL = Integer.getInteger("net.corda.core.intern.concurrency", DEFAULT_CONCURRENCY_LEVEL).toInt()
         private val DISABLE = java.lang.Boolean.getBoolean("net.corda.core.intern.disable")
 
