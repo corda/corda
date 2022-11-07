eb070ea2f380dd4aad9786941cdf5387ded4d9b3
mcgovc <connel.mcgovern@r3.com>
2022-11-07 12:05:54 +0000

    NOTICK: bump version to 4.10-RC02

--- docker/src/bash/example-mini-network.sh
+++ docker/src/bash/example-mini-network.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 NODE_LIST=("dockerNode1" "dockerNode2" "dockerNode3")
 NETWORK_NAME=mininet
-CORDAPP_VERSION="4.10-SNAPSHOT"
-DOCKER_IMAGE_VERSION="corda-zulu-4.10-snapshot"
+CORDAPP_VERSION="4.10-RC02"
+DOCKER_IMAGE_VERSION="corda-zulu-4.10-RC02"
 
 mkdir cordapps
 rm -f cordapps/*
