8a9386cd0259182b45391d1afa08f4a8f67d50b1
rick.parker <rick.parker@r3cev.com>
2023-04-17 11:44:27 +0100

    Fix import changes clash during rebase

--- node-api/src/main/kotlin/net/corda/nodeapi/internal/crypto/X509Utilities.kt
+++ node-api/src/main/kotlin/net/corda/nodeapi/internal/crypto/X509Utilities.kt
@@ -52,6 +52,7 @@ import java.security.cert.Certificate
 import java.security.cert.CertificateException
 import java.security.cert.CertificateFactory
 import java.security.cert.TrustAnchor
+import java.security.cert.X509CRL
 import java.security.cert.X509Certificate
 import java.time.Duration
 import java.time.Instant
