c17459a55654637984313c6e18bed2efd9531f69
rick.parker <rick.parker@r3cev.com>
2023-04-25 08:45:33 +0100

    Switch to use Guava weak keyed map since nothing special about our map implementation.

--- core/src/main/kotlin/net/corda/core/internal/ContractStateClassCache.kt
+++ core/src/main/kotlin/net/corda/core/internal/ContractStateClassCache.kt
@@ -1,47 +1,18 @@
 package net.corda.core.internal
 
+import com.google.common.collect.MapMaker
 import net.corda.core.contracts.ContractState
-import java.lang.ref.ReferenceQueue
-import java.lang.ref.WeakReference
-import java.util.concurrent.ConcurrentHashMap
 
 object ContractStateClassCache {
-    private val collectedWeakClassKeys = ReferenceQueue<Class<*>>()
-
-    private class WeakClassKey(key: Class<*>) : WeakReference<Class<*>>(key, collectedWeakClassKeys) {
-        private val hashCode = key.hashCode()
-
-        override fun hashCode(): Int = hashCode
-        override fun equals(other: Any?): Boolean {
-            if (this === other) return true
-            if (other !is WeakClassKey) return false
-            if (this.hashCode != other.hashCode) return false
-            val thisGet = this.get()
-            val otherGet = other.get()
-            if (thisGet == null || otherGet == null) return false
-            return thisGet == otherGet
-        }
-    }
-
-    private val classToString = ConcurrentHashMap<WeakClassKey, String>()
-
-    private fun reapCollectedWeakPubKeys() {
-        while (true) {
-            val weakClassKey = (collectedWeakClassKeys.poll() as? WeakClassKey) ?: break
-            classToString.remove(weakClassKey)
-        }
-    }
+    private val classToString = MapMaker().weakKeys().makeMap<Class<*>, String>()
 
     fun contractClassName(key: Class<ContractState>): String? {
-        val weakClassKey = WeakClassKey(key)
-        return classToString[weakClassKey]
+        return classToString[key]
     }
 
     fun cacheContractClassName(key: Class<ContractState>, contractClassName: String?): String? {
         if (contractClassName == null) return null
-        reapCollectedWeakPubKeys()
-        val weakClassKey = WeakClassKey(key)
-        classToString.putIfAbsent(weakClassKey, contractClassName)
+        classToString.putIfAbsent(key, contractClassName)
         return contractClassName
     }
 }
