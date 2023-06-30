afd8a4998c46c52e42c005bb1f5a66e7012ed67a
Jose Coll <jose.coll@r3.com>
2023-06-30 10:30:58 +0100

    Clean-up.

--- core/src/main/kotlin/net/corda/core/flows/ReceiveRecoverableTransactionFlow.kt
+++ core/src/main/kotlin/net/corda/core/flows/ReceiveRecoverableTransactionFlow.kt
@@ -1,22 +1,16 @@
 package net.corda.core.flows
 
-import net.corda.core.internal.ResolveTransactionsFlow
 import net.corda.core.internal.ServiceHubCoreInternal
 import net.corda.core.node.StatesToRecord
 import net.corda.core.transactions.SignedTransaction
 
 /**
- * This flow extends [ReceiveTransactionFlow] to store recovery metadata
+ * This flow extends [ReceiveTransactionFlow] to store transaction recovery metadata for the purpose of ledger recovery.
  */
 open class ReceiveRecoverableTransactionFlow constructor(private val otherSideSession: FlowSession,
-                                                         private val checkSufficientSignatures: Boolean = true,
+                                                         checkSufficientSignatures: Boolean = true,
                                                          private val statesToRecord: StatesToRecord = StatesToRecord.NONE,
-                                                         private val deferredAck: Boolean = false) : ReceiveTransactionFlow(otherSideSession, checkSufficientSignatures, statesToRecord, deferredAck) {
-    @JvmOverloads constructor(
-            otherSideSession: FlowSession,
-            checkSufficientSignatures: Boolean = true,
-            statesToRecord: StatesToRecord = StatesToRecord.NONE
-    ) : this(otherSideSession, checkSufficientSignatures, statesToRecord, false)
+                                                         deferredAck: Boolean = true) : ReceiveTransactionFlow(otherSideSession, checkSufficientSignatures, statesToRecord, deferredAck) {
 
     override fun resolvePayload(payload: Any): SignedTransaction {
         return if (payload is SignedTransactionWithDistributionList) {
