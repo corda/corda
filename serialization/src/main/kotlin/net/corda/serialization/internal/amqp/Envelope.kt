01df22953c35319f65cf9d053f75921112413bb9
rick.parker <rick.parker@r3cev.com>
2023-07-19 16:16:04 +0100

    More PR feedback

--- serialization/src/main/kotlin/net/corda/serialization/internal/amqp/Envelope.kt
+++ serialization/src/main/kotlin/net/corda/serialization/internal/amqp/Envelope.kt
@@ -5,7 +5,6 @@ import org.apache.qpid.proton.ProtonException
 import org.apache.qpid.proton.amqp.DescribedType
 import org.apache.qpid.proton.codec.Data
 import org.apache.qpid.proton.codec.DecoderImpl
-import org.apache.qpid.proton.codec.DescribedTypeConstructor
 import org.apache.qpid.proton.codec.EncodingCodes
 import org.apache.qpid.proton.codec.FastPathDescribedTypeConstructor
 import java.nio.Buffer
@@ -24,58 +23,13 @@ class Envelope(val obj: Any?, resolveSchema: () -> Pair<Schema, TransformsSchema
     val schema: Schema get() = resolvedSchema.first
     val transformsSchema: TransformsSchema get() = resolvedSchema.second
 
-    companion object : DescribedTypeConstructor<Envelope> {
+    companion object {
         val DESCRIPTOR = AMQPDescriptorRegistry.ENVELOPE.amqpDescriptor
         val DESCRIPTOR_OBJECT = Descriptor(null, DESCRIPTOR)
 
         // described list should either be two or three elements long
         private const val ENVELOPE_WITHOUT_TRANSFORMS = 2
         private const val ENVELOPE_WITH_TRANSFORMS = 3
-
-        private const val BLOB_IDX = 0
-        private const val SCHEMA_IDX = 1
-        private const val TRANSFORMS_SCHEMA_IDX = 2
-
-        fun get(data: Data): Envelope {
-            data.next()
-            val describedType = data.`object` as DescribedType
-            if (describedType.descriptor != DESCRIPTOR) {
-                throw AMQPNoTypeNotSerializableException(
-                        "Unexpected descriptor ${describedType.descriptor}, should be $DESCRIPTOR.")
-            }
-            val list = describedType.described as List<*>
-
-            // We need to cope with objects serialised without the transforms header element in the
-            // envelope
-            val transformSchema: Any? = when (list.size) {
-                ENVELOPE_WITHOUT_TRANSFORMS -> null
-                ENVELOPE_WITH_TRANSFORMS -> list[TRANSFORMS_SCHEMA_IDX]
-                else -> throw AMQPNoTypeNotSerializableException(
-                        "Malformed list, bad length of ${list.size} (should be 2 or 3)")
-            }
-
-            return newInstance(listOf(list[BLOB_IDX], Schema.get(list[SCHEMA_IDX]!!),
-                    TransformsSchema.newInstance(transformSchema)))
-        }
-
-        // This separation of functions is needed as this will be the entry point for the default
-        // AMQP decoder if one is used (see the unit tests).
-        override fun newInstance(described: Any?): Envelope {
-            val list = described as? List<*> ?: throw IllegalStateException("Was expecting a list")
-
-            // We need to cope with objects serialised without the transforms header element in the
-            // envelope
-            val transformSchema = when (list.size) {
-                ENVELOPE_WITHOUT_TRANSFORMS -> TransformsSchema.newInstance(null)
-                ENVELOPE_WITH_TRANSFORMS -> list[TRANSFORMS_SCHEMA_IDX] as TransformsSchema
-                else -> throw AMQPNoTypeNotSerializableException(
-                        "Malformed list, bad length of ${list.size} (should be 2 or 3)")
-            }
-
-            return Envelope(list[BLOB_IDX]) { list[SCHEMA_IDX] as Schema to transformSchema }
-        }
-
-        override fun getTypeClass(): Class<*> = Envelope::class.java
     }
 
     class FastPathConstructor(private val decoder: DecoderImpl) : FastPathDescribedTypeConstructor<Envelope> {
@@ -99,6 +53,9 @@ class Envelope(val obj: Any?, resolveSchema: () -> Pair<Schema, TransformsSchema
         override fun readValue(): Envelope? {
             val buffer = _buffer
             val size = readEncodingAndReturnSize(buffer, false)
+            if (size != ENVELOPE_WITHOUT_TRANSFORMS && size != ENVELOPE_WITH_TRANSFORMS) {
+                throw AMQPNoTypeNotSerializableException("Malformed list, bad length of $size (should be 2 or 3)")
+            }
             val data = Data.Factory.create()
             data.decode(buffer)
             val obj = data.`object`
