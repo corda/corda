8bb6d1ba7d06af2f447aa2caa47d506c8dec845c
Adel El-Beik <adel.el-beik@r3.com>
2023-09-22 17:39:23 +0100

    ENT-9943: Converted over FinalityFlowTests to use secure hash for party id.

--- node/src/main/kotlin/net/corda/node/services/persistence/HashedDistributionList.kt
+++ node/src/main/kotlin/net/corda/node/services/persistence/HashedDistributionList.kt
@@ -14,7 +14,7 @@ import java.time.Instant
 @CordaSerializable
 data class HashedDistributionList(
         val senderStatesToRecord: StatesToRecord,
-        internal val peerHashToStatesToRecord: Map<SecureHash, StatesToRecord>,
+        val peerHashToStatesToRecord: Map<SecureHash, StatesToRecord>,
         val publicHeader: PublicHeader
 ) {
     /**
