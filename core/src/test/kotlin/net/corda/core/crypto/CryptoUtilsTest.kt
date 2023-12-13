75d3ce63695e6af1f63c3a73ae8fd9761554c50f
Jose Coll <jose.coll@r3.com>
2023-12-13 09:32:30 +0000

    Remove @Ignore (fixed with re-instated SunEC crypto provider)

--- core/src/test/kotlin/net/corda/core/crypto/CryptoUtilsTest.kt
+++ core/src/test/kotlin/net/corda/core/crypto/CryptoUtilsTest.kt
@@ -27,7 +27,6 @@ import org.bouncycastle.jce.spec.ECNamedCurveParameterSpec
 import org.bouncycastle.pqc.jcajce.provider.sphincs.BCSphincs256PrivateKey
 import org.bouncycastle.pqc.jcajce.provider.sphincs.BCSphincs256PublicKey
 import org.junit.Assert.assertNotEquals
-import org.junit.Ignore
 import org.junit.Test
 import java.math.BigInteger
 import java.security.KeyPairGenerator
@@ -670,7 +669,6 @@ class CryptoUtilsTest {
     }
 
     @Test(timeout = 300_000)
-    @Ignore("TODO JDK17: Fixme")
     fun `Unsupported EC public key type on curve`() {
         val keyGen = KeyPairGenerator.getInstance("EC") // sun.security.ec.ECPublicKeyImpl
         keyGen.initialize(256, newSecureRandom())
