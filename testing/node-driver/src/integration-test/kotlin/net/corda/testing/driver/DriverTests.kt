3874fd7d0bc0749359662e172df57d6d791ca992
Balwant Kothari <balwant.kothari@r3.com>
2024-01-02 21:45:27 +0530

    ENT-11113 Removing ignore annotation in DriverTests

--- testing/node-driver/src/integration-test/kotlin/net/corda/testing/driver/DriverTests.kt
+++ testing/node-driver/src/integration-test/kotlin/net/corda/testing/driver/DriverTests.kt
@@ -79,7 +79,6 @@ class DriverTests {
     }
 
     @Test(timeout=300_000)
-    @Ignore("TODO JDK17: Fixme - intermittent on jenkins")
 	fun `default notary is visible when the startNode future completes`() {
         // Based on local testing, running this 3 times gives us a high confidence that we'll spot if the feature is not working
         repeat(3) {
@@ -91,7 +90,6 @@ class DriverTests {
     }
 
     @Test(timeout=300_000)
-    @Ignore("TODO JDK17: Fixme - Stage 2")
 	fun `debug mode enables debug logging level`() {
         // Make sure we're using the log4j2 config which writes to the log file
         val logConfigFile = projectRootDir / "config" / "dev" / "log4j2.xml"
