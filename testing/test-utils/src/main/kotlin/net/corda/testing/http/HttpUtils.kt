51fe1ccfdf9ed6a2f4724d155274a62124113da0
Chris Cochrane <chris.cochrane@r3.com>
2024-01-16 17:55:55 +0000

    Resolve detekt errors

--- testing/test-utils/src/main/kotlin/net/corda/testing/http/HttpUtils.kt
+++ testing/test-utils/src/main/kotlin/net/corda/testing/http/HttpUtils.kt
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.databind.ObjectMapper
 import okhttp3.MediaType.Companion.toMediaTypeOrNull
 import okhttp3.OkHttpClient
 import okhttp3.Request
-import okhttp3.RequestBody
 import okhttp3.RequestBody.Companion.toRequestBody
 import java.io.IOException
 import java.net.URL
