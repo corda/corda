ee944b9e7e65852d212c330f14f4aa563273da95
Shams Asari <shams.asari@r3.com>
2024-01-23 13:19:59 +0000

    ENT-11251: Upgrade to Kotlin language version 1.9

--- node/src/main/kotlin/net/corda/node/utilities/NonInvalidatingCache.kt
+++ node/src/main/kotlin/net/corda/node/utilities/NonInvalidatingCache.kt
@@ -6,21 +6,17 @@ import com.github.benmanes.caffeine.cache.LoadingCache
 import com.github.benmanes.caffeine.cache.Weigher
 import net.corda.core.internal.NamedCacheFactory
 
-class NonInvalidatingCache<K, V> private constructor(
-        val cache: LoadingCache<K, V>
-) : LoadingCache<K, V> by cache {
-
-    constructor(cacheFactory: NamedCacheFactory, name: String, loadFunction: (K) -> V) :
-            this(buildCache(cacheFactory, name, loadFunction))
+class NonInvalidatingCache<K : Any, V : Any> private constructor(val cache: LoadingCache<K, V>) : LoadingCache<K, V> by cache {
+    constructor(cacheFactory: NamedCacheFactory, name: String, loadFunction: (K) -> V?) : this(buildCache(cacheFactory, name, loadFunction))
 
     private companion object {
-        private fun <K, V> buildCache(cacheFactory: NamedCacheFactory, name: String, loadFunction: (K) -> V): LoadingCache<K, V> {
+        private fun <K : Any, V : Any> buildCache(cacheFactory: NamedCacheFactory, name: String, loadFunction: (K) -> V?): LoadingCache<K, V> {
             return cacheFactory.buildNamed(name, NonInvalidatingCacheLoader(loadFunction))
         }
     }
 
     // TODO look into overriding loadAll() if we ever use it
-    class NonInvalidatingCacheLoader<K, V>(val loadFunction: (K) -> V) : CacheLoader<K, V> {
+    class NonInvalidatingCacheLoader<K : Any, V : Any>(val loadFunction: (K) -> V?) : CacheLoader<K, V> {
         override fun reload(key: K, oldValue: V): V {
             throw IllegalStateException("Non invalidating cache refreshed")
         }
@@ -29,14 +25,15 @@ class NonInvalidatingCache<K, V> private constructor(
     }
 }
 
-class NonInvalidatingWeightBasedCache<K, V> private constructor(
-        val cache: LoadingCache<K, V>
-) : LoadingCache<K, V> by cache {
-    constructor (cacheFactory: NamedCacheFactory, name: String, weigher: Weigher<K, V>, loadFunction: (K) -> V) :
+class NonInvalidatingWeightBasedCache<K : Any, V : Any> private constructor(val cache: LoadingCache<K, V>) : LoadingCache<K, V> by cache {
+    constructor(cacheFactory: NamedCacheFactory, name: String, weigher: Weigher<K, V>, loadFunction: (K) -> V?) :
             this(buildCache(cacheFactory, name, weigher, loadFunction))
 
     private companion object {
-        private fun <K, V> buildCache(cacheFactory: NamedCacheFactory, name: String, weigher: Weigher<K, V>, loadFunction: (K) -> V): LoadingCache<K, V> {
+        private fun <K : Any, V : Any> buildCache(cacheFactory: NamedCacheFactory,
+                                                  name: String,
+                                                  weigher: Weigher<K, V>,
+                                                  loadFunction: (K) -> V?): LoadingCache<K, V> {
             val builder = Caffeine.newBuilder().weigher(weigher)
             return cacheFactory.buildNamed(builder, name, NonInvalidatingCache.NonInvalidatingCacheLoader(loadFunction))
         }
