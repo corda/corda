0c4cc05efa545d4b8123dafc000fa01c65447504
r3-build <build@r3.com>
2024-02-22 14:57:35 +0000

    CORE-11136: release prep for release-os-4.11.2

--- common/logging/src/main/kotlin/net/corda/common/logging/Constants.kt
+++ common/logging/src/main/kotlin/net/corda/common/logging/Constants.kt
@@ -9,4 +9,4 @@ package net.corda.common.logging
  * (originally added to source control for ease of use)
  */
 
-internal const val CURRENT_MAJOR_RELEASE = "4.11.2-RC03"
+internal const val CURRENT_MAJOR_RELEASE = "4.11.2"
