5c85e74814c14c4542316fde541d880dc44eb103
r3-build <build@r3.com>
2024-10-04 13:09:28 +0000

    CORE-11136: release prep for release-os-4.12.2-RC01

--- docker/src/bash/example-mini-network.sh
+++ docker/src/bash/example-mini-network.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 NODE_LIST=("dockerNode1" "dockerNode2" "dockerNode3")
 NETWORK_NAME=mininet
-CORDAPP_VERSION="4.12-SNAPSHOT"
-DOCKER_IMAGE_VERSION="corda-zulu-4.12-snapshot"
+CORDAPP_VERSION="4.12.2-RC01"
+DOCKER_IMAGE_VERSION="corda-zulu-4.12.2-RC01"
 
 mkdir cordapps
 rm -f cordapps/*
