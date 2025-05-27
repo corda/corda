8d291df2c0dd200dbd18949e05223f08e76539b9
Lucas Siqueira <lucas.siqueira@r3.com>
2025-05-27 15:04:44 +0100

    refactor

--- core/src/main/kotlin/net/corda/core/flows/SystemFlow.kt
+++ core/src/main/kotlin/net/corda/core/flows/SystemFlow.kt
@@ -4,8 +4,9 @@ import kotlin.annotation.AnnotationTarget.CLASS
 
 /**
  * Classifies [FlowLogic] classes that are part of the system flows.
- * @param supersedes The class name of the flow that this flow supersedes.
+ *
+ * @property [supersedes] The [String] class name of the flow that this flow supersedes.
  */
 @Target(CLASS)
 @MustBeDocumented
-annotation class SystemFlow( val supersedes: String = "")
+annotation class SystemFlow(val supersedes: String = "")
