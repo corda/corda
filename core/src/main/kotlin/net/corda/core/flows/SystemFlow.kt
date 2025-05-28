98fe52d730be0cd93ae9fce26e5f9e78dc75ddea
Lucas Siqueira <lucas.siqueira@r3.com>
2025-05-28 16:15:29 +0100

    ENT-12829: add supersedes property to SystemFlow annotation (#7935)
    * add supersedes property to SystemFlow annotation
    
    * update api

--- core/src/main/kotlin/net/corda/core/flows/SystemFlow.kt
+++ core/src/main/kotlin/net/corda/core/flows/SystemFlow.kt
@@ -4,7 +4,9 @@ import kotlin.annotation.AnnotationTarget.CLASS
 
 /**
  * Classifies [FlowLogic] classes that are part of the system flows.
+ *
+ * @property [supersedes] The [String] class name of the flow that this flow supersedes.
  */
 @Target(CLASS)
 @MustBeDocumented
-annotation class SystemFlow
+annotation class SystemFlow(val supersedes: String = "")
