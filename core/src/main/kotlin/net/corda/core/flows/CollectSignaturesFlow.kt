9c3012ad904ea258f9be152f0846a4acd34eb57b
Christian Sailer <christian.sailer@r3.com>
2025-07-14 17:27:48 +0100

    Fix uneeded access to WireTx

--- core/src/main/kotlin/net/corda/core/flows/CollectSignaturesFlow.kt
+++ core/src/main/kotlin/net/corda/core/flows/CollectSignaturesFlow.kt
@@ -332,7 +332,7 @@ abstract class SignTransactionFlow @JvmOverloads constructor(val otherSideSessio
 
     @Suspendable
     private fun checkMySignaturesRequired(stx: SignedTransaction, signingKeys: Iterable<PublicKey>) {
-        require(signingKeys.all { it in stx.tx.requiredSigningKeys }) {
+        require(signingKeys.all { it in stx.requiredSigningKeys }) {
             "A signature was requested for a key that isn't part of the required signing keys for transaction ${stx.id}"
         }
     }
