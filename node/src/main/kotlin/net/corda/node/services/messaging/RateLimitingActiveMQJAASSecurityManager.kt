52d47a584e4378b4026a25032d55fb0ed7d40246
Jakub Zadroga <jakub.zadroga@r3.com>
2026-02-25 21:17:59 +0100

    ENT-14787: Fix for the rate-limiting backoff (#8124)
    1. Fix for the logic bug in `RateLimitingActiveMQJAASSecurityManager`:
    catch and throw the auth exception after all the rate-limiting checks
    completed, instead of not catching it and throwing right away
    2. Moved the tests to `AuthDBTests` where they belong better
    
    Tested the corda.jar locally through the new rate-limiting CRAFT test,
    works fine.
    
    
    # PR Checklist:
    
    - [ ] Have you run the unit, integration and smoke tests as described
    [here](https://docs.r3.com/testing.html)?
    - [ ] If you added public APIs, did you write the JavaDocs/kdocs?
    - [ ] If the changes are of interest to application developers, have you
    added them to the changelog, and potentially the [release
    notes](https://docs.r3.com/release-notes.html)
    (`https://docs.r3.com/release-notes.html`)?
    - [ ] If you are contributing for the first time, please read the
    [contributor agreement](https://docs.r3.com/contributing.html) now and
    add a comment to this pull request stating that your PR is in accordance
    with the [Developer's Certificate of
    Origin](https://docs.r3.com/contributing.html).
    
    Thanks for your code, it's appreciated! :)

--- node/src/main/kotlin/net/corda/node/services/messaging/RateLimitingActiveMQJAASSecurityManager.kt
+++ node/src/main/kotlin/net/corda/node/services/messaging/RateLimitingActiveMQJAASSecurityManager.kt
@@ -72,7 +72,13 @@ class RateLimitingActiveMQJAASSecurityManager(
         }
 
         // 2. Attempt authentication
-        val subject = super.authenticate(user, password, remotingConnection, securityDomain)
+        var thrown: Exception? = null
+        val subject = try {
+            super.authenticate(user, password, remotingConnection, securityDomain)
+        } catch (e: Exception) {
+            thrown = e
+            null
+        }
         if (subject != null) {
             // success - clear user cache only
             if (userKey != null) {
@@ -110,7 +116,8 @@ class RateLimitingActiveMQJAASSecurityManager(
         }
 
         // 7. Plain authentication failure
-        return null
+        if (thrown != null) throw thrown
+        throw NoCacheLoginException("Uncached failure")
     }
 
     private fun recordFailure(
