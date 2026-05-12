775544663a3ca495833f5d9cf01657aa5149108a
shubham singh <shubham.singh@r3.com>
2026-05-12 12:05:13 +0530

    ENT-15115 Removing dependency-checker and irs-demo

--- samples/irs-demo/cordapp/contracts-irs/src/main/kotlin/net/corda/irs/contract/IRSExport.kt
+++
@@ -1,7 +0,0 @@
-package net.corda.irs.contract
-
-fun InterestRateSwap.State.exportIRSToCSV(): String =
-        "Fixed Leg\n" + FixedRatePaymentEvent.CSVHeader + "\n" +
-                this.calculation.fixedLegPaymentSchedule.toSortedMap().values.joinToString("\n") { it.asCSV() } + "\n" +
-                "Floating Leg\n" + FloatingRatePaymentEvent.CSVHeader + "\n" +
-                this.calculation.floatingLegPaymentSchedule.toSortedMap().values.joinToString("\n") { it.asCSV() } + "\n"
