4111f2961d5110550199508cc273dd52d6c2ed63
Chris Cochrane <78791827+chriscochrane@users.noreply.github.com>
2026-06-04 16:43:17 +0100

    ENT-15116 - Security dependency updates (#8185)
    Dependency and waiver updates to address security issues:
    
    **Azure Resource Manager upgrade**
    CVE-2026-33117	Improper Authentication
    
    **Jackson upgrade**
    CWE-770	Allocation of Resources Without Limits or Throttling
    
    _Additional changes to accommodate the Jackson upgrade._
    For deserialization, Jackson 2.19.x enforces stricter type checks on
    returned types than the previous version. Previously Jackson would let a
    deserializer return a supertype instance and use it anyway; now it
    validates the returned object is actually assignment-compatible with the
    requested type and throws an exception if not.
    A new deserializer `DigitalSignatureDeserializer` has been added to
    handle the `DigitalSignature` class, which is derived from
    `OpaqueBytes`. Previously Jackson was happy for the `OpaqueBytes`
    deserializer to be used.
    Additionally some deprecated function calls have been updated in
    `HttpUtils` and `FlowStackSnapshot`, and wherever `jackson-annotations`
    is referenced the version specifier has been removed because version
    2.21.3 does not exist. Instead it is left for the `jackson-bom` to
    determine the version of `jackson-annotations.`
    
    **Commons-configuration2 forced-override**
    CVE-2026-45205	Uncontrolled Recursion
    
    **Log4j upgrade**
    CVE-2026-34477	Improper Validation of Certificate with Host Mismatch
    CVE-2026-34478	Improper Output Neutralization for Logs
    CVE-2026-34480	Improper Encoding or Escaping of Output
    CVE-2026-34479	Improper Encoding or Escaping of Output
    
    **Artemis upgrade**
    No specific vulnerability - just to use the latest version available.
    
    _Additional changes to accommodate Artemis upgrade_
    Version 2.54.0 saw a change whereby attempting to connect to a broker
    using the default cluster credentials throws a different (more generic)
    type of security exception than version 2.53.0. A test in
    `MQSecurityAsNodeTest` has been updated to reflect this change in
    behaviour.
    
    **Apache Shiro upgrade**
    CVE-2026-43827	Session Fixation
    CVE-2026-43828 Sensitive Cookie in HTTPS Session Without “Secure”
    Attribute

--- node/src/integration-test/kotlin/net/corda/services/messaging/MQSecurityAsNodeTest.kt
+++ node/src/integration-test/kotlin/net/corda/services/messaging/MQSecurityAsNodeTest.kt
@@ -23,7 +23,6 @@ import net.corda.testing.core.CHARLIE_NAME
 import net.corda.testing.core.singleIdentity
 import net.corda.testing.internal.createDevIntermediateCaCertPath
 import org.apache.activemq.artemis.api.config.ActiveMQDefaultConfiguration
-import org.apache.activemq.artemis.api.core.ActiveMQClusterSecurityException
 import org.apache.activemq.artemis.api.core.ActiveMQNotConnectedException
 import org.apache.activemq.artemis.api.core.ActiveMQSecurityException
 import org.assertj.core.api.Assertions.assertThatExceptionOfType
@@ -66,8 +65,9 @@ class MQSecurityAsNodeTest : P2PMQSecurityTest() {
 
     @Test(timeout=300_000)
 	fun `login as the default cluster user`() {
+        // Artemis guards against connecting with the default cluster credentials
         val attacker = clientTo(alice.node.configuration.p2pAddress)
-        assertThatExceptionOfType(ActiveMQClusterSecurityException::class.java).isThrownBy {
+        assertThatExceptionOfType(ActiveMQSecurityException::class.java).isThrownBy {
             attacker.start(ActiveMQDefaultConfiguration.getDefaultClusterUser(), ActiveMQDefaultConfiguration.getDefaultClusterPassword())
         }
     }
