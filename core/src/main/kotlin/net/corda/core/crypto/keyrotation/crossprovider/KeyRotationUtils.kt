27fc32d18d44f817d7b1e8220d35247f4244d85c
Filipe Oliveira <filipe.oliveira@r3.com>
2026-08-18 16:46:01 +0100

    Update the method `getMissingSigners`. Only the original key should be collected. Intermediate keys should not be used because they are never expected to be part of the required keys.

--- core/src/main/kotlin/net/corda/core/crypto/keyrotation/crossprovider/KeyRotationUtils.kt
+++ core/src/main/kotlin/net/corda/core/crypto/keyrotation/crossprovider/KeyRotationUtils.kt
@@ -3,10 +3,6 @@ package net.corda.core.crypto.keyrotation.crossprovider
 import net.corda.core.crypto.TransactionSignature
 import java.security.PublicKey
 
-fun List<TransactionSignature>.getKeyLineage(): Set<PublicKey> {
-    return mapNotNull { signature -> signature.signatureMetadata.proofChain?.getKeyLineage() }.flatten().toSet()
-}
-
 fun TransactionSignature.getOriginalKey(): PublicKey {
     if (signatureMetadata.proofChain != null && signatureMetadata.proofChain.isNotEmpty()) {
         // If there is a proof chain, the original key is the first key in the chain.
