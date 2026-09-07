2869dd631b80a98f67d7d88c5b13e3f4d41bb9ad
Adel El-Beik <adel.el-beik@r3.com>
2026-09-07 12:45:30 +0100

    ENT-16259: Fixed imports.

--- node/src/integration-test/kotlin/net/corda/node/BootTests.kt
+++ node/src/integration-test/kotlin/net/corda/node/BootTests.kt
@@ -6,6 +6,7 @@ import net.corda.core.CordaRuntimeException
 import net.corda.core.flows.FlowLogic
 import net.corda.core.flows.StartableByRPC
 import net.corda.core.messaging.startFlow
+import net.corda.core.serialization.internal._rpcClientSerializationEnv
 import net.corda.core.utilities.getOrThrow
 import net.corda.coretesting.internal.stubs.CertificateStoreStubs
 import net.corda.node.internal.NodeStartup
@@ -32,7 +33,6 @@ import java.io.Serializable
 import kotlin.io.path.deleteExisting
 import kotlin.io.path.div
 import kotlin.io.path.isRegularFile
-import kotlin.io.path.name
 import kotlin.io.path.readLines
 import kotlin.io.path.useDirectoryEntries
 import kotlin.io.path.useLines
