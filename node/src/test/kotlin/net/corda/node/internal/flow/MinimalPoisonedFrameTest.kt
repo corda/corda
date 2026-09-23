63eeba88c6a977684b8ff42eac94d14098a16acb
Rick Parker <rick.parker@r3.com>
2026-09-23 13:42:09 +0100

    ENT-15078 Use fixed quasar version to resolve stack frame poisoning bug (#8289)
    Upgrade to Quasar 0.9.3_r3 to fix stack frame poisoning bug and
    introduce tests to validate
    
    `PLATFORM_VERSION` is incremented from 140 to 141 to force prior
    checkpoints to be rejected since the Quasar checkpoint format has been
    modified during the bug fix. Will need to add to release notes and
    update
    https://docs.r3.com/en/platform/corda/4.12/enterprise/app-upgrade-notes.html
    
    Looks like the contributor of the test / support issue is already
    credited in CONTRIBUTORS.md, but special thanks to Jan Buszka for the
    test submission that helped us track down and fix this bug.

---
+++ node/src/test/kotlin/net/corda/node/internal/flow/MinimalPoisonedFrameTest.kt
@@ -0,0 +1,90 @@
+package net.corda.node.internal.flow
+
+import co.paralleluniverse.fibers.Suspendable
+import net.corda.core.flows.FlowLogic
+import net.corda.core.utilities.getOrThrow
+import net.corda.testing.node.MockNetwork
+import net.corda.testing.node.MockNetworkParameters
+import net.corda.testing.node.StartedMockNode
+import net.corda.testing.node.internal.enclosedCordapp
+import org.junit.After
+import org.junit.Before
+import org.junit.jupiter.api.Assertions.assertEquals
+import java.time.Duration
+import kotlin.test.Test
+
+class MinimalPoisonedFrameTest {
+    private lateinit var mockNet: MockNetwork
+    private lateinit var node: StartedMockNode
+
+    @Before
+    fun setUp() {
+        mockNet = MockNetwork(
+                MockNetworkParameters(
+                        cordappsForAllNodes = listOf(
+                                enclosedCordapp()
+                                //TestCordapp.findCordapp("net.corda.node.internal.flow")
+                        )
+                )
+        )
+        node = mockNet.createPartyNode()
+        mockNet.startNodes()
+    }
+
+    @After
+    fun tearDown() = mockNet.stopNodes()
+
+    @Test
+    fun valueBelow2Pow50_flowCompletes() {
+        val safe = (1L shl 50) - 1          // top 14 bits = 0
+        assertEquals(16 * safe, run(safe))
+    }
+
+    /**
+     * See the comments associated with StalePrimitiveSlotTest in https://github.com/corda/quasar/pull/187
+     * for full details behind the magic values in this test.
+     */
+    @Test
+    fun valueAbove2Pow50_flowCompletes() {
+        val poison = 1L shl 50              // top 14 bits = 1
+        assertEquals(16 * poison, run(poison))
+        // Before quasar fix in 0.9.3_r3 / ENT-15078, this test would fail as per below commented out code
+        //assertFailsWith<TimeoutException> { run(poison) }
+    }
+
+    private fun run(value: Long): Long {
+        val future = node.startFlow(MinimalParentFlow(value))
+        mockNet.runNetwork()
+        return future.getOrThrow(Duration.ofSeconds(30))
+    }
+
+    class MinimalParentFlow(private val value: Long) : FlowLogic<Long>() {
+        @Suspendable
+        override fun call(): Long = subFlow(MinimalSubFlow(value))   // LeaveSubFlow for MinimalSubFlow fires in here
+    }
+
+    class MinimalSubFlow(private val value: Long) : FlowLogic<Long>() {
+        @Suspendable
+        override fun call(): Long {
+            val p0 = value;
+            val p1 = value;
+            val p2 = value;
+            val p3 = value
+            val p4 = value;
+            val p5 = value;
+            val p6 = value;
+            val p7 = value
+            val p8 = value;
+            val p9 = value;
+            val p10 = value;
+            val p11 = value
+            val p12 = value;
+            val p13 = value;
+            val p14 = value;
+            val p15 = value
+            sleep(Duration.ZERO)                 // checkpoint; the sixteen longs are saved into the fiber stack here
+            return p0 + p1 + p2 + p3 + p4 + p5 + p6 + p7 + p8 + p9 + p10 + p11 + p12 + p13 + p14 + p15
+        }
+    }
+}
+
